5018e48b304b219fbb6dd124252a2349a230f496
wssgcg1213 <i@zeroling.com>
2018-01-11 12:38:43 +0800

    feat: rename layout

---
+++ layouts/FixedHeaderFooterResponsiveLayout/demo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import '@icedesign/base/lib/_components/@alife/next-core/lib/index.scss';
+import Layout from '../src';
+
+const props = {
+  // ...
+};
+
+render(
+  <Layout {...props}>
+    sdfdsfsfd
+    sdffsfsd
+  </Layout>,
+  document.querySelector('#mountNode')
+);
---
+++ layouts/HeaderFooterLayout/README.md
@@ -0,0 +1,5 @@
+# HeaderFooterLayout
+
+简介：基础 layout, 顶栏-底栏-布局
+
+![截图](https://img.alicdn.com/tfs/TB1Qh4rir_I8KJjy1XaXXbsxpXa-2536-1246.png)
