d216e10bb69b88ee60379ea3e531cb3e2c9e84d2
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-01-18 18:03:28 +0800

    fix: clear code

--- scaffords/ice-design-pro/src/pages/Landing/components/LandingIntroBanner/LandingIntroBanner.jsx
+++ scaffords/ice-design-pro/src/pages/Landing/components/LandingIntroBanner/LandingIntroBanner.jsx
@@ -22,7 +22,7 @@ export default class LandingIntroBanner extends Component {
         <div
           style={{
             ...styles.landingIntroBackground,
-            backgroundImage: `url(${backgroundImage})`,
+            backgroundImage: `url(${backgroundImage})`
           }}
         />
         <div
@@ -34,14 +34,14 @@ export default class LandingIntroBanner extends Component {
               更快，更优，更 Cool !<br />欢迎使用 ICE
             </h2>
             <div style={styles.buttons}>
-              <a href="//ice.alibaba-inc.com/docs/ice-design">
+              <a href="//alibaba.github.io/ice/design.html">
                 <Button
                   style={{
                     marginRight: 20,
                     height: 50,
                     padding: '0 58px',
                     fontSize: 16,
-                    color: '#fff',
+                    color: '#fff'
                   }}
                   type="primary"
                   size="large"
@@ -70,7 +70,7 @@ export default class LandingIntroBanner extends Component {
 const styles = {
   landingIntro: {
     position: 'relative',
-    height: '100vh',
+    height: '100vh'
   },
   landingIntroBackground: {
     position: 'absolute',
@@ -78,7 +78,7 @@ const styles = {
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundSize: 'cover',
+    backgroundSize: 'cover'
   },
   contentWrapper: {
     position: 'absolute',
@@ -88,14 +88,14 @@ const styles = {
     bottom: 0,
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   title: {
     textAlign: 'center',
     color: '#fff',
     fontSize: '32px',
     letterSpacing: '1.94px',
-    lineHeight: '48px',
+    lineHeight: '48px'
   },
-  buttons: { textAlign: 'center', marginTop: 70 },
+  buttons: { textAlign: 'center', marginTop: 70 }
 };
