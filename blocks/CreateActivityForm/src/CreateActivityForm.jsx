a15296e3abf5447e42cc54a8b74e8673c36fc1dc
浩睿 <jiangshui.yjs@alibaba-inc.com>
2018-01-18 11:20:22 +0800

    feat: finish create activity form block

--- blocks/CreateActivityForm/src/CreateActivityForm.jsx
+++ blocks/CreateActivityForm/src/CreateActivityForm.jsx
@@ -4,6 +4,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import IceContainer from '@icedesign/container';
 import {
+  FormBinderWrapper as IceFormBinderWrapper,
+  FormBinder as IceFormBinder,
+  FormError as IceFormError
+} from '@icedesign/form-binder';
+import {
   Form,
   Input,
   Button,
@@ -16,6 +21,9 @@ import {
 
 import './CreateActivityForm.scss';
 
+// Form 和 FormItem 用于描述表单 UI 结构
+// FormBinder 用于获取表单组件的数据，通过标准受控 API value 和 onChange 来双向操作数据
+// Inpput 等为具体表单组件
 const FormItem = Form.Item;
 const formItemLayout = {
   labelCol: {
@@ -27,6 +35,22 @@ const formItemLayout = {
 };
 const CheckboxGroup = Checkbox.Group;
 const RadioGroup = Radio.Group;
+const { RangePicker } = DatePicker;
+
+// Switch 组件的选中等 props 是 checked 不符合表单规范的 value 在此做转换
+const SwitchForForm = props => {
+  const checked = props.checked === undefined ? props.value : props.checked;
+
+  return (
+    <Switch
+      {...props}
+      checked={checked}
+      onChange={checked => {
+        props.onChange && props.onChange(checked);
+      }}
+    />
+  );
+};
 
 export default class CreateActivityForm extends Component {
   static displayName = 'CreateActivityForm';
@@ -37,61 +61,140 @@ export default class CreateActivityForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: {
+        name: '',
+        area: 'location1',
+        time: [],
+        delivery: false,
+        type: ['地推活动'],
+        resource: '线下场地免费',
+        extra: ''
+      }
+    };
   }
+
+  onFormChange = value => {
+    this.setState({
+      value
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      value: {
+        name: '',
+        area: 'location1',
+        time: [],
+        delivery: false,
+        type: ['地推活动'],
+        resource: '线下场地免费',
+        extra: ''
+      }
+    });
+  };
+
+  submit = () => {
+    this.refs.form.validateAll((errors, values) => {
+      console.log('errors', errors, 'values', values);
+      if (errors) {
+        // 处理表单报错
+        return;
+      }
+      // 提交当前填写的数据
+    });
+  };
 
   render() {
     return (
       <div className="create-activity-form">
         <IceContainer title="活动发布">
-          <Form direction="ver">
-            <FormItem label="活动名称：" {...formItemLayout}>
-              <Input />
-            </FormItem>
-            <FormItem label="活动区域：" {...formItemLayout}>
-              <Select
-                className="next-form-text-align"
-                dataSource={[
-                  { label: '区域一', value: 'location1' },
-                  { label: '区域二', value: 'location2' }
-                ]}
-              />
-            </FormItem>
-            <FormItem label="活动时间：" {...formItemLayout}>
-              <DatePicker />
-            </FormItem>
-            <FormItem label="即时配送：" {...formItemLayout}>
-              <Switch />
-            </FormItem>
-            <FormItem label="活动性质：" {...formItemLayout}>
-              <CheckboxGroup
-                className="next-form-text-align"
-                dataSource={[
-                  { label: '美食线上活动', value: '美食线上活动' },
-                  { label: '地推活动', value: '地推活动' },
-                  { label: '线下主题活动', value: '线下主题活动' },
-                  { label: '单纯品牌曝光', value: '单纯品牌曝光' }
-                ]}
-              />
-            </FormItem>
-            <FormItem label="特殊资源：" {...formItemLayout}>
-              <RadioGroup
-                className="next-form-text-align"
-                dataSource={[
-                  { label: '线上品牌商赞助', value: '线上品牌商赞助' },
-                  { label: '线下场地免费', value: '线下场地免费' }
-                ]}
-              />
-            </FormItem>
-            <FormItem label="活动形式：" {...formItemLayout}>
-              <Input multiple />
-            </FormItem>
+          <IceFormBinderWrapper
+            ref="form"
+            value={this.state.value}
+            onChange={this.onFormChange}
+          >
+            <Form direction="ver">
+              <FormItem label="活动名称：" required {...formItemLayout}>
+                <IceFormBinder name="name" required message="活动名称必须填写">
+                  <Input />
+                </IceFormBinder>
+                <IceFormError name="name" />
+              </FormItem>
+              <FormItem label="活动区域：" {...formItemLayout}>
+                <IceFormBinder name="area">
+                  <Select
+                    className="next-form-text-align"
+                    dataSource={[
+                      { label: '区域一', value: 'location1' },
+                      { label: '区域二', value: 'location2' }
+                    ]}
+                  />
+                </IceFormBinder>
+              </FormItem>
+              <FormItem label="活动时间：" {...formItemLayout}>
+                <IceFormBinder
+                  name="time"
+                  type="array"
+                  // 使用 RangePicker 组件输出的第二个参数字符串格式的日期
+                  valueFormatter={(date, dateStr) => dateStr}
+                >
+                  <RangePicker showTime={true} />
+                </IceFormBinder>
+              </FormItem>
+              <FormItem label="即时配送：" {...formItemLayout}>
+                <IceFormBinder name="delivery">
+                  <SwitchForForm />
+                </IceFormBinder>
+              </FormItem>
+              <FormItem label="活动性质：" {...formItemLayout}>
+                <IceFormBinder
+                  name="type"
+                  len={2}
+                  type="array"
+                  message="活动性质必须选择两个"
+                >
+                  <CheckboxGroup
+                    className="next-form-text-align"
+                    dataSource={[
+                      { label: '美食线上活动', value: '美食线上活动' },
+                      { label: '地推活动', value: '地推活动' },
+                      { label: '线下主题活动', value: '线下主题活动' },
+                      { label: '单纯品牌曝光', value: '单纯品牌曝光' }
+                    ]}
+                  />
+                </IceFormBinder>
+                <div>
+                  <IceFormError name="type" />
+                </div>
+              </FormItem>
+              <FormItem label="特殊资源：" {...formItemLayout}>
+                <IceFormBinder name="resource">
+                  <RadioGroup
+                    className="next-form-text-align"
+                    dataSource={[
+                      { label: '线上品牌商赞助', value: '线上品牌商赞助' },
+                      { label: '线下场地免费', value: '线下场地免费' }
+                    ]}
+                  />
+                </IceFormBinder>
+              </FormItem>
+              <FormItem label="活动形式：" {...formItemLayout}>
+                <IceFormBinder name="extra">
+                  <Input multiple />
+                </IceFormBinder>
+              </FormItem>
 
-            <FormItem label=" " {...formItemLayout}>
-              <Button type="primary">立即创建</Button>
-              <Button style={styles.resetBtn}>重置</Button>
-            </FormItem>
-          </Form>
+              <FormItem label=" " {...formItemLayout}>
+                <Button type="primary" onClick={this.submit}>
+                  立即创建
+                </Button>
+                <Button style={styles.resetBtn} onClick={this.reset}>
+                  重置
+                </Button>
+              </FormItem>
+            </Form>
+          </IceFormBinderWrapper>
         </IceContainer>
       </div>
     );
