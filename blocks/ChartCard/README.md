32d3210b938f50c52f9b00c642491b77f5ff0666
ZeroLing <i@zeroling.com>
2018-01-26 13:13:25 +0800

    [WIP] Webpack plugin import (#39)
    * chore: delete ChartCard block
    
    * docs: remove config dir desc
    
    * fix: repace head image (#37)
    
    * chore(release): publish %s
    
     - @icedesign/fixed-header-footer-responsive-layout@0.1.11
     - @icedesign/header-aside-footer-layout@0.1.11
     - @icedesign/header-aside-footer-responsive-layout@0.1.12
     - @icedesign/header-footer-layout@0.1.11
     - @icedesign/scafford-pro@0.1.6
    
    * fix: lint
    
    * fix(hotfix): block pricecard export path
    
    * chore(release): publish %s
    
     - @icedesign/price-card-block@0.1.5
     - @icedesign/fixed-header-footer-responsive-layout@0.1.12
     - @icedesign/header-aside-footer-layout@0.1.12
     - @icedesign/header-aside-footer-responsive-layout@0.1.13
     - @icedesign/header-footer-layout@0.1.12
    
    * feat: initial webpack plugin import
    
    * fix: temp lint
    
    * feat: modify test
    
    * feat: webpack plugin test

--- blocks/ChartCard/README.md
+++
@@ -1,5 +0,0 @@
-# chart-card
-
-简介：图表卡片
-
-![截图](https://img.alicdn.com/tfs/TB1K03rnb_I8KJjy1XaXXbsxpXa-961-179.png)
---
+++ tools/webpack-plugin-import/tests/entry.js
@@ -0,0 +1,4 @@
+import { Button } from '@icedesign/base';
+import Container from '@icedesign/container';
+
+console.log(Button, Container);
