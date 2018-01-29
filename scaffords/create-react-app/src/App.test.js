b90866c8a3f680a80fa6801e5735bfbc3b4b5076
浩睿 <jiangshui.yjs@alibaba-inc.com>
2018-01-29 14:44:23 +0800

    chore: for lint

--- scaffords/create-react-app/src/App.test.js
+++ scaffords/create-react-app/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
