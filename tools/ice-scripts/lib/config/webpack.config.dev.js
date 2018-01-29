e1d9152858684b3737937326a21de16d2669567b
wssgcg1213 <i@zeroling.com>
2018-01-29 16:15:41 +0800

    feat: add webpack bundle analyzer for dev mode

--- tools/ice-scripts/lib/config/webpack.config.dev.js
+++ tools/ice-scripts/lib/config/webpack.config.dev.js
@@ -1,5 +1,6 @@
 const webpackMerge = require('webpack-merge');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
 const getWebpackConfigBasic = require('./webpack.config.basic');
 
@@ -15,6 +16,10 @@ module.exports = function getWebpackConfigDev(entry, paths, options = {}) {
       new WatchMissingNodeModulesPlugin(paths.appNodeModules),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NamedModulesPlugin(),
+      new BundleAnalyzerPlugin({
+        analyzerPort: 49123,
+        // http://127.0.0.1:49123/report.html
+      }),
     ],
   });
 };
