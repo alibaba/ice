849231f1161a00d3a14a7be6e7ae773ddbaaafc6
wssgcg1213 <i@zeroling.com>
2018-01-30 18:14:58 +0800

    chore: change eslintrc to support test

--- scaffords/create-react-app/src/App.test.js
+++ scaffords/create-react-app/src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// eslint-disable-next-line
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
