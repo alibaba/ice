c0bcfe4841037fd720bc5c998ace7a2e593fe8d4
wssgcg1213 <i@zeroling.com>
2018-02-23 13:59:59 +0800

    chore: replace long reset.scss path to short one

--- layouts/HeaderAsideFooterResponsiveLayout/demo/index.js
+++ layouts/HeaderAsideFooterResponsiveLayout/demo/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import '@icedesign/base/lib/_components/@alife/next-core/lib/index.scss';
+import '@icedesign/base/reset.scss';
 import Layout from '../src';
 
 const props = {
