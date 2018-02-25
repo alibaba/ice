df4ea06d122dc94077032058197fc10c9293c097
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-02-25 21:00:35 +0800

    feat: init scaffold-cms

---
+++ scaffolds/ice-design-cms/src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import './Home.scss';
+
+export default class Home extends Component {
+  static displayName = 'Home';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        
+      </div>
+    );
+  }
+}
---
+++ scaffolds/ice-design-cms/src/pages/UserList/components/TabTable/components/CustomTable.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Table } from '@icedesign/base';
+
+export default class CustomTable extends Component {
+  static displayName = 'CustomTable';
+
+  static propTypes = {
+    dataSource: PropTypes.array,
+    columns: PropTypes.array.isRequired,
+  };
+
+  static defaultProps = {
+    dataSource: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  renderColumns = () => {
+    const { columns } = this.props;
+    return columns.map((item) => {
+      if (typeof item.render === 'function') {
+        return (
+          <Table.Column
+            title={item.title}
+            key={item.key}
+            cell={item.render}
+            width={item.width}
+          />
+        );
+      }
+
+      return (
+        <Table.Column
+          key={item.key}
+          title={item.title}
+          dataIndex={item.dataIndex}
+          width={item.width}
+        />
+      );
+    });
+  };
+
+  render() {
+    return (
+      <Table style={{ width: 1100 }} {...this.props}>
+        {this.renderColumns()}
+      </Table>
+    );
+  }
+}
