e0621e27e4572dc13bd0814e48bcadd2ec9cc77f
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-02-25 21:00:35 +0800

    feat: init scaffold-cms

---
+++ scaffolds/ice-design-cms/src/components/CustomBreadcrumb/CustomBreadcrumb.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Breadcrumb } from '@icedesign/base';
+import IceContainer from '@icedesign/container';
+
+export default class CustomBreadcrumb extends Component {
+  static displayName = 'CustomBreadcrumb';
+
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    dataSource: [],
+  };
+
+  static propTypes = {
+    dataSource: PropTypes.array,
+  };
+
+  render() {
+    console.log(this.props);
+    const { dataSource } = this.props;
+    return (
+      <IceContainer>
+        <Breadcrumb style={{ margin: 0 }}>
+          {dataSource.map((item, index) => {
+            return (
+              <Breadcrumb.Item link={item.link} key={index}>
+                {item.text}
+              </Breadcrumb.Item>
+            );
+          })}
+        </Breadcrumb>
+      </IceContainer>
+    );
+  }
+}
---
+++ scaffolds/ice-design-cms/src/pages/CreateTag/components/SimpleFluencyForm/SimpleFluencyForm.jsx
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import IceContainer from '@icedesign/container';
+import { Grid, Input, Button } from '@icedesign/base';
+import {
+  FormBinderWrapper,
+  FormBinder,
+  FormError,
+} from '@icedesign/form-binder';
+import './SimpleFluencyForm.scss';
+
+const { Row, Col } = Grid;
+
+export default class SimpleFluencyForm extends Component {
+  static displayName = 'SimpleFluencyForm';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValue: {
+        name: '',
+        shortName: '',
+      },
+    };
+  }
+
+  formChange = (newValue) => {
+    this.setState({
+      formValue: newValue,
+    });
+  };
+
+  handleSubmit = () => {
+    this.form.validateAll((errors, values) => {
+      console.log('errors', errors, 'values', values);
+    });
+  };
+
+  render() {
+    return (
+      <div className="simple-fluency-form" style={styles.simpleFluencyForm}>
+        <IceContainer style={styles.form}>
+          <FormBinderWrapper
+            ref={(form) => {
+              this.form = form;
+            }}
+            value={this.state.formValue}
+            onChange={this.formChange}
+          >
+            <div style={styles.formContent}>
+              <h2 style={styles.formTitle}>添加标签</h2>
+              <Row style={styles.formRow}>
+                <Col span={2} style={styles.formLabel}>
+                  <span>标签名称：</span>
+                </Col>
+                <Col span={6}>
+                  <FormBinder required message="必填项">
+                    <Input name="name" />
+                  </FormBinder>
+                  <div style={styles.formErrorWrapper}>
+                    <FormError name="name" />
+                  </div>
+                </Col>
+              </Row>
+              <Row style={styles.formRow}>
+                <Col span={2} style={styles.formLabel}>
+                  <span>缩略名称：</span>
+                </Col>
+                <Col span={6}>
+                  <FormBinder required message="必填项">
+                    <Input name="shortName" />
+                  </FormBinder>
+                  <div style={styles.formErrorWrapper}>
+                    <FormError name="shortName" />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col offset={2}>
+                  <Button
+                    onClick={this.handleSubmit}
+                    type="primary"
+                    size="large"
+                  >
+                    确认
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          </FormBinderWrapper>
+        </IceContainer>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  formTitle: {
+    margin: '0 0 20px',
+    paddingBottom: '10px',
+    borderBottom: '1px solid #eee',
+  },
+  formLabel: {
+    textAlign: 'right',
+    lineHeight: '1.7rem',
+    paddingRight: '10px',
+  },
+  formRow: {
+    marginBottom: '20px',
+  },
+  formErrorWrapper: {
+    marginTop: '5px',
+  },
+  simpleFluencyForm: {},
+};
