a07fb3eaa0804e7664949f8eb128236e026cfdc0
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-03-01 16:31:35 +0800

    feat: add FlowStatistics block

---
+++ blocks/FlowStatistics/src/ColumnChart.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Chart, Axis, Geom, Tooltip } from 'bizcharts';
+import { DataView } from '@antv/data-set';
+
+export default class ColumnChart extends Component {
+  static displayName = 'ColumnChart';
+
+  static propTypes = {
+    value: PropTypes.string,
+  };
+
+  static defaultProps = {
+    value: 'string data',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { color } = this.props;
+    const data = [
+      { year: '1951 年', sales: 38 },
+      { year: '1952 年', sales: 52 },
+      { year: '1956 年', sales: 61 },
+      { year: '1957 年', sales: 145 },
+      { year: '1958 年', sales: 48 },
+      { year: '1959 年', sales: 38 },
+      { year: '1960 年', sales: 38 },
+      { year: '1962 年', sales: 38 },
+    ];
+    const cols = {
+      sales: { tickInterval: 20 },
+    };
+    return (
+      <Chart
+        height={30}
+        width={60}
+        padding={[2, 3, 2, 3]}
+        data={data}
+        scale={cols}
+      >
+        <Geom
+          type="interval"
+          position="year*sales"
+          color={color || '#3fa1ff'}
+        />
+      </Chart>
+    );
+  }
+}
