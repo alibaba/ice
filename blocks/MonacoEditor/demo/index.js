3f73363fb77b13d639362d7f3b06fa351f63f417
wssgcg1213 <i@zeroling.com>
2018-03-03 21:04:50 +0800

    fix: monaco editor failed then load second time

--- blocks/MonacoEditor/demo/index.js
+++ blocks/MonacoEditor/demo/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 // 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
 import '@icedesign/base/reset.scss';
@@ -7,13 +8,12 @@ import '@icedesign/base/reset.scss';
 import DemoLayout from '@icedesign/demo-layout';
 import Block from '../src';
 
-const props = {
-  // ...
-};
-
 render(
-  <DemoLayout type="ice-design">
-    <Block {...props} />
-  </DemoLayout>,
+  <Router history={hashHistory}>
+    <Route path="/" component={DemoLayout}>
+      <IndexRoute component={Block} />
+      <Route path="*" component="div" />
+    </Route>
+  </Router>,
   document.querySelector('#mountNode')
 );
