52f401723a6f6affcb4598331f81258f26462d82
ICE Team <ice-admin@alibaba-inc.com>
2018-03-05 22:02:46 +0800

    Auto Publish ICE Site

--- monaco-editor-worker-loader-proxy.js
+++ monaco-editor-worker-loader-proxy.js
@@ -1,4 +1,5 @@
 self.MonacoEnvironment = {
   baseUrl: 'https://cdn.bootcss.com/monaco-editor/0.10.1/min'
 };
+// eslint-disable-next-line
 importScripts('https://cdn.bootcss.com/monaco-editor/0.10.1/min/vs/base/worker/workerMain.js');
