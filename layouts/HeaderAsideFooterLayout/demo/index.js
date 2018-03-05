0c9f3e2ba135210eb0388f214ed4ed0475802bb7
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-03-05 09:48:48 +0800

    feat: layout responsive support

--- layouts/HeaderAsideFooterLayout/demo/index.js
+++ layouts/HeaderAsideFooterLayout/demo/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+
+// 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
 import '@icedesign/base/reset.scss';
 import Layout from '../src';
 
-const props = {
-  // ...
-};
-
-render(<Layout {...props} />, document.querySelector('#mountNode'));
+render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component="div" />
+      <Route path="*" component="div" />
+    </Route>
+  </Router>,
+  document.querySelector('#mountNode')
+);
