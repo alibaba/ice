108888bab69a64a8143dafe065329b41c1711daa
wssgcg1213 <i@zeroling.com>
2018-03-06 11:39:59 +0800

    feat: close monify for ice users

--- tools/ice-scripts/lib/config/webpack.config.prod.js
+++ tools/ice-scripts/lib/config/webpack.config.prod.js
@@ -15,18 +15,20 @@ module.exports = function getWebpackConfigDev(entry, paths, options = {}) {
       new HtmlWebpackPlugin({
         inject: true,
         template: paths.appHtml,
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true,
-        },
+        // disable minify for ice users
+        minify: false,
+        // minify: {
+        //   removeComments: true,
+        //   collapseWhitespace: true,
+        //   removeRedundantAttributes: true,
+        //   useShortDoctype: true,
+        //   removeEmptyAttributes: true,
+        //   removeStyleLinkTypeAttributes: true,
+        //   keepClosingSlash: true,
+        //   minifyJS: true,
+        //   minifyCSS: true,
+        //   minifyURLs: true,
+        // },
       }),
     ],
   });
