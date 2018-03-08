92d9c663dcb1ca4b0c92062478edea894bba122e
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-03-08 18:30:39 +0800

    feat: new blocks

---
+++ blocks/CircleProgress/README.md
@@ -0,0 +1,5 @@
+# circle-progress
+
+简介：环形进度条
+
+![截图](https://img.alicdn.com/tfs/TB1xOKYcwmTBuNjy1XbXXaMrVXa-2352-522.png)
---
+++ blocks/SimpleSlider/src/SimpleSlider.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import IceContainer from '@icedesign/container';
+import { Slider } from '@icedesign/base';
+
+const slides = [
+  {
+    url: 'https://img.alicdn.com/tps/TB1bewbNVXXXXc5XXXXXXXXXXXX-1000-300.png',
+    text: '手机淘宝皮肤征集',
+  },
+  {
+    url: 'https://img.alicdn.com/tps/TB1xuUcNVXXXXcRXXXXXXXXXXXX-1000-300.jpg',
+    text: '阿里公益T恤设计大概',
+  },
+  {
+    url: 'https://img.alicdn.com/tps/TB1ikP.NVXXXXaYXpXXXXXXXXXX-1000-300.jpg',
+    text: '淘公仔设计大赛',
+  },
+  {
+    url: 'https://img.alicdn.com/tps/TB1s1_JNVXXXXbhaXXXXXXXXXXX-1000-300.jpg',
+    text: '磁带播放器换肤设计大赛',
+  },
+];
+
+export default class SimpleSlider extends Component {
+  static displayName = 'SimpleSlider';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <IceContainer>
+        <Slider>
+          {slides.map((item, index) => (
+            <div key={index} className="slider-img-wrapper">
+              <img src={item.url} alt={item.text} style={styles.itemImg} />
+            </div>
+          ))}
+        </Slider>
+      </IceContainer>
+    );
+  }
+}
+
+const styles = {
+  itemImg: {
+    width: '100%',
+  },
+};
