d71cf3fc3dcd08c17a946feefe7eedb51ec42719
浩睿 <jiangshui.yjs@alibaba-inc.com>
2018-03-09 10:39:01 +0800

    feat: add RWD support

--- blocks/DownloadCard/src/DownloadCard.jsx
+++ blocks/DownloadCard/src/DownloadCard.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import IceContainer from '@icedesign/container';
 import axios from 'axios';
-import { Tab, Button, Icon } from '@icedesign/base';
+import { Tab, Button, Icon, Grid } from '@icedesign/base';
 import './DownloadCard.scss';
 
+const { Row, Col } = Grid;
 const { TabPane } = Tab;
 
 export default class DownloadCard extends Component {
@@ -43,53 +44,51 @@ export default class DownloadCard extends Component {
   renderContent = (data) => {
     return data.map((item, index) => {
       return (
-        <div
-          key={index}
-          className="column-card-item"
-          style={styles.columnCardItem}
-        >
-          <div style={styles.cardBody}>
-            <div style={styles.avatarWrapper}>
-              <img style={styles.img} src={item.img} alt="头像" />
+        <Col key={index}>
+          <div key={index} style={styles.columnCardItem}>
+            <div style={styles.cardBody}>
+              <div style={styles.avatarWrapper}>
+                <img style={styles.img} src={item.img} alt="头像" />
+              </div>
+              <p style={styles.title}>{item.title}</p>
+              <p style={styles.desc}>{item.desc}</p>
             </div>
-            <p style={styles.title}>{item.title}</p>
-            <p style={styles.desc}>{item.desc}</p>
-          </div>
 
-          <div style={styles.downloadButtons}>
-            <Button
-              href={item.androidSDK}
-              download
-              style={styles.leftButton}
-              type="primary"
-            >
-              <Icon type="download" /> Android SDK
-            </Button>
-            <Button
-              href={item.iosSDK}
-              download
-              style={styles.rightButton}
-              type="primary"
-            >
-              <Icon type="download" /> IOS SDK
-            </Button>
-          </div>
+            <div style={styles.downloadButtons}>
+              <Button
+                href={item.androidSDK}
+                download
+                style={styles.leftButton}
+                type="primary"
+              >
+                <Icon type="download" /> Android SDK
+              </Button>
+              <Button
+                href={item.iosSDK}
+                download
+                style={styles.rightButton}
+                type="primary"
+              >
+                <Icon type="download" /> IOS SDK
+              </Button>
+            </div>
 
-          <div style={styles.cardBottom}>
-            <a href={item.version} style={styles.bottomText}>
-              版本记录
-            </a>
-            <a href={item.docs} style={styles.bottomText}>
-              集成文档
-            </a>
-            <a href={item.guide} style={styles.bottomText}>
-              使用指南
-            </a>
-            <a href={item.faq} style={styles.bottomText}>
-              FAQ
-            </a>
+            <div style={styles.cardBottom}>
+              <a href={item.version} style={styles.bottomText}>
+                版本记录
+              </a>
+              <a href={item.docs} style={styles.bottomText}>
+                集成文档
+              </a>
+              <a href={item.guide} style={styles.bottomText}>
+                使用指南
+              </a>
+              <a href={item.faq} style={styles.bottomText}>
+                FAQ
+              </a>
+            </div>
           </div>
-        </div>
+        </Col>
       );
     });
   };
@@ -99,16 +98,20 @@ export default class DownloadCard extends Component {
     return (
       <div className="download-card" style={styles.downloadCard}>
         <IceContainer>
-          <Tab type="bar">
+          <Tab type="bar" contentStyle={{ padding: '20px 5px' }}>
             <TabPane tab="客户端SDK" key="1">
-              {tabData.clientSDK
-                ? this.renderContent(tabData.clientSDK)
-                : '暂无数据'}
+              <Row gutter="20" wrap={true}>
+                {tabData.clientSDK
+                  ? this.renderContent(tabData.clientSDK)
+                  : '暂无数据'}
+              </Row>
             </TabPane>
             <TabPane tab="服务端SDK" key="2">
-              {tabData.serverSDK
-                ? this.renderContent(tabData.serverSDK)
-                : '暂无数据'}
+              <Row gutter="20" wrap={true}>
+                {tabData.serverSDK
+                  ? this.renderContent(tabData.serverSDK)
+                  : '暂无数据'}
+              </Row>
             </TabPane>
           </Tab>
         </IceContainer>
@@ -119,9 +122,11 @@ export default class DownloadCard extends Component {
 
 const styles = {
   columnCardItem: {
+    marginBottom: 20,
     position: 'relative',
     float: 'left',
-    width: '284px',
+    width: '100%',
+    minWidth: '284px',
     // height: '280px',
     padding: '0px',
     overflow: 'hidden',
