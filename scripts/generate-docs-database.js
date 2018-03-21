f69b5c1bb9f4a5347bf6a681dd02ae1ebf703ddd
wssgcg1213 <i@zeroling.com>
2018-03-21 16:27:40 +0800

    chore: participle chinese

--- scripts/generate-docs-database.js
+++ scripts/generate-docs-database.js
@@ -3,10 +3,55 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const glob = require('glob');
 const markTwain = require('mark-twain');
-
+const jieba = require('nodejieba');
+// load dict at first time
+jieba.load({
+  userDict: './scripts/userDict.utf8',
+});
 const destDir = path.join(__dirname, '../databases');
 const dest = path.join(destDir, 'docs.db.json');
 const sourceDir = path.join(__dirname, '../docs');
+
+const ignoreWordsHash = {};
+const ignoreWords = [
+  ',',
+  '.',
+  ':',
+  '。',
+  '；',
+  '：',
+  '（',
+  '）',
+  '-',
+  '*',
+  '<',
+  '>',
+].forEach((v) => {
+  ignoreWordsHash[v] = true;
+});
+/**
+ * 中文分词, 默认间隔符为空格
+ * @param {String} str
+ */
+function cut(str = '', splitBy = ' ', limit = 500) {
+  return jieba
+    .extract(str, limit)
+    .map(({ word }) => word)
+    .filter((word) => {
+      if (word.trim() === '') {
+        return false;
+      }
+      if (ignoreWordsHash[word]) {
+        return false;
+      }
+      // 过滤 CDN 图片地址
+      if (/^TB\w+/.test(word)) {
+        return false;
+      }
+      return true;
+    })
+    .join(splitBy);
+}
 
 glob(
   '**/*.md',
@@ -36,10 +81,17 @@ glob(
         throw new Error(`${file} 缺失标题, 请补充`);
       }
 
+      // 分词 payload
+      const participle = {
+        title: cut(jsonML.meta.title),
+        content: cut(content),
+      };
+
       const sourceData = {
         filename: file,
         path: file.replace(/\.md$/, ''),
         ...jsonML.meta,
+        participle,
 
         jsonml: jsonML.content,
       };
