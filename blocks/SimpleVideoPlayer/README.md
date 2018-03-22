2969ff3b0dbb6bd07eeb746b2b8b7ab5dc527e3e
浩睿 <jiangshui.yjs@alibaba-inc.com>
2018-03-22 19:53:46 +0800

    feat: finish basic functions

--- blocks/SimpleVideoPlayer/README.md
+++ blocks/SimpleVideoPlayer/README.md
@@ -4,4 +4,4 @@
 
 简单的视频播放器
 
-![截图]()
+![截图](https://img.alicdn.com/tfs/TB1rIZEfXOWBuNjy0FiXXXFxVXa-2314-1064.png)
---
+++ blocks/SimpleVideoPlayer/src/Video.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Video extends Component {
+  static displayName = 'Video';
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    const { sources, ...others } = this.props;
+
+    if (!sources || !sources.length) {
+      return null;
+    }
+
+    return (
+      <video controls style={styles.video} {...others}>
+        <track kind="captions" />
+        {sources.map((video, index) => {
+          return <source {...video} key={index} />;
+        })}
+      </video>
+    );
+  }
+}
+
+const styles = {
+  video: {
+    width: '100%',
+    height: 500,
+  },
+};
