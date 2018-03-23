85ed16963c11d69c3ec3d8a3d690d6fe41f22906
ZeroLing <i@zeroling.com>
2018-03-23 17:32:29 +0800

    fix: ice-scripts (#340)
    * fix: ice-scripts
    
    * fix: review
    
    * fix: remove unneed themeConfig

---
+++ tools/ice-scripts/lib/config/packageJson.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const getPaths = require('./paths');
+const paths = getPaths(process.cwd());
+const pkg = {};
+
+try {
+  Object.assign(
+    pkg,
+    JSON.parse(fs.readFileSync(paths.appPackageJson, 'utf-8'))
+  );
+} catch (err) {
+  console.log(err);
+  console.log('获取项目 package.json 失败, 检查是否有语法错误. ');
+}
+
+module.exports = pkg;
