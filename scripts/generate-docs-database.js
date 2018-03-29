89eb6c29869fb0b8a1c89f0f95f1f6dc7d6ecff6
wssgcg1213 <i@zeroling.com>
2018-03-29 12:12:58 +0800

    feat(chore): add participle for all databases

--- scripts/generate-docs-database.js
+++ scripts/generate-docs-database.js
@@ -3,55 +3,10 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 const glob = require('glob');
 const markTwain = require('mark-twain');
-const jieba = require('nodejieba');
-// load dict at first time
-jieba.load({
-  userDict: './scripts/userDict.utf8',
-});
+const { cut } = require('./participle');
 const destDir = path.join(__dirname, '../databases');
 const dest = path.join(destDir, 'docs.db.json');
 const sourceDir = path.join(__dirname, '../docs');
-
-const ignoreWordsHash = {};
-const ignoreWords = [
-  ',',
-  '.',
-  ':',
-  '。',
-  '；',
-  '：',
-  '（',
-  '）',
-  '-',
-  '*',
-  '<',
-  '>',
-].forEach((v) => {
-  ignoreWordsHash[v] = true;
-});
-/**
- * 中文分词, 默认间隔符为空格
- * @param {String} str
- */
-function cut(str = '', splitBy = ' ', limit = 500) {
-  return jieba
-    .extract(str, limit)
-    .map(({ word }) => word)
-    .filter((word) => {
-      if (word.trim() === '') {
-        return false;
-      }
-      if (ignoreWordsHash[word]) {
-        return false;
-      }
-      // 过滤 CDN 图片地址
-      if (/^TB\w+/.test(word)) {
-        return false;
-      }
-      return true;
-    })
-    .join(splitBy);
-}
 
 glob(
   '**/*.md',
