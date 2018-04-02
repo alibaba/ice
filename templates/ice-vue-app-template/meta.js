5450bbaf7ac4c94995bc304367fda99cc700164f
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-02 11:11:38 +0800

    feat: add ice-vue-app-template

---
+++ templates/ice-vue-app-template/meta.js
@@ -0,0 +1,27 @@
+module.exports = {
+  prompts: {
+    title: {
+      type: 'input',
+      message: '标题',
+      default: 'ice 模板',
+      validate: (value) => {
+        value = value.trim();
+        if (!value) {
+          return '标题不能为空，请重新输入';
+        }
+        return true;
+      },
+    },
+    version: {
+      type: 'string',
+      required: true,
+      message: '版本',
+      default: '1.0.0',
+    },
+    description: {
+      type: 'string',
+      required: true,
+      message: '描述(可选)',
+    },
+  },
+};
