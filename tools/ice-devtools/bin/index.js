3177ca4c9522bdf24fad46e4b084bdf2d5cb5730
wssgcg1213 <i@zeroling.com>
2018-04-03 14:50:39 +0800

    feat: add start to preview blocks

--- tools/ice-devtools/bin/index.js
+++ tools/ice-devtools/bin/index.js
@@ -5,4 +5,5 @@ require('commander')
   .usage('<command> [options]')
   .command('init', 'generate a new project from a template')
   .command('add', 'add a new block/layout/scaffold from a official templates')
+  .command('start', 'Start Dev Server')
   .parse(process.argv);
---
+++ tools/ice-devtools/lib/config/getWebpackConfig.js
@@ -0,0 +1,73 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const getBabelConfig = require('./getBabelConfig');
+const blockTemplate = require.resolve('../template/block.html');
+const STYLE_LOADER = require.resolve('style-loader');
+const CSS_LOADER = require.resolve('css-loader');
+const SASS_LOADER = require.resolve('sass-loader');
+const WebpackPluginImport = require('webpack-plugin-import');
+
+const baseConfig = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  output: {
+    library: '[name]',
+    libraryTarget: 'window',
+  },
+  entry: {
+    DEMOLAYOUT: require.resolve('@icedesign/demo-layout'),
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        options: getBabelConfig(),
+      },
+      {
+        test: /\.css$/,
+        use: [STYLE_LOADER, CSS_LOADER],
+      },
+      {
+        test: /\.scss$/,
+        use: [STYLE_LOADER, CSS_LOADER, SASS_LOADER],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      'webpack-hot-client/client': require.resolve('webpack-hot-client/client'),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: blockTemplate,
+    }),
+    new WebpackPluginImport([
+      {
+        libraryName: /^@icedesign\/base\/lib\/([^/]+)/,
+        stylePath: 'style.js',
+      },
+      {
+        libraryName: /@icedesign\/.*/,
+        stylePath: 'style.js',
+      },
+    ]),
+  ],
+};
+
+module.exports = function getWebpackConfig(entry) {
+  const config = { entry };
+
+  return merge(baseConfig, config);
+};
