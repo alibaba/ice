b5e498eb98b55ccce97a11cf71149312e02f2505
wssgcg1213 <i@zeroling.com>
2018-04-09 21:00:51 +0800

    fix: multi entry

--- tools/ice-devtools/bin/dev-server.js
+++ tools/ice-devtools/bin/dev-server.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const argv = require('commander')
-  .version(require('../package').version)
-  .usage('<command> [options]')
-  .parse(process.argv);
+// const argv = require('commander')
+//   .version(require('../package').version)
+//   .usage('<command> [options]')
+//   .parse(process.argv);
 
 const cwd = process.cwd();
 require('../lib/server')({
