720ef988f6ac73ec7128327a977cae8800e6eebf
wssgcg1213 <i@zeroling.com>
2018-04-09 17:18:57 +0800

    fix: compiler

--- tools/ice-devtools/lib/server/controllers/block.js
+++ tools/ice-devtools/lib/server/controllers/block.js
@@ -1,8 +1,26 @@
-module.exports = (ctx) => {
+const path = require('path');
+const MultiEntryPlugin = require('webpack/lib/MultiEntryPlugin');
+const cwd = process.cwd();
+
+module.exports = async (ctx) => {
   const { params } = ctx;
   if (!params.blockName) {
     return ctx.render('403.hbs');
   }
+
+  const currentMaterial = ctx.params.material;
+  const entryPath = path.resolve(
+    cwd,
+    currentMaterial,
+    'blocks',
+    params.blockName,
+    'index'
+  );
+
+  const chunkName = currentMaterial + '/' + params.blockName;
+  ctx.compiler.apply(new MultiEntryPlugin(cwd, [entryPath], chunkName));
+  // ctx.webpack.invalidate();
+  console.log(cwd, [entryPath], chunkName);
 
   const { blockName } = params;
 
