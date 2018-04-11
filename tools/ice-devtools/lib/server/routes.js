e1d092931021d7bc8eb6a9bdbdde183a302e02cf
wssgcg1213 <i@zeroling.com>
2018-04-11 17:47:18 +0800

    fix: layout

--- tools/ice-devtools/lib/server/routes.js
+++ tools/ice-devtools/lib/server/routes.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
-const blockListCtrl = require('./controllers/blockList');
+const materialsListCtrl = require('./controllers/materialList');
 const blockCtrl = require('./controllers/block');
+const layoutCtrl = require('./controllers/layout');
 
 const router = new Router();
 
-router.get('/:material/block', blockListCtrl);
+router.get('/:material', materialsListCtrl);
 router.get('/:material/block/:blockName', blockCtrl);
+router.get('/:material/layout/:layoutName', blockCtrl);
 
 module.exports = router.routes();
