ca16a1d4e7575a4df8b2de48ea544ab706ea88f1
wssgcg1213 <i@zeroling.com>
2018-04-11 11:53:00 +0800

    feat: generate database db

---
+++ tools/ice-devtools/shared/participle/index.js
@@ -0,0 +1,47 @@
+const jieba = require('nodejieba');
+// load dict at first time
+jieba.load({
+  userDict: require.resolve('./userDict.utf8'),
+});
+const ignoreWordsHash = {};
+const ignoreWords = [
+  ',',
+  '.',
+  ':',
+  '。',
+  '；',
+  '：',
+  '（',
+  '）',
+  '-',
+  '*',
+  '<',
+  '>',
+].forEach((v) => {
+  ignoreWordsHash[v] = true;
+});
+/**
+ * 中文分词, 默认间隔符为空格
+ * @param {String} str
+ */
+function cut(str = '', splitBy = ' ', limit = 500) {
+  return jieba
+    .extract(str, limit)
+    .map(({ word }) => word)
+    .filter((word) => {
+      if (word.trim() === '') {
+        return false;
+      }
+      if (ignoreWordsHash[word]) {
+        return false;
+      }
+      // 过滤 CDN 图片地址
+      if (/^TB\w+/.test(word)) {
+        return false;
+      }
+      return true;
+    })
+    .join(splitBy);
+}
+
+exports.cut = cut;
