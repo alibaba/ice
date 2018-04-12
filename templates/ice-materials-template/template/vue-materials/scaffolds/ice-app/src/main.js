5eb89401a459553715d0705747898440dd4f97e8
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-12 09:52:29 +0800

    feat: add example to materials-template

---
+++ templates/ice-materials-template/template/vue-materials/scaffolds/ice-app/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './routes'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
