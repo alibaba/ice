b77cbb12b2827f0b1f25b4ccf216bda9f0415e24
noyobo <nongyoubao@alibaba-inc.com>
2018-04-19 16:44:32 +0800

    feat: 代理中间件

---
+++ tools/ice-scripts/lib/config/getProxyConfig.js
@@ -0,0 +1,37 @@
+/**
+ * 获取用户在项目中定义的 .webpackrc.js
+ */
+const path = require('path');
+const fs = require('fs');
+const colors = require('chalk');
+
+// TODO 抽离为单独的组件
+module.exports = (opts = {}) => {
+  const { cwd = process.cwd() } = opts;
+
+  const packageFilePath = path.resolve(cwd, 'package.json');
+
+  if (fs.existsSync(packageFilePath)) {
+    try {
+      const pkgContext = fs.readFileSync(packageFilePath);
+      const pkgData = JSON.parse(pkgContext.toString());
+
+      if (pkgData.proxyConfig) {
+        console.log(
+          colors.blue('\nTIPS:'),
+          '读取 package.json 里 proxyConfig 代理配置.'
+        );
+
+        if (
+          Object.prototype.toString.apply(pkgData.proxyConfig) ==
+          '[object Object]'
+        ) {
+          console.log(pkgData.proxyConfig);
+          return pkgData.proxyConfig;
+        }
+      }
+    } catch (e) {}
+  }
+
+  return null;
+};
