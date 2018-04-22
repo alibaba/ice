5336f8a5306f537892f8078998e0f68098315fbb
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-22 10:52:12 +0800

    fix: lint error

--- vue-materials/layouts/HeaderAsideLayout/src/components/index.js
+++ vue-materials/layouts/HeaderAsideLayout/src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Navbar } from './Navbar'
-export { default as Sidebar } from './Sidebar'
-export { default as AppMain } from './AppMain'
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as AppMain } from './AppMain';
