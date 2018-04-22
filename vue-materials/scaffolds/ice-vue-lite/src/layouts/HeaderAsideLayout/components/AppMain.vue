6b44fee5a623524d86aa610112b645b06e7fd9b3
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-22 18:14:15 +0800

    feat: add ice-vue-lite scaffold

---
+++ vue-materials/scaffolds/ice-vue-lite/src/layouts/HeaderAsideLayout/components/AppMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="app-main">
+    <transition name="fade" mode="out-in">
+      <!-- <router-view :key="key"></router-view> -->
+      <router-view></router-view>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+}
+</script>
