0606ca0ea33a16a601f720a9e8058a3573573762
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-26 18:53:52 +0800

    feat: add ice-website-homepage scaffold

---
+++ react-materials/scaffolds/ice-website-homepage/src/navs.js
@@ -0,0 +1,67 @@
+// <!-- auto generated navs start -->
+const autoGenHeaderNavs = [];
+const autoGenAsideNavs = [];
+
+// <!-- auto generated navs end -->
+
+const customHeaderNavs = [
+  {
+    text: '首页',
+    to: '/',
+    icon: 'home',
+  },
+  {
+    text: '反馈',
+    to: 'https://github.com/alibaba/ice',
+    external: true,
+    newWindow: true,
+    icon: 'message',
+  },
+  {
+    text: '帮助',
+    to: 'https://alibaba.github.io/ice',
+    external: true,
+    newWindow: true,
+    icon: 'bangzhu',
+  },
+];
+
+const customAsideNavs = [
+  {
+    text: '首页',
+    to: '/',
+    icon: 'home',
+  },
+  {
+    text: '用户管理',
+    to: '/user',
+    icon: 'yonghu',
+  },
+  {
+    text: '系统设置',
+    to: '/setting',
+    icon: 'shezhi',
+    children: [
+      {
+        text: '基本设置',
+        to: '/base',
+      },
+      {
+        text: '评论设置',
+        to: '/comment',
+      },
+    ],
+  },
+];
+
+function transform(navs) {
+  // custom logical
+  return [...navs];
+}
+
+export const headerNavs = transform([
+  ...autoGenHeaderNavs,
+  ...customHeaderNavs,
+]);
+
+export const asideNavs = transform([...autoGenAsideNavs, ...customAsideNavs]);
