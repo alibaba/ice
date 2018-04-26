7d582e8bc120cc6d8323ced66c8f8015e8daa323
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-26 17:25:42 +0800

    feat: add ecommerce scaffold

---
+++ react-materials/scaffolds/ice-design-ecommerce/src/layouts/AsideLayout/Layout.jsx
@@ -0,0 +1,250 @@
+/* eslint no-undef:0, no-unused-expressions:0, array-callback-return:0 */
+import React, { Component } from 'react';
+import cx from 'classnames';
+import Layout from '@icedesign/layout';
+import Menu, { SubMenu, Item as MenuItem } from '@icedesign/menu';
+import { Icon } from '@icedesign/base';
+import { Link } from 'react-router';
+import FoundationSymbol from 'foundation-symbol';
+import { enquire } from 'enquire-js';
+import IceImg from '@icedesign/img';
+import Logo from './components/Logo';
+import { asideNavs } from '../../navs';
+import './scss/light.scss';
+import './scss/dark.scss';
+
+// 设置默认的皮肤配置，支持 dark 和 light 两套皮肤配置
+const theme = typeof THEME === 'undefined' ? 'light' : THEME;
+export default class HeaderAsideFooterResponsiveLayout extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDrawer: false,
+      isScreen: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.enquireScreenRegister();
+  }
+
+  /**
+   * 注册监听屏幕的变化，可根据不同分辨率做对应的处理
+   */
+  enquireScreenRegister = () => {
+    const isMobile = 'screen and (max-width: 720px)';
+    const isTablet = 'screen and (min-width: 721px) and (max-width: 1199px)';
+    const isDesktop = 'screen and (min-width: 1200px)';
+
+    enquire.register(isMobile, this.enquireScreenHandle('isMobile'));
+    enquire.register(isTablet, this.enquireScreenHandle('isTablet'));
+    enquire.register(isDesktop, this.enquireScreenHandle('isDesktop'));
+  };
+
+  enquireScreenHandle = (type) => {
+    let collapse;
+    if (type === 'isMobile') {
+      collapse = false;
+    } else if (type === 'isTablet') {
+      collapse = true;
+    } else {
+      collapse = this.state.collapse;
+    }
+
+    const handler = {
+      match: () => {
+        this.setState({
+          isScreen: type,
+          collapse,
+        });
+      },
+      unmatch: () => {
+        // handler unmatched
+      },
+    };
+
+    return handler;
+  };
+
+  /**
+   * 响应式通过抽屉形式切换菜单
+   */
+  toggleMenu = () => {
+    const { openDrawer } = this.state;
+    this.setState({
+      openDrawer: !openDrawer,
+    });
+  };
+
+  /**
+   * 左侧菜单收缩切换
+   */
+  onMenuClick = () => {
+    this.toggleMenu();
+  };
+
+  /**
+   * 当前展开的菜单项
+   */
+  getOpenKeys = () => {
+    const { routes } = this.props;
+    const matched = routes[0].path;
+    let openKeys = '';
+
+    asideNavs &&
+      asideNavs.length > 0 &&
+      asideNavs.map((item, index) => {
+        if (item.to === matched) {
+          openKeys = index;
+        }
+      });
+
+    return openKeys;
+  };
+
+  render() {
+    const {
+      location: { pathname },
+    } = this.props;
+
+    return (
+      <Layout
+        style={{ minHeight: '100vh' }}
+        className={cx(`ice-design-header-aside-footer-layout-${theme}`, {
+          'ice-design-layout': true,
+        })}
+      >
+        <Layout.Section className="ice-design-layout-body">
+          {this.state.isScreen !== 'isDesktop' && (
+            <a className="menu-btn" onClick={this.toggleMenu}>
+              <Icon type="category" size="small" />
+            </a>
+          )}
+          {this.state.openDrawer && (
+            <div className="open-drawer-bg" onClick={this.toggleMenu} />
+          )}
+          <Layout.Aside
+            width="auto"
+            theme={theme}
+            className={cx('ice-design-layout-aside', {
+              'open-drawer': this.state.openDrawer,
+            })}
+          >
+            <Logo />
+            <Menu
+              style={{ width: 100 }}
+              onClick={this.onMenuClick}
+              selectedKeys={[pathname]}
+              defaultSelectedKeys={[pathname]}
+              defaultOpenKeys={[`${this.getOpenKeys()}`]}
+              mode="inline"
+            >
+              {asideNavs &&
+                asideNavs.length > 0 &&
+                asideNavs.map((nav, index) => {
+                  if (nav.children && nav.children.length > 0) {
+                    return (
+                      <SubMenu
+                        key={index}
+                        title={
+                          <span>
+                            {nav.icon ? (
+                              <FoundationSymbol size="small" type={nav.icon} />
+                            ) : null}
+                            <span className="ice-menu-collapse-hide">
+                              {nav.text}
+                            </span>
+                          </span>
+                        }
+                      >
+                        {nav.children.map((item) => {
+                          const linkProps = {};
+                          if (item.newWindow) {
+                            linkProps.href = item.to;
+                            linkProps.target = '_blank';
+                          } else if (item.external) {
+                            linkProps.href = item.to;
+                          } else {
+                            linkProps.to = item.to;
+                          }
+                          return (
+                            <MenuItem key={item.to}>
+                              <Link {...linkProps}>{item.text}</Link>
+                            </MenuItem>
+                          );
+                        })}
+                      </SubMenu>
+                    );
+                  }
+                  const linkProps = {};
+                  if (nav.newWindow) {
+                    linkProps.href = nav.to;
+                    linkProps.target = '_blank';
+                  } else if (nav.external) {
+                    linkProps.href = nav.to;
+                  } else {
+                    linkProps.to = nav.to;
+                  }
+                  return (
+                    <MenuItem key={nav.to}>
+                      <Link {...linkProps}>
+                        <span>
+                          {nav.icon ? (
+                            <FoundationSymbol size="small" type={nav.icon} />
+                          ) : null}
+                          <span className="ice-menu-collapse-hide">
+                            {nav.text}
+                          </span>
+                        </span>
+                      </Link>
+                    </MenuItem>
+                  );
+                })}
+            </Menu>
+
+            {/* Header 右侧内容块 */}
+
+            <div
+              className="ice-design-header-userpannel"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                fontSize: 12,
+              }}
+            >
+              <IceImg
+                height={40}
+                width={40}
+                src="https://img.alicdn.com/tfs/TB1ONhloamWBuNjy1XaXXXCbXXa-200-200.png"
+                className="user-avatar"
+              />
+              <div className="user-profile">
+                <span
+                  className="user-name"
+                  style={{ fontSize: '13px', display: 'inline-block' }}
+                >
+                  淘小宝
+                </span>
+                <br />
+                <span
+                  className="user-department"
+                  style={{ fontSize: '12px', color: '#999' }}
+                >
+                  技术部
+                </span>
+              </div>
+            </div>
+
+            {/* 侧边菜单项 end */}
+          </Layout.Aside>
+          {/* 主体内容 */}
+          <Layout.Main>{this.props.children}</Layout.Main>
+        </Layout.Section>
+      </Layout>
+    );
+  }
+}
