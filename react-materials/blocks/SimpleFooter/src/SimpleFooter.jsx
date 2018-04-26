cec3bb7ff2e398aad84de6027cb33ca3ba4be181
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-04-26 20:19:34 +0800

    feat: add SimpleFooter block

---
+++ react-materials/blocks/SimpleFooter/src/SimpleFooter.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import IceContainer from '@icedesign/container';
+import { Grid } from '@icedesign/base';
+
+const { Row, Col } = Grid;
+
+export default class SimpleFooter extends Component {
+  static displayName = 'SimpleFooter';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="simple-footer">
+        <IceContainer>
+          <Row wrap style={styles.content}>
+            <Col l="4" xxs="24">
+              <a href="#" style={styles.brand}>
+                <img
+                  src="https://img.alicdn.com/tfs/TB1saOBbYGYBuNjy0FoXXciBFXa-218-58.png"
+                  alt=""
+                  style={styles.logo}
+                />
+              </a>
+            </Col>
+            <Col l="20" xxs="24">
+              <div style={styles.nav}>
+                <a href="#" style={styles.navLink}>
+                  Home
+                </a>
+                <a href="#" style={styles.navLink}>
+                  Shop
+                </a>
+                <a href="#" style={styles.navLink}>
+                  Blog
+                </a>
+                <a href="#" style={styles.navLink}>
+                  Service
+                </a>
+                <a href="#" style={styles.navLink}>
+                  About
+                </a>
+                <a href="#" style={{ ...styles.navLink, marginRight: 0 }}>
+                  Contact
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </IceContainer>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  content: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: '86px',
+  },
+  nav: {
+    textAlign: 'right',
+  },
+  navLink: {
+    marginRight: '20px',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+};
