23b67d073e5ef6b8021894b18e92cbe1e0911927
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-05-04 01:16:57 +0800

    feat: ice-design-project-management upgrade react-router v4

--- react-materials/scaffolds/ice-design-project-management/src/index.js
+++ react-materials/scaffolds/ice-design-project-management/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 // 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
 import '@icedesign/base/reset.scss';
-import routes from './routes';
+import router from './router';
 
 const ICE_CONTAINER = document.getElementById('ice-container');
 
@@ -9,4 +9,4 @@ if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
 
-ReactDOM.render(routes, ICE_CONTAINER);
+ReactDOM.render(router, ICE_CONTAINER);
