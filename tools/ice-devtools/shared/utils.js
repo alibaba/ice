0263172f8bd1083de031b76d03b8d09781c00b17
wssgcg1213 <i@zeroling.com>
2018-05-04 10:35:40 +0800

    feat: add mdb sync

--- tools/ice-devtools/shared/utils.js
+++ tools/ice-devtools/shared/utils.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const { readFileSync } = require('fs');
+const axios = require('axios');
 
 function keys(obj) {
   return Object.keys(obj);
@@ -25,3 +26,28 @@ function getPkgJson() {
   return json;
 }
 exports.getPkgJson = getPkgJson;
+
+function queryNpmTime(
+  npm,
+  version = 'latest',
+  registry = 'http://registry.npmjs.com'
+) {
+  return axios(`${registry}/${npm.replace(/\//g, '%2f')}/`)
+    .then((response) => response.data)
+    .then((data) => data.time)
+    .catch((err) => {
+      if (err.response && err.response.status === 404) {
+        console.error(
+          '[WARN queryNpmTime] 未发布的 npm 包',
+          npm,
+          '@',
+          version,
+          '发布时间和更新时间为 Null'
+        );
+      } else {
+        console.error('[WARN queryNpmTime] failed request with err');
+      }
+      throw err;
+    });
+}
+exports.queryNpmTime = queryNpmTime;
