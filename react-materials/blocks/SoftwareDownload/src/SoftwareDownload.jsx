06f02635b0e3c4c03be7e8dc7c3feab95b9e06dd
noyobo <nongyoubao@alibaba-inc.com>
2018-05-04 10:45:36 +0800

    feat: update SoftwareDownload block

--- react-materials/blocks/SoftwareDownload/src/SoftwareDownload.jsx
+++ react-materials/blocks/SoftwareDownload/src/SoftwareDownload.jsx
@@ -1,14 +1,92 @@
 import React, { Component } from 'react';
+import { Menu, Button, Feedback } from '@icedesign/base';
+
+const SplitButton = Button.Split;
+
+const descriptor = {
+  'darwin-x64-prod': {
+    download: 'For MacOS',
+    env: 'macOS 10.9 及以上',
+  },
+  'win-x64-prod': {
+    download: 'For Windows X64',
+    env: '64位，Win 7 及以上',
+  },
+};
 
 export default class SoftwareDownload extends Component {
   static displayName = 'SoftwareDownload';
 
   constructor(props) {
     super(props);
-    this.state = {};
+    const ua = navigator.userAgent;
+    let osType = 'darwin-x64-prod'; // mac for default
+    if (/Mac/.test(ua)) {
+      // this is macOS ha
+    } else if (/Windows/.test(ua)) {
+      osType = 'win-x64-prod';
+    }
+    this.state = {
+      osType,
+      loading: false,
+      data: {
+        'darwin-x64-prod': {
+          name: 'iceworks',
+          description: 'ICE Desktop Application.',
+          install:
+            'http://iceworks.oss-cn-hangzhou.aliyuncs.com/mac/Iceworks-1.6.2.dmg',
+          version: '1.6.2',
+          releaseDate: '2018-04-23',
+        },
+        'win-x64-prod': {
+          name: 'iceworks',
+          description: 'ICE Desktop Application.',
+          install:
+            'http://iceworks.oss-cn-hangzhou.aliyuncs.com/win/Iceworks-setup-1.6.2.exe',
+          version: '1.6.2',
+          releaseDate: '2018-04-23',
+        },
+      },
+    };
   }
 
+  componentWillMount() {}
+
+  changeSelectMenu = (select) => {
+    this.setState({ osType: select });
+  };
+
+  download = () => {
+    const { data, loading, osType } = this.state;
+    if (loading) {
+      Feedback.toast.success('请稍等');
+    } else {
+      location.href = data[osType].install;
+    }
+    this.showBtnAnimate();
+  };
+
   render() {
+    const { data, loading, osType } = this.state;
+    const menu = (
+      <Menu>
+        <Menu.Item
+          onClick={this.changeSelectMenu.bind(this, 'darwin-x64-prod')}
+          key="darwin-x64-prod"
+        >
+          For MacOS 版本
+        </Menu.Item>
+        <Menu.Item
+          onClick={this.changeSelectMenu.bind(this, 'win-x64-prod')}
+          key="win-x64-prod"
+        >
+          For Windows 版本(64位)
+        </Menu.Item>
+      </Menu>
+    );
+    const ver = loading ? '0' : data[osType].version;
+    const subTitle = ver[0] === '0' ? 'Beta' : '';
+
     return (
       <div style={styles.wrapper}>
         <div style={styles.bgImage}>
@@ -17,10 +95,22 @@ export default class SoftwareDownload extends Component {
         <div style={styles.bgImage2} />
         <div style={styles.software} />
         <div style={styles.softwareIntro}>
-          <div style={styles.title}>Iceworks</div>
+          <div style={styles.title}>
+            Iceworks <span style={styles.subtitle}>{subTitle}</span>
+          </div>
           <div style={styles.slogan}>让前端工程变的轻松便捷</div>
           <div style={{ paddingTop: 65 }}>
-            <div style={styles.downloadBtn}>立即下载 Iceworks for MacOS</div>
+            <SplitButton
+              menu={menu}
+              onClick={this.download}
+              size="large"
+              style={styles.downloadBtn}
+            >
+              立即下载
+              <span style={{ marginLeft: '5px', fontSize: '12px' }}>
+                {descriptor[osType].download}
+              </span>
+            </SplitButton>
           </div>
           <div style={styles.softwareDetail}>
             <div style={styles.version}>
@@ -66,13 +156,8 @@ const styles = {
     textShadow: '0 0 3px rgba(0,0,0,0.3)',
   },
   downloadBtn: {
-    height: 48,
     color: '#3080FE',
     fontSize: 16,
-    lineHeight: '48px',
-    backgroundColor: '#fff',
-    borderRadius: '48px',
-    textAlign: 'center',
   },
   softwareDetail: {
     paddingTop: 24,
@@ -87,9 +172,10 @@ const styles = {
   },
   software: {
     backgroundImage:
-      'url(//img.alicdn.com/tfs/TB1YPWIpTtYBeNjy1XdXXXXyVXa-779-633.png)',
+      'url(//img.alicdn.com/tfs/TB1i8hfpQyWBuNjy0FpXXassXXa-1558-1266.png)',
     width: 779,
     height: 633,
+    backgroundSize: '779px 633px',
     position: 'absolute',
     top: 100,
     left: '38%',
@@ -124,6 +210,10 @@ const styles = {
     height: 750,
     backgroundImage:
       'url(//img.alicdn.com/tfs/TB1HvqHpTtYBeNjy1XdXXXXyVXa-1314-750.jpg)',
+  },
+  subtitle: {
+    fontSize: '16px',
+    fontWeight: 'normal',
   },
   bgImageMask: {
     width: 1314,
