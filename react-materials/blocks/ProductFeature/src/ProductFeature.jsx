ccbd1eef76f1159c10c4b9644baa4fd0c2ad86d4
noyobo <nongyoubao@alibaba-inc.com>
2018-05-04 22:53:41 +0800

    feat: update iceworks blocks

--- react-materials/blocks/ProductFeature/src/ProductFeature.jsx
+++ react-materials/blocks/ProductFeature/src/ProductFeature.jsx
@@ -3,30 +3,35 @@ import React, { Component } from 'react';
 export default class ProductFeature extends Component {
   static displayName = 'ProductFeature';
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  static defaultProps = {
+    title: '项目仪表盘插件化',
+    desc:
+      '丰富多样的项目信息面板，页面信息，路由信息 依赖管理等，配置专属的Iceworks 界面',
+    img: {
+      width: 640,
+      height: 430,
+      url: '//img.alicdn.com/tfs/TB1pPQppv1TBuNjy0FjXXajyXXa-1280-860.png',
+    },
+  };
 
   render() {
     return (
-      <div style={styles.wrapper}>
-        <div style={styles.feature}>
-          <div style={styles.title}>项目仪表盘插件化</div>
-          <div style={styles.line}>
-            <div style={styles.lineHeader} />
+      <div style={styles.wrapperContainer}>
+        <div style={styles.wrapper}>
+          <div style={styles.feature}>
+            <div style={styles.title}>{this.props.title}</div>
+            <div style={styles.line}>
+              <div style={styles.lineHeader} />
+            </div>
+            <div style={styles.desc}>{this.props.desc}</div>
           </div>
-          <div style={styles.desc}>
-            丰富多样的项目信息面板，页面信息，路由信息 依赖管理等，配置专属的
-            Iceworks 界面
+          <div style={styles.cover}>
+            <img
+              alt="特点图"
+              style={{ ...styles.coverImage, ...this.props.img }}
+              src={this.props.img.url}
+            />
           </div>
-        </div>
-        <div style={styles.cover}>
-          <img
-            alt="特点图"
-            style={styles.coverImage}
-            src="https://img.alicdn.com/tfs/TB1pPQppv1TBuNjy0FjXXajyXXa-1280-860.png"
-          />
         </div>
       </div>
     );
@@ -34,9 +39,15 @@ export default class ProductFeature extends Component {
 }
 
 const styles = {
+  wrapperContainer: {
+    backgroundColor: '#f6f6f6',
+  },
   wrapper: {
     position: 'relative',
     backgroundColor: '#f6f6f6',
+    height: 500,
+    maxWidth: 1190,
+    margin: '0 auto',
   },
   feature: {
     marginLeft: '50%',
@@ -73,9 +84,10 @@ const styles = {
   cover: {
     position: 'absolute',
     right: '55%',
-    top: 0,
+    bottom: 0,
   },
   coverImage: {
     maxHeight: 430,
+    display: 'block',
   },
 };
--- react-materials/blocks/SoftwareDownload/src/SoftwareDownload.jsx
+++ react-materials/blocks/SoftwareDownload/src/SoftwareDownload.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import { Menu, Button, Feedback } from '@icedesign/base';
+import React, { Component } from 'react';
 
 const SplitButton = Button.Split;
 
@@ -50,8 +50,6 @@ export default class SoftwareDownload extends Component {
     };
   }
 
-  componentWillMount() {}
-
   changeSelectMenu = (select) => {
     this.setState({ osType: select });
   };
@@ -61,9 +59,9 @@ export default class SoftwareDownload extends Component {
     if (loading) {
       Feedback.toast.success('请稍等');
     } else {
+      // 开始下载
       location.href = data[osType].install;
     }
-    this.showBtnAnimate();
   };
 
   render() {
@@ -88,40 +86,71 @@ export default class SoftwareDownload extends Component {
     const subTitle = ver[0] === '0' ? 'Beta' : '';
 
     return (
-      <div style={styles.wrapper}>
-        <div style={styles.bgImage}>
-          <div style={styles.bgImageMask} />
-        </div>
+      <div style={styles.wrapperContainer}>
         <div style={styles.bgImage2} />
-        <div style={styles.software} />
-        <div style={styles.softwareIntro}>
-          <div style={styles.title}>
-            Iceworks <span style={styles.subtitle}>{subTitle}</span>
+        <div style={styles.wrapper}>
+          <div style={styles.bgImage}>
+            <div style={styles.bgImageMask} />
           </div>
-          <div style={styles.slogan}>让前端工程变的轻松便捷</div>
-          <div style={{ paddingTop: 65 }}>
-            <SplitButton
-              menu={menu}
-              onClick={this.download}
-              size="large"
-              style={styles.downloadBtn}
-            >
-              立即下载
-              <span style={{ marginLeft: '5px', fontSize: '12px' }}>
-                {descriptor[osType].download}
-              </span>
-            </SplitButton>
-          </div>
-          <div style={styles.softwareDetail}>
-            <div style={styles.version}>
-              <span style={{ display: 'block' }}>v1.5.0</span>
-              <span style={{ display: 'block' }}>当前版本</span>
+          <div style={styles.wrapperBody}>
+            <div style={styles.softwareIntro}>
+              <div style={styles.title}>
+                Iceworks <span style={styles.subtitle}>{subTitle}</span>
+              </div>
+              <div style={styles.slogan}>让前端工程变的轻松便捷</div>
+              <div
+                style={{
+                  marginLeft: -45,
+                  paddingTop: 45,
+                  textAlign: 'center',
+                }}
+              >
+                <SplitButton
+                  menu={menu}
+                  onClick={this.download}
+                  size="large"
+                  className="iceworks-download-btn"
+                >
+                  立即下载
+                  <span style={{ marginLeft: '5px', fontSize: '12px' }}>
+                    {descriptor[osType].download}
+                  </span>
+                </SplitButton>
+              </div>
+              {loading ? null : (
+                <div style={styles.softwareDetail}>
+                  <div style={styles.version}>
+                    <span style={{ display: 'block' }}>
+                      {data[osType].version}
+                    </span>
+                    <span style={{ display: 'block', fontSize: 12 }}>
+                      当前版本
+                    </span>
+                  </div>
+
+                  <div style={styles.separator} />
+                  <div style={styles.history}>
+                    <span style={{ display: 'block' }}>
+                      {data[osType].releaseDate}
+                    </span>
+                    <span style={{ display: 'block', fontSize: 12 }}>
+                      发布日期
+                    </span>
+                  </div>
+                </div>
+              )}
+              <div style={{ paddingTop: 20, width: 200, textAlign: 'center' }}>
+                <div style={{ fontSize: 12, color: '#fff' }}>
+                  运行环境：{descriptor[osType].env}
+                </div>
+              </div>
+              <div style={{ paddingTop: 20, textAlign: 'center', width: 200 }}>
+                <a style={{ color: '#fff' }} href={'#get-started'}>
+                  立即开始
+                </a>
+              </div>
             </div>
-            <div style={styles.separator} />
-            <div style={styles.history}>
-              <span style={{ display: 'block' }}>2018-05-04</span>
-              <span style={{ display: 'block' }}>查看日志</span>
-            </div>
+            <div style={styles.software} />
           </div>
         </div>
       </div>
@@ -130,16 +159,42 @@ export default class SoftwareDownload extends Component {
 }
 
 const styles = {
+  wrapperContainer: {
+    marginBottom: -400,
+    height: 750 + 400,
+    position: 'relative',
+  },
+  bgImage2: {
+    width: 1400,
+    height: 782,
+    borderRadius: 782 / 2,
+    background:
+      'linear-gradient(135deg, rgba(94,136,255,1) 0%,rgba(111,48,254,1) 100%)',
+    transform: 'rotate(-45deg)',
+    boxShadow: '2px 2px 60px rgba(0,0,0,0.06)',
+    position: 'absolute',
+    left: '35%',
+    bottom: 550,
+    zIndex: 1,
+  },
   wrapper: {
     height: 750,
-    backgroundSize: 'cover',
+    backgroundColor: '#f6f6f6',
     position: 'relative',
-    overflowX: 'hidden',
+    width: '100%',
+  },
+  wrapperBody: {
+    maxWidth: 1190,
+    height: 750,
+    position: 'relative',
+    margin: '0 auto',
+    zIndex: 2,
   },
   softwareIntro: {
     position: 'absolute',
-    left: '10%',
+    left: '5%',
     paddingTop: '230px',
+    zIndex: 3,
   },
   title: {
     fontSize: 46,
@@ -155,10 +210,6 @@ const styles = {
     lineHeight: '34px',
     textShadow: '0 0 3px rgba(0,0,0,0.3)',
   },
-  downloadBtn: {
-    color: '#3080FE',
-    fontSize: 16,
-  },
   softwareDetail: {
     paddingTop: 24,
     display: 'flex',
@@ -172,10 +223,10 @@ const styles = {
   },
   software: {
     backgroundImage:
-      'url(//img.alicdn.com/tfs/TB1i8hfpQyWBuNjy0FpXXassXXa-1558-1266.png)',
-    width: 779,
-    height: 633,
-    backgroundSize: '779px 633px',
+      'url(//img.alicdn.com/tfs/TB1uPagpQyWBuNjy0FpXXassXXa-1446-1148.png)',
+    width: 1446 / 2,
+    height: 1148 / 2,
+    backgroundSize: '723px 574px',
     position: 'absolute',
     top: 100,
     left: '38%',
@@ -195,28 +246,20 @@ const styles = {
     lineHeight: '20px',
     textAlign: 'center',
   },
-  bgImage2: {
-    backgroundImage:
-      'url(//img.alicdn.com/tfs/TB1tuCIpTtYBeNjy1XdXXXXyVXa-1192-853.png)',
-    position: 'absolute',
-    left: '30%',
-    width: 1192,
-    height: 853,
-  },
   bgImage: {
     position: 'absolute',
-    right: '45%',
-    width: 1314,
+    width: '100%',
     height: 750,
+    backgroundSize: 'cover',
     backgroundImage:
-      'url(//img.alicdn.com/tfs/TB1HvqHpTtYBeNjy1XdXXXXyVXa-1314-750.jpg)',
+      'url(//img.alicdn.com/tfs/TB1eoehpKOSBuNjy0FdXXbDnVXa-3798-1500.jpg)',
   },
   subtitle: {
     fontSize: '16px',
     fontWeight: 'normal',
   },
   bgImageMask: {
-    width: 1314,
+    width: '100%',
     height: 750,
     background:
       'linear-gradient(135deg, rgba(94,136,255,0.85) 0%,rgba(111,48,254,0.85) 100%)' /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */,
