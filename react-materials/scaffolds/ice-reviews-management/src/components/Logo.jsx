84a34e700a41096c52f93cad93328b3d1edd0f23
浩睿 <jiangshui.yjs@alibaba-inc.com>
2018-05-21 11:22:34 +0800

    feat: improve review management

--- react-materials/scaffolds/ice-reviews-management/src/components/Logo.jsx
+++ react-materials/scaffolds/ice-reviews-management/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Logo extends PureComponent {
   render() {
     return (
-      <div className="logo" style={{}}>
+      <div className="logo" style={this.props.style}>
         <Link to="/" className="logo-text">
           LOGO
         </Link>
