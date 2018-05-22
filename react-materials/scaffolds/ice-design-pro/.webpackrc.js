a713bf0a39c7196f23a2c92d66e51476575de38b
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-05-22 10:45:28 +0800

    feat: add enableCodeSplitting config

--- react-materials/scaffolds/ice-design-pro/.webpackrc.js
+++ react-materials/scaffolds/ice-design-pro/.webpackrc.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, '..', dir);
+  return path.join(__dirname, dir);
 }
 
 module.exports = {
   resolve: {
     alias: {
-      '@': path.resolve('src'),
+      '@': resolve('src'),
     },
   },
 };
