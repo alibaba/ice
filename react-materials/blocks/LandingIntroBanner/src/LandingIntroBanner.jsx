2108bc4a1b36aeeb753b3238d78ba9320a0c54a7
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-05-29 15:31:31 +0800

    fix: upgrade react-router to v4

--- react-materials/blocks/LandingIntroBanner/src/LandingIntroBanner.jsx
+++ react-materials/blocks/LandingIntroBanner/src/LandingIntroBanner.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from '@icedesign/base';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import './LandingIntroBanner.scss';
 
 // 寻找背景图片可以从 https://unsplash.com/ 寻找
