750fd72e553d3f45fe79e9dbbeddfc38464eb8d6
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-05-31 21:28:10 +0800

    fix: remove react-router deps

--- react-materials/scaffolds/ice-reviews-management/src/router.jsx
+++ react-materials/scaffolds/ice-reviews-management/src/router.jsx
@@ -1,8 +1,7 @@
 /**
  * 定义应用路
  */
-import { HashRouter as Router } from 'react-router-dom';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, HashRouter as Router } from 'react-router-dom';
 import React from 'react';
 
 import routerConfig from './routerConfig';
