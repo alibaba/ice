8eb9dd1a65b5e59b8de941496da58487b7b594b6
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-07-09 09:52:29 +0800

    feat: add ice-design-dashboard scaffold

---
+++ react-materials/scaffolds/ice-design-dashboard/src/menuConfig.js
@@ -0,0 +1,39 @@
+// 菜单配置
+// asideMenuConfig：侧边导航配置
+
+const asideMenuConfig = [
+  {
+    name: 'overview',
+    path: '/',
+  },
+  {
+    name: 'invoices',
+    path: '/iceworks',
+  },
+  {
+    name: 'clients',
+    path: '/clients',
+  },
+  {
+    name: 'payments',
+    path: '/payments',
+  },
+  {
+    name: 'profile',
+    path: '/profile',
+  },
+  {
+    name: 'notifications',
+    path: '/notifications',
+  },
+  {
+    name: 'settings',
+    path: '/settings',
+  },
+  {
+    name: 'contact us',
+    path: '/contact',
+  },
+];
+
+export default asideMenuConfig;
