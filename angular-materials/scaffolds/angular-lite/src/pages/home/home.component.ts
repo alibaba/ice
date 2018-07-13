3d5b75f4f35b360a2bec1d19b0f7383bbef791af
宝码 <nongyoubao@alibaba-inc.com>
2018-07-13 19:31:39 +0800

    Beta (#707)
    * feat: angular app scaffold
    
    * chore: angular-materials
    
    * chore: folder
    
    * chore: optimize style
    
    * feat: angular block card-list
    
    * chore: generate angular materials
    
    * feat: upgrade angular scaffold
    
    * chore: rename angular-lite
    
    * feat: anlular-lite
    
    * chore: @angular-materials/card-list@1.0.1
    
    * feat: modify assets path
    
    * feat: ngx-admin
    
    * feat: remove angular-lite scaffold
    
    * feat: beta env
    
    * fix: master assets path
    
    * chore: add scaffoldConfig to ngx-admin
    
    * fix: syntax error
    
    * feat: add angualr blocks
    
    * chore: bump versions
    
    * fix: export component

--- angular-materials/scaffolds/angular-lite/src/pages/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'page-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {
-  title = 'Angular Lite';
-}
---
+++ angular-materials/scaffolds/ngx-admin/src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <div class="socials">
+      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+    </div>
+  `,
+})
+export class FooterComponent {
+}
