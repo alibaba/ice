42cf60fa93f74e0e7df094411eff031f9661d657
noyobo <nongyoubao@alibaba-inc.com>
2018-07-17 14:49:33 +0800

    fix: catch assets download error

--- tools/extract-css-assets-webpack-plugin/test/webpack-integration.test.js
+++ tools/extract-css-assets-webpack-plugin/test/webpack-integration.test.js
@@ -10,29 +10,38 @@ const cases = process.env.CASES
   : fs.readdirSync(path.join(__dirname, 'cases')).filter(junk.not);
 
 describe('Webpack Integration Tests', () => {
-  cases.forEach((testCase) => {
-    it(testCase, (done) => {
-      let options = { entry: { test: './index.js' } };
-      const testDirectory = path.join(__dirname, 'cases', testCase);
-      const outputDirectory = path.join(__dirname, 'actual', testCase);
-      const configFile = path.join(testDirectory, 'webpack.config.js');
+  cases
+    .filter((testCase) => {
+      // return testCase.indexOf('timeout') > -1;
+      return true;
+    })
+    .forEach((testCase) => {
+      it(testCase, (done) => {
+        let options = { entry: { test: './index.js' } };
+        const testDirectory = path.join(__dirname, 'cases', testCase);
+        const outputDirectory = path.join(__dirname, 'actual', testCase);
+        const configFile = path.join(testDirectory, 'webpack.config.js');
 
-      if (fs.existsSync(configFile)) {
-        options = require(configFile);
-      }
-      options.context = testDirectory;
+        if (fs.existsSync(configFile)) {
+          options = require(configFile);
+        }
+        options.context = testDirectory;
 
-      if (!options.output) options.output = { filename: '[name].js' };
-      if (!options.output.path) options.output.path = outputDirectory;
-      if (process.env.CASES) {
-        console.log(
-          `\nwebpack.${testCase}.config.js ${JSON.stringify(options, null, 2)}`
-        );
-      }
+        if (!options.output) options.output = { filename: '[name].js' };
+        if (!options.output.path) options.output.path = outputDirectory;
+        if (process.env.CASES) {
+          console.log(
+            `\nwebpack.${testCase}.config.js ${JSON.stringify(
+              options,
+              null,
+              2
+            )}`
+          );
+        }
 
-      webpack(options, (err, stats) => {
-        done();
+        webpack(options, (err, stats) => {
+          done();
+        });
       });
     });
-  });
 });
