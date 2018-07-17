a997b9ca39f37ab008f315684df51b11034c2b58
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-07-18 00:52:56 +0800

    feat: init material-dashboard template

---
+++ react-materials/scaffolds/ice-material-dashboard/src/components/Card/cardBodyStyle.jsx
@@ -0,0 +1,17 @@
+const cardBodyStyle = {
+  cardBody: {
+    padding: '0.9375rem 20px',
+    flex: '1 1 auto',
+    WebkitBoxFlex: '1',
+    position: 'relative',
+  },
+  cardBodyPlain: {
+    paddingLeft: '5px',
+    paddingRight: '5px',
+  },
+  cardBodyProfile: {
+    marginTop: '15px',
+  },
+};
+
+export default cardBodyStyle;
