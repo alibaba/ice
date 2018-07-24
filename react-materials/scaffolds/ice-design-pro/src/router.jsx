086b24668bc4cd64829ead129dceeebd6c83c593
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-07-24 18:27:14 +0800

    refactor: refactor router

--- react-materials/scaffolds/ice-design-pro/src/router.jsx
+++ react-materials/scaffolds/ice-design-pro/src/router.jsx
@@ -1,30 +1,20 @@
-/* eslint arrow-parens:0 */
 /**
  * 定义应用路由
  */
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import React from 'react';
-import { routerData } from './routerConfig';
 
+import UserLayout from './layouts/UserLayout';
+import BasicLayout from './layouts/BasicLayout';
+
+// 按照 Layout 归类分组可以按照如下方式组织路由
 const router = () => {
   return (
     <Switch>
-      {routerData.map((item, index) => {
-        return <Route key={index} path={item.path} component={item.layout} />;
-      })}
-      <Redirect exact from="/" to="/dashboard" />
+      <Route path="/user" component={UserLayout} />
+      <Route path="/" component={BasicLayout} />
     </Switch>
   );
 };
-
-// 按照 Layout 归类分组可以按照如下方式组织路由
-// const router = () => {
-//   return (
-//     <Switch>
-//       <Route path="/user" component={UserLayout} />
-//       <Route path="/" component={BasicLayout} />
-//     </Switch>
-//   );
-// };
 
 export default router;
