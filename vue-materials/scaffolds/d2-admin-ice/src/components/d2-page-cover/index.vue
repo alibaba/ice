ff23911b000befedf5b49a1b3591582bcb830175
liyang <1711467488@qq.com>
2018-07-27 18:04:55 +0800

    add D2AdminICE and @vue/eslint-config-standard

---
+++ vue-materials/scaffolds/d2-admin-ice/src/components/d2-page-cover/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="page-cover">
+    <div class="page-cover__logo">
+      <slot/>
+    </div>
+    <p class="page-cover__title">{{title}}</p>
+    <p class="page-cover__sub-title d2-mt-0">{{subTitle}}</p>
+    <slot name="footer"/>
+    <a target="blank" href="https://github.com/d2-projects/d2-admin">
+      <img
+        style="position: absolute; top: 0; right: 0; border: 0; width: 150px;"
+        src="./image/darkblue@2x.png"
+        alt="Fork me on GitHub">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: 'Title'
+    },
+    subTitle: {
+      type: String,
+      required: false,
+      default: 'subTitle'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/public.scss';
+.page-index-article-header {
+  overflow: hidden;
+  .page-index-article-header__title {
+    display: inline-block;
+    padding: 12px 20px;
+  }
+}
+.page-cover {
+  @extend %full;
+  @extend %unable-select;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  .page-cover__logo {
+    img {
+      width: 200px;
+    }
+  }
+  .page-cover__title {
+    color: $color-text-main;
+  }
+  .page-cover__sub-title {
+    color: $color-text-sub;
+  }
+}
+</style>
---
+++ vue-materials/scaffolds/d2-admin-ice/src/layouts/HeaderAside/components/header-user/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-dropdown class="d2-mr">
+    <span class="btn-text">你好 {{userInfo.name}}</span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item @click.native="logOff">
+        <d2-icon name="power-off" class="d2-mr-5"/>
+        注销
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      userInfo: state => state.d2admin.userInfo
+    })
+  },
+  methods: {
+    ...mapActions([
+      'd2adminLogout'
+    ]),
+    /**
+     * @description 登出
+     */
+    logOff () {
+      this.d2adminLogout({
+        vm: this,
+        confirm: true
+      })
+    }
+  }
+}
+</script>
