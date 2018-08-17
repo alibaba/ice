2a967a4b5bb14599e66375c8bbbd887db16b60f9
noyobo <nongyoubao@alibaba-inc.com>
2018-08-17 21:21:48 +0800

    fix: proxyCofnig log

--- tools/ice-scripts/lib/devMiddleware.js
+++ tools/ice-scripts/lib/devMiddleware.js
@@ -1,11 +1,8 @@
 const httpProxyMiddleware = require('http-proxy-middleware');
-const getProxyConfig = require('./config/getProxyConfig');
 
 const webpackDevMock = require('webpack-dev-mock');
 // 附加中间件流程
-module.exports = (app) => {
-  const proxyConfig = getProxyConfig();
-
+module.exports = (app, proxyConfig) => {
   if (proxyConfig) {
     const proxyRules = Object.entries(proxyConfig);
 
