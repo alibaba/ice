2764387372511132837bd2fd5bc47a21c5677c02
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-08-23 21:51:29 +0800

    feat: add @icedesign/creative-login-scaffold

---
+++ react-materials/scaffolds/ice-creative-login/src/components/CustomFormItem/CustomFormItem.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CustomFormItem extends Component {
+  static displayName = 'CustomFormItem';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>12</div>;
+  }
+}
+
+export default CustomFormItem;
