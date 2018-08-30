765235f62d310f6bf6acff0c0fab3e3b2b48d77c
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-08-30 23:04:50 +0800

    chore: fix blocks

--- react-materials/blocks/AddressInfo/src/AddressInfo.jsx
+++ react-materials/blocks/AddressInfo/src/AddressInfo.jsx
@@ -19,7 +19,7 @@ export default class AddressInfo extends Component {
           <div style={styles.mainTitle}>ADDRESS</div>
           <div style={styles.mainDesc}>会议地址及场馆信息</div>
           <img
-            src="https://img.alicdn.com/tfs/TB17C4AuKOSBuNjy0FdXXbDnVXa-2520-1080.jpg"
+            src={require('./images/TB17C4AuKOSBuNjy0FdXXbDnVXa-2520-1080.jpg')}
             alt=""
             style={styles.img}
           />
