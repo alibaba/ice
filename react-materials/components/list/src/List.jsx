f34b96e38177fa1faca194d92bb7e95a304b7b02
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2018-09-17 18:37:05 +0800

    feat: init list component

---
+++ react-materials/components/list/src/List.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class List extends Component {
+  static displayName = 'List';
+
+  render() {
+    return (
+      <div className="list">Hello List</div>
+    );
+  }
+}
+
