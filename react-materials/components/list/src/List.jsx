7098c148a8b983fbc300554cee7d21348760b8d0
noyobo <nongyoubao@alibaba-inc.com>
2018-09-20 21:23:58 +0800

    feat: stateless list component

--- react-materials/components/list/src/List.jsx
+++ react-materials/components/list/src/List.jsx
@@ -1,34 +1,33 @@
-import React, {Component, cloneElement, Children} from 'react';
+import React, { cloneElement, Children } from 'react';
 import omit from 'lodash.omit';
 
 /**
  * List容器组件
  */
-export default class List extends Component {
-  render() {
-    const {column, spacing = 10, style, children} = this.props;
-    const {width} = style;
+const List = ({ column, spacing = 10, style, children }) => {
+  const { width } = style;
+  return (
+    <div
+      {...omit(this.props, ['column', 'spacing'])}
+      style={{
+        display: 'flex',
+        flexFlow: 'row wrap',
+        justifyContent: 'flex-start',
+        border: '1px solid transparent',
+        paddingTop: `${spacing}px`,
+        paddingLeft: `${spacing}px`,
+        ...style,
+      }}
+    >
+      {Children.map(children, (child, index) => {
+        const cloneChildren = cloneElement(child, {
+          width: (width - spacing * (column + 1) - 2) / column,
+          spacing,
+        });
+        return cloneChildren;
+      })}
+    </div>
+  );
+};
 
-    return (
-      <div {...omit(this.props, ['column', 'spacing'])}
-        style={{
-          display: 'flex',
-          flexFlow: 'row wrap',
-          justifyContent: 'flex-start',
-          border: '1px solid transparent',
-          paddingTop: `${spacing}px`,
-          paddingLeft: `${spacing}px`,
-          ...style
-        }}
-      >
-        {Children.map(children, (child, index) => {
-          const cloneChildren = cloneElement(child, {
-            width: (width - spacing * (column + 1) - 2) / column,
-            spacing
-          });
-          return cloneChildren;
-        })}
-      </div>
-    );
-  }
-}
+export default List;
