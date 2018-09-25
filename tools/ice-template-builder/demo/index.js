7b416f826e007c699588f6cd0b0a85a8589c064b
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-09-25 15:20:43 +0800

    chore: remove unnecessary files

--- tools/ice-template-builder/demo/index.js
+++
@@ -1,4 +0,0 @@
-window["react"] = require("react");
-window["react-dom"] = require("react-dom");
-window["@icedesign/template-builder"] = require("@icedesign/template-builder");
-require('/Users/chenbin/Documents/openSource/ice/tools/ice-template-builder/node_modules/.ice_tmp/style.js');
