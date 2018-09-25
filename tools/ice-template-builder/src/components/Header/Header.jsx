e374c4f20fd603d1f12c7c6f2530647604fe124d
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-09-25 15:40:03 +0800

    chore: replace images

--- tools/ice-template-builder/src/components/Header/Header.jsx
+++ tools/ice-template-builder/src/components/Header/Header.jsx
@@ -64,7 +64,7 @@ export default class Header extends PureComponent {
                 <IceImg
                   height={40}
                   width={40}
-                  src="https://img.alicdn.com/tfs/TB1L6tBXQyWBuNjy0FpXXassXXa-80-80.png"
+                  src={require('./images/avatar.png')}
                   className="user-avatar"
                 />
                 <div className="user-profile">
@@ -85,17 +85,20 @@ export default class Header extends PureComponent {
             <ul>
               <li className="user-profile-menu-item">
                 <a href="#">
-                  <FoundationSymbol type="person" size="small" />我的主页
+                  <FoundationSymbol type="person" size="small" />
+                  我的主页
                 </a>
               </li>
               <li className="user-profile-menu-item">
                 <a href="#">
-                  <FoundationSymbol type="repair" size="small" />设置
+                  <FoundationSymbol type="repair" size="small" />
+                  设置
                 </a>
               </li>
               <li className="user-profile-menu-item">
                 <a href="#">
-                  <FoundationSymbol type="compass" size="small" />退出
+                  <FoundationSymbol type="compass" size="small" />
+                  退出
                 </a>
               </li>
             </ul>
--- tools/ice-template-builder/templates/redux/src/layouts/UserLayout/UserLayout.jsx
+++ tools/ice-template-builder/templates/redux/src/layouts/UserLayout/UserLayout.jsx
@@ -19,7 +19,7 @@ export default class UserLayout extends Component {
           <a href="#" className="meta">
             <img
               className="logo"
-              src="https://img.alicdn.com/tfs/TB13UQpnYGYBuNjy0FoXXciBFXa-242-134.png"
+              src={require('./images/logo.png')}
               alt="logo"
             />
             <span className="title">飞冰</span>
@@ -54,8 +54,7 @@ const styles = {
     height: '100vh',
     paddingTop: '100px',
     background: '#f0f2f5',
-    backgroundImage:
-      'url(https://img.alicdn.com/tfs/TB1kOoAqv1TBuNjy0FjXXajyXXa-600-600.png)',
+    backgroundImage: `url(${require('./images/bg.png')})`,
     backgroundSize: 'contain',
   },
 };
