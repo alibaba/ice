585600df823d52207f0ecf49477e669a6ef48b95
noyobo <nongyoubao@alibaba-inc.com>
2018-09-27 16:07:06 +0800

    feat: ice-scripts server

--- tools/ice-scripts/bin/ice.js
+++ tools/ice-scripts/bin/ice.js
@@ -3,15 +3,34 @@
 'use strict';
 
 const program = require('commander');
+const chalk = require('chalk');
+
 const packageInfo = require('../package.json');
 
 console.log(packageInfo.name, packageInfo.version);
 program
   .version(packageInfo.version)
+  .usage('<command> [options]')
+
+program
   .command('build', 'build project')
   .command('dev', 'start server')
-  .command('server', 'start a static assets server, alias `http-server`')
-  .parse(process.argv);
+  .command('server', 'start a static assets server, alias `http-server`');
+
+// output help information on unknown commands
+program.arguments('<command>').action((cmd) => {
+  const commandNames = program.commands.map((c) => {
+    return c.name();
+  });
+
+  if (!commandNames.includes(cmd)) {
+    program.outputHelp();
+    console.log(`\n  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+    console.log();
+  }
+});
+
+program.parse(process.argv);
 
 const proc = program.runningCommand;
 
@@ -23,6 +42,6 @@ if (proc) {
 }
 
 const subCmd = program.args[0];
-if (!subCmd || (subCmd !== 'build' && subCmd !== 'dev')) {
+if (!subCmd) {
   program.help();
 }
