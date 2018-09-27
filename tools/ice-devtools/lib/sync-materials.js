f844bcbd124904b9927e974a92d33d89d2d9563e
暮尘 <i@muchen.me>
2018-09-27 11:16:26 +0800

    feat: finish sync

--- tools/ice-devtools/lib/sync-materials.js
+++ tools/ice-devtools/lib/sync-materials.js
@@ -3,29 +3,32 @@ const inquirer = require('inquirer');
 const pathExists = require('path-exists');
 const path = require('path');
 const fs = require('fs');
+const ora = require('ora');
+
 const {promisify} = require('util');
 const [readFile, writeFile] = [fs.readFile, fs.writeFile].map(fn => promisify(fn));
 
 const BASE_URL = 'http://localhost:7001';
 const AUTH_HEADER_KEY = 'x-auth-token';
-const BLOCK_URL = `http://${BASE_URL}/auth_api/v1/npm/block`;
-const SCALLFOLD_URL = `http://${BASE_URL}/auth_api/v1/npm/scaffold`;
+const BLOCK_URL = `${BASE_URL}/auth_api/v1/npm/block`;
+const SCALLFOLD_URL = `${BASE_URL}/auth_api/v1/npm/scaffold`;
 const TOKEN_PATH = path.join(__dirname, '../chore/.token');
 async function tokenPrepare() {
   let token;
   const tokenExists = await pathExists(TOKEN_PATH);
-  if (tokenExists) {
-    return await writeToken();
+  if (!tokenExists) {
+    return writeToken();
   }
-  token = await readFile(TOKEN_PATH, 'utf-8');
+  tokenFile = await readFile(TOKEN_PATH, 'utf-8');
+  token = JSON.parse(tokenFile).token;
   if (!token) {
-
+    return writeToken();
   }
-
+  return token;
 }
 
 async function clearToken() {
-  await writeFile(CLI_ROOT, '', 'utf-8');
+  await writeFile(TOKEN_PATH, '{"token": ""}', 'utf-8');
 }
 
 async function writeToken() {
@@ -43,7 +46,7 @@ async function writeToken() {
     }
   }]);
   const {token} = answers;
-  await writeFile(TOKEN_PATH, token, 'utf-8');
+  await writeFile(TOKEN_PATH, JSON.stringify({token}, null, 2), 'utf-8');
   return token;
 }
 
@@ -64,15 +67,75 @@ async function dbPrepare(cwd) {
   return db;
 }
 
+function requestAPI(materials) {
+  const spin = ora({
+    text: 'start sync your materials to https://fusion.design',
+    color: 'blue',
+  });
+  spin.start();
+  let promise = Promise.resolve();
+  for (let index = 0; index < materials.length; index++) {
+    const element = materials[index];
+
+    promise = promise.then(() => {
+      const options = {
+        method: 'POST',
+        uri: element.url,
+        body: element.data,
+        headers: {
+          [AUTH_HEADER_KEY]: element.token,
+        },
+        json: true // Automatically stringifies the body to JSON
+      }
+      return rp(options).then(function (body) {
+        const {code} = body;
+        if (code === 1) {
+          spin.stopAndPersist({symbol: '(￣▽￣)~* ', text: `${element.name} sync success`});
+          return;
+        }
+        return Promise.reject({statusCode: 200, body});
+
+      }).catch(err => {
+          const {statusCode, response} = err;
+          if (statusCode === 403 && response.body && response.body.code === 403) {
+            clearToken();
+            spin.stopAndPersist({symbol: '(╯°□°）╯︵┻━┻ ', text: `token authorization fail, you can find your token at https://fusion.design`});
+          } else if (statusCode === 200) {
+            spin.stopAndPersist({symbol: '(╯°□°）╯︵┻━┻ ', text: `${element.name} sync fail for season: ${body.message}`});
+          } else {
+            console.error('\n', err);
+            spin.fail(`${element.name} sync fail: \n`);
+          }
+
+          return Promise.reject(); // 返回错误只是为了打断后续流程
+      });
+    });
+  }
+
+  return promise.then(() => spin.succeed('all done')).catch(() => spin.fail('sync fail'));
+}
+
+
 
 async function syncMaterials(opt) {
+
   const {cwd} = opt;
   const db = await dbPrepare(cwd);
   if (!db) {
     return;
   }
-  await writeToken();
+  const token = await tokenPrepare();
+  if (!token) {
+    return;
+  }
 
+  const {blocks, scaffolds} = db;
+  const requestData = [
+    ...blocks.map(block => ({url: BLOCK_URL, token, data: block, name: block.name})),
+    ...scaffolds.map(scaffold => ({url: SCALLFOLD_URL, token, data: scaffold, name: scaffold.name}))
+  ];
+
+  await requestAPI(requestData);
 }
 
-module.exports = syncMaterials
+module.exports = syncMaterials;
