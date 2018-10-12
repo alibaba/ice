f9611ccf1959f3e4d606381b8645c91f1648b7cd
暮尘 <i@muchen.me>
2018-10-12 11:47:42 +0800

    feat: add materials path config get

--- tools/ice-devtools/lib/build-blocks.js
+++ tools/ice-devtools/lib/build-blocks.js
@@ -6,6 +6,7 @@ const npminstall = require('npminstall');
 const buildblockCmd = require('./build-block');
 const screenshotCmd = require('./screenshot');
 const chalk = require('chalk');
+const debug = require('debug')('ice:build-blocks');
 
 async function buildBlock(blockPath) {
   console.log(chalk.cyan(`${blockPath} build start`));
@@ -29,10 +30,22 @@ module.exports = function(opts) {
   if (!fs.existsSync(pkgPath)) {
     throw new Error('package.json not exists.');
   }
+  const pkgJSON = require(pkgPath);
+  const reactMaterials = pkgJSON.materials.filter(material => material.type === 'react');
 
-  const blocksPath = path.join(cwd, '/react-materials/blocks/*');
+  let reactMaterial;
+  if (reactMaterials.length < 1) {
+    reactMaterial = {directory: 'react-materials'}
+  } else {
+    reactMaterial = reactMaterials[0];
+  }
 
+  const reactMaterialBaseDir = reactMaterial.directory;
+  debug('reactMaterialBaseDir %s', reactMaterialBaseDir);
+  const blocksPath = path.join(cwd, reactMaterialBaseDir, '/blocks/*');
+  debug('blocksPath: %s', blocksPath);
   const blockPaths = glob.sync(blocksPath);
+  debug('blockPaths: %j', blockPaths);
   let promise = blockPaths.reduce((promise, blockpath) => promise.then(() => buildBlock(blockpath)), Promise.resolve());
   promise.then(() => console.log(chalk.blue('all done'))).catch(err => console.error(err));
 };
