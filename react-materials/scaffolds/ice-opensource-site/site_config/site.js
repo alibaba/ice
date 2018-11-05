a77a9c991d4590231d9f9ec24d947849c0445dc7
qingjue <yudong.myd@alibaba-inc.com>
2018-11-05 20:44:20 +0800

    feat: publish ice-opensource-site scaffold

--- react-materials/scaffolds/ice-opensource-site/site_config/site.js
+++ react-materials/scaffolds/ice-opensource-site/site_config/site.js
@@ -1,6 +1,6 @@
 // 全局的一些配置
 export default {
-  rootPath: '/product', // 发布到服务器的根目录，需以/开头但不能有尾/，如果只有/，请填写空字符串
+  rootPath: '/ice/scaffold-preview/ice-opensource-site', // 发布到服务器的根目录，需以/开头但不能有尾/，如果只有/，请填写空字符串
   port: 8080, // 本地开发服务器的启动端口
   defaultLanguage: 'en-us',
   'en-us': {
