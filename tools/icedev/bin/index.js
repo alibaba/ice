885bfea9c130b286d26152688f3b7a70bcbda48d
暮尘 <i@muchen.me>
2018-11-30 15:53:18 +0800

    feat: add option for cli

--- tools/icedev/bin/index.js
+++ tools/icedev/bin/index.js
@@ -5,33 +5,56 @@ const cwd = process.cwd();
 const checkVersion = require('../util/check-version');
 
 const supportCommands = {
-  init: 'generate a new project from a template',
-  add: 'add a new block/layout/scaffold from a official templates',
-  start: 'Start Dev Server',
-  build: 'Build Component',
-  screenshot: 'Create screenshot.png',
-  generate: 'Generate database json',
-  sync: 'Sync materials to https://fusion.design',
+  init: {
+    desc: 'generate a new project from a template',
+    options: [{
+      name: '--offline',
+      desc: 'use cached template',
+    }]
+  },
+  add: {
+    desc: 'add a new block/layout/scaffold from a official templates',
+  },
+  start: {
+    desc: 'Start Dev Server',
+  },
+  build: {
+    desc: 'Build Component',
+  },
+  screenshot: {
+    desc: 'Create screenshot.png',
+  },
+  generate: {
+    desc: 'Generate database json',
+  },
+  sync: {
+    desc: 'Sync materials to https://fusion.design',
+  },
 };
 
 program
   .version(require('../package').version).usage('[command] [options]');
 
 Object.entries(supportCommands).forEach(entry => {
-  program
+  const command = program
     .command(entry[0])
-    .description(chalk.green(entry[1]))
-    .action(function() {
-      const fn = require(`../lib/${entry[0]}`);
-      const args = [cwd].concat(Array.prototype.slice.call(arguments))
-      fn.apply(global, args);
-    });
+    .description(chalk.green(entry[1].desc));
+
+  entry[1].options && entry[1].options.forEach(opt => {
+    command.option(opt.name, opt.desc);
+  });
+
+  command.action(function () {
+    const fn = require(`../lib/${entry[0]}`);
+    const args = [cwd].concat(Array.prototype.slice.call(arguments))
+    fn.apply(global, args);
+  });
 });
 
-program.command('*').description(chalk.gray('help')).action(function() {
+program.command('*').description(chalk.gray('help')).action(function () {
   console.log(chalk.red('No sub command matched, please use --help'));
 })
 
-checkVersion(function() {
+checkVersion(function () {
   program.parse(process.argv);
 });
