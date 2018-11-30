512a9247cdb88d902386e73ffd96fd2e44e07324
暮尘 <i@muchen.me>
2018-11-30 12:15:47 +0800

    feat: init commit for new devtools

--- tools/icedev/bin/ice-devtools.js
+++ tools/icedev/bin/ice-devtools.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 require('commander')
   .version(require('../package').version)
   .usage('<command> [options]')
@@ -8,7 +9,4 @@ require('commander')
   .command('build', 'Build Component')
   .command('screenshot', 'Create screenshot.png')
   .command('generate', 'Generate database json')
-  .command('build-block', 'Build block demo')
-  .command('build-blocks', 'Build All block demos')
-  .command('sync', 'Sync materials to https://fusion.design')
   .parse(process.argv);
