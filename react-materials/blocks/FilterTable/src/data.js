63e84c3660fc864f3e28cc96252ca5ad1dc1dac1
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-12-14 18:05:18 +0800

    refactor: remove demo files

---
+++ react-materials/blocks/FilterTable/src/data.js
@@ -0,0 +1,100 @@
+const data = [
+  {
+    id: 1,
+    title: '2017秋冬新款背带裙复古格子连衣裙清新背心裙a字裙短裙子',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 2,
+    title: '2017秋冬新款 高质感特定纱线欧美宽松马海毛羊毛毛衣',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 3,
+    title: '日式天然玉米皮草编碗垫锅垫隔热垫茶垫加厚餐垫GD-29',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 4,
+    title: '2017秋冬新款 绑带腰封设计感超顺滑质感落肩铜氨丝连衣裙',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 5,
+    title: '日式糖果色陶瓷柄不锈钢餐具西餐牛扒刀叉勺子咖啡勺',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 6,
+    title: '日式和风深蓝素色文艺餐巾餐垫围裙锅垫隔热手套厨房桌布',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 7,
+    title: '日式雪点樱花手绘陶瓷餐具米饭碗拉面碗寿司盘子汤碗',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 8,
+    title: '川岛屋 釉下彩复古日式陶瓷盘子菜盘圆盘调味碟 米饭碗日式餐具',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+];
+
+export default data;
