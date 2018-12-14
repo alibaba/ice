c7881d6a28226fa993d580e9dec6b8c74d399b23
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-12-14 18:28:46 +0800

    feat: 区块重构 (#1162)
    * fix: remove unnecessary unpkg files
    
    * refactor: remove ChartPoint block
    
    * refactor: remive block demo
    
    * refactor: code optim
    
    * fix: update preview url
    
    * refactor: remove demo files (#1168)
    
    * fix: fix homepage url

---
+++ react-materials/blocks/SimpleTable/src/data.js
@@ -0,0 +1,116 @@
+const data = [
+  {
+    id: 1,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i3/120976213/TB2O4nSnblmpuFjSZFlXXbdQXXa_!!120976213.jpg_240x240.jpg',
+    title: '于momo2017秋冬新款背带裙复古格子连衣裙清新背心裙a字裙短裙子',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 2,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i4/TB1GiPSinJ_SKJjSZPiYXH3LpXa_M2.SS2_100x100.jpg',
+    title: 'SOTHEA 2017秋冬新款 高质感特定纱线欧美宽松马海毛羊毛毛衣',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 3,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i3/120976213/TB2O4nSnblmpuFjSZFlXXbdQXXa_!!120976213.jpg_240x240.jpg',
+    title: '川岛屋 日式天然玉米皮草编碗垫锅垫隔热垫茶垫加厚餐垫GD-29',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 4,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i7/TB1QpMvk3n.PuJjSZFkYXI_lpXa_M2.SS2_100x100.jpg',
+    title: 'SOTHEA 2017秋冬新款 绑带腰封设计感超顺滑质感落肩铜氨丝连衣裙',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 5,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i4/120976213/TB2GiVsdS0mpuFjSZPiXXbssVXa_!!120976213.jpg_240x240.jpg',
+    title: '川岛屋 日式糖果色陶瓷柄不锈钢餐具西餐牛扒刀叉勺子咖啡勺S-27',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 6,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i3/120976213/TB2bxHGtpXXXXXVXXXXXXXXXXXX_!!120976213.jpg_240x240.jpg',
+    title: '日式和风深蓝素色文艺餐巾餐垫围裙锅垫隔热手套厨房桌布GD-11',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 7,
+    cover:
+      'https://img.alicdn.com/bao/uploaded/i4/120976213/TB2bEcHnXXXXXbgXXXXXXXXXXXX_!!120976213.jpg_100x100.jpg',
+    title: '川岛屋 日式雪点樱花手绘陶瓷餐具米饭碗拉面碗寿司盘子汤碗PZ-36',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+  {
+    id: 8,
+    cover:
+      '//img.alicdn.com/bao/uploaded/i2/TB11DjAIFXXXXaTXFXXXXXXXXXX_!!0-item_pic.jpg_100x100.jpg',
+    title: '川岛屋 釉下彩复古日式陶瓷盘子菜盘圆盘调味碟 米饭碗日式餐具',
+    url: 'https://item.taobao.com/item.htm?id=558559528377',
+    type: '清单',
+    publishTime: '17-04-28 20:29:20',
+    publishStatus: '已发布',
+    pushStatus: '已推送至订阅号',
+    operation: {
+      edit: true,
+    },
+  },
+];
+
+export default data;
