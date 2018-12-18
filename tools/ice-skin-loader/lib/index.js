ff6203dbe6f7b01bf94f6a108c29702d92d15123
大果 <sobear.me@gmail.com>
2018-12-18 21:21:25 +0800

    fix: 重构 ice-skin-loader (#1181)
    ## 改动点
    
    - 不删除 `import variables.scss` 语句，不确定之前为什么这么做，事实上是有副作用的，比如主题包里的变量不全
    - 取代 css-prefix-plugin 的功能，发布后删除 css-prefix 这个包
      - 将配置从 buildConfig 移到 themeConfig 打脸x1
      - 考虑到需要结合 configProvider，用户还是得自定义 prefix，所以讲原先的 bool 改回 string，打脸x2
    - themeConfig 由 ice-scripts 传入，loader 自己读文件不合理
    - 修复没有 theme 就不进 skin-loader 的 bug
    - 代码重构&补充文档
    
    ## 发布
    
    - ice-scripts 1.7.9 -> 1.8.0
    - ice-skin-loader 0.1.0 -> 0.2.0

--- tools/ice-skin-loader/lib/index.js
+++ tools/ice-skin-loader/lib/index.js
@@ -1,64 +1,53 @@
-const chalk = require('chalk');
 const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 const loaderUtils = require('loader-utils');
-const md5 = require('md5');
-const pathExists = require('path-exists');
-const createSkinExtraContent = require('./createSkinExtraContent');
+const generateVarsByThemeConfig = require('./generateVarsByThemeConfig');
 
-const skinContentCache = {};
+let themeFileVars = '';
+let themeConfigVars = '';
+
+module.exports = function (source) {
+  const options = loaderUtils.getOptions(this);
+  const { themeFile, themeConfig } = options;
+  const modulePath = path.relative(this.rootContext, this.resourcePath);
+
+  let prefixVars = '';
+  if (themeConfig.nextPrefix && /@alifd\/next\/lib\/(.+).scss$/.test(modulePath)) {
+    // 将 next 1.x 的 prefix 从 next- 改为自定义前缀，解决 0.x&1.x 混用的问题
+    prefixVars = `$css-prefix: "${themeConfig.nextPrefix}";`;
+  }
+
+  if (themeFile) {
+    if (!themeFileVars) {
+      try {
+        themeFileVars = deleteEmptyLine(fs.readFileSync(themeFile).toString());
+      } catch (err) {
+        console.log(chalk.red('\n[Error] 不存在主题文件：'), themeFile, err);
+        // 防止重复读取不存在的文件
+        themeFileVars = '  ';
+      }
+    }
+  }
+
+  try {
+    themeConfigVars = themeConfigVars || generateVarsByThemeConfig(themeConfig);
+  } catch (err) {
+    console.error(chalk.red('\n[Error] generateVarsByThemeConfig 出错'), err);
+    themeConfigVars = '  ';
+  }
+
+  // 权重 prefixVars > themeConfigVars > themeFileVars > source
+  return `${themeFileVars}\n${themeConfigVars}\n${prefixVars}\n${source}`;
+};
+
 
 // delete empty lines
 function deleteEmptyLine(str) {
-  const filterLines = str.split('\n').filter(function(line) {
+  const filterLines = str.split('\n').filter((line) => {
     return line !== '';
   });
 
   filterLines.push('');
   return filterLines.join('\n');
 }
-
-// 移除手动引入的变量文件, 防止被原始皮肤覆盖
-function deleteVariablesImport(source = '') {
-  return source
-    .split('\n')
-    .filter(function(line) {
-      return !/\/variables\.scss/.test(line);
-    })
-    .join('\n');
-}
-
-// init user defined variables
-const extraContent = createSkinExtraContent();
-
-let hasEmittedHelp = false;
-module.exports = function(source) {
-  const options = loaderUtils.getOptions(this);
-  const themeFile = options.themeFile;
-  if (!themeFile) {
-    return source;
-  }
-
-  // 计算 md5 值，避免中文路径的问题
-  const filePathHash = md5(themeFile);
-
-  source = deleteVariablesImport(source);
-
-  if (skinContentCache[filePathHash]) {
-    return skinContentCache[filePathHash] + '\n' + extraContent + '\n' + source;
-  }
-
-  // 缓存 skinLoader
-  if (pathExists.sync(themeFile)) {
-    const themeFileContent = fs.readFileSync(themeFile).toString();
-    skinContentCache[filePathHash] = deleteEmptyLine(themeFileContent);
-    return skinContentCache[filePathHash] + '\n' + extraContent + '\n' + source;
-  }
-
-  // only tip once
-  if (!hasEmittedHelp) {
-    console.log(chalk.red('\n[Error] 不存在皮肤文件:'), themeFile);
-    hasEmittedHelp = true;
-  }
-
-  return source;
-};
