28faa7d49676a1a3eb875414f996dbb6d29284ec
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2018-12-19 20:35:32 +0800

    [scaffold-v2]refactor: refacrot operation-admin (#1185)
    * refactor: refacrot operation-admin
    
    * feat: add dashboard page
    
    * chore: update screenshot
    
    * fix: add Users card
    
    * chore: optim dashboard

---
+++ react-materials/scaffolds/ice-operation-admin/src/pages/Edit/Edit.jsx
@@ -0,0 +1,202 @@
+/* eslint react/no-string-refs:0 */
+import React, { Component } from 'react';
+import IceContainer from '@icedesign/container';
+import {
+  Input,
+  Button,
+  Select,
+  DatePicker,
+  Radio,
+  Feedback,
+} from '@icedesign/base';
+import {
+  FormBinderWrapper as IceFormBinderWrapper,
+  FormBinder as IceFormBinder,
+  FormError as IceFormError,
+} from '@icedesign/form-binder';
+
+const { Option } = Select;
+const { Group: RadioGroup } = Radio;
+
+export default class DonationForm extends Component {
+  static displayName = 'DonationForm';
+
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: {
+        status: 'pending',
+      },
+    };
+  }
+
+  formChange = (value) => {
+    console.log('value', value);
+    this.setState({
+      value,
+    });
+  };
+
+  validateAllFormField = () => {
+    this.refs.form.validateAll((errors, values) => {
+      if (errors) {
+        console.log({ errors });
+        return;
+      }
+      console.log({ values });
+      Feedback.toast.success('提交成功');
+    });
+  };
+
+  render() {
+    return (
+      <IceContainer style={styles.container}>
+        <div style={styles.title}>设备编辑</div>
+        <IceFormBinderWrapper
+          value={this.state.value}
+          onChange={this.formChange}
+          ref="form"
+        >
+          <div style={styles.formContent}>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>设备型号-名称</div>
+              <IceFormBinder
+                required
+                triggerType="onBlur"
+                message="设备名称不能为空"
+                name="modelName"
+              >
+                <Input
+                  placeholder="请输入设备名称"
+                  size="large"
+                  style={{ width: '400px' }}
+                />
+              </IceFormBinder>
+              <div style={styles.formError}>
+                <IceFormError name="modelName" />
+              </div>
+            </div>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>设备型号-ID</div>
+              <IceFormBinder
+                required
+                triggerType="onBlur"
+                message="设备型号-ID不能为空"
+                name="modelId"
+              >
+                <Input
+                  placeholder="请输入设备型号-ID"
+                  size="large"
+                  style={{ width: '400px' }}
+                />
+              </IceFormBinder>
+              <div style={styles.formError}>
+                <IceFormError name="modelId" />
+              </div>
+            </div>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>设备类别</div>
+              <IceFormBinder name="cate">
+                <Select
+                  placeholder="请选择"
+                  multiple
+                  size="large"
+                  style={{ width: '400px' }}
+                >
+                  <Option value="1">传统领域</Option>
+                  <Option value="2">互联网领域</Option>
+                  <Option value="3">其他</Option>
+                </Select>
+              </IceFormBinder>
+            </div>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>设备责任人</div>
+              <IceFormBinder
+                required
+                name="responsible"
+                triggerType="onBlur"
+                message="责任人不能为空"
+              >
+                <Input
+                  placeholder="请输入"
+                  size="large"
+                  style={{ width: '400px' }}
+                />
+              </IceFormBinder>
+              <div style={styles.formError}>
+                <IceFormError name="responsible" />
+              </div>
+            </div>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>设备生产时间</div>
+              <IceFormBinder name="time">
+                <DatePicker size="large" style={{ width: '400px' }} />
+              </IceFormBinder>
+            </div>
+            <div style={styles.formItem}>
+              <div style={styles.formLabel}>绑定状态</div>
+              <IceFormBinder name="status">
+                <RadioGroup
+                  dataSource={[
+                    {
+                      value: 'pending',
+                      label: '绑定中',
+                    },
+                    {
+                      value: 'no',
+                      label: '未绑定',
+                    },
+                    {
+                      value: 'yes',
+                      label: '已绑定',
+                    },
+                  ]}
+                />
+              </IceFormBinder>
+            </div>
+            <Button
+              type="primary"
+              size="large"
+              style={styles.submitButton}
+              onClick={this.validateAllFormField}
+            >
+              提 交
+            </Button>
+          </div>
+        </IceFormBinderWrapper>
+      </IceContainer>
+    );
+  }
+}
+
+const styles = {
+  title: {
+    marginBottom: '30px',
+    fontSize: '18px',
+    fontWeight: '500',
+    color: 'rgba(0, 0, 0,.85)',
+  },
+  formContent: {
+    marginLeft: '30px',
+  },
+  formItem: {
+    marginBottom: '25px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  formLabel: {
+    width: '120px',
+    marginRight: '15px',
+    textAlign: 'right',
+  },
+  formError: {
+    marginLeft: '10px',
+  },
+  submitButton: {
+    marginLeft: '140px',
+  },
+};
