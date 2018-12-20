58c84012a633841440c7cc9bebebf41760caf452
暮郴 <zhengtao.lzt@alibaba-inc.com>
2018-12-20 15:51:19 +0800

    feat: remove useless file

--- tools/ice-devtools/.temp/block-react-index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import Block from '/Users/muchen/code/iceapp/app3/blocks/FileTestListNow/src/index.js';
-
-const mountNode = document.querySelector('#mountNode');
-ReactDOM.render(
-  <Block />,
-  mountNode
-);
