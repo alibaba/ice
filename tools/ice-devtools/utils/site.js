1d1ca3553b37351d5f8844be633ee485dff8f424
暮郴 <zhengtao.lzt@alibaba-inc.com>
2018-12-20 20:25:54 +0800

    feat: add error info

--- tools/ice-devtools/utils/site.js
+++ tools/ice-devtools/utils/site.js
@@ -81,17 +81,38 @@ async function fetchSites(token) {
         resl(body.data);
       });
     });
+    if (!sites || !sites.length) {
+      console.log(chalk.white('-'.repeat(62)));
+      console.log();
+      console.log('获取站点失败。您可以自己创建一个站点或者请其他站点把您添加为成员');
+      console.log(`创建站点文档: ${chalk.yellow('https://fusion.design/help.html#/dev-create-site')}`);
+      console.log(`添加成员文档: ${chalk.yellow('https://fusion.design/help.html#/dev-create-site')}`);
+      console.log();
+      console.log(chalk.white('-'.repeat(62)));
+    }
   } catch (error) {
     const { response } = error;
     if (response && (response.statusCode === 401 || response.statusCode === 403 || response.success === false)) {
+      console.log(chalk.white('-'.repeat(62)));
       console.log();
-      console.log(chalk.red('  token authorization fail, you can find your token at https://fusion.design'));
+      console.log(`鉴权失败,请前往 https://fusion.design 重新获取Token.\n请查看文档: ${chalk.yellow('https://fusion.design/help.html#/dev-create-site')}`);
+      console.log();
+      console.log(chalk.white('-'.repeat(62)));
       tokenUtil.clearToken();
     } else {
       logger.fatal(error);
     }
   }
+
   return sites;
+}
+
+function TokenFirstLyMessage() {
+  console.log(chalk.white('-'.repeat(62)));
+  console.log();
+  console.log(`如果这是你第一次使用该功能,或者不知道如何获取Token。\n请查看文档: ${chalk.yellow('https://fusion.design/help.html#/dev-create-site')}`);
+  console.log();
+  console.log(chalk.white('-'.repeat(62)));
 }
 
 async function selectSite(sites) {
@@ -117,6 +138,9 @@ async function getSite(cwd, token) {
   let site = getLocalSite(cwd);
   // 有则直接用本地的site
   if (site) {
+    console.log();
+    console.log(chalk.green('已从packge.json中,获取Fusion站点....'));
+    console.log();
     return site;
   }
 
