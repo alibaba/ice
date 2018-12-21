bb06f8cc7e0ff31b5908bf48a0c3ab4096eac5ba
水澜 <shuilan.cj@taobao.com>
2018-12-21 11:51:11 +0800

    feat: sync 提示优化

--- tools/ice-devtools/lib/sync-npm.js
+++ tools/ice-devtools/lib/sync-npm.js
@@ -11,18 +11,26 @@ module.exports = function sync(cwd, opt) {
   const cmd = isTnpm ? 'tnpm' : 'npm';
 
   const process = spawn(cmd, ['publish'], { stdio: 'inherit' });
-  
+
   process.on('close', (code) => {
     if (code == 0) {
       console.log();
       const url = (isTnpm ? 'https://unpkg.alibaba-inc.com/' : 'https://unpkg.com/') + pkgJSON.name + '/build/react-materials.json';
-      console.log(chalk.green('material json sync success!'));
+      // console.log(chalk.cyan('material json sync success!'));
+      console.log(chalk.cyan('物料源同步成功!'));
       console.log();
-      console.log(url);
+      console.log(chalk.yellow(url));
+      console.log();
+      console.log('刷新 ICEWorks 物料面板，即可查看更新后的物料');
+      console.log();
+      console.log('初次使用，请拷贝上面的物料地址，到 ICEWorks 中开始私有物料配置');
+      console.log();
+      console.log('物料配置文档: https://alibaba.github.io/ice/docs/materials/quick-start');
       console.log();
     } else {
       console.log();
-      console.log(chalk.red('material json sync failed!'));
+      // console.log(chalk.red('material json sync failed!'));
+      console.log('物料源同步失败!');
       console.log();
     }
   });
