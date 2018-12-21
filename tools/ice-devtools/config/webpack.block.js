066dc4764f9f4b3499a7561ab060cd976af45cb9
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-12-21 11:09:11 +0800

    fix: windows compatibility

--- tools/ice-devtools/config/webpack.block.js
+++ tools/ice-devtools/config/webpack.block.js
@@ -3,8 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const hbs = require('handlebars');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const getWebpackBaseConfig = require('./webpack.base');
+
+console.log('process.platform:', process.platform);
+
 const hbsPath = path.join(__dirname, '../template/preview/block.html.hbs');
 
 module.exports = function getWebpacksConfig(cwd) {
@@ -12,25 +14,31 @@ module.exports = function getWebpacksConfig(cwd) {
 
   // 增加入口文件  index.js
   const entry = path.join(cwd, 'src/index.js');
-  const jsTemplatePath = path.join(__dirname, '../template/preview/block-react-index.js.hbs');
-  const jsPath = path.join(__dirname, '../.temp/', `block-react-index.js`);
-  debug('%j', {entry, jsPath});
-  const jsTemplateContent = fs.readFileSync(jsTemplatePath, 'utf-8');
-  const template = hbs.compile(jsTemplateContent);
+  const hbsTemplatePath = path.join(
+    __dirname,
+    '../template/preview/block-react-index.js.hbs'
+  );
+  const jsPath = path.join(__dirname, '../.temp/', 'block-react-index.js');
+  debug('%j', { entry, jsPath });
+  const hbsTemplateContent = fs.readFileSync(hbsTemplatePath, 'utf-8');
+  const compileTemplateContent = hbs.compile(hbsTemplateContent);
+  const isWin = process.platform === 'win32';
+  const realEntry = isWin ? entry.replace(/\\/g, '\\\\') : entry;
+  const jsTemplateContent = compileTemplateContent({ entry: realEntry });
 
-  fs.writeFileSync(jsPath, template({entry}));
+  fs.writeFileSync(jsPath, jsTemplateContent);
 
-  config.entry('index').add(jsPath)
+  config.entry('index').add(jsPath);
   config.context(cwd);
   config.output
     .path(path.join(cwd, 'build'))
     .filename('[name].js')
     .publicPath('./');
 
-  config
-    .plugin('html')
-    .use(HtmlWebpackPlugin, [{
+  config.plugin('html').use(HtmlWebpackPlugin, [
+    {
       template: hbsPath,
-    }]);
+    },
+  ]);
   return config;
-}
+};
