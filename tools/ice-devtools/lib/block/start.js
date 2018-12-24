31f45a68bc363ada6bfdc36f8c8016002fb18136
水澜 <shuilan.cj@taobao.com>
2018-12-24 15:56:22 +0800

    fix: warning in log

--- tools/ice-devtools/lib/block/start.js
+++ tools/ice-devtools/lib/block/start.js
@@ -18,6 +18,7 @@ module.exports = function blockDevStart(cwd, opt) {
   config.devServer
     .compress(false)
     .host('localhost')
+    .disableHostCheck(true)
     .stats({
       colors: true,
       chunks: false,
