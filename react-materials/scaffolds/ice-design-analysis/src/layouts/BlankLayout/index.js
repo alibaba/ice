8a26bb7b6bded896a34f09ecc410fe4ee5df33a6
思忠 <chenbinbin.cb@alibaba-inc.com>
2018-12-27 15:42:27 +0800

    refactor: refactor analysis-dashboard

--- react-materials/scaffolds/ice-design-analysis/src/layouts/BlankLayout/index.js
+++ react-materials/scaffolds/ice-design-analysis/src/layouts/BlankLayout/index.js
@@ -1,3 +1,19 @@
-import Layout from './Layout';
+import React, { Component } from 'react';
+import Layout from '@icedesign/layout';
+import MainRoutes from './MainRoutes';
 
-export default Layout;
+export default class BlankLayout extends Component {
+  render() {
+    return (
+      <Layout style={styles.container}>
+        <MainRoutes />
+      </Layout>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    minHeight: '100vh',
+  },
+};
