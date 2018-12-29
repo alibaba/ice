fcb823f8127a92ca0bffc7b562a607f23f4e28d4
jichen <fenglai0802@163.com>
2018-12-29 10:23:46 +0800

    chore: function rename
    可以再抽象 但是没必要

--- tools/iceworks/renderer/src/pages/Home/ProjectDashboard/Def/gitTools.js
+++ tools/iceworks/renderer/src/pages/Home/ProjectDashboard/Def/gitTools.js
@@ -17,11 +17,10 @@ class GitTools {
       ),
     });
   }
-
-  catchErrWrap = async (fnName, errMsg, ...opts) => {
-    opts = opts[0] === undefined ? null : opts;
+  
+  gitHandle = async (fnName, errMsg, ...opts) => {
     try {
-      if (opts !== null) {
+      if (opts[0] !== undefined) {
         return await this.git()[fnName](...opts);
       } else {
         return await this.git()[fnName]();
@@ -39,7 +38,7 @@ class GitTools {
   // 获取远程仓库
   gitOriginRemote = async () => {
     const errMsg = 'git getRemotes 失败';
-    let remotes = await this.catchErrWrap('getRemotes', errMsg, true);
+    let remotes = await this.gitHandle('getRemotes', errMsg, true);
     remotes = remotes.filter(({ name }) => name == 'origin');
     return remotes;
   }
@@ -47,79 +46,79 @@ class GitTools {
   // 获取所有分支
   gitBranches = async () => {
     const errMsg = 'git branchLocal 失败';
-    return await this.catchErrWrap('branchLocal', errMsg);
+    return await this.gitHandle('branchLocal', errMsg);
   };
 
   // 获取当前分支文件状态
   gitStatus = async () => {
     const errMsg = 'git status 失败';
-    return await this.catchErrWrap('status', errMsg);
+    return await this.gitHandle('status', errMsg);
   };
 
   // 获取最新的 Commit 信息
   gitLastCommit = async (opts) => {
     const errMsg = 'git lastCommit 失败';
-    return await this.catchErrWrap('log', errMsg, opts);
+    return await this.gitHandle('log', errMsg, opts);
   };
 
   gitCheckIsRepo = async () => {
     const errMsg = '仓库地址错误';
-    return await this.catchErrWrap('checkIsRepo', errMsg);
+    return await this.gitHandle('checkIsRepo', errMsg);
   }
   
   gitAdd = async (opts) => {
     const errMsg = 'git add 失败请重试';
-    return await this.catchErrWrap('add', errMsg, opts);
+    return await this.gitHandle('add', errMsg, opts);
   }
 
   gitCommit = async (commitMsg) => {
     const errMsg = 'git commit 失败请重试';
-    return await this.catchErrWrap('commit', errMsg, commitMsg);
+    return await this.gitHandle('commit', errMsg, commitMsg);
   }
 
   gitPush = async (remote = 'origin', currentBranch) => {
     const errMsg = 'git push 失败请重试';
-    return await this.catchErrWrap('push', errMsg, remote, currentBranch);
+    return await this.gitHandle('push', errMsg, remote, currentBranch);
   }
 
   gitInit = async () => {
     const errMsg = 'git init 失败请重试';
-    return await this.catchErrWrap('init', errMsg);
+    return await this.gitHandle('init', errMsg);
   }
 
   gitRemoveRemote = async (name = 'origin') => {
     const errMsg = 'git remote remove 失败请重试';
-    return await this.catchErrWrap('removeRemote', errMsg, name);
+    return await this.gitHandle('removeRemote', errMsg, name);
   }
 
   gitAddRemote = async (name = 'origin', url) => {
     const errMsg = 'git remote add 失败请重试';
-    return await this.catchErrWrap('addRemote', errMsg, name, url);
+    return await this.gitHandle('addRemote', errMsg, name, url);
   }
 
   gitFetch = async () => {
     const errMsg = 'git fetch 失败请重试';
-    return await this.catchErrWrap('fetch', errMsg);
+    return await this.gitHandle('fetch', errMsg);
   }
 
   gitBranch = async (opts) => {
     const errMsg = 'git branch 失败请重试';
-    return await this.catchErrWrap('branch', errMsg, opts);
+    return await this.gitHandle('branch', errMsg, opts);
   }
   
   gitCheckout = async (checkoutBranch) => {
     const errMsg = 'git checkout 失败请重试';
-    return await this.catchErrWrap('checkout', errMsg, checkoutBranch);
+    return await this.gitHandle('checkout', errMsg, checkoutBranch);
   }
 
   gitCheckoutBranch = async (checkoutBranch, branchOrigin) => {
     const errMsg = 'git checkout -b 失败请重试';
-    return await this.catchErrWrap('checkout', errMsg, checkoutBranch, branchOrigin);
+    return await this.gitHandle('checkout', errMsg, checkoutBranch, branchOrigin);
   }
 
   gitcheckoutLocalBranch = async (newBranch) => {
     const errMsg = 'git checkout -b 失败请重试';
-    return await this.catchErrWrap('checkoutLocalBranch', errMsg, newBranch);
+    return await this.gitHandle('checkoutLocalBranch', errMsg, newBranch);
   }
 }
 
