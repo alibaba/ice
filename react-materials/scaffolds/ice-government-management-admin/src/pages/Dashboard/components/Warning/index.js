fa6bb7efd2dc225dddc2dfbe6351caa30401ded9
暮尘 <i@muchen.me>
2019-01-10 21:08:35 +0800

    feat: mod variable

--- react-materials/scaffolds/ice-government-management-admin/src/pages/Dashboard/components/Warning/index.js
+++ react-materials/scaffolds/ice-government-management-admin/src/pages/Dashboard/components/Warning/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Message } from '@alifd/next';
 import timeIcon from './images/time.svg';
+import styles from  './index.module.scss';
 
 const mock = [
   {
@@ -37,30 +38,26 @@ export default class Warning extends Component {
 
   render() {
     return (
-      <div style={styles.container}>
-        <div style={styles.card}>
-          <h4 style={styles.title}>预警区</h4>
-          <div style={styles.content}>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <h4 className={styles.title}>预警区</h4>
+          <div className={styles.content}>
             {mock.map((item, index) => {
               return (
-                <div style={styles.item} key={index} onClick={this.handleClick}>
+                <div className={styles.item} key={index} onClick={this.handleClick}>
                   <div
-                    style={{
-                      ...styles.image,
-                      background: `${item.backgroundColor}`,
-                    }}
+                    className={styles.image}
+                    style={{background: `${item.backgroundColor}`}}
                   >
                     <img
                       alt=""
                       src={timeIcon}
-                      style={{
-                        ...styles.iconImage,
-                        transform: `rotate(${index * 72}deg)`,
-                      }}
+                      className={styles.iconImage}
+                      style={{transform: `rotate(${index * 72}deg)`}}
                     />
-                    <div style={styles.count}>{item.count}</div>
+                    <div className={styles.count}>{item.count}</div>
                   </div>
-                  <p style={styles.itemTitle}>{item.title}</p>
+                  <p className={styles.itemTitle}>{item.title}</p>
                 </div>
               );
             })}
@@ -70,74 +67,3 @@ export default class Warning extends Component {
     );
   }
 }
-
-const styles = {
-  container: {
-    width: '50%',
-    boxSizing: 'border-box',
-    padding: '10px',
-  },
-  card: {
-    width: '100%',
-    padding: '20px',
-    color: '#42436b',
-    backgroundColor: 'white',
-    borderRadius: '16px',
-    overflow: 'hidden',
-    position: 'relative',
-    height: '286px',
-  },
-  title: {
-    marginTop: '0',
-    marginBottom: '20px',
-    borderLeft: '5px solid #447eff',
-    paddingLeft: '10px',
-    lineHeight: '20px',
-  },
-  content: {
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    flexWrap: 'wrap',
-    height: '200px',
-    margin: '0 -10px',
-    paddingTop: '56px',
-  },
-  item: {
-    width: '20%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    cursor: 'pointer',
-  },
-  image: {
-    height: '50px',
-    width: '50px',
-    textAlign: 'center',
-    padding: '10px 0',
-    borderRadius: '8px',
-    position: 'relative',
-  },
-  itemTitle: {
-    fontSize: '12px',
-    textAlign: 'center',
-  },
-  iconImage: {
-    width: '30px',
-    height: '30px',
-  },
-  count: {
-    color: 'white',
-    backgroundColor: '#ff382f',
-    borderRadius: '50%',
-    fontSize: '12px',
-    height: '18px',
-    width: '18px',
-    lineHeight: '18px',
-    textAlign: 'center',
-    position: 'absolute',
-    right: '-6px',
-    top: '-6px',
-  },
-};
