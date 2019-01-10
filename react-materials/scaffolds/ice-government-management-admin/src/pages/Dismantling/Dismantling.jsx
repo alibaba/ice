9f8b34f311997f84a9f26420a1ab2c9cbdd94294
暮尘 <i@muchen.me>
2019-01-10 20:49:38 +0800

    feat: add variable

--- react-materials/scaffolds/ice-government-management-admin/src/pages/Dismantling/Dismantling.jsx
+++ react-materials/scaffolds/ice-government-management-admin/src/pages/Dismantling/Dismantling.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Container from '@icedesign/container';
 import SearchBar from './components/SearchBar';
 import DismantlingTable from './components/DismantlingTable';
+import styles from  './index.module.scss';
 
 export default class Dismantling extends Component {
   static displayName = 'Dismantling';
@@ -9,32 +10,14 @@ export default class Dismantling extends Component {
   render() {
     return (
       <div>
-        <div style={styles.nav}>
-          <h2 style={styles.breadcrumb}>拆预收案</h2>
+        <div className={styles.nav}>
+          <h2 className={styles.breadcrumb}>拆预收案</h2>
         </div>
         <SearchBar />
-        <Container style={styles.container}>
+        <Container className={styles.container}>
           <DismantlingTable />
         </Container>
       </div>
     );
   }
 }
-
-const styles = {
-  nav: {
-    background: 'white',
-    height: '72px',
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  breadcrumb: {
-    borderLeft: '5px solid #447eff',
-    paddingLeft: '16px',
-    margin: '0 0 0 20px',
-  },
-  container: {
-    margin: '20px',
-  },
-};
