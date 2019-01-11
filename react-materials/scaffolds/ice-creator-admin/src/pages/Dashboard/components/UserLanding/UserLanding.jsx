6a28028ae40f7edaac7ebe73e6b7791a1a29f7bd
暮尘 <i@muchen.me>
2019-01-11 17:40:25 +0800

    feat: mod user style

--- react-materials/scaffolds/ice-creator-admin/src/pages/Dashboard/components/UserLanding/UserLanding.jsx
+++ react-materials/scaffolds/ice-creator-admin/src/pages/Dashboard/components/UserLanding/UserLanding.jsx
@@ -3,6 +3,8 @@ import Container from '@icedesign/container';
 import { Grid, Button } from '@alifd/next';
 import { withRouter } from 'react-router-dom';
 import Img from '@icedesign/img';
+import styles from './index.module.scss';
+
 
 const { Row, Col } = Grid;
 @withRouter
@@ -21,55 +23,53 @@ class UserLanding extends Component {
       <Row wrap gutter="20">
         <Col l="18">
           <Container
+            className={styles.container}
             style={{
-              ...styles.container,
               alignItems: 'center',
               height: 160,
             }}
           >
-            <div style={styles.avatarWrapper}>
+            <div className={styles.avatarWrapper}>
               <a href="#">
                 <Img
                   width={64}
                   height={64}
                   src={require('./images/avatar.jpg')}
-                  style={styles.avatar}
+                  className={styles.avatar}
                 />
               </a>
               <img
                 alt="用户等级"
                 src={require('./images/level.png')}
-                style={styles.level}
+                className={styles.level}
               />
             </div>
-            <div style={styles.userInfo}>
-              <div style={styles.userDetail}>
+            <div className={styles.userInfo}>
+              <div className={styles.userDetail}>
                 <a href="#">
-                  <span style={styles.userName}>桥下小猫2</span>
+                  <span className={styles.userName}>桥下小猫2</span>
                 </a>
-                <div style={styles.userLabel}>官方账号</div>
+                <div className={styles.userLabel}>官方账号</div>
               </div>
-              <div style={styles.userOther}>绑定机构：阿里巴巴飞冰团队</div>
-              <div style={styles.userOther}>认证信息：hello 大家好！</div>
+              <div className={styles.userOther}>绑定机构：阿里巴巴飞冰团队</div>
+              <div className={styles.userOther}>认证信息：hello 大家好！</div>
             </div>
-            <div style={styles.userAttribute}>
-              <div style={styles.userLevelWrapper}>
-                <div style={styles.userLevelLine} />
+            <div className={styles.userAttribute}>
+              <div className={styles.userLevelWrapper}>
+                <div className={styles.userLevelLine} />
                 {this.state.userLevel.map((level, index) => {
                   const isCurrent = this.state.userCurrentLevel === level;
                   return (
                     <div
+                      className={isCurrent ? styles.userlevelItemCurrent : styles.userlevelItem}
                       style={{
-                        ...styles.userlevelItem,
                         marginLeft: index === 0 ? 0 : 14,
-                        backgroundColor: isCurrent ? '#ee706d' : '#fff',
-                        color: isCurrent ? '#fff' : '#666',
                       }}
                       key={index}
                     >
                       {level}
                       {isCurrent && (
-                        <div style={styles.userLevelLight}>当前等级</div>
+                        <div className={styles.userLevelLight}>当前等级</div>
                       )}
                     </div>
                   );
@@ -117,91 +117,5 @@ class UserLanding extends Component {
   }
 }
 
-const styles = {
-  container: { display: 'flex' },
-  avatarWrapper: {
-    width: 64,
-    height: 65,
-    position: 'relative',
-    // filter: 'drop-shadow(0 0 2px rgba(0,0,0,0.3))',
-  },
-  avatar: {
-    borderRadius: 5,
-    width: '100%',
-    height: '100%',
-  },
-  level: {
-    position: 'absolute',
-    bottom: -2,
-    right: -2,
-    width: 22,
-    height: 22,
-  },
-  userInfo: {
-    marginLeft: 20,
-    color: '#999',
-    flex: 'auto',
-  },
-  userDetail: {
-    display: 'flex',
-    height: 24,
-    alignItems: 'center',
-  },
-  userName: {
-    fontSize: 18,
-    color: '#666',
-    marginRight: 5,
-    display: 'inline-block',
-    maxWidth: 200,
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  },
-  userLabel: {
-    height: 16,
-    lineHeight: '16px',
-    borderRadius: 2,
-    padding: '0 5px',
-    marginLeft: 10,
-    fontSize: 12,
-    textAlign: 'center',
-    color: '#ee706d',
-    backgroundColor: 'rgb(255, 242, 244)',
-    whiteSpace: 'nowrap',
-  },
-  userOther: {
-    fontSize: 12,
-    lineHeight: '20px',
-  },
-  userLevelWrapper: {
-    display: 'flex',
-    position: 'relative',
-  },
-  userlevelItem: {
-    backgroundColor: '#fff',
-    border: '1px solid #ee706d',
-    borderRadius: '12px',
-    padding: '2px 6px',
-    marginLeft: 14,
-    position: 'relative',
-  },
-  userLevelLight: {
-    position: 'absolute',
-    color: '#ee706d',
-    top: 30,
-    width: 48,
-    fontSize: 12,
-    left: -12,
-  },
-  userLevelLine: {
-    height: 2,
-    backgroundColor: '#ee706d',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 11,
-    zIndex: 0,
-  },
-};
 
 export default UserLanding;
