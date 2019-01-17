bff9362ee784e5b27bcf58416bb8273e6897cd70
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-01-17 21:12:05 +0800

    chore: scaffold optim

--- react-materials/scaffolds/ice-monitor-dashboard/src/menuConfig.js
+++ react-materials/scaffolds/ice-monitor-dashboard/src/menuConfig.js
@@ -6,32 +6,26 @@ const asideMenuConfig = [
   {
     name: '业务概览',
     path: '/dashboard',
-    icon: 'home2',
   },
   {
     name: '数据中心',
     path: '/datacenter',
-    icon: 'cascades',
   },
   {
     name: '流量分析',
     path: '/traffic/statistics',
-    icon: 'cascades',
   },
   {
     name: '用户分析',
     path: '/user/statistics',
-    icon: 'cascades',
   },
   {
     name: '留存与活跃',
     path: '/user/activities',
-    icon: 'cascades',
   },
   {
     name: '反馈',
     path: 'https://github.com/alibaba/ice/issues/new',
-    icon: 'question2',
     external: true,
     newWindow: true,
   },
