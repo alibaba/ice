d70636c1bf635527b1005c15ede295c763d47c8b
暮尘 <i@muchen.me>
2019-01-18 14:37:32 +0800

    fix:  style error

--- react-materials/scaffolds/ice-light-bootstrap-dashboard/config-overrides.js
+++ react-materials/scaffolds/ice-light-bootstrap-dashboard/config-overrides.js
@@ -4,7 +4,26 @@ const rewireSass = require('./rewire-scss');
 
 module.exports = function override(config) {
 
-  config = rewireSass(config);
+  config = injectBabelPlugin(
+    ['import', { libraryName: '@alifd/next' }],
+    config
+  );
 
+  config = injectBabelPlugin('transform-decorators-legacy', config);
+
+  config.plugins.push(
+    new WebpackPluginImport([
+      {
+        libraryName: /^@alifd\/next\/lib\/([^/]+)/,
+        stylePath: 'style.js',
+      },
+      {
+        libraryName: /@icedesign\/.*/,
+        stylePath: 'style.js',
+      },
+    ])
+  );
+
+    config = rewireSass(config);
   return config;
 };
