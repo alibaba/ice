3897bb4b1bf611b1561e3f70f4cc6e7765ac04bc
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-01-18 12:13:36 +0800

    chore: 路由优化

--- react-materials/scaffolds/ice-yunqi-homepage/src/layouts/HeaderFooterLayout/HeaderFooterLayout.jsx
+++ react-materials/scaffolds/ice-yunqi-homepage/src/layouts/HeaderFooterLayout/HeaderFooterLayout.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Layout from '@icedesign/layout';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import routerData from '../../routerConfig';
+import NotFound from '../../components/NotFound';
 
 export default class HeaderFooterLayout extends Component {
   static propTypes = {};
@@ -12,7 +15,20 @@ export default class HeaderFooterLayout extends Component {
     return (
       <Layout style={styles.container}>
         <Header />
-        {this.props.children}
+        <Switch>
+          {routerData.map((item, index) => {
+            return item.component ? (
+              <Route
+                key={index}
+                path={item.path}
+                component={item.component}
+              />
+            ) : null;
+          })}
+
+          {/* 未匹配到的路由重定向到 NotFound */}
+          <Route component={NotFound} />
+        </Switch>
         <Footer />
       </Layout>
     );
