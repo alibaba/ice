6c2283c0baf07d1bb81bd88e1362c2b14302372e
MuChen <tao1991123@qq.com>
2019-01-19 14:47:05 +0800

    feat: upgrade create-react-app to fusion design (#1362)

--- react-materials/scaffolds/create-react-app/src/App.js
+++ react-materials/scaffolds/create-react-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Next from '@alifd/next';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
