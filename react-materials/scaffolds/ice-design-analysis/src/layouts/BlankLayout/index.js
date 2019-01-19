ad32edffa3801d00f5a08d8a0427a457573324cb
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2019-01-19 14:45:50 +0800

    feat: upgrade v2 (#1316)

--- react-materials/scaffolds/ice-design-analysis/src/layouts/BlankLayout/index.js
+++ react-materials/scaffolds/ice-design-analysis/src/layouts/BlankLayout/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Layout from '@icedesign/layout';
 import MainRoutes from './MainRoutes';
 
 export default class BlankLayout extends Component {
   render() {
     return (
-      <Layout style={styles.container}>
+      <div style={styles.container}>
         <MainRoutes />
-      </Layout>
+      </div>
     );
   }
 }
