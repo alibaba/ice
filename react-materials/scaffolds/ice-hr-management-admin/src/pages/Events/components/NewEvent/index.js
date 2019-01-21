3a8f657d9b1367d08788c2bd6dca921b31d00ad0
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-01-21 12:11:42 +0800

    feat: add dialog

--- react-materials/scaffolds/ice-hr-management-admin/src/pages/Events/components/NewEvent/index.js
+++ react-materials/scaffolds/ice-hr-management-admin/src/pages/Events/components/NewEvent/index.js
@@ -1,15 +1,135 @@
+/* eslint react/no-string-refs: 0 */
 import React, { Component } from 'react';
-import { Button } from '@icedesign/base';
+import { Button, Dialog, Input, Feedback } from '@icedesign/base';
 import IceContainer from '@icedesign/container';
+import {
+  FormBinderWrapper as IceFormBinderWrapper,
+  FormBinder as IceFormBinder,
+  FormError as IceFormError,
+} from '@icedesign/form-binder';
 
 export default class NewEvent extends Component {
+  state = {
+    visible: false,
+    value: {},
+  };
+
+  onOpen = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  formChange = (value) => {
+    this.setState({
+      value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.refs.form.validateAll((errors, values) => {
+      if (errors) {
+        console.log('errors', errors);
+        return;
+      }
+      console.log(values);
+      Feedback.toast.success('添加成功');
+      this.setState({
+        visible: false,
+      });
+    });
+  };
+
   render() {
     return (
       <IceContainer>
-        <Button type="primary" size="large" style={{ width: '100%' }}>
+        <Button
+          type="primary"
+          size="large"
+          style={{ width: '100%' }}
+          onClick={this.onOpen}
+        >
           添加事项
         </Button>
+        <Dialog
+          visible={this.state.visible}
+          onOk={this.handleSubmit}
+          onClose={this.onClose}
+          onCancel={this.onClose}
+          title="添加事项"
+        >
+          <IceFormBinderWrapper
+            value={this.state.value}
+            onChange={this.formChange}
+            ref="form"
+          >
+            <div style={styles.formItems}>
+              <div style={styles.formItem}>
+                <div style={styles.formLabel}>标题</div>
+                <IceFormBinder name="title" required message="必填">
+                  <Input
+                    size="large"
+                    maxLength={20}
+                    placeholder="请输入标题"
+                    style={styles.inputCol}
+                  />
+                </IceFormBinder>
+                <IceFormError name="title" />
+              </div>
+
+              <div style={styles.formItem}>
+                <div style={styles.formLabel}>描述</div>
+                <IceFormBinder name="description" required message="必填">
+                  <Input
+                    size="large"
+                    maxLength={20}
+                    placeholder="请输入描述"
+                    style={styles.inputCol}
+                  />
+                </IceFormBinder>
+                <IceFormError name="description" />
+              </div>
+
+              <div style={styles.formItem}>
+                <div style={styles.formLabel}>地址</div>
+                <IceFormBinder name="address">
+                  <Input
+                    size="large"
+                    maxLength={20}
+                    placeholder="选题"
+                    style={styles.inputCol}
+                  />
+                </IceFormBinder>
+              </div>
+            </div>
+          </IceFormBinderWrapper>
+        </Dialog>
       </IceContainer>
     );
   }
 }
+
+const styles = {
+  formItems: {
+    padding: '20px',
+  },
+  formItem: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '20px',
+  },
+  formLabel: {
+    marginRight: '20px',
+  },
+  inputCol: {
+    width: '300px',
+  },
+};
