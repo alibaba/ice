c2e9c4ad760f74ed1a247911f066f6a357d31bac
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2019-01-25 03:53:13 +0800

    chore: optim scaffold (#1395)

--- react-materials/scaffolds/ice-asset-management-admin/src/layouts/BasicLayout/components/Aside/index.jsx
+++ react-materials/scaffolds/ice-asset-management-admin/src/layouts/BasicLayout/components/Aside/index.jsx
@@ -1,84 +1,143 @@
 /* eslint no-undef:0, no-unused-expressions:0, array-callback-return:0 */
-import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import FoundationSymbol from '@icedesign/foundation-symbol';
-import { Nav, Icon } from '@alifd/next';
-import cls from 'classname';
+import Layout from '@icedesign/layout';
+import { Nav } from '@alifd/next';
+import React, { Component } from 'react';
 import { asideMenuConfig } from '../../../../menuConfig';
-import styles from './index.module.scss';
+import Logo from '../Logo';
+import './index.scss';
 
 @withRouter
 export default class Aside extends Component {
-
-  state = {
-    collapse: false,
+  constructor(props) {
+    super(props);
+    const openKeys = this.getDefaultOpenKeys();
+    this.state = {
+      openKeys,
+    };
+    this.openKeysCache = openKeys;
   }
 
-  toggleCollapse = () => {
-    const { collapse } = this.state;
-
+  /**
+   * 当前展开的菜单项
+   */
+  onOpenChange = (openKeys) => {
     this.setState({
-      collapse: !collapse,
+      openKeys,
     });
-  }
+    this.openKeysCache = openKeys;
+  };
 
-  renderNavItem = (nav) => {
-    const linkProps = {};
-    linkProps.to = nav.path;
+  /**
+   * 获取当前展开的菜单项
+   */
+  getDefaultOpenKeys = () => {
+    const { location = {} } = this.props;
+    const { pathname } = location;
 
-    if (nav.newWindow) {
-      linkProps.target = '_blank';
+    let openKeys = [];
+    if (Array.isArray(asideMenuConfig)) {
+      asideMenuConfig.forEach((item, index) => {
+        if (pathname.startsWith(item.path)) {
+          openKeys = [`${index}`];
+        }
+      });
     }
 
-    return (
-      <Nav.Item key={nav.path} icon={nav.icon}>
-        <Link {...linkProps}>{nav.name}</Link>
-      </Nav.Item>
-    );
-  }
+    return openKeys;
+  };
 
   render() {
     const { location } = this.props;
     const { pathname } = location;
-    const { collapse } = this.state;
-
-    const className = cls(styles.aside, {
-      [styles.asideCollapsed]: collapse,
-    });
 
     return (
-      <div className={className}>
-        <div className={styles.collapseBtn} onClick={this.toggleCollapse}>
-          <Icon
-            type={collapse ? 'arrow-right' : 'arrow-left'}
-            size="small"
-          />
+      <Layout.Aside width="240" theme="light" className="custom-aside">
+        <div className="aside-logo">
+          <Logo />
         </div>
         <Nav
-          type="primary"
-          direction="ver"
-          iconOnly={collapse}
-          hasTooltip
+          defaultSelectedKeys={[pathname]}
+          mode="inline"
           selectedKeys={[pathname]}
+          openKeys={this.state.openKeys}
+          onOpen={this.onOpenChange}
+          onClick={this.onMenuClick}
+          className="custom-menu"
+          type="line"
         >
-          {(asideMenuConfig || []).map((nav, index) => {
+          {Array.isArray(asideMenuConfig) &&
+            asideMenuConfig.length > 0 &&
+            asideMenuConfig.map((nav, index) => {
               if (nav.children && nav.children.length > 0) {
                 return (
                   <Nav.SubNav
                     key={index}
-                    icon={nav.icon}
-                    label={nav.name}
+                    label={
+                      <span>
+                        {nav.icon ? (
+                          <FoundationSymbol
+                            style={{ marginRight: '8px' }}
+                            size="small"
+                            type={nav.icon}
+                          />
+                        ) : null}
+                        <span className="ice-menu-collapse-hide">
+                          {nav.name}
+                        </span>
+                      </span>
+                    }
                   >
-                    {nav.children.map(this.renderNavItem)}
+                    {nav.children.map((item) => {
+                      const linkProps = {};
+                      if (item.newWindow) {
+                        linkProps.href = item.path;
+                        linkProps.target = '_blank';
+                      } else if (item.external) {
+                        linkProps.href = item.path;
+                      } else {
+                        linkProps.to = item.path;
+                      }
+                      return (
+                        <Nav.Item key={item.path}>
+                          <Link {...linkProps}>{item.name}</Link>
+                        </Nav.Item>
+                      );
+                    })}
                   </Nav.SubNav>
                 );
               }
-              return this.renderNavItem(nav);
+              const linkProps = {};
+              if (nav.newWindow) {
+                linkProps.href = nav.path;
+                linkProps.target = '_blank';
+              } else if (nav.external) {
+                linkProps.href = nav.path;
+              } else {
+                linkProps.to = nav.path;
+              }
+              return (
+                <Nav.Item key={nav.path}>
+                  <Link {...linkProps}>
+                    <span>
+                      {nav.icon ? (
+                        <FoundationSymbol
+                          style={{ marginRight: '8px' }}
+                          size="small"
+                          type={nav.icon}
+                        />
+                      ) : null}
+                      <span className="ice-menu-collapse-hide">{nav.name}</span>
+                    </span>
+                  </Link>
+                </Nav.Item>
+              );
             })}
         </Nav>
         {/* 侧边菜单项 end */}
-      </div>
+      </Layout.Aside>
     );
   }
 }
