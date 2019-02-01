d64640298f27a2b65cd3826af680cb7136e3242e
大果 <sobear.me@gmail.com>
2019-02-01 16:45:58 +0800

    fix: ice layout export (#1431)

--- react-materials/components/layout/src/index.js
+++ react-materials/components/layout/src/index.js
@@ -5,6 +5,7 @@ import Footer from './Footer';
 import Aside from './Aside';
 import Main from './Main';
 
+// 不合理的写法，为了兼容先保留着
 Layout.Section = Section;
 Layout.Header = Header;
 Layout.Footer = Footer;
@@ -12,3 +13,4 @@ Layout.Aside = Aside;
 Layout.Main = Main;
 
 export default Layout;
+export { Section, Header, Footer, Aside, Main };
