cc7f556b0046fcda70a00209795ea2fc56d4e1de
冰森 <lellansin@gmail.com>
2019-02-02 12:25:16 +0800

    feat: add asset prefix for ejs view/

--- templates/ice-midway-template/templates/server/src/config/config.default.ts
+++ templates/ice-midway-template/templates/server/src/config/config.default.ts
@@ -1,26 +1,27 @@
 import * as path from 'path';
 
 module.exports = (appInfo: any) => {
-  const config: any = exports = {};
+  const config: any = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1541510581780_3944';
 
-  // add your config here
-  config.middleware = [
-  ];
+  // 配置自定义中间件
+  config.middleware = ['assetsInfo'];
 
-	// https://www.npmjs.com/package/egg-static#configuration
+  // https://www.npmjs.com/package/egg-static#configuration
   config.static = {
     // maxAge: 31536000,
     prefix: '/',
-		dir: path.join(__dirname, '../../../client/build')
+    dir: path.join(__dirname, '../../../client/build'),
   };
 
-	config.view = {
-		// defaultViewEngine: 'ejs',
-		// root: path.join(__dirname, '../../../client/build')
-  }
+  config.view = {
+    mapping: {
+      '.ejs': 'ejs',
+    },
+    // root: path.join(__dirname, '../../../client/build')
+  };
 
   return config;
 };
