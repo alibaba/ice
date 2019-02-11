2bc71ca057d2c80b9d03022f448d6252618c5855
水澜 <shuilan.cj@taobao.com>
2019-02-11 16:12:52 +0800

    fix: add vue config

--- tools/ice-devtools/config/webpack.vue.block.js
+++ tools/ice-devtools/config/webpack.vue.block.js
@@ -7,6 +7,14 @@ const getBaseConfig = require('./webpack.block');
 module.exports = function getWebpacksConfig(cwd, type) {
   const config = getBaseConfig(cwd, type);
 
+  config
+    .externals({
+      vue: 'Vue',
+    });
+
+  config.resolve.extensions
+    .add('.vue');
+
   config.module
     .rule('scss')
     .use('vue-style-loader')
