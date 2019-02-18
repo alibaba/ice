8503ea92b7eb87f27488ed3078fc053c3afb40a5
暮尘 <i@muchen.me>
2019-02-18 14:37:29 +0800

    temp: add url base64

--- tools/ice-devtools/utils/inner-url.js
+++ tools/ice-devtools/utils/inner-url.js
@@ -1,7 +1,11 @@
 const debug = require('debug')('ice:util:inner-url');
-const FUSION_DESIGN_DAILY_URL = 'https://fusion.alibaba.net';
-const FUSION_DESIGN_PRE_URL = 'https://pre-fusion.alibaba-inc.com';
-const FUSION_DESIGN_URL = 'https://fusion.alibaba-inc.com';
+const FUSION_DESIGN_DAILY_URL = 'aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS5uZXQ=';
+const FUSION_DESIGN_PRE_URL = 'aHR0cHM6Ly9wcmUtZnVzaW9uLmFsaWJhYmEtaW5jLmNvbQ==';
+const FUSION_DESIGN_URL = 'aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29t';
+
+function base64ToAscii(str) {
+  return Buffer.from(str, 'base64').toString('ascii');
+}
 
 const URLS = {
   daily: {
@@ -25,6 +29,8 @@ module.exports = function getUrl() {
   if (!url) {
     url = URLS.prod;
   }
+  debug('url before decode: %j', url);
+  url.fusionDesignUrl = base64ToAscii(url.fusionDesignUrl);
   debug('url: %j', url);
   return url;
 }
