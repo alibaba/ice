f2ea55149877548b1937732e86d65f9542289dca
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-02-25 21:12:03 +0800

    chore: refactor code

--- react-materials/scaffolds/ice-typescript-starter/src/router.tsx
+++ react-materials/scaffolds/ice-typescript-starter/src/router.tsx
@@ -1,15 +1,17 @@
 /**
  * 定义应用路由
  */
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import BasicLayout from './layouts/BasicLayout';
 
 const router = () => {
   return (
-    <Switch>
-      <Route path="/" component={BasicLayout} />
-    </Switch>
+    <HashRouter>
+      <Switch>
+        <Route path="/" component={BasicLayout} />
+      </Switch>
+    </HashRouter>
   );
 };
 
