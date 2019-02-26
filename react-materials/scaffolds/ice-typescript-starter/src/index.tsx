b575e5a9895bd0bb172436186175aa42aa150c90
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2019-02-26 17:56:12 +0800

    feat: ts support (#1482)
    * feat: add typescript scaffold
    
    * fix: fix deps
    
    * feat: ice-script ts support
    
    * docs: add typescirpt docs
    
    * refactor: refactor ts-loader config
    
    * chore: update docs
    
    * chore: refactor code
    
    * chore: @icedesign/ts-scaffold@1.0.1
    
    * chore: update docs
    
    * chore: add ice-typescript-starter to scaffolds
    
    * chore: 优化 ts 文档
    
    * chore: 统一 index.js
    
    * fix: doc

---
+++ react-materials/scaffolds/ice-typescript-starter/src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+
+// 载入默认全局样式 normalize
+import '@alifd/next/reset.scss';
+
+import router from './router';
+
+const ICE_CONTAINER = document.getElementById('ice-container');
+
+if (!ICE_CONTAINER) {
+  throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
+}
+
+ReactDOM.render(router(), ICE_CONTAINER);
