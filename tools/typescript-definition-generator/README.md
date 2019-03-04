edf46f4925167e5cfa37c93928d2d4ad50d1c9f1
大果 <sobear.me@gmail.com>
2019-03-04 22:23:56 +0800

    chore: remove layouts & typescript-definition-generator (#1542)

--- tools/typescript-definition-generator/README.md
+++
@@ -1,3 +0,0 @@
-# typescript-definition-generator
-
-迁移至：https://github.com/alibaba-fusion/dts-generator
--- vue-materials/layouts/HeaderAsideLayout/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as SideBar } from './SideBar';
-export { default as AppMain } from './AppMain';
