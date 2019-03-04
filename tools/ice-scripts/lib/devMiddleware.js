d262bc06b6618588415d6b3c9b90b77bf77814d2
pixcai <pixcai@163.com>
2019-03-04 12:05:14 +0800

    fix: proxy config

--- tools/ice-scripts/lib/devMiddleware.js
+++ tools/ice-scripts/lib/devMiddleware.js
@@ -1,28 +1,30 @@
 const httpProxyMiddleware = require('http-proxy-middleware');
-
 const webpackDevMock = require('webpack-dev-mock');
+const merge = require('lodash/merge');
+
 // 附加中间件流程
 module.exports = (app, proxyConfig) => {
   if (proxyConfig) {
     const proxyRules = Object.entries(proxyConfig);
 
     proxyRules.forEach(([match, opts]) => {
-      if (opts.enable) {
-        const proxyOptions = {
-          context: match,
-          target: opts.target,
+      const { enable, target, ...proxyRule } = opts;
+
+      if (enable) {
+        const proxyOptions = merge({
+          target,
           changeOrigin: true,
           logLevel: 'warn',
           onProxyRes: function onProxyReq(proxyRes, req, res) {
             proxyRes.headers['x-proxy-by'] = 'ice-proxy';
             proxyRes.headers['x-proxy-match'] = match;
-            proxyRes.headers['x-proxy-target'] = opts.target;
-            if (opts.target && opts.target.endsWith('/')) {
-              opts.target = opts.target.replace(/\/$/, '');
+            proxyRes.headers['x-proxy-target'] = target;
+            if (target && target.endsWith('/')) {
+              target = target.replace(/\/$/, '');
             }
-            proxyRes.headers['x-proxy-target-path'] = opts.target + req.url;
+            proxyRes.headers['x-proxy-target-path'] = target + req.url;
           },
-        };
+        }, proxyRule, { context: match });
 
         const exampleProxy = httpProxyMiddleware(match, proxyOptions);
         app.use(exampleProxy);
