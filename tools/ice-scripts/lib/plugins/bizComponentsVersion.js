52f522487f8d98870b699a85a723067d2ff3daae
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-03-06 22:23:01 +0800

    chore: 补全组件

--- tools/ice-scripts/lib/plugins/bizComponentsVersion.js
+++ tools/ice-scripts/lib/plugins/bizComponentsVersion.js
@@ -1,4 +1,5 @@
-// 这个跟随业务组件升级可能需要持续维护
+// 这个跟随业务组件升级需要持续维护
+
 module.exports = {
   '@icedesign/balloon-confirm': {
     '0.x': '<1.x',
@@ -71,5 +72,35 @@ module.exports = {
   '@ali/ice-img-space': {
     '0.x': '<1.x',
     '1.x': '>=1.x'
-  }
+  },
+  '@ali/ice-loading': {
+    '0.x': '<1.x'
+  },
+  '@ali/ice-addres': {
+    '0.x': '<4.x',
+  },
+  '@ali/ice-add-image': {
+    '0.x': '<2.x',
+  },
+  '@ali/ice-add-item': {
+    '0.x': '<1.x',
+  },
+  '@ali/ice-add-link': {
+    '0.x': '<1.x',
+  },
+  '@ali/ice-add-tag': {
+    '0.x': '<1.x',
+  },
+  '@ali/ice-light-wangwang': {
+    '0.x': '<2.x',
+  },
+  '@ali/ice-bind': {
+    '0.x': '<1.x',
+  },
+  '@ali/ice-cookie': {
+    '0.x': '<1.x',
+  },
+  '@ali/ice-events': {
+    '0.x': '<1.x',
+  },
 }
