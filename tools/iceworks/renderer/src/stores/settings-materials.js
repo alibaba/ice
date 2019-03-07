4c241d36000310a9d108baadc22e7d9a25061ef4
jichen <fenglai0802@163.com>
2019-03-07 12:27:14 +0800

    chore: 重置物料源时 把物料源从备份切回正常

--- tools/iceworks/renderer/src/stores/settings-materials.js
+++ tools/iceworks/renderer/src/stores/settings-materials.js
@@ -80,6 +80,7 @@ class SettingsMaterials {
     const saveMaterials = defaultMaterials.map((item) => ({ ...item }));
     this.builtInMaterialsValue = this.filterBuiltInMaterials(saveMaterials);
     settings.set('materials', saveMaterials);
+    settings.set('isMaterialsBackup', false);
     this.notification('官方物料源重置成功');
   }
 
