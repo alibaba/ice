e3fd7d8505a065b98e74fa8a1c264dc3dfc83fa8
水澜 <shuilan.cj@taobao.com>
2019-03-07 17:16:50 +0800

    fix: demo name

--- tools/ice-devtools/utils/generate-marterials-demo.js
+++ tools/ice-devtools/utils/generate-marterials-demo.js
@@ -8,18 +8,20 @@ module.exports = async function generateMaterialsDemo(tmpPath, appPath) {
   await generate({
     src: path.join(appPath, '.template/block'),
     dest: path.join(appPath, 'blocks/ExampleBlock'),
-    name: `block-example`,
-    npmName: `${pkg.name}-block-example`,
+    name: `example-block`,
+    npmName: `${pkg.name}-example-block`,
     version: '1.0.0',
+    title: "demo block",
     description: '示例区块'
   });
 
   await generate({
     src: path.join(appPath, '.template/scaffold'),
     dest: path.join(appPath, 'scaffolds/ExampleScaffold'),
-    name: `scaffold-example`,
-    npmName: `${pkg.name}-scaffold-example`,
+    name: `example-scaffold`,
+    npmName: `${pkg.name}-example-scaffold`,
     version: '1.0.0',
+    title: "demo scaffold",
     description: '示例模板'
   });
 
@@ -30,9 +32,10 @@ module.exports = async function generateMaterialsDemo(tmpPath, appPath) {
   await generate({
     src: path.join(appPath, '.template/component'),
     dest: path.join(appPath, 'components/ExampleComponent'),
-    name: `component-example`,
-    npmName: `${pkg.name}-component-example`,
+    name: `example-component`,
+    npmName: `${pkg.name}-example-component`,
     version: '1.0.0',
+    title: "demo component",
     description: '示例组件'
   });
 }
