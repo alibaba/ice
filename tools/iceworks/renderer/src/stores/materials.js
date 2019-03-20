3cff1ccb558915320a9a6a1b6e54436e5309ac9d
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-03-20 21:19:41 +0800

    feat: use built-in data when the request failed

--- tools/iceworks/renderer/src/stores/materials.js
+++ tools/iceworks/renderer/src/stores/materials.js
@@ -11,8 +11,11 @@ import AdditionalComponents from './additional-components';
 import filterMaterial from '../lib/filter-material';
 import { isIceMaterial } from '../lib/utils';
 import services from '../services';
-import history from '../history';
 import projects from './projects';
+
+const BUILTIN_DATA = require('../datacenter/react-materials');
+
+console.log('BUILT_IN_DATA:', BUILTIN_DATA);
 
 const { settings, shared } = services;
 
@@ -29,6 +32,9 @@ class Materials {
   tabComponentActiveKey = '';
   @observable
   startRecommendMaterials = {};
+
+  // 是否启用内置的物料数据
+  useBuiltinData = false;
 
   constructor() {
     // 加载物料数据
@@ -126,112 +132,111 @@ class Materials {
   @action
   loadStartRecommendMaterials() {
     const recommendMaterialSource = settings.get('materials')[0];
-    const isMaterialsBackup = settings.get('isMaterialsBackup');
     const startRecommendMaterials = this.startRecommendMaterials;
-    const source = isMaterialsBackup ? recommendMaterialSource.backupSource : recommendMaterialSource.source;
+    const fn = (data) => {
+      const scaffolds = data.scaffolds || [];
+      const { startRecommendScaffolds } = new AdditionalScaffolds(scaffolds);
+      startRecommendMaterials.scaffolds = startRecommendScaffolds || [];
+      startRecommendMaterials.loaded = true;
+      startRecommendMaterials.error = null;
+    };
 
-    this.fetchByMaterial(source)
-      .then((body = {}) => {
-        const scaffolds = body.scaffolds || [];
-        const { startRecommendScaffolds } = new AdditionalScaffolds(scaffolds);
-
-        startRecommendMaterials.scaffolds = startRecommendScaffolds || [];
-        startRecommendMaterials.loaded = true;
-        startRecommendMaterials.error = null;
-      })
-      .catch((error) => {
-        // 如果alicdn物料源访问超时 切换备份物料源
-        if (
-          error.code &&
-          (error.code === 'ETIMEDOUT' || error.code === 'ESOCKETTIMEDOUT')
-        ) {
-          if (!isMaterialsBackup) {
-            settings.set('isMaterialsBackup', true);
+    if (!this.useBuiltinData) {
+      this.fetchByMaterial(recommendMaterialSource.source)
+        .then((body = {}) => {
+          fn(body);
+        })
+        .catch(() => {
+          if (!this.useBuiltinData) {
+            this.useBuiltinData = true;
             this.loadStartRecommendMaterials();
           } else {
             startRecommendMaterials.loaded = true;
-            startRecommendMaterials.error = `物料源加载失败，请确认网络是否能直接访问此链接 ${source}，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题`;
+            startRecommendMaterials.error =
+              '物料源加载失败，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题';
           }
-        } else {
-          startRecommendMaterials.loaded = true;
-          startRecommendMaterials.error = '物料源加载失败，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题';
-        }
-      });
-  }
-
-  switchToBackupMaterials(fn = () => {}) {
-    settings.set('isMaterialsBackup', true);
-    fn();
+        });
+    } else {
+      fn(BUILTIN_DATA);
+    }
   }
 
   @action
   loaderMaterial(index) {
     const material = this.materials[index];
     if (!material) return;
-    const isMaterialsBackup = settings.get('isMaterialsBackup');
-    const source = (isMaterialsBackup && material.backupSource) ? material.backupSource : material.source;
+    const source = material.source;
+    const fn = (data, iceBaseComponents) => {
+      const {
+        blocks = [],
+        scaffolds = [],
+        components = [],
+        name,
+        ...attrs
+      } = data;
 
-    if (material && !material.loaded) {
-      let promiseAll;
-      if (isIceMaterial(material.source)) {
-        // HACK: 获取 ICE 物料源时一同获取基础组件数据
-        const { currentProject } = projects;
-        const iceVersion = currentProject ? currentProject.iceVersion : '1.x';
-        const { iceBaseMaterials } = shared;
-        const iceBaseMaterial = iceVersion === '0.x' ? iceBaseMaterials[0] : iceBaseMaterials[1];
-
-        promiseAll = Promise.all([
-          this.fetchByMaterial(source),
-          this.fetchByMaterial(iceBaseMaterial.source).catch((err) => {
-            // 获取基础组件列表失败，不终止流程
-            material.componentsError = `基础组件列表加载失败，请确认网络是否能直接访问此链接 ${iceBaseMaterial.source}，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题`;
-            return null;
-          }),
-        ]);
-      } else {
-        promiseAll = Promise.all([
-          this.fetchByMaterial(source),
-        ]);
-      }
-
-      promiseAll.then(([
-        body = {},
-        iceBaseComponents,
-      ]) => {
-        const {
-          blocks = [],
-          scaffolds = [],
-          components = [],
-          name,
-          ...attrs
-        } = body;
-
-        Object.keys(attrs).forEach((key) => {
-          material[key] = attrs[key];
-        })
-
-        // 双向绑定数据
-        material.blocks = new AdditionalBlocks(blocks);
-        material.scaffolds = new AdditionalScaffolds(scaffolds, material);
-
-        // if (iceBaseComponents) {
-          material.components = new AdditionalComponents(components, material, iceBaseComponents);
-        // }
-
-        material.loaded = true;
-        material.data = body;
-        material.error = null;
-      }).catch((error) => {
-        console.error('promiseAll.then error', error);
-        if (!isMaterialsBackup) {
-          settings.set('isMaterialsBackup', true);
-          this.loaderMaterial(index);
-        } else {
-          material.loaded = true;
-          const errMsg = `物料源加载失败，请确认网络是否能直接访问此链接 ${source}，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题`;
-          material.error = errMsg;
-        }
+      Object.keys(attrs).forEach((key) => {
+        material[key] = attrs[key];
       });
+
+      // 双向绑定数据
+      material.blocks = new AdditionalBlocks(blocks);
+      material.scaffolds = new AdditionalScaffolds(scaffolds, material);
+      material.components = new AdditionalComponents(
+        components,
+        material,
+        iceBaseComponents
+      );
+
+      material.loaded = true;
+      material.data = data;
+      material.error = null;
+    };
+
+    if (!this.useBuiltinData) {
+      if (material && !material.loaded) {
+        let promiseAll;
+        if (isIceMaterial(material.source)) {
+          // HACK: 获取 ICE 物料源时一同获取基础组件数据
+          // 因为 ICE 物料源会有一份单独的基础组件数据，其他的没有？
+          const { currentProject } = projects;
+          const iceVersion = currentProject ? currentProject.iceVersion : '1.x';
+          const { iceBaseMaterials } = shared;
+          const iceBaseMaterial =
+            iceVersion === '0.x' ? iceBaseMaterials[0] : iceBaseMaterials[1];
+
+          promiseAll = Promise.all([
+            this.fetchByMaterial(source),
+            this.fetchByMaterial(iceBaseMaterial.source).catch(() => {
+              // 获取基础组件列表失败，不终止流程
+              material.componentsError = `基础组件列表加载失败，请确认网络是否能直接访问此链接 ${
+                iceBaseMaterial.source
+              }，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题`;
+              return null;
+            }),
+          ]);
+        } else {
+          promiseAll = Promise.all([this.fetchByMaterial(source)]);
+        }
+
+        promiseAll
+          .then(([body = {}, iceBaseComponents]) => {
+            fn(body, iceBaseComponents);
+          })
+          .catch((error) => {
+            console.error('promiseAll.then error', error);
+            if (!this.useBuiltinData && isIceMaterial(material.source)) {
+              this.useBuiltinData = true;
+              this.loaderMaterial(index);
+            } else {
+              material.loaded = true;
+              const errMsg = `物料源加载失败，请确认网络是否能直接访问此链接 ${source}，建议将此问题反馈给飞冰（ICE）团队，在菜单中点击: 帮助 => 反馈问题`;
+              material.error = errMsg;
+            }
+          });
+      }
+    } else {
+      fn(BUILTIN_DATA);
     }
   }
 
@@ -272,7 +277,11 @@ class Materials {
       const material = this.materials[index];
       const components = material.components.iceBusinessComponents;
       const iceBaseComponents = material.components.iceBaseComponents;
-      material.components = new AdditionalComponents(components, material, iceBaseComponents);
+      material.components = new AdditionalComponents(
+        components,
+        material,
+        iceBaseComponents
+      );
     }
   }
 }
