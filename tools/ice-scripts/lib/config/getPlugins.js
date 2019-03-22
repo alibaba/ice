d46f7c85eb952a178e22c65de80df3da180bfb34
大果 <sobear.me@gmail.com>
2019-03-22 15:38:21 +0800

    fix: dev css chunkname error (#1647)
    * fix: design-typo-error (#1646)
    
    * chore: 去除 MiniCssExtractPlugin 的 chunckFileName 属性配置
    
    * chore: version

--- tools/ice-scripts/lib/config/getPlugins.js
+++ tools/ice-scripts/lib/config/getPlugins.js
@@ -29,13 +29,11 @@ module.exports = ({ buildConfig = {}, themeConfig = {}, entry, pkg = {} }) => {
   }
 
   const filename = cliInstance.get('hash') ? '[name].[hash:6].css' : '[name].css';
-  const chunkFilename = cliInstance.get('hash') ? '[id].[hash:6].css': '[id].css';
 
   const plugins = [
     new webpack.DefinePlugin(defineVriables),
     new MiniCssExtractPlugin({
-      filename: path.join(buildConfig.outputAssetsPath.css || '', filename),
-      chunkFilename: path.join(buildConfig.outputAssetsPath.css || '', chunkFilename),
+      filename: path.join(buildConfig.outputAssetsPath.css || '', filename)
     }),
     // FIX ISSUE: https://github.com/webpack-contrib/mini-css-extract-plugin/issues/250
     new FilterWarningsPlugin({
