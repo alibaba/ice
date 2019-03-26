387a4bbb17d33159653c4f67655f97c0ac91b2f3
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-03-26 14:44:40 +0800

    feat: 补全 buildDemo 之后修正 homepage 的逻辑

--- tools/ice-scripts/lib/component/build.js
+++ tools/ice-scripts/lib/component/build.js
@@ -1,9 +1,12 @@
 /**
  * 构建组件
  */
+const fse = require('fs-extra');
+const path = require('path');
 const cliInstance = require('../utils/cliInstance');
 const buildSrc = require('./buildSrc');
 const buildDemo = require('./buildDemo');
+const { appDirectory } = require('../config/paths');
 
 module.exports = function (buildConfig = {}) {
   if (cliInstance.get('skipDemo')) {
@@ -14,7 +17,30 @@ module.exports = function (buildConfig = {}) {
   // HACK：放在回调中执行，是为了避免两个任务的 log 信息混在一起
   buildDemo(buildConfig, (err) => {
     if (!err) {
+      // modify package.json homepage
+      modifyPkgHomePage();
+
       buildSrc(buildConfig);
     }
   });
 };
+
+
+function modifyPkgHomePage() {
+  const pkgPath = path.resolve(appDirectory, 'package.json');
+  const pkg = fse.readJsonSync(pkgPath);
+
+  const version = pkg.version;
+  const pkgName = pkg.name;
+
+  if (/^(@alife|@ali|@alipay)/.test(pkgName)) {
+    homepage = `https://unpkg.alibaba-inc.com/${pkgName}@${version}/build/index.html`;
+  } else {
+    homepage = `https://unpkg.com/${pkgName}@${version}/build/index.html`;
+  }
+
+  pkg.homepage = homepage;
+  fse.writeJsonSync(pkgPath, pkg, {
+    spaces: 2
+  });
+}
