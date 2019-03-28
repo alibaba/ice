fef9ebecef748d2f0d2a4a308c9b06df3ad9c600
许文涛 <alvin.hui@qq.com>
2019-03-28 09:33:56 +0800

    [iceworks] request 的容错 (#1702)
    * fixed: 上报错误的接口必须容错
    
    * chore: 删除无用的代码
    
    * refactor: 优化重试实现
    
    * fixed: err 的读取逻辑有误，err 可能为空。
    
    * feat: 上报的数据添加 error 对象完整信息
    
    * fixed: 变量引用有误

--- tools/iceworks/app/main/scaffolder/lib/extractor.js
+++
@@ -1,7 +0,0 @@
-exports.extractScaffording = function extractScaffording() {};
-
-exports.extractLayout = function extractScaffording() {};
-
-exports.extractBlock = function extractBlock() {};
-
-exports.extractBlocks = function extractBlocks() {};
