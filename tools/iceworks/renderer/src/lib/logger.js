31633319e7cde9ff2884c8ac6ebbefea95077dfb
alvinhui <alvin.hui@qq.com>
2019-04-02 22:43:14 +0800

    refactor: 渲染进程使用 electron-log

--- tools/iceworks/renderer/src/lib/logger.js
+++ tools/iceworks/renderer/src/lib/logger.js
@@ -1,43 +1,6 @@
-import { Logger, Transport } from 'egg-logger';
+import electronLog from 'electron-log';
 
-class ConsoleTransport extends Transport {
-  log(level, args, meta) {
-    const msg = super.log(level, args, meta);
-    return console[level.toLowerCase()](msg);
-  }
-}
+electronLog.transports.file.fileName = 'renderer.log';
+electronLog.transports.file.level = 'info';
 
-function pad(number, zeros) {
-  zeros = zeros || 2;
-  return (new Array(zeros + 1).join('0') + number).substr(-zeros, zeros);
-}
-
-const format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
-function formatTimeZone(minutesOffset) {
-  var m = Math.abs(minutesOffset);
-  return (minutesOffset >= 0 ? '-' : '+')
-    + pad(Math.floor(m / 60)) + ':'
-    + pad(m % 60);
-}
-
-const logger = new Logger();
-logger.set('console', new ConsoleTransport({
-  level: 'DEBUG',
-  formatter: function(meta) {
-    const {level, message} = meta;
-    const date = new Date();
-    return format
-      .replace('{level}', level)
-      .replace('{text}', message)
-      .replace('{y}', String(date.getFullYear()))
-      .replace('{m}', pad(date.getMonth() + 1))
-      .replace('{d}', pad(date.getDate()))
-      .replace('{h}', pad(date.getHours()))
-      .replace('{i}', pad(date.getMinutes()))
-      .replace('{s}', pad(date.getSeconds()))
-      .replace('{ms}', pad(date.getMilliseconds(), 3))
-      .replace('{z}', formatTimeZone(date.getTimezoneOffset()));
-  }
-}));
-
-export default logger;
+export default electronLog;
