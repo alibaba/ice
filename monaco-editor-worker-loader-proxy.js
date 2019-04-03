dc5d764d25f7abe9a89a8d473eec78ba24aaf836
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-04-03 21:20:56 +0800

    chore: 移除无用代码&升级静态资源

--- monaco-editor-worker-loader-proxy.js
+++
@@ -1,5 +0,0 @@
-self.MonacoEnvironment = {
-  baseUrl: 'https://cdn.bootcss.com/monaco-editor/0.10.1/min'
-};
-// eslint-disable-next-line
-importScripts('https://cdn.bootcss.com/monaco-editor/0.10.1/min/vs/base/worker/workerMain.js');
