150481d6971b7175ddd6281b8e60ad5106bb704a
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-04-03 12:35:41 +0800

    feat: 移除多个物料源仓库的逻辑

--- tools/ice-devtools/lib/generate.js
+++ tools/ice-devtools/lib/generate.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const chalk = require('chalk');
 const pkg = require('../utils/pkg-json');
 const logger = require('../utils/logger');
@@ -6,55 +5,24 @@ const message = require('../utils/message');
 
 const generateMaterialsDatabases = require('../utils/generate-marterials-database');
 
-function generateDatabase({ name, type, filepath, options }) {
-  generateMaterialsDatabases(name, type, filepath, options).then(() => {
-    showLog();
-  });
-}
-
-function generateMultiDatabase(materials, cwd) {
-  const tasks = materials.map((material) => {
-    return () => {
-      return generateMaterialsDatabases(
-        material.type ? `${material.type}-materials` : 'db',
-        material.type,
-        path.join(cwd, material.directory),
-      );
-    };
-  });
-
-  tasks.reduce((p, f) => {
-    return p.then(f);
-  }, Promise.resolve()).then(() => {
-    showLog();
-  });
-}
-
-function showLog() {
-  console.log(chalk.cyan('Success! materials db generated'));
-  console.log();
-  console.log('The build folder is ready to be deployed.');
-  console.log();
-}
-
 module.exports = function generate(cwd) {
   const pkgJson = pkg.getPkgJSON(cwd);
-  const { materialConfig, materials } = pkgJson;
-
-  if (materials && materials.length) {
-    generateMultiDatabase(materials, cwd);
-    return;
-  }
+  const { materialConfig } = pkgJson;
 
   if (!materialConfig) {
     logger.fatal(message.invalid);
   }
 
   const { type } = materialConfig;
-  generateDatabase({
-    name: type ? `${type}-materials` : 'materials',
-    type,
-    filepath: cwd,
-    options: materialConfig,
+
+  generateMaterialsDatabases(
+    type ? `${type}-materials` : 'materials',
+    cwd,
+    materialConfig
+  ).then(() => {
+    console.log(chalk.cyan('Success! materials db generated'));
+    console.log();
+    console.log('The build folder is ready to be deployed.');
+    console.log();
   });
 };
