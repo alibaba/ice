765b5826f5f07b917a1a9bf282d4432ef6650e60
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-04-04 16:20:46 +0800

    chore: 考虑用户目录不存在的情况

--- tools/ice-devtools/utils/config-path.js
+++ tools/ice-devtools/utils/config-path.js
@@ -3,7 +3,8 @@ const path = require('path');
 const userHome = require('user-home');
 
 module.exports = function getConfigPath() {
-  const CONFIG_PATH = path.join(userHome, '.idev');
+  // userHome 有可能不存在
+  const CONFIG_PATH = path.join(userHome || __dirname, '.idev');
   if (!fs.existsSync(CONFIG_PATH)) {
     fs.mkdirSync(CONFIG_PATH);
   }
