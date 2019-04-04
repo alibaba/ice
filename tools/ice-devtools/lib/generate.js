5c73795316f516538b578eca2c45d33968ae01c2
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-04-04 10:58:05 +0800

    fix: 移除 type 的兼容逻辑& getNpmTime 出错直接终止生成

--- tools/ice-devtools/lib/generate.js
+++ tools/ice-devtools/lib/generate.js
@@ -9,19 +9,14 @@ module.exports = function generate(cwd) {
   const pkgJson = pkg.getPkgJSON(cwd);
   const { materialConfig } = pkgJson;
 
-  // 全局的 materialConfig，字段会生成在全局 json 上
+  // 全局的 materialConfig，字段会生成在全局 json 上，仅用于标识物料源根目录以及自定义全局字段
   if (!materialConfig) {
     logger.fatal(message.invalid);
   }
 
-  const { type } = materialConfig;
-  // 生成的物料名称，type 属于兼容字段
-  const filename = type ? `${type}-materials` : 'materials';
-
   generateMaterialsDatabases(
     pkgJson.name,
     cwd,
-    filename,
     materialConfig,
   ).then(() => {
     console.log(chalk.cyan('Success! materials db generated'));
