b1d9b10ae34354c795ca5a9271b93e729508fc6b
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2019-04-08 16:43:25 +0800

    [release] ice-scripts/release-1.9.4 (#1753)
    - 将 webpack 固定在 `4.28.x`，解决 npm 扁平化 + webpack 4.29 导致动态 import 语法报错问题 https://github.com/alibaba/ice/issues/1721
    - 优化 getEntry 逻辑，支持传统多页面需求  Close https://github.com/alibaba/ice/issues/1739
    - 增加 copyplugin，将 public 里的文件 copy 到 build https://github.com/alibaba/ice/issues/1536
    - uniteBaseComponent 支持指定基础包名 Close #1764 
    - 补全业务组件的升级版本数据
    - 修复 babelImportConfig 不支持数组的问题 #1771

--- tools/ice-scripts/lib/plugins/bizComponentsVersion.js
+++ tools/ice-scripts/lib/plugins/bizComponentsVersion.js
@@ -73,34 +73,34 @@ module.exports = {
     '0.x': '<1.x',
     '1.x': '>=1.x',
   },
-  '@ali/ice-loading': {
+  '@ali/ice-add-image': {
+    '0.x': '<2.x',
+    '1.x': '>=2.x',
+  },
+  '@ali/ice-add-item': {
+    '0.x': '<1.x',
+    '1.x': '>=1.x',
+  },
+  '@ali/ice-add-tag': {
+    '0.x': '<1.x',
+    '1.x': '>=1.x',
+  },
+  '@ali/ice-bind': {
     '0.x': '<1.x',
   },
   '@ali/ice-addres': {
     '0.x': '<4.x',
   },
-  '@ali/ice-add-image': {
-    '0.x': '<2.x',
-  },
-  '@ali/ice-add-item': {
-    '0.x': '<1.x',
-  },
   '@ali/ice-add-link': {
     '0.x': '<1.x',
   },
-  '@ali/ice-add-tag': {
+  '@ali/ice-form': {
     '0.x': '<1.x',
   },
-  '@ali/ice-light-wangwang': {
-    '0.x': '<2.x',
-  },
-  '@ali/ice-bind': {
+  '@ali/ice-tag-picker': {
     '0.x': '<1.x',
   },
-  '@ali/ice-cookie': {
-    '0.x': '<1.x',
-  },
-  '@ali/ice-events': {
+  '@ali/ice-h5-preview': {
     '0.x': '<1.x',
   },
 };
