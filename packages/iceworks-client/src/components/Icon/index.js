165c8da9f4a92de751aa47b3bfff05db60f0db34
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-04-17 14:36:50 +0800

    feat: add Icon component

---
+++ packages/iceworks-client/src/components/Icon/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+const fontFamily = 'iceworks';
+const prefix = 'iceworks';
+
+// icon url: http://www.iconfont.cn/manage/index?manage_type=myprojects&projectId=568722&keyword=
+const Icon = ({
+  className, size, type, ...other
+}) => {
+  const classes = cx({
+    [`${prefix}-${size}`]: !!size,
+    [`${fontFamily}`]: true,
+    [`${prefix}-${type}`]: !!type,
+    [className]: !!className,
+  });
+  return <i {...other} className={classes} />;
+};
+
+Icon.propTypes = {
+  type: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  size: PropTypes.oneOf([
+    'xxs',
+    'xs',
+    'small',
+    'medium',
+    'large',
+    'xl',
+    'xxl',
+    'xxxl',
+  ]),
+};
+
+Icon.defaultProps = {
+  size: 'medium',
+  className: '',
+};
+
+export default Icon;
