c2ec2915d00095a673d648df780b4373cd3ed0f7
Yun Dong <yundong.me@gmail.com>
2019-04-22 16:21:51 +0800

    feat: add angular template source code..

---
+++ templates/ice-angular-material-template/template/block/src/index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'block-root',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss'],
+})
+export class {{className}} {
+  title = '{{name}}';
+}
---
+++ templates/ice-angular-material-template/template/block/src/index.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { {{className}} } from './index.component';
+
+@NgModule({
+  declarations: [{{className}}],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [{{className}}],
+})
+export class BlockModule {}
