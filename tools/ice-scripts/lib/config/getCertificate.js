b9d4806cae8d8a372a0483152f3187d18099c997
狒狒神 <350740249@qq.com>
2019-04-23 20:29:27 +0800

    [ice-scripts]fix: fix cert is invaild (#1841)
    * fix: fix cert is invaild
    
    * fix: lint

---
+++ tools/ice-scripts/lib/config/getCertificate.js
@@ -0,0 +1,45 @@
+const mkcert = require('mkcert');
+const fs = require('fs-extra');
+const path = require('path');
+const log = require('../utils/log');
+
+const rootDirPath = path.resolve(__dirname, '../ICE_CA');
+
+function generateCA() {
+  return new Promise((resolve, reject) => {
+    mkcert.createCA({
+      organization: 'ICE Team',
+      countryCode: 'CN',
+      state: 'ZJ',
+      locality: 'HZ',
+      validityDays: 3650,
+    }).then((ca) => {
+      fs.writeFileSync('../ICE_CA/rootCa.key', ca.key);
+      fs.writeFileSync('../ICE_CA/rootCa.crt', ca.cert);
+      resolve({
+        key: path.join(rootDirPath, 'rootCa.key'),
+        cert: path.join(rootDirPath, 'rootCa.cert'),
+      });
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
+module.exports = async function getCertificate() {
+  const certPath = path.join(rootDirPath, 'rootCa.crt');
+  const keyPath = path.join(rootDirPath, 'rootCa.key');
+  if (!fs.existsSync(certPath) || !fs.existsSync(keyPath)) {
+    await generateCA();
+  }
+  log.info('当前使用的 HTTPS 证书路径(如有需要请手动信任此文件)');
+  console.log('   ', certPath);
+  return new Promise((resolve, reject) => {
+    mkcert.createCert({
+      domains: ['127.0.0.1', 'localhost'],
+      validityDays: 365,
+      caKey: fs.readFileSync(keyPath),
+      caCert: fs.readFileSync(certPath),
+    }).then(resolve).catch(reject);
+  });
+};
