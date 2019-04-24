94a73627571c73f3fb74ca186f61e0a8cfc1577b
alvinhui <alvin.hui@qq.com>
2019-04-24 17:05:50 +0800

    refactor: fiy API 变更

--- packages/iceworks-client/src/stores/index.js
+++ packages/iceworks-client/src/stores/index.js
@@ -1,10 +1,9 @@
-import react from 'react';
 import Fiy from 'fiy';
 import projects from './projects';
 import currentProject from './currentProject';
 import materials from './materials';
 
-const fiy = new Fiy(react);
+const fiy = new Fiy();
 fiy.registerStore('materials', materials);
 fiy.registerStore('project', currentProject);
 fiy.registerStore('projects', projects);
