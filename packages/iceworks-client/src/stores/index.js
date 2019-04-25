6cbbb34178c83e7c32d15f6b0b806ce7c8b5a010
alvinhui <alvin.hui@qq.com>
2019-04-25 15:32:19 +0800

    chore: rename

--- packages/iceworks-client/src/stores/index.js
+++ packages/iceworks-client/src/stores/index.js
@@ -1,11 +1,11 @@
-import Fiy from 'fiy';
+import Icestore from 'icestore';
 import projects from './projects';
 import currentProject from './currentProject';
 import materials from './materials';
 
-const fiy = new Fiy();
-fiy.registerStore('materials', materials);
-fiy.registerStore('project', currentProject);
-fiy.registerStore('projects', projects);
+const icestore = new Icestore();
+icestore.registerStore('materials', materials);
+icestore.registerStore('project', currentProject);
+icestore.registerStore('projects', projects);
 
-export default fiy;
+export default icestore;
