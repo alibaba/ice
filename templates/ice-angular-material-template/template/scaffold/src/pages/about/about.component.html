e35bf1c71ba12be1dd9f3b015e3c1ae540648017
大果 <sobear.me@gmail.com>
2019-04-25 20:37:28 +0800

    release: ice-devtools/release-2.3.0 (#1796)
    * chore: 2.2.7
    
    * chore: refactor code, remove unless code (#1789)
    
    * refactor: remove code for build&start
    
    * refactor: merge demo  generatelogic
    
    * refactor: remove unused files
    
    * refactor: generate
    
    * fix: exit when generate  error
    
    * fix: denpencies
    
    * revert: generate
    
    * revert: generate
    
    * fix: lint
    
    * fix: catch error and exit with code 1 (#1855)
    
    * fix: catch error and exit with code 1
    
    * refactor: refactor catch strategy
    
    * refactor: refactor Promise.
    
    * feat: add angular material template&use ejs replace handlebars (#1842)
    
    * feat: add angular material template..
    
    * fix: fix lint error.
    
    * fix: fix git info error.
    
    * feat: add angular template source code..
    
    * fix: remove console.
    
    * fix: fix package typos.
    
    * fix: update package version.
    
    * feat: 将模版从 handlebars 迁移到 ejs，防止与框架语法冲突.
    
    * fix: fix template render error
    
    * fix: 修复模版中 readme 文件语法问题 (#1863)
    
    * fix: 修复 angular 模版资源 404 的问题 (#1864)
    
    * chore: update package version and changelog.
    
    * chore: update angular template package version.

---
+++ templates/ice-angular-material-template/template/scaffold/src/pages/about/about.component.html
@@ -0,0 +1,1 @@
+<h2>This is about page.</h2>
---
+++ templates/ice-angular-material-template/template/scaffold/src/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
