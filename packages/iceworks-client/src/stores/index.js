ae4630cd6f63c7100a7320e69552274cbe80f24f
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-05-08 17:53:31 +0800

    chore: rename filename

--- packages/iceworks-client/src/stores/index.js
+++ packages/iceworks-client/src/stores/index.js
@@ -1,11 +1,11 @@
 import Icestore from 'icestore';
 import projects from './projects';
-import currentProject from './currentProject';
+import project from './project';
 import materials from './materials';
 
 const icestore = new Icestore();
 icestore.registerStore('materials', materials);
-icestore.registerStore('project', currentProject);
+icestore.registerStore('project', project);
 icestore.registerStore('projects', projects);
 
 export default icestore;
