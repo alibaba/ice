41341163e3250236f8bf8a61a7bb6b40ddaef7e8
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-05-08 14:56:53 +0800

    chore: mock devSettings data

--- packages/iceworks-server/src/app/io/controller/project/dev.ts
+++ packages/iceworks-server/src/app/io/controller/project/dev.ts
@@ -1,11 +1,49 @@
 export default (app) => {
   return class DevController extends app.Controller {
     async settings(ctx) {
-      const message = ctx.args[0];
-      await ctx.socket.emit(
-        'project.dev.settings.data',
-        `Hi! I've got your message: ${message}`
-      );
+      const { args } = ctx;
+      const callback = args[args.length - 1];
+      callback({
+        error: 0,
+        data: [
+          {
+            label: '服务器端口',
+            name: 'port',
+            description: '',
+            link: '',
+            componentName: 'Input',
+            componentProps: {
+              placeholder: '4444',
+            },
+          },
+          {
+            label: '服务器主机名',
+            name: 'host',
+            description: '',
+            link: '',
+            componentName: 'Input',
+            componentProps: {
+              placeholder: '127.0.0.1',
+            },
+          },
+          {
+            label: '自动打开浏览器',
+            name: 'browser',
+            description: '',
+            link: '',
+            componentName: 'Switch',
+            componentProps: {},
+          },
+          {
+            label: '启动 HTTPS',
+            name: 'https',
+            description: '',
+            link: '',
+            componentName: 'Switch',
+            componentProps: {},
+          },
+        ],
+      });
     }
   };
 };
