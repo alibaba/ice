d8b525f7924bf32850a5c85912b241d6e2998256
ClarkXia <xiawenwu41@gmail.com>
2019-05-08 18:06:57 +0800

    chore: 移除theme相关插件

--- tools/ice-scripts/lib/config/getPlugins.js
+++ tools/ice-scripts/lib/config/getPlugins.js
@@ -8,10 +8,7 @@ const SimpleProgressPlugin = require('webpack-simple-progress-plugin');
 const webpack = require('webpack');
 const WebpackPluginImport = require('webpack-plugin-import');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const AppendStyleWebpackPlugin = require('../plugins/append-style-webpack-plugin');
 const CheckIceComponentsDepsPlugin = require('../plugins/check-ice-components-dep');
-const normalizeEntry = require('../utils/normalizeEntry');
 const paths = require('./paths');
 const getEntryHtmlPlugins = require('./getEntryHtmlPlugins');
 const cliInstance = require('../utils/cliInstance');
@@ -88,75 +85,6 @@ module.exports = ({ buildConfig = {}, themeConfig = {}, entry, pkg = {} }) => {
       new ExtractCssAssetsWebpackPlugin({
         outputPath: 'assets',
         relativeCssPath: '../',
-      })
-    );
-  }
-
-  const themePackage = buildConfig.theme || buildConfig.themePackage;
-  let iconScssPath;
-  let skinOverridePath;
-  let variableFilePath;
-  let themeNextVersion;
-
-  if (themePackage) {
-    variableFilePath = path.resolve(
-      paths.appNodeModules,
-      `${themePackage}/variables.scss`
-    );
-    iconScssPath = path.resolve(
-      paths.appNodeModules,
-      `${themePackage}/icons.scss`
-    );
-    skinOverridePath = path.join(
-      paths.appNodeModules,
-      themePackage,
-      'override.scss'
-    );
-
-    themeNextVersion = (/^@alif(e|d)\/theme-/.test(themePackage) || themePackage === '@icedesign/theme') ? '1.x' : '0.x';
-  }
-
-  if (iconScssPath && fs.existsSync(iconScssPath)) {
-    const appendStylePluginOption = {
-      type: 'sass',
-      srcFile: iconScssPath,
-      variableFile: variableFilePath,
-      compileThemeIcon: true,
-      themeNextVersion,
-      pkg,
-      distMatch: (chunkName, compilerEntry, compilationPreparedChunks) => {
-        const entriesAndPreparedChunkNames = normalizeEntry(
-          compilerEntry,
-          compilationPreparedChunks
-        );
-        // 仅对 css 的 chunk 做 处理
-        if (entriesAndPreparedChunkNames.length && /\.css$/.test(chunkName)) {
-          // css/index.css -> index
-          const assetsFromEntry = path.basename(chunkName, path.extname(chunkName));
-          if (entriesAndPreparedChunkNames.indexOf(assetsFromEntry) !== -1) {
-            return true;
-          }
-        }
-        return false;
-      },
-    };
-    plugins.push(new AppendStyleWebpackPlugin(appendStylePluginOption));
-  }
-
-  // HACK 1.x 不会走到这个逻辑
-  if (skinOverridePath && fs.existsSync(skinOverridePath)) {
-    // eslint-disable-next-line no-console
-    log.info('皮肤 override 文件存在', path.join(themePackage, 'override.scss'));
-    plugins.push(
-      new AppendStyleWebpackPlugin({
-        variableFile: variableFilePath,
-        appendPosition: 'footer',
-        // type: 'sass', // 不需要指定 type，与 distMatch 互斥
-        srcFile: skinOverridePath,
-        distMatch: /\.css/,
-        themeNextVersion,
-        compileThemeIcon: false,
-        pkg,
       })
     );
   }
