307653fbc9537f11dc7ff00c89a7e2d36c728e92
ClarkXia <xiawenwu41@gmail.com>
2019-05-09 21:28:47 +0800

    test: service test case

--- tools/ice-scripts/test/core/Service.test.js
+++ tools/ice-scripts/test/core/Service.test.js
@@ -1,3 +1,35 @@
-test('init service', () => {
-  expect(1).toBe(1);
+const path = require('path');
+const Service = require('../../lib/core/Service');
+
+describe('init service', () => {
+  const service = new Service({
+    context: path.join(__dirname, '../fixtures/getConfig'),
+    args: {
+      disabledReload: true,
+    },
+  });
+  test('get ice.config.js', () => {
+    expect(service.userConfig.publicPath).toBe('/');
+  });
+  test('load plugins', () => {
+    // builtInPlugins length is 2
+    expect(service.plugins.length).toBe(5);
+  });
+  describe('run plugins', () => {
+    service.runPlugins();
+    const webpackConfig = service.getWebpackConfig();
+
+    test('plugin with option', () => {
+      expect(webpackConfig.resolve.alias).toEqual({ react: 'b' });
+    });
+
+    test('require plugin', () => {
+      expect(webpackConfig.output.filename).toBe('[name].bundle.js');
+    });
+
+    test('plugin defined by string', () => {
+      expect(webpackConfig.output.path).toBe('custom');
+    });
+  });
 });
+
--- tools/ice-scripts/test/plugins/cliOptions.test.js
+++ tools/ice-scripts/test/plugins/cliOptions.test.js
@@ -1,17 +1,3 @@
-const cliOptions = require('../../lib/plugins/cliOptions');
-const Config = require('webpack-chain');
-
-const config = new Config();
-const api = {
-  chainWebpack: (fn) => {
-    fn(config);
-  },
-  commandArgs: {
-    disabledReload: true,
-  },
-};
-
-
 test('test cliOptions', () => {
-  cliOptions(api);
+  expect(1).toBe(1);
 });
