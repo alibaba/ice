5d1f995eea06dacf2548af537a3f68e041612cca
Yun Dong <yundong.me@gmail.com>
2019-05-13 14:11:15 +0800

    chore: remove backup files

--- tools/ice-scripts/lib/config_backup/getDefaultWebpackConfig.js
+++
@@ -1,9 +0,0 @@
-const getWebpackConfigDev = require('./webpack.config.dev');
-const getWebpackConfigProd = require('./webpack.config.prod');
-
-module.exports = function getDefaultWebpackConfig() {
-  // TODO get default webpack config with webpack-chain ?
-  return process.env.NODE_ENV === 'production'
-    ? getWebpackConfigProd({ buildConfig: {} })
-    : getWebpackConfigDev({ buildConfig: {} });
-};
