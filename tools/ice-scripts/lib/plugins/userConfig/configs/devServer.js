be55f7cd1a42177e816ed27c1086cd4285915c99
ClarkXia <xiawenwu41@gmail.com>
2019-05-13 15:01:49 +0800

    feat: proxy config

--- tools/ice-scripts/lib/plugins/userConfig/configs/devServer.js
+++ tools/ice-scripts/lib/plugins/userConfig/configs/devServer.js
@@ -1,5 +1,10 @@
 module.exports = (api, devServer) => {
+  const { userConfig } = api.service;
   api.chainWepack((config) => {
+    // make sure to use config proxy instead of config devServer.proxy
+    if (userConfig.proxy && devServer.proxy) {
+      delete devServer.proxy;
+    }
     config.merge({ devServer });
   });
 };
