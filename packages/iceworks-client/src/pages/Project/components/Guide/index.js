9d476a90e97f709f6c7c97d6bcbe3b841b9db2a0
wuji.xwt <wuji.xwt@alibaba-inc.com>
2019-05-14 18:26:26 +0800

    chore: guide for project

---
+++ packages/iceworks-client/src/pages/Project/components/Guide/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon, Button } from '@alifd/next';
+import styles from './index.module.scss';
+
+const Guide = ({ onOpenProject, onCreateProject }) => {
+  return (
+    <div className={styles.guide}>
+      <div className={styles.text}>
+          从热门模板开始初始化你的项目
+      </div>
+      <div className={styles.opts}>
+        <Button className={styles.btn} type="secondary" size="large" onClick={onOpenProject}>
+          <Icon type="download" size="xl" />
+          <span>打开项目</span>
+        </Button>
+        <Button className={styles.btn} type="primary" size="large" onClick={onCreateProject}>
+          <Icon type="add" size="xl" />
+          <span>更多模板</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+Guide.defaultProps = {
+};
+
+Guide.propTypes = {
+  onCreateProject: PropTypes.func.isRequired,
+  onOpenProject: PropTypes.func.isRequired,
+};
+
+export default Guide;
--- packages/iceworks-client/src/pages/Project/index.js
+++ packages/iceworks-client/src/pages/Project/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Icon, Button } from '@alifd/next';
 import stores from '@stores';
 import logger from '@utils/logger';
 import useModal from '@hooks/useModal';
@@ -7,6 +6,7 @@ import useModal from '@hooks/useModal';
 // import Dependency from './components/Dependencies';
 import SubMenu from './components/SubMenu';
 import OpenProject from './components/OpenProject';
+import Guide from './components/Guide';
 import projectStores from './stores';
 import styles from './index.module.scss';
 
@@ -79,21 +79,10 @@ const Project = () => {
           <div className={styles.main}>
             testing...
           </div> :
-          <div className={styles.guide}>
-            <div className={styles.text}>
-              从热门模板开始初始化你的项目
-            </div>
-            <div className={styles.opts}>
-              <Button className={styles.btn} type="secondary" size="large" onClick={onOpenProject}>
-                <Icon type="download" size="xl" />
-                <span>打开项目</span>
-              </Button>
-              <Button className={styles.btn} type="primary" size="large" onClick={onCreateProject}>
-                <Icon type="add" size="xl" />
-                <span>更多模板</span>
-              </Button>
-            </div>
-          </div>
+          <Guide
+            onOpenProject={onOpenProject}
+            onCreateProject={onCreateProject}
+          />
       }
     </div>
   );
