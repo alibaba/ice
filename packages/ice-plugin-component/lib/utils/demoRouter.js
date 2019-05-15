3a41aa46224262d5a5a3cd818d98a629fd687979
ClarkXia <xiawenwu41@gmail.com>
2019-05-15 14:26:00 +0800

    feat: init ice-plugin-component

---
+++ packages/ice-plugin-component/lib/utils/demoRouter.js
@@ -0,0 +1,72 @@
+/**
+ * component dev routers
+ */
+const fs = require('fs');
+const path = require('path');
+const hbs = require('handlebars');
+
+const getDemos = require('./getDemos');
+const { getPkgJSON } = require('./pkgJson');
+
+const DEMO_TEMPLATE = path.join(__dirname, '../template/demo.hbs');
+const HOME_TEMPLATE = path.join(__dirname, '../template/home.hbs');
+
+function compile(hbsPath) {
+  const hbsTemplateContent = fs.readFileSync(hbsPath, 'utf-8');
+  const compileTemplateContent = hbs.compile(hbsTemplateContent);
+
+  return compileTemplateContent;
+}
+
+module.exports = (context, markdownParser) => {
+  return function router(app) {
+    const demos = getDemos(context, markdownParser);
+    const pkg = getPkgJSON(context);
+    const pkgName = pkg.name;
+
+    app.get('/preview', async (req, res) => {
+      const { demo } = req.query;
+
+      if (undefined === demo) {
+        res.redirect('/');
+        return;
+      }
+
+      const demoFile = path.join(context, 'demo', `${demo}.md`);
+      if (!fs.existsSync(demoFile)) {
+        res.redirect('/');
+        return;
+      }
+
+      const demoContent = fs.readFileSync(demoFile, 'utf-8');
+      const { highlightedCode, content, meta, highlightedStyle } = markdownParser(demoContent, {
+        sliceCode: true,
+      });
+
+      const compileTemplateContent = compile(DEMO_TEMPLATE);
+
+      const jsTemplateContent = compileTemplateContent({
+        demoName: demo,
+        name: pkgName, // 组件 npm名
+        meta: Object.keys(meta).map((key) => ({ key, value: meta[key] })),
+        highlightedCode,
+        markdownContent: content,
+        demos,
+        highlightedStyle,
+      });
+
+      res.send(jsTemplateContent);
+    });
+
+    app.get('/', async (req, res) => {
+      const compileTemplateContent = compile(HOME_TEMPLATE);
+
+      const jsTemplateContent = compileTemplateContent({
+        demos,
+        pkg: pkgName,
+      });
+
+      res.send(jsTemplateContent);
+    });
+  };
+};
