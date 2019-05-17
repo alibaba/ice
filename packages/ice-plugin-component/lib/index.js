1a27dd723ac22b9c830ad669bc05787c2c2bd54c
ClarkXia <xiawenwu41@gmail.com>
2019-05-17 17:07:25 +0800

    refactor: plugin utils

--- packages/ice-plugin-component/lib/index.js
+++ packages/ice-plugin-component/lib/index.js
@@ -1,51 +1,69 @@
 const path = require('path');
-const { last } = require('lodash');
+const fse = require('fs-extra');
+const npmUtils = require('ice-npm-utils');
 const demoRouter = require('./utils/demoRouter');
 const { getPkgJSON } = require('./utils/pkgJson');
-const { parseMarkdownParts } = require('./utils/markdownHelper');
+const { parseMarkdownParts } = require('./compile/markdownHelper');
 const getDemos = require('./utils/getDemos');
 const generateEntryJs = require('./utils/generateEntryJs');
 const formatPathForWin = require('./utils/formatPathForWin');
 const getReadme = require('./utils/getReadme');
-const buildSrc = require('./utils/buildSrc');
-
-function getFilename(filePath) {
-  return last((filePath || '').split('/'));
-}
+const setAssetsPath = require('./utils/setAssetsPath');
+const configBabel = require('./utils/configBabel');
+const buildSrc = require('./compile/buildSrc');
 
 module.exports = (api) => {
   const { command, context } = api.service;
+  let babelConfig;
   const pkg = getPkgJSON(context);
 
-  function setAssetsPath(config, outputAssetsPath = { js: '', css: '' }) {
-    const filename = getFilename(config.output.get('filename'));
-    config.output.filename(path.join(outputAssetsPath.js, filename));
-    const options = config.plugin('MiniCssExtractPlugin').get('args')[0];
-    config.plugin('MiniCssExtractPlugin').tap((args) => [Object.assign(...args, {
-      filename: path.join(outputAssetsPath.css, getFilename(options.filename)),
-    })]);
+  // check adaptor folder
+  const hasAdaptor = fse.existsSync(path.join(context, 'adaptor'));
+  // generate adaptor entry
+  let adaptorEntry;
+  if (hasAdaptor) {
+    adaptorEntry = generateEntryJs({
+      template: 'adaptor.js.hbs',
+      context,
+      params: {
+        adaptor: path.resolve(context, 'adaptor/index.js'),
+        adaptorGenerate: path.resolve(context, 'node_modules/@alifd/adaptor-generate'),
+      },
+    });
   }
 
-  // check adaptor folder
-  // generate adaptor entry
-  const adaptorEntry = generateEntryJs({
-    template: 'adaptor.js.hbs',
-    context,
-    params: {
-      adaptor: path.resolve(context, 'adaptor/index.js'),
-      adaptorGenerate: path.resolve(context, 'node_modules/@alifd/adaptor-generate'),
-    },
-  });
-
-  let babelConfig;
-
   api.chainWebpack((config) => {
+    // add @babel/plugin-transform-runtime
+    // @babel/preset-env modules: commonjs
+    configBabel(config, {
+      presets: [
+        {
+          name: '@babel/preset-env',
+          opts: {
+            modules: 'commonjs',
+          },
+        },
+      ],
+      plugins: [
+        {
+          name: '@babel/plugin-transform-runtime',
+          pageBuiltIn: true,
+          opts: {
+            corejs: false,
+            helpers: true,
+            regenerator: true,
+            useESModules: false,
+          },
+        },
+      ],
+    });
+    // get babel config for component compile
     babelConfig = config.module.rule('jsx').use('babel-loader').get('options');
+    // babel option do not known cacheDirectory
     delete babelConfig.cacheDirectory;
     const markdownParser = parseMarkdownParts(babelConfig);
     // disable vendor
     config.optimization.splitChunks({ cacheGroups: {} });
-    // modify entry
     const demos = getDemos(context, markdownParser);
     let entry;
     if (command === 'dev') {
@@ -77,7 +95,6 @@ module.exports = (api) => {
           config.module.rule(rule).use('MiniCssExtractPlugin.loader').tap(() => ({ publicPath: '../' }));
         }
       });
-      // modify outputAssetsPath
       // set outputAssetsPath { js: '', css: '' }
       setAssetsPath(config);
       // modify HtmlWebpackPlugin when build demo
@@ -89,49 +106,27 @@ module.exports = (api) => {
         demos,
       }]);
     }
+    // modify entry
     config.entryPoints.clear();
     config.merge({ entry });
-    // remove CopyWebpackPlugin
+    // remove CopyWebpackPlugin (component compile do not have public folder)
     config.plugins.delete('CopyWebpackPlugin');
-
     // add demo loader
     config.module.rule('demo-loader').test(/\.md$/i).use('demo')
-      .loader(require.resolve('./utils/loaders/componentDemoLoader'))
+      .loader(require.resolve('./loaders/componentDemoLoader'))
       .options({ parser: markdownParser });
     // modify webpack devServer
     config.devServer
-      .after(demoRouter(api.service.context, markdownParser))
+      .after(demoRouter({ context, markdownParser, demos, pkg, hasAdaptor }))
       .contentBase(false);
     // add packagename to webpack alias
     config.resolve.alias
       .set(pkg.name, path.resolve(context, 'src/index'));
-    // add @babel/plugin-transform-runtime
-    // @babel/preset-env modules: commonjs
-    ['jsx', 'tsx'].forEach((rule) => {
-      config.module
-        .rule(rule)
-        .use('babel-loader')
-        .tap((options) => {
-          const presets = (options.presets || []).map((preset) => {
-            if (Array.isArray(preset) && preset[0].indexOf('preset-env')) {
-              return [
-                preset[0],
-                {
-                  ...preset[1],
-                  modules: 'commonjs',
-                },
-              ];
-            }
-            return preset;
-          });
-          return Object.assign(options, { presets });
-        });
-    });
-    // modify pkg home page
 
     // modify webpack chain for adaptor build
-    if (process.env.BUILD_ADAPTOR === JSON.stringify(true) && command === 'build') {
+    if (process.env.BUILD_ADAPTOR === JSON.stringify(true) && command === 'build' && hasAdaptor) {
       config.plugins.delete('HtmlWebpackPlugin');
+      // output umd
       config.output
         .library('Adaptor')
         .libraryExport('default')
@@ -171,11 +166,25 @@ module.exports = (api) => {
   });
   api.onHooks('afterBuild', () => {
     if (!process.env.BUILD_ADAPTOR) {
+      // modify pkg home page
+      const { name, version } = pkg;
+      const pkgPath = path.resolve(context, 'package.json');
+      const homepage = `${npmUtils.getUnpkgHost(name)}/${name}@${version}/build/index.html`;
+      pkg.homepage = homepage;
+      fse.writeJsonSync(pkgPath, pkg, {
+        spaces: 2,
+      });
+
       // component build
       buildSrc(babelConfig, context);
-      // adaptor build
-      process.env.BUILD_ADAPTOR = true;
-      api.service.run();
+
+      if (hasAdaptor) {
+        // generate adaptor index.scss
+
+        // adaptor build
+        process.env.BUILD_ADAPTOR = true;
+        api.service.run();
+      }
     }
   });
 };
