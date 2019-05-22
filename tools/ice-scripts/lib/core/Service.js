d59b5c0437ffa21ee5949b64394da8344e1185aa
大果 <sobear.me@gmail.com>
2019-05-22 13:50:34 +0800

    [ice-scripts] add options to chainWebpack  (#1990)
    * chore: add chainWebpack options
    
    * fix: options

--- tools/ice-scripts/lib/core/Service.js
+++ tools/ice-scripts/lib/core/Service.js
@@ -121,7 +121,16 @@ module.exports = class Service {
     });
 
     if (this.userConfig.chainWebpack) {
-      this.userConfig.chainWebpack(config);
+      try {
+        this.userConfig.chainWebpack(config, {
+          command: this.command,
+          commandArgs: this.commandArgs,
+        });
+      } catch (err) {
+        log.error('Fail to exec userConfig.chainWebpack');
+        console.error(err);
+        process.exit(1);
+      }
     }
     // add polyfill/hotdev before origin entry
     this.processWepackEntry(config);
