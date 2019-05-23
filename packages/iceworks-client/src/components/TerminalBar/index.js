cd1665df8d61b73cb83f9f94df9a61c864d5acfe
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-05-23 12:05:37 +0800

    feat: add TerminalBar component

---
+++ packages/iceworks-client/src/components/TerminalBar/index.js
@@ -0,0 +1,71 @@
+/* eslint object-curly-newline:0 */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@alifd/next';
+import Icon from '@components/Icon';
+import styles from './index.module.scss';
+
+const TerminalBar = ({ isWorking, onStart, onStop, onSetting }) => {
+  return (
+    <div className={styles.actionBar}>
+      {/* Left Button Group */}
+      <div className={styles.leftActionBar}>
+        {!isWorking ? (
+          <Button
+            type="primary"
+            className={styles.leftButton}
+            onClick={onStart}
+          >
+            <Icon type="start" className={styles.icon} />
+            运行
+          </Button>
+        ) : (
+          <Button type="primary" className={styles.leftButton} onClick={onStop}>
+            <Icon type="stop" className={styles.icon} />
+            停止
+          </Button>
+        )}
+        <Button
+          type="secondary"
+          className={styles.leftButton}
+          onClick={onSetting}
+          disabled={isWorking}
+        >
+          <Icon type="settings" className={styles.icon} />
+          设置
+        </Button>
+      </div>
+
+      {/* Right Button Group */}
+      <div className={styles.rightActionBar}>
+        <Button.Group>
+          <Button type="primary" className={styles.rightButton}>
+            <Icon type="pc" /> 日志
+          </Button>
+          <Button type="secondary" className={styles.rightButton}>
+            <Icon type="projects" /> 仪表盘
+          </Button>
+          <Button type="secondary" className={styles.rightButton}>
+            <Icon type="wrencha" /> 构建分析
+          </Button>
+        </Button.Group>
+      </div>
+    </div>
+  );
+};
+
+TerminalBar.defaultProps = {
+  isWorking: false,
+  onStart: () => {},
+  onStop: () => {},
+  onSetting: () => {},
+};
+
+TerminalBar.propTypes = {
+  isWorking: PropTypes.bool,
+  onStart: PropTypes.func,
+  onStop: PropTypes.func,
+  onSetting: PropTypes.func,
+};
+
+export default TerminalBar;
