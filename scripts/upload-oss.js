6e012feeeb7771bb34c934085c3ace18fb0dd8d6
大果 <sobear.me@gmail.com>
2019-05-27 15:29:45 +0800

    feat: rework docs generate for different versions&locales (#2009)
    * chore: 生成文档改造，独立 json、国际化
    
    * chore: 添加英文文档 demo
    
    * chore: 文档不区分正式和预发
    
    * chore: 配置数据单独文件
    
    * chore: 配置数据单独文件
    
    * chore: 中英文分别生成两个文件
    
    * chore: 增加版本信息
    
    * chore: 注释 versions 信息

--- scripts/upload-oss.js
+++ scripts/upload-oss.js
@@ -3,14 +3,15 @@
  */
 const oss = require('ali-oss');
 const path = require('path');
+const glob = require('glob');
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 const branch = process.env.TRAVIS_BRANCH;
-const assetsPath = branch === 'production' ? 'assets' : 'pre-assets';
+const assetsPath = branch === 'master' ? 'assets' : 'pre-assets';
 
-if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
+if (branch === 'master' || /docs/.test(branch)) {
   const ossClient = oss({
     bucket,
     endpoint: 'oss-cn-hangzhou.aliyuncs.com',
@@ -19,16 +20,25 @@ if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
     time: '120s',
   });
 
-  const fromPath = path.resolve(__dirname, '../build/docs.json');
-  const toPath = path.join(assetsPath, 'docs.json');
+  const baseDir = path.resolve(__dirname, '../build');
+  const files = glob.sync('*.json', {
+    nodir: true,
+    cwd: baseDir,
+  });
 
-  console.log('start upload oss', fromPath, toPath);
+  const promiseQueue = files.map((file) => {
+    const fromPath = path.resolve(baseDir, file);
+    const toPath = path.join(assetsPath, 'docs', file);
 
-  ossClient.put(toPath, fromPath).then((result) => {
-    console.log('upload success', result);
+    console.log('ossput', fromPath, toPath);
+    return ossClient.put(toPath, fromPath);
+  });
+
+  Promise.all(promiseQueue).then(() => {
+    console.log('upload success');
   });
 } else {
-  console.log('当前分支非 master/production/docs*, 不执行文档同步脚本');
+  console.log('当前分支非 master/docs*, 不执行文档同步脚本');
   console.log(`TRAVIS_BRANCH=${branch}`);
   process.exit(0);
 }
