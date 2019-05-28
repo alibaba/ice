d17283f7bfcd9972718b13eec6949de52f6ca134
ClarkXia <xiawenwu41@gmail.com>
2019-05-28 17:51:26 +0800

    fix: add default config

--- templates/ice-react-material-template/template/scaffold/ice.config.js
+++ templates/ice-react-material-template/template/scaffold/ice.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  entry: 'src/index.js',
   publicPath: './',
   plugins: [
-    ['ice-plugin-fusion', {}],
+    ['ice-plugin-fusion', {
+      themePackage: '@icedesign/theme',
+    }],
+    ['ice-plugin-moment-locale', {
+      locales: ['zh-cn'],
+    }],
   ],
 };
