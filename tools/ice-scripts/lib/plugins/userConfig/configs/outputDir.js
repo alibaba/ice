8a214704f5495243c7988eda0cd9bc63781b2e10
ClarkXia <xiawenwu41@gmail.com>
2019-05-30 23:11:53 +0800

    fix: copy public folder to outputDir

--- tools/ice-scripts/lib/plugins/userConfig/configs/outputDir.js
+++ tools/ice-scripts/lib/plugins/userConfig/configs/outputDir.js
@@ -3,6 +3,13 @@ const path = require('path');
 module.exports = ({ context, chainWebpack }, outputDir) => {
   const { rootDir } = context;
   chainWebpack((config) => {
-    config.output.path(path.resolve(rootDir, outputDir));
+    const outputPath = path.resolve(rootDir, outputDir);
+    config.output.path(outputPath);
+    // copy public folder to outputDir
+    // copy-webpack-plugin patterns must be an array
+    config.plugin('CopyWebpackPlugin').tap(([args]) => [[{
+      ...(args[0] || {}),
+      to: outputPath,
+    }]]);
   });
 };
