f7869e27811f7c298c1162dfc2e10197b48591c8
alvinhui <alvin.hui@qq.com>
2019-05-31 18:02:40 +0800

    feat: install logic

--- tools/iceworks-app/main.js
+++ tools/iceworks-app/main.js
@@ -1,20 +1,40 @@
 const { app, BrowserWindow } = require('electron');
-const is = require('electron-is');
+// const is = require('electron-is');
 const address = require('address');
+const execa = require('execa');
+const path = require('path');
+const detectPort = require('detect-port');
 
 const ip = address.ip();
-const url = is.production() ? '' : `http://${ip}:4444/`;
+const serverDir = path.join(__dirname, 'server');
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow();
-
-  mainWindow.loadURL(url);
-
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  mainWindow.loadFile('loading.html');
+
+  (async () => {
+    let setPort = '7001';
+    setPort = await detectPort(setPort);
+    try {
+      const { stdout } = await execa('npm', ['start'], {
+        cwd: serverDir,
+        env: {
+          PORT: setPort,
+        },
+      });
+      console.log(stdout);
+      const url = true ? `http://${ip}:${setPort}/` : `http://${ip}:4444/`;
+      mainWindow.loadURL(url);
+    } catch (error) {
+      mainWindow.loadFile('error.html');
+    }
+  })();
 }
 
 app.on('ready', createWindow);
