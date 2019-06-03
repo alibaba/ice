29a8ca0b13153daad5926dd1897350f121c7163f
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-06-04 00:33:08 +0800

    chore: fix lint

--- packages/iceworks-client/src/components/Icon/index.js
+++ packages/iceworks-client/src/components/Icon/index.js
@@ -3,7 +3,7 @@ import DynamicIcon from '@icedesign/dynamic-icon';
 const Icon = DynamicIcon.create({
   fontFamily: 'iceworks',
   prefix: 'iceworks',
-  css: '//at.alicdn.com/t/font_568722_1ujnei0qic3.css'
+  css: '//at.alicdn.com/t/font_568722_1ujnei0qic3.css',
 });
 
 export default Icon;
