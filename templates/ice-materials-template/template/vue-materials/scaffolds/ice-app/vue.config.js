609d280c2053a72aa74fb43b4400fc18dcedc91e
大果 <sobear.me@gmail.com>
2019-06-03 17:37:19 +0800

    chore: remove unless templates (#2084)

--- templates/ice-materials-template/template/vue-materials/scaffolds/ice-app/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-  configureWebpack: {
-    resolve: {
-      extensions: ['.js', '.json', '.vue'],
-    },
-  },
-};
--- templates/ice-vue-app-template/README.md
+++
@@ -1,1 +0,0 @@
-# ice-vue-app-template
