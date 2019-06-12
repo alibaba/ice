04aa23cd6a3b74180d4022b2b6b541275f0cbc57
alvinhui <alvin.hui@qq.com>
2019-06-12 09:56:22 +0800

    refactor: login tips

--- packages/iceworks-client/src/pages/Project/components/DEFPanel/index.js
+++ packages/iceworks-client/src/pages/Project/components/DEFPanel/index.js
@@ -23,15 +23,6 @@ const DEFPanel = () => {
   const { isLogin, workId } = user;
 
   async function onPush(target) {
-    if (!isLogin) {
-      Message.show({
-        align: 'tr tr',
-        type: 'error',
-        content: '请先登录！',
-      });
-      return;
-    }
-
     const lastCommit = await socket.emit('project.git.getLog', [currentBranch]);
     if (!lastCommit) {
       Message.show({
@@ -78,38 +69,47 @@ const DEFPanel = () => {
     }
   });
 
+  let mainElement = null;
+  if (isLogin) {
+    if (isRepository) {
+      mainElement = (
+        <div className={styles.main}>
+          <div className={styles.tips}>
+            DEF 发布要求分支名为： prefix/x.y.z，例如：daily/1.0.0
+          </div>
+          <Button.Group>
+            <Button
+              size="small"
+              type="secondary"
+              onClick={async () => {
+                await onPush('daily');
+              }}
+            >
+              日常发布
+            </Button>
+            <Button
+              size="small"
+              type="primary"
+              onClick={async () => {
+                await onPush('pro');
+              }}
+            >
+              正式发布
+            </Button>
+          </Button.Group>
+        </div>
+      );
+    } else {
+      mainElement = '该项目不是一个 Git 仓库，请在 Git 面板关联仓库后使用 DEF 发布功能。';
+    }
+  } else {
+    mainElement = '请点击左侧面板进行登录后使用 DEF 发布功能。';
+  }
+
   return (
     <Panel header={<h3><FormattedMessage id="iceworks.project.panel.def.title" /></h3>}>
       <div className={styles.wrap}>
-        {
-          isRepository ?
-            <div className={styles.main}>
-              <div className={styles.tips}>
-                DEF 发布要求分支名为： prefix/x.y.z，例如：daily/1.0.0
-              </div>
-              <Button.Group>
-                <Button
-                  size="small"
-                  type="secondary"
-                  onClick={async () => {
-                    await onPush('daily');
-                  }}
-                >
-                  日常发布
-                </Button>
-                <Button
-                  size="small"
-                  type="primary"
-                  onClick={async () => {
-                    await onPush('pro');
-                  }}
-                >
-                  正式发布
-                </Button>
-              </Button.Group>
-            </div> :
-            '该项目不是一个 Git 仓库，请在 Git 面板关联仓库后使用 DEF 发布功能。'
-        }
+        {mainElement}
       </div>
     </Panel>
   );
