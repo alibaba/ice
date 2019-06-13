6c6417793fb6ffda3eaccc34c740316591c7c97a
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-06-13 23:07:46 +0800

    feat: adjust theme by  global bar

--- packages/iceworks-client/ice.config.js
+++ packages/iceworks-client/ice.config.js
@@ -18,25 +18,17 @@ module.exports = {
 
   plugins: [
     ['ice-plugin-fusion', {
-      // themePackage: '@alifd/theme-iceworks-dark',
-      // themeConfig: {
-      //   'sub-menu-bg': '#434557',
-      //   'next-card-bg': '#434557',
-      //   'next-card-border-color': '#333646',
-      // },
       themePackage: [{
         name: '@alifd/theme-iceworks-dark',
         themeConfig: {
           'sub-menu-bg': '#434557',
-          'next-card-bg': '#434557',
-          'next-card-border-color': '#333646',
+          'global-bar-bg': '#333646',
         },
       }, {
         name: '@alifd/theme-2',
         themeConfig: {
           'sub-menu-bg': '#fff',
-          'next-card-bg': '#fff',
-          'next-card-border-color': '#eee',
+          'global-bar-bg': '#5584FF',
         },
       }],
     }],
--- packages/iceworks-client/src/components/Card/index.js
+++ packages/iceworks-client/src/components/Card/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card as NextCard } from '@alifd/next';
-import styles from './index.module.scss';
 
 const Card = ({ children, style, ...props }) => {
   return (
     <NextCard
-      className={styles.card}
       style={{
         border: 'none',
         height: '100%',
