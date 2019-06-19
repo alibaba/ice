79778806c55fc05916d62949d43741600e4234be
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-06-19 11:05:37 +0800

    refactor: remove panel setting

--- packages/iceworks-client/src/locales/zh-CN/index.js
+++ packages/iceworks-client/src/locales/zh-CN/index.js
@@ -26,10 +26,6 @@ export default {
   'iceworks.menu.setting': '设置',
   'iceworks.menu.setting.general': '通用设置',
   'iceworks.menu.setting.general.desc': '设置主题和语言',
-  'iceworks.menu.setting.panel': '面板设置',
-  'iceworks.menu.setting.panel.desc': '项目面板的展示',
-  'iceworks.menu.setting.advanced': '高级设置',
-  'iceworks.menu.setting.advanced.desc': '设置编辑器和 npm 源',
 
   // setting
   'iceworks.setting.title': '设置',
