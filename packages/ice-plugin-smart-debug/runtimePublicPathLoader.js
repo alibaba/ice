253711bd22237c1ddd751df135c800bbd52de7bc
ClarkXia <xiawenwu41@gmail.com>
2019-06-20 16:40:33 +0800

    feat: ice-plugin-smart-debug (#2202)
    * feat: ice plugin for smart loader
    
    * chore: changelog and readme
    
    * fix: hash regexp
    
    * chore: version and changelog
    
    * fix: include regexp and __webpack_public_path__
    
    * chore: rename plugin

---
+++ packages/ice-plugin-smart-debug/runtimePublicPathLoader.js
@@ -0,0 +1,9 @@
+module.exports = (content) => {
+  return `
+    if (window.ICE && window.ICE.debug) {
+      var debug = window.ICE.debug;
+      __webpack_public_path__ = '//' + debug.origin + ':' + debug.port + (debug.output ? ('/' + debug.output + '/') : '/');
+    }
+    ${content}
+  `;
+};
