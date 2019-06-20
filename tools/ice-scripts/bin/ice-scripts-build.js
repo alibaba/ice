fe5f5aaf6b3204f43cff0a5173fb69aa14529042
ClarkXia <xiawenwu41@gmail.com>
2019-06-20 12:38:05 +0800

    feat: support --config to use custom config (#2216)
    * feat: support --config to use custom config
    
    * fix: modify error message

--- tools/ice-scripts/bin/ice-scripts-build.js
+++ tools/ice-scripts/bin/ice-scripts-build.js
@@ -7,6 +7,7 @@ const Context = require('../lib/core/Context');
 const log = require('../lib/utils/log');
 
 program
+  .option('--config <config>', 'use custom config')
   .parse(process.argv);
 
 validationSassAvailable();
