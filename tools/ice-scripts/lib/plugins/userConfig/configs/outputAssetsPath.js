d447273b120660af6a28f16d5d1d0152b393f55d
ClarkXia <xiawenwu41@gmail.com>
2019-06-20 13:02:04 +0800

    fix: formate windows output path (#2214)

--- tools/ice-scripts/lib/plugins/userConfig/configs/outputAssetsPath.js
+++ tools/ice-scripts/lib/plugins/userConfig/configs/outputAssetsPath.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { last } = require('lodash');
+const formatWinPath = require('../utils/formatWinPath');
 
 function getFilename(filePath) {
   return last((filePath || '').split('/'));
@@ -7,11 +8,11 @@ function getFilename(filePath) {
 module.exports = ({ chainWebpack }, outputAssetsPath) => {
   chainWebpack((config) => {
     const filename = getFilename(config.output.get('filename'));
-    config.output.filename(path.join(outputAssetsPath.js || '', filename));
+    config.output.filename(formatWinPath(path.join(outputAssetsPath.js || '', filename)));
 
     const options = config.plugin('MiniCssExtractPlugin').get('args')[0];
     config.plugin('MiniCssExtractPlugin').tap((args) => [Object.assign(...args, {
-      filename: path.join(outputAssetsPath.css || '', getFilename(options.filename)),
+      filename: formatWinPath(path.join(outputAssetsPath.css || '', getFilename(options.filename))),
     })]);
   });
 };
