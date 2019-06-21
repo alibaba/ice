b6ec9c3c3e02540f0102ec9ffd5ac9b6fcb34a57
alvinhui <alvin.hui@qq.com>
2019-06-21 11:48:39 +0800

    feat: themeConfig

--- packages/ice-template-builder/templates/redux/ice.config.js
+++ packages/ice-template-builder/templates/redux/ice.config.js
@@ -4,6 +4,7 @@ module.exports = {
       'ice-plugin-fusion',
       {
         themePackage: '@icedesign/theme',
+        <% if (themeConfig) { %>themeConfig: <%- JSON.stringify(themeConfig) %><% } %>
       },
     ],
   ],
