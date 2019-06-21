27677357227f56f8e04b61e9ce989a34f5684b05
boiawang <zhengwang2314@gmail.com>
2019-06-21 10:57:10 +0800

    chore: fix icon bug

--- packages/iceworks-client/src/pages/Project/components/LayoutPanel/index.js
+++ packages/iceworks-client/src/pages/Project/components/LayoutPanel/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Message } from '@alifd/next';
 import { FormattedMessage } from 'react-intl';
+import Icon from '@components/Icon';
 import Panel from '../Panel';
 import stores from '../../stores';
 import styles from './index.module.scss';
@@ -9,12 +10,21 @@ const LayoutPanel = () => {
   const layouts = stores.useStore('layouts');
   const { dataSource } = layouts;
 
+  function onRefresh() {
+    layouts.refresh();
+  }
+
   return (
     <Panel
       header={
-        <h3>
-          <FormattedMessage id="iceworks.project.panel.layout.title" />
-        </h3>
+        <div className={styles.header}>
+          <h3>
+            <FormattedMessage id="iceworks.project.panel.layout.title" />
+          </h3>
+          <div className={styles.icons}>
+            <Icon className={styles.icon} type="reload" size="small" onClick={onRefresh} />
+          </div>
+        </div>
       }
     >
       {dataSource.length ? (
