d266b847c38b5b98448c39010ca7a37e552e0a21
Dong Yun <yundong.me@gmail.com>
2019-06-25 10:28:55 +0800

    feat: add ice-screentshot package to support screent ability  (#2171)
    * feat: install puppeteer when execute screenshot command
    
    * chore: fix typos
    
    * chore: remove ice-devtools screenshot command
    
    * feat: new screenshot cli
    
    * fix: fix typos
    
    * feat: support set up local server
    
    * fix: throw error if could not find element
    
    * feat: add changelog
    
    * chore: update user tips
    
    * chore: fix typos
    
    * chore: fix upgrage guide 404

---
+++ packages/ice-screenshot/README.md
@@ -0,0 +1,46 @@
+# ice-screenshot
+
+Take a screenshot of web page in the terminal.
+
+Usage: screenshot -u https://www.example.com
+
+Options:
+  -V, --version              output the version number
+  -u, --url <url>            The target url or path to local server
+  -l, --local [local]        Set up a local server in a special directory and take screenshot, defaults set up in `./`
+  -s, --selector <selector>  Select a element through CSS selector
+  -o, --output <output>      Output path
+  -h, --help                 output usage information
+
+## Usage
+
+For ICE materials, you can take screenshot use following commands:
+
+```bash
+# for scaffold
+$ npm run build && screenshot -l
+
+# for block
+$ npm run build && screenshot -l -s \#mountNode
+```
+
+Others:
+
+```bash
+# take a screenshot of taobao.com
+$ screenshot -u https://www.taobao.com
+
+# take screenshot of a element
+$ screenshot -u https://www.taobao.com -s .service.J_Service
+
+# take screenshot export to ~/taobao.jpg
+$ screenshot -u https://www.taobao.com -o ~/taboa.jpg
+
+# Set up local server in current working directory and take screenshot of http://localhost:8100/public/index.html
+# By default, this will run the contents of current directory on a local server, the URL is http://localhost:8100
+$ screenshot -u /public/index.html -l
+
+# Set up local server in ./public directory and take screenshot of http://localhost:8100/index.html
+# By default, this will run the contents of `public/` directory on a local server, the URL is http://localhost:8100
+$ screenshot -u /index.html -l ./public
+```
--- tools/ice-devtools/lib/screenshot.js
+++ tools/ice-devtools/lib/screenshot.js
@@ -1,69 +1,19 @@
-const debug = require('debug')('ice:screenshot:general');
-const path = require('path');
-const imagemin = require('imagemin');
-const fs = require('fs');
-const ora = require('ora');
-const { getUnpkgHost } = require('ice-npm-utils');
-const pkgJSON = require('../utils/pkg-json');
-const getType = require('../utils/type');
-const logger = require('../utils/logger');
-const createlocalServer = require('../utils/local-server');
-const screenshotDOMElement = require('../utils/screenshot');
-const message = require('../utils/message');
-const getTempPath = require('../utils/temp-path');
 
-module.exports = function screenshot(cwd, opt) {
-  const type = getType(cwd);
-  debug('type %s', type);
-  if (!type) {
-    logger.fatal(message.invalid);
-  }
+const chalk = require('chalk');
 
-  const port = opt.port || 8990;
-
-  /* eslint-disable-next-line import/no-dynamic-require */
-  const { selector, servePath, urlpath } = require(`./${type}/screenshot`)(cwd);
-  const tempPath = getTempPath();
-  const screenshotTempPath = path.join(tempPath, 'screenshot.png'); // 临时地址
-
-  const url = `http://127.0.0.1:${port}${urlpath}`;
-
-  /* eslint wrap-iife:0 */
-  (async function execute() {
-    const server = createlocalServer(servePath, port);
-    const spin = ora('screenshoting ...');
-    spin.start();
-    try {
-      // 截取页面中某个元素的快照
-      await screenshotDOMElement(url, selector, screenshotTempPath);
-
-      // 图片压缩
-      await imagemin([screenshotTempPath], cwd);
-
-      // 删除未压缩的图像
-      fs.unlinkSync(screenshotTempPath);
-      spin.succeed('generate screenshot.png sucesss');
-    } catch (error) {
-      spin.fail('generate screenshot.png fail');
-      logger.fatal(error);
-    } finally {
-      server.close();
-      spin.stop();
-
-      // 截图后更新 package.json 的字段
-      const pkg = pkgJSON.getPkgJSON(cwd);
-      const version = pkg.version;
-      const pkgName = pkg.name;
-
-      const screenshotUrl = `${getUnpkgHost(pkgName)}/${pkgName}@${version}/screenshot.png`;
-      const homepage = `${getUnpkgHost(pkgName)}/${pkgName}@${version}/build/index.html`;
-
-      pkg.homepage = homepage;
-      pkg[`${type}Config`].screenshot = screenshotUrl;
-
-      if (opt.callback) {
-        opt.callback();
-      }
-    }
-  })();
+module.exports = function build() {
+  console.log();
+  console.log(chalk.yellow('[ERROR] 此功能已废弃，请安装 ice-screenshot 截图'));
+  console.log();
+  console.log(chalk.cyan('    npm install ice-screenshot --save-dev'));
+  console.log();
+  console.log(chalk.yellow('安装完成后，请更改 package.json 中的 npm scripts'));
+  console.log();
+  console.log(chalk.yellow('对于 scaffold:'));
+  console.log(chalk.cyan('"screenshot": "idev screenshot" -> "screenshot": "screenshot -l"'));
+  console.log(chalk.yellow('对于 block:'));
+  console.log(chalk.cyan('"screenshot": "idev screenshot" -> "screenshot": "screenshot -l -s \\\\#mountNode"'));
+  console.log();
+  console.log('升级访问 https://www.yuque.com/ice-team/wiki/cb36go');
+  console.log();
 };
