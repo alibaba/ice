e55742e017531b0730deeceb79eca097fadc4a1b
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-01 20:50:41 +0800

    refactor: refactor panel head

--- packages/iceworks-client/src/pages/Project/components/TaskItem/index.js
+++ packages/iceworks-client/src/pages/Project/components/TaskItem/index.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { Button } from '@alifd/next';
 import Icon from '@components/Icon';
 import TaskButton from '@components/TaskButton';
 import Panel from '../Panel';
+import PanelHead from '../Panel/head';
 import styles from './index.module.scss';
 
-const TaskItem = ({ title, desc, icon, path, history, status, onStart, onStop }) => {
-  function renderHeader() {
-    return (
-      <div className={styles.header}>
-        <h3><FormattedMessage id="iceworks.project.panel.task.title" /></h3>
-      </div>
-    );
-  }
-
+const TaskItem = ({ title, desc, icon, path, intl, history, status, onStart, onStop }) => {
   function handleClick() {
     history.push(path);
   }
 
   return (
-    <Panel header={renderHeader()}>
+    <Panel header={
+      <PanelHead
+        title={intl.formatMessage({ id: 'iceworks.project.panel.task.title' })}
+      />}
+    >
       <div className={styles.taskItem}>
         <div className={styles.taskInfo}>
           <Icon type={icon} />
@@ -58,9 +55,10 @@ TaskItem.propTypes = {
   icon: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
   history: PropTypes.object.isRequired,
+  intl: PropTypes.object.isRequired,
   status: PropTypes.bool,
   onStart: PropTypes.func,
   onStop: PropTypes.func,
 };
 
-export default withRouter(TaskItem);
+export default injectIntl(withRouter(TaskItem));
