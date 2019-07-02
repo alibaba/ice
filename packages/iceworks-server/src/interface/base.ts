e5be4530c57c59798081bebcd7540a48e7fb3a82
oraaange <oraaangecolin@gmail.com>
2019-07-02 23:49:31 +0800

    Fix vue adapter (#2323)
    * fix: fix vue config
    
    * fix: rm des

--- packages/iceworks-server/src/interface/base.ts
+++ packages/iceworks-server/src/interface/base.ts
@@ -223,7 +223,7 @@ export interface IConfSchema {
   /**
    * 描述
    */
-  description: string;
+  description?: string;
 
   /**
    * 链接
--- packages/iceworks-server/src/lib/adapter-vue-v1/modules/task/index.ts
+++ packages/iceworks-server/src/lib/adapter-vue-v1/modules/task/index.ts
@@ -1,12 +1,15 @@
+import * as path from 'path';
 import { baseModules } from '../../../adapter';
 import getTaskConfig from './getTaskConfig';
 
 export default class Task extends baseModules.Task {
   public cliConfFilename = 'vue.config.js';
+  public cliConfPath: string;
 
   public getTaskConfig = getTaskConfig;
 
   constructor(params) {
     super(params);
+    this.cliConfPath = path.join(this.project.path, this.cliConfFilename);
   }
 }
