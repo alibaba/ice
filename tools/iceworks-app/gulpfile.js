5529d2f9732adab891fd80162e79afdc68dee35d
alvinhui <alvin.hui@qq.com>
2019-07-02 23:24:19 +0800

    feat: init desktop apps

---
+++ tools/iceworks-app/gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const execa = require('execa');
+const builder = require('electron-builder');
+const { getNpmTarball, getAndExtractTarball } = require('ice-npm-utils');
+const packageJSON = require('./package.json');
+
+const Platform = builder.Platform;
+
+gulp.task('dist', (done) => {
+  const targets = Platform.MAC.createTarget();
+  const serverDir = path.join(__dirname, 'server');
+
+  getNpmTarball('iceworks-server')
+    .then((tarball) => {
+      if (fs.existsSync(serverDir)) {
+        rimraf.sync(serverDir);
+      }
+      return getAndExtractTarball(serverDir, tarball);
+    })
+    .then(() => {
+      return execa.shell('npm install', {
+        stdio: 'inherit',
+        cwd: serverDir,
+      });
+    })
+    .then(() => {
+      return builder.build({
+        targets,
+        config: packageJSON.build,
+      });
+    })
+    .then(done)
+    .catch(console.error);
+});
