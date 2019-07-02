7d2a3aed23d91f17bb6cc3f5c2c30ae1048c5a21
Orange-C <741434157@qq.com>
2019-07-02 20:45:52 +0800

    fix: fix vue config

--- packages/iceworks-server/src/lib/adapter-vue-v1/modules/configuration/index.ts
+++ packages/iceworks-server/src/lib/adapter-vue-v1/modules/configuration/index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import { baseModules } from '../../../adapter';
 import getConfigSchema from './getConfigSchema';
 
@@ -5,7 +6,10 @@ export default class Configuration extends baseModules.Configuration {
   public cliConfFilename = 'vue.config.js';
   public getConfigSchema = getConfigSchema;
 
+  public cliConfPath: string;
+
   constructor(params) {
     super(params);
+    this.cliConfPath = path.join(this.project.path, this.cliConfFilename);
   }
 }
