1cb2ea60ca5d096a22f5f15f179eb911fccca3a7
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-03 15:03:36 +0800

    fix: engineering configuration write failed

--- packages/iceworks-client/src/pages/Engineering/components/Configuration/index.js
+++ packages/iceworks-client/src/pages/Engineering/components/Configuration/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Message } from '@alifd/next';
 import logger from '@utils/logger';
 import Card from '@components/Card';
@@ -17,7 +17,7 @@ const formItemLayout = {
 };
 
 const Configuration = () => {
-  const conf = engineeringStores.useStore('configuration');
+  const confStore = engineeringStores.useStore('configuration');
 
   async function onChange(values) {
     const params = {};
@@ -31,7 +31,7 @@ const Configuration = () => {
     logger.info(params);
 
     try {
-      await conf.setCLIConf(params);
+      await confStore.setCLIConf(params);
       Message.show({
         type: 'success',
         title: '提示',
@@ -48,10 +48,14 @@ const Configuration = () => {
     }
   }
 
+  useEffect(() => {
+    confStore.getCLIConf();
+  }, []);
+
   return (
     <Card title="自定义配置" contentHeight="100%">
       <DynamicForm
-        config={conf.dataSource.cli}
+        config={confStore.dataSource.cli}
         formItemLayout={formItemLayout}
         onChange={onChange}
       />
