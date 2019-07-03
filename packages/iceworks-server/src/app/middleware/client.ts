e53b4f76eea44d6763526061f6ceb4e454a023d8
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-03 20:18:19 +0800

    feat: dynamic set port

--- packages/iceworks-server/src/app/middleware/client.ts
+++ packages/iceworks-server/src/app/middleware/client.ts
@@ -10,9 +10,9 @@ export default function() {
     ctx.clientConfig = {
       // TODO: 区分环境和端口检测
       // default use iceworks-client@latest
-      clientPath: '//unpkg.com/iceworks-client@latest/build/',
-      socketUrl: '//127.0.0.1:7001/',
-      apiUrl: '//127.0.0.1:7001/api/',
+      clientPath: 'https://unpkg.com/iceworks-client@1.0.0-beta.3/build/',
+      socketUrl: `//127.0.0.1:${process.env.PORT}/`,
+      apiUrl: `//127.0.0.1:${process.env.PORT}/api/`,
       isAliInternal: await checkAliInternal(),
     };
 
