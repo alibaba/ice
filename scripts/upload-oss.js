9f6ec98b2d4dba9021c527d36779255db275ab35
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-08 15:50:37 +0800

    feat: upload client assets to oss

--- scripts/upload-oss.js
+++ scripts/upload-oss.js
@@ -1,16 +1,17 @@
 /**
- * 将 build/docs.json 上传到 oss
+ * 将站点文档和 iceworks-client 静态资源上传到 oss
  */
 const oss = require('ali-oss');
 const path = require('path');
+const glob = require('glob');
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 const branch = process.env.TRAVIS_BRANCH;
-const assetsPath = branch === 'production' ? 'assets' : 'pre-assets';
+const cwd = process.cwd();
 
-if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
+function uploadAssetsToOSS(fromPath, toPath, globPatterns) {
   const ossClient = oss({
     bucket,
     endpoint: 'oss-cn-hangzhou.aliyuncs.com',
@@ -19,16 +20,36 @@ if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
     time: '120s',
   });
 
-  const fromPath = path.resolve(__dirname, '../build/docs.json');
-  const toPath = path.join(assetsPath, 'docs.json');
-
-  console.log('start upload oss', fromPath, toPath);
-
-  ossClient.put(toPath, fromPath).then((result) => {
-    console.log('upload success', result);
+  const files = glob.sync(globPatterns, {
+    nodir: true,
+    cwd: fromPath,
   });
+
+  const promiseQueue = files.map((file) => {
+    const from = path.join(fromPath, file);
+    const to = path.join(toPath, file);
+
+    console.log('ossput', `${from} => ${to}`);
+    return ossClient.put(to, from);
+  });
+
+  Promise.all(promiseQueue).then(() => {
+    console.log('upload success');
+  });
+}
+
+if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
+  // 同步文档静态资源
+  const docsFromPath = path.join(cwd, 'build');
+  const docsToPath = branch === 'production' ? 'assets' : 'pre-assets';
+  uploadAssetsToOSS(docsFromPath, docsToPath, '*.json');
+
+  // 同步 iceworks-client 静态资源
+  const clientFromPath = path.join(cwd, 'packages', 'iceworks-client', 'build');
+  const clientToPath = 'client-assets/build';
+  uploadAssetsToOSS(clientFromPath, clientToPath, '*/**');
 } else {
-  console.log('当前分支非 master/production/docs*, 不执行文档同步脚本');
+  console.log('当前分支非 master/production/docs*, 不执行同步脚本');
   console.log(`TRAVIS_BRANCH=${branch}`);
   process.exit(0);
 }
