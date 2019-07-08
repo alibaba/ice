dd11425033213fa9f4d21652cd95f1b1f5816b63
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-08 15:50:37 +0800

    feat: upload assets to oss

--- scripts/upload-oss.js
+++ scripts/upload-oss.js
@@ -1,5 +1,5 @@
 /**
- * 将 build/docs.json 上传到 oss
+ * 将站点文档和 iceworks-client 静态资源上传到 oss
  */
 const oss = require('ali-oss');
 const path = require('path');
@@ -9,9 +9,9 @@ const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 const branch = process.env.TRAVIS_BRANCH;
-const assetsPath = branch === 'master' ? 'assets' : 'pre-assets';
+const cwd = process.cwd();
 
-if (branch === 'master' || /docs/.test(branch)) {
+function uploadAssetsToOSS(fromPath, toPath, globPatterns) {
   const ossClient = oss({
     bucket,
     endpoint: 'oss-cn-hangzhou.aliyuncs.com',
@@ -20,23 +20,34 @@ if (branch === 'master' || /docs/.test(branch)) {
     time: '120s',
   });
 
-  const baseDir = path.resolve(__dirname, '../build');
-  const files = glob.sync('*.json', {
+  const files = glob.sync(globPatterns, {
     nodir: true,
-    cwd: baseDir,
+    cwd: fromPath,
   });
 
   const promiseQueue = files.map((file) => {
-    const fromPath = path.resolve(baseDir, file);
-    const toPath = path.join(assetsPath, 'docs', file);
+    const from = path.join(fromPath, file);
+    const to = path.join(toPath, file);
 
-    console.log('ossput', fromPath, toPath);
-    return ossClient.put(toPath, fromPath);
+    console.log('ossput', `${from} => ${to}`);
+    return ossClient.put(to, from);
   });
 
   Promise.all(promiseQueue).then(() => {
     console.log('upload success');
   });
+}
+
+if (['master', 'production'].indexOf(branch) !== -1 || /docs/.test(branch)) {
+  // 同步文档静态资源
+  const docsFromPath = path.join(cwd, 'build');
+  const docsToPath = branch === 'production' ? 'assets' : 'pre-assets';
+  uploadAssetsToOSS(docsFromPath, docsToPath, '*.json');
+
+  // 同步 iceworks-client 静态资源
+  const clientFromPath = path.join(cwd, 'packages', 'iceworks-client', 'build');
+  const clientToPath = 'client-assets/build';
+  uploadAssetsToOSS(clientFromPath, clientToPath, '*/**');
 } else {
   console.log('当前分支非 master/docs*, 不执行文档同步脚本');
   console.log(`TRAVIS_BRANCH=${branch}`);
