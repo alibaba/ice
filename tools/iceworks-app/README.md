0bf1f163c00efff447663d5ab1ba4fc7e43ff22a
zhaofeng-shu33 <616545598@qq.com>
2019-07-09 13:50:34 +0800

    feat: dist iceworks-3.0 as electron app start kit

---
+++ tools/iceworks-app/README.md
@@ -0,0 +1,28 @@
+# Iceworks App
+
+## Develop
+
+0. Start iceworks server: 
+
+    ```bash
+    $ cd ../../pacakges/iceworks-server
+    $ npm run start
+    ```
+0. Start iceworks client:
+
+    ```bash
+    $ cd ../../packages/iceworks-client
+    $ npm run start
+    ```
+0. Start icewroks app:
+
+    ```
+    $ cd ../../packages/iceworks-app
+    $ npm run start
+    ```
+
+## Build
+
+```
+$ npm run dist
+```
---
+++ tools/iceworks-app/gulpfile.js
@@ -0,0 +1,59 @@
+const os = require('os');
+const gulp = require('gulp');
+const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const execa = require('execa');
+const builder = require('electron-builder');
+const { getNpmTarball, getAndExtractTarball } = require('ice-npm-utils');
+const packageJSON = require('./package.json');
+
+const Platform = builder.Platform;
+
+gulp.task('dist', (done) => {
+  let targets;
+  if (os.platform() === 'win32') {
+    targets = Platform.WINDOWS.createTarget();
+  } else {
+    targets = Platform.MAC.createTarget();
+  }
+  const serverDir = path.join(__dirname, 'server');
+
+  getNpmTarball('iceworks-server')
+    .then((tarball) => {
+      if (fs.existsSync(serverDir)) {
+        rimraf.sync(serverDir);
+      }
+      return getAndExtractTarball(serverDir, tarball);
+    })
+    .then(() => {
+      return execa.shell('npm install', {
+        stdio: 'inherit',
+        cwd: serverDir,
+      });
+    })
+    .then(() => {
+      return builder.build({
+        targets,
+        config: packageJSON.build,
+      });
+    })
+    .then(done)
+    .catch(console.error);
+});
+
+gulp.task('dist-dev', (done) => {
+  let targets;
+  if (os.platform() === 'win32') {
+    targets = Platform.WINDOWS.createTarget();
+  } else {
+    targets = Platform.MAC.createTarget();
+  }
+  // the server directory is creade and installed manually.
+  builder.build({
+    targets,
+    config: packageJSON.build,
+  })
+    .then(done)
+    .catch(console.error);
+});
