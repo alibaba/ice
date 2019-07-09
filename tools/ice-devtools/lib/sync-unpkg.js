605f942d546d72b9722d28d71f5255526ee69e08
Yun Dong <yundong.me@gmail.com>
2019-07-09 14:06:43 +0800

    fix: fix typo

--- tools/ice-devtools/lib/sync-unpkg.js
+++ tools/ice-devtools/lib/sync-unpkg.js
@@ -8,7 +8,7 @@ module.exports = function build() {
   console.log(chalk.cyan('    npm publish'));
   console.log();
   console.log(chalk.yellow('安装成功后，根据以下规则生成物料数据 URL：'));
-  console.log(chalk.cyan('https://unpkg.com/{{pkgJSON.name}}@latest/build/materials.json'));
+  console.log(chalk.cyan('https://unpkg.com/{{npmName}}@latest/build/materials.json'));
   console.log();
   console.log('升级访问 https://www.yuque.com/ice-team/wiki/cb36go');
   console.log();
