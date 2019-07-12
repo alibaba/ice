30ddad3f87d7f61d06bc06ca5849bfac962ed747
ClarkXia <xiawenwu41@gmail.com>
2019-07-12 20:57:40 +0800

    feat: add lint file when init react material

--- tools/ice-devtools/lib/material/add.js
+++ tools/ice-devtools/lib/material/add.js
@@ -7,6 +7,7 @@ const boxenLog = require('../../utils/boxen-log');
 const logger = require('../../utils/logger');
 const { generateNpmNameByPrefix } = require('../../utils/npm');
 const templateRender = require('../../utils/template-render');
+const pkgJSON = require('../../utils/pkg-json');
 
 module.exports = async function addMaterial(cwd, opts = {}) {
   logger.verbose('init -> init single material', opts);
@@ -17,6 +18,7 @@ module.exports = async function addMaterial(cwd, opts = {}) {
     templatePath: src,
     standalone,
     materialConfig,
+    initMaterial,
   } = opts;
 
   const questions = getQuestions(npmPrefix);
@@ -39,8 +41,30 @@ module.exports = async function addMaterial(cwd, opts = {}) {
     dest,
     npmName,
     materialConfig,
+    transformRegexp: initMaterial ? /.+/ : /_package.json/,
     skipGitIgnore: !standalone, // 物料仓库中，不处理 _gitignore 文件
   });
+
+  // generate lint file only material type is react
+  if (initMaterial && materialConfig.type === 'react') {
+    // render lint files
+    const lintTemplatePath = path.join(__dirname, '../../template/lint');
+    await templateRender({
+      src: lintTemplatePath,
+      dest,
+    });
+
+    const pkgInfo = pkgJSON.getPkgJSON(dest);
+    // add lint scripts to package.json
+    pkgInfo.scripts.lint = 'npm run eslint && npm run stylelint';
+    pkgInfo.scripts.eslint = 'eslint --cache --ext .js,.jsx ./';
+    pkgInfo.scripts.stylelint = 'stylelint ./**/*.scss';
+    // add lint devDependencies to package.json
+    pkgInfo.devDependencies['@ice/spec'] = '^0.1.1';
+    pkgInfo.devDependencies.eslint = '^6.0.1';
+    pkgInfo.devDependencies.stylelint = '^10.1.0';
+    pkgJSON.writePkgJSON(pkgInfo, dest);
+  }
 
   completedMessage(type, name, dest, standalone);
 };
