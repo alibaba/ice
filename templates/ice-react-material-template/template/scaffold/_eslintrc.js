f6aff440728f71b17138fafdea71fa4dc1595a54
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-13 11:04:03 +0800

    chore: migrate ice-devtools to ice-lab/ice-devtools

--- templates/ice-react-material-template/template/scaffold/_eslintrc.js
+++
@@ -1,6 +0,0 @@
-const { eslint, deepmerge } = require('@ice/spec');
-
-module.exports = deepmerge(eslint, {
-  rules: {
-  },
-});
--- tools/ice-devtools/utils/message.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = {
-  invalid: `Invalid ice materials project ${chalk.cyan('docs: https://ice.work/docs/materials/about')}`,
-  missingMaterialConfig: 'Missing `materialConfig` property in package.json file.',
-  missingTemplate: 'Missing `materialConfig.template` property in package.json file.',
-};
