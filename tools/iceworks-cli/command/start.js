1a5bcf6746c771a8599d849d0ca5ca85d3f23ca8
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-14 17:07:15 +0800

    feat: dynamic install for iceworks-server

--- tools/iceworks-cli/command/start.js
+++ tools/iceworks-cli/command/start.js
@@ -4,9 +4,38 @@ const portfinder = require('portfinder');
 const chalk = require('chalk');
 const ora = require('ora');
 const open = require('open');
+const inquirer = require('inquirer');
 const goldlog = require('../lib/goldlog');
+const checkVersion = require('../lib/checkVersion');
+
+const SERVER_PATH = path.join(__dirname, '../', 'server');
 
 async function start(options = {}) {
+  const answers = await checkServerVersion();
+  if (answers && answers.update) {
+    const child = spawn('npm', ['run', 'sync'], {
+      stdio: ['pipe'],
+    });
+
+    child.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    child.on('error', (err) => {
+      console.log('update failed:', err);
+    });
+
+    child.on('close', (code) => {
+      if (code === 0) {
+        listen(options);
+      }
+    });
+  } else {
+    listen(options);
+  }
+}
+
+async function listen(options) {
   const host = options.host || 'http://127.0.0.1';
 
   let port = options.port;
@@ -24,7 +53,7 @@ async function start(options = {}) {
   spinner.start();
   const child = spawn('npm', ['start'], {
     stdio: ['pipe'],
-    cwd: path.join(__dirname, '../', 'server'),
+    cwd: SERVER_PATH,
     env,
   });
 
@@ -63,6 +92,30 @@ function failedMsg(error) {
   console.log();
   console.log(error);
   console.log();
+}
+
+/**
+ * Get the server package version
+ */
+async function checkServerVersion() {
+  // eslint-disable-next-line
+  const packageConfig = require(path.join(SERVER_PATH, 'package.json'));
+  const packageName = packageConfig.name;
+  const packageVersion = packageConfig.version;
+
+  const result = await checkVersion(packageName, packageVersion);
+  if (result) {
+    const answers = await inquirer.prompt([
+      {
+        type: 'confirm',
+        message: 'A newer version of iceworks-server is available',
+        name: 'update',
+        default: false,
+      },
+    ]);
+
+    return answers;
+  }
 }
 
 module.exports = (...args) => {
