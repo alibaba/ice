7e3f24e33dc99c2973e7a68ac6d8caa829dcefd2
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-15 11:52:41 +0800

    chore: code optim

--- tools/iceworks-cli/lib/checkVersion.js
+++ tools/iceworks-cli/lib/checkVersion.js
@@ -1,14 +1,9 @@
-/* eslint prefer-template:0 */
 const semver = require('semver');
-const request = require('request-promise-native');
+const { getNpmLatestSemverVersion } = require('ice-npm-utils');
 
 module.exports = async (packageName, packageVersion) => {
   try {
-    const body = await request({
-      url: `https://registry.cnpmjs.org/${packageName}`,
-      timeout: 3000,
-    });
-    const latestVersion = JSON.parse(body)['dist-tags'].latest;
+    const latestVersion = await getNpmLatestSemverVersion(packageName, packageVersion);
     if (semver.lt(packageVersion, latestVersion)) {
       return latestVersion;
     }
