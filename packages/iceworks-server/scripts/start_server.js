6d6642f67dab4b741cab1f30b7ec9acf8bc0cb5c
zhaofeng-shu33 <616545598@qq.com>
2019-07-16 17:59:58 +0800

    fix: remove midway start script

--- packages/iceworks-server/scripts/start_server.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const detectPort = require('detect-port');
-const mv = require('midway');
-
-const baseDir = __dirname;
-let setPort = '7001';
-setPort = detectPort(setPort);
-mv.startCluster({ sticky: true, baseDir, port: setPort, workers: 1, typescript: true });
