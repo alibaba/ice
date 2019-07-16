05551727538504099c059e7f38bb902121073cfe
zhaofeng-shu33 <616545598@qq.com>
2019-07-16 22:51:31 +0800

    fix: travis not install by default

--- packages/iceworks-server/scripts/start.js
+++ packages/iceworks-server/scripts/start.js
@@ -1,10 +1,7 @@
 const StartCommand = require('egg-scripts').StartCommand;
-const detectPort = require('detect-port');
 const path = require("path");
 
 const baseDir = path.join(__dirname, '../');
-let setPort = '7001';
-setPort = detectPort(setPort);
 const sc = new StartCommand([baseDir, '--title=egg-server-iceworks-server', 
-    '--framework=midway', '--port=' + setPort, '--workers=1']);
+  '--framework=midway',  '--workers=1']);
 sc.start();
--- packages/iceworks-server/scripts/stop.js
+++ packages/iceworks-server/scripts/stop.js
@@ -1,3 +1,4 @@
 const StopCommand = require('egg-scripts').StopCommand;
+
 const sc = new StopCommand();
 sc.start(['--title=egg-server-iceworks-server'])
