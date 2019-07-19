38a2d2f36bd82d3a7524314cba104fb6b05b90d2
tanhengyeow <E0032242@u.nus.edu>
2019-07-19 17:54:40 +0800

    chore: fix typo

--- packages/iceworks-client/src/pages/Project/components/QuickBuildPanel/index.js
+++ packages/iceworks-client/src/pages/Project/components/QuickBuildPanel/index.js
@@ -5,7 +5,7 @@ import useTask from '@hooks/useTask';
 import writeGlobalLog from '@utils/writeGlobalLog';
 import TaskItem from '../TaskItem';
 
-const QuickDevPanel = ({ intl, title, description }) => {
+const QuickBuildPanel = ({ intl, title, description }) => {
   function writeLog() {
     const msg = intl.formatMessage({ id: 'iceworks.task.dev.start.msg' });
     writeGlobalLog(msg);
@@ -25,10 +25,10 @@ const QuickDevPanel = ({ intl, title, description }) => {
 };
 
 
-QuickDevPanel.propTypes = {
+QuickBuildPanel.propTypes = {
   intl: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
 
-export default injectIntl(QuickDevPanel);
+export default injectIntl(QuickBuildPanel);
