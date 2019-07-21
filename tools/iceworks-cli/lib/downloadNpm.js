286c3ff80bd41b58b98455cefa3254f59cbbbdd6
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-21 22:06:29 +0800

    chore: add log.verbose

--- tools/iceworks-cli/lib/downloadNpm.js
+++ tools/iceworks-cli/lib/downloadNpm.js
@@ -1,9 +1,11 @@
 const { getNpmTarball } = require('ice-npm-utils');
 const extractTarball = require('./extractTarball');
+const log = require('./log');
 
 module.exports = ({ npmName, destDir }) => {
   return getNpmTarball(npmName, 'latest')
     .then((url) => {
+      log.verbose('getNpmTarball', url);
       return extractTarball(url, destDir);
     });
 };
