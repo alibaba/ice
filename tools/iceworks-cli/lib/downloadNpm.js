a3b297f38a28267179244183727d85093b6b5298
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-21 19:09:09 +0800

    chore: use ice-npm-utils

--- tools/iceworks-cli/lib/downloadNpm.js
+++ tools/iceworks-cli/lib/downloadNpm.js
@@ -1,9 +1,8 @@
-
-const getTarball = require('./getTarball');
+const { getNpmTarball } = require('ice-npm-utils');
 const extractTarball = require('./extractTarball');
 
 module.exports = ({ npmName, destDir }) => {
-  return getTarball(npmName)
+  return getNpmTarball(npmName, 'latest')
     .then((url) => {
       return extractTarball(url, destDir);
     });
