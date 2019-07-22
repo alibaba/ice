702f84f73e9f9dd15f8bb7e917b7abc61bb8637b
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2019-07-22 21:35:47 +0800

    refactor: code optim (#2513)
    * refactor: code optim
    
    * chore: remove console log

--- tools/iceworks-cli/lib/checkVersion.js
+++ tools/iceworks-cli/lib/checkVersion.js
@@ -2,13 +2,9 @@ const semver = require('semver');
 const { getNpmLatestSemverVersion } = require('ice-npm-utils');
 
 module.exports = (packageName, packageVersion) => {
-  try {
-    return getNpmLatestSemverVersion(packageName, packageVersion).then(latestVersion => {
-      if (semver.lt(packageVersion, latestVersion)) {
-        return latestVersion;
-      }
-    })
-  } catch (error) {
-    // ...
-  }
+  return getNpmLatestSemverVersion(packageName, packageVersion).then(latestVersion => {
+    if (latestVersion && semver.lt(packageVersion, latestVersion)) {
+      return Promise.resolve(latestVersion);
+    }
+  })
 };
