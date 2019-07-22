055beb3e4445f5a1fe2f5517dc6d34c27b2b7089
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-22 21:34:14 +0800

    chore: remove console log

--- tools/iceworks-cli/lib/checkVersion.js
+++ tools/iceworks-cli/lib/checkVersion.js
@@ -4,7 +4,6 @@ const { getNpmLatestSemverVersion } = require('ice-npm-utils');
 module.exports = (packageName, packageVersion) => {
   return getNpmLatestSemverVersion(packageName, packageVersion).then(latestVersion => {
     if (latestVersion && semver.lt(packageVersion, latestVersion)) {
-      console.log(latestVersion);
       return Promise.resolve(latestVersion);
     }
   })
