44a261c651790d1b0331eab552ec4d7857651a7a
alvinhui <alvin.hui@qq.com>
2019-07-22 12:11:44 +0800

    feat: add logs

--- tools/iceworks-app/app/error.js
+++ tools/iceworks-app/app/error.js
@@ -1,7 +1,13 @@
-const { remote } = require('electron')
+const { remote, shell } = require('electron')
 const log = require('electron-log');
 
+const logPath = log.transports.file.findLogPath(remote.app.getName());
+
 document.addEventListener('DOMContentLoaded', function() {
-  const logPath = log.transports.file.findLogPath(remote.app.getName());
   document.getElementById('log_text').innerText = logPath;
-}, false)
+}, false);
+
+document.getElementById('log_text').addEventListener('click', function() {
+  shell.showItemInFolder(logPath);
+  shell.openItem(logPath);
+});
