7ba743af74362d5b09ee24362c2d5294e3a5f922
luhc228 <luhengchang228@gmail.com>
2019-07-30 11:22:41 +0800

    test: update scripts and goldlog.test

--- packages/iceworks-server/test/app/controller/goldlog.test.ts
+++ packages/iceworks-server/test/app/controller/goldlog.test.ts
@@ -1,6 +1,4 @@
-/* tslint:disable */
-const { app, assert } = require('midway-mock/bootstrap');
-/* tslint:enable */
+const { app } = require('midway-mock/bootstrap');
 
 describe('test /app/controller/goldlog.test.ts', () => {
   it('should status 200 and get the request body', () => {
@@ -9,18 +7,9 @@ describe('test /app/controller/goldlog.test.ts', () => {
       .post('/api/goldlog/record')
       .type('form')
       .send({
-        namespace: 'adapter',
-        module: 'page',
-        action: 'getAll',
+        namespace: 'goldlog-test',
+        action: 'test',
         data: {},
-        visit_time: '2019-07-29 14:14:05',
-        UA: {
-          name: 'chrome',
-          version: '75.0.3770',
-          versionNumber: 75.0377,
-          mobile: false,
-          os: 'Windows 10',
-        },
       })
       .expect(200)
       .expect({
