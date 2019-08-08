55f7c0a30cc155988f8d3931410edf797f4a60a4
luhc228 <luhengchang228@gmail.com>
2019-08-09 01:48:29 +0800

    refrator writeGlobalLog function

--- packages/iceworks-client/src/utils/writeGlobalLog.js
+++ packages/iceworks-client/src/utils/writeGlobalLog.js
@@ -1,17 +1,9 @@
 import termManager from './termManager';
 
 function writeGlobalLog(msg, isStdout = true) {
-  let term;
-  if (isStdout) {
-    term = termManager.find('globalProcessLog');
-    if (term) {
-      term.writeChunk(msg);
-    }
-  } else {
-    term = termManager.find('globalOperationLog');
-    if (term) {
-      term.writeLog(msg);
-    }
+  let term = termManager.find(isStdout ? 'globalProcessLog' : 'globalOperationLog');
+  if (term) {
+    term[isStdout ? 'writeChunk' : 'writeLog'](msg);
   }
 }
 
