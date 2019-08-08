fc5f9edad0dd31a00c9a1b7830c90d6f57dac8bc
luhc228 <luhengchang228@gmail.com>
2019-08-09 01:51:25 +0800

    fix: fix lint error

--- packages/iceworks-client/src/utils/writeGlobalLog.js
+++ packages/iceworks-client/src/utils/writeGlobalLog.js
@@ -1,7 +1,7 @@
 import termManager from './termManager';
 
 function writeGlobalLog(msg, isStdout = true) {
-  let term = termManager.find(isStdout ? 'globalProcessLog' : 'globalOperationLog');
+  const term = termManager.find(isStdout ? 'globalProcessLog' : 'globalOperationLog');
   if (term) {
     term[isStdout ? 'writeChunk' : 'writeLog'](msg);
   }
