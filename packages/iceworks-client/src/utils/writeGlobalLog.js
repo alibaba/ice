d8846fca99cf32eaf71b951c1ae440a89d00eb4e
luhc228 <luhengchang228@gmail.com>
2019-08-09 00:09:35 +0800

    fix: fix bar font color

--- packages/iceworks-client/src/utils/writeGlobalLog.js
+++ packages/iceworks-client/src/utils/writeGlobalLog.js
@@ -4,10 +4,14 @@ function writeGlobalLog(msg, isStdout = true) {
   let term;
   if (isStdout) {
     term = termManager.find('globalProcessLog');
-    term.writeChunk(msg);
+    if (term) {
+      term.writeChunk(msg);
+    }
   } else {
     term = termManager.find('globalOperationLog');
-    term.writeLog(msg);
+    if (term) {
+      term.writeLog(msg);
+    }
   }
 }
 
