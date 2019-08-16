7d16c9355e0fc338aa9d041585b25fbd5c0e857b
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-08-16 13:57:42 +0800

    fix: reset default value

--- tools/iceworks-cli/command/start.js
+++ tools/iceworks-cli/command/start.js
@@ -31,7 +31,7 @@ async function start(options = {}) {
       }
     }
   } else {
-    process.env.ICEWORKS_CORE_VERSION = undefined;
+    process.env.ICEWORKS_CORE_VERSION = '';
     const answers = await checkServerVersion();
     if (answers && answers.update) {
       downloadAndListen(options);
