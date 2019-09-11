afca1ddfbeda313aecc2a4f58b24fb86a478f68b
alvinhui <alvin.hui@qq.com>
2019-09-11 14:50:54 +0800

    feat: delete upgrade 3.0.0 logic

--- tools/iceworks/renderer/src/Layout/Layout.jsx
+++ tools/iceworks/renderer/src/Layout/Layout.jsx
@@ -22,10 +22,6 @@ class SidebarLayout extends Component {
     title: PropTypes.string,
   };
 
-  state = {
-    showTips: true
-  }
-
   static defaultProps = {
     // 业务替换为自己的页面标题
     title: 'Iceworks',
@@ -48,7 +44,6 @@ class SidebarLayout extends Component {
 
   render() {
     const { title, className } = this.props;
-    const { showTips } = this.state;
     const isAlibaba = settings.get('isAlibaba');
     return (
       <DocumentTitle title={title}>
@@ -141,15 +136,6 @@ class SidebarLayout extends Component {
             </Layout.Aside>
             {/* 主体内容 */}
             <Layout.Main>
-              {showTips ? <div className="tips-for-upgrade">
-                iceworks 已推出本地 CLI 版本，请通过 `npm install iceworks -g` 进行使用！
-                <a href="https://ice.work/docs/iceworks/quick-start" target="_blank">更多详情 ></a>
-                <Icon type="close" className="icon" onClick={() => {
-                  this.setState({
-                    showTips: false
-                  });
-                }} />
-              </div> : null}
               {this.props.children}
             </Layout.Main>
           </Layout.Section>
