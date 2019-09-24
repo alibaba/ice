890781686cda5318a9d91ed4dbe2c0b1f14db61c
alvinhui <alvin.hui@qq.com>
2019-09-24 12:27:39 +0800

    chore: set tarball

--- packages/iceworks-server/scripts/generateProject.js
+++ packages/iceworks-server/scripts/generateProject.js
@@ -5,10 +5,7 @@ const { getNpmTarball, getAndExtractTarball } = require('ice-npm-utils');
 
 const generateAdapterTestProject = async () => {
   const tmpPath = path.join(__dirname, '../test/lib/adapter/tmp');
-
-  // The stable version should be specified
-  const tarball = await getNpmTarball('@icedesign/lite-scaffold', '3.0.5');
-  await getAndExtractTarball(tmpPath, tarball);
+  await getAndExtractTarball(tmpPath, 'https://registry.npm.taobao.org/@icedesign/lite-scaffold/download/@icedesign/lite-scaffold-3.0.5.tgz');
 
   // note: the build files should be removed. If not, it will throw the error.
   const rimrafAsync = util.promisify(rimraf);
