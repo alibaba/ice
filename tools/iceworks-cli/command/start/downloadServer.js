5054135419b43926d76e419e4b9847dfb6c0cead
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-15 16:31:51 +0800

    feat: 精细化处理 start 异常

--- tools/iceworks-cli/command/start/downloadServer.js
+++ tools/iceworks-cli/command/start/downloadServer.js
@@ -15,61 +15,75 @@ const REGISTRY = process.env.REGISTRY || 'https://registry.npm.taobao.org';
  * @param {string} npmName npm package name
  * @param {string} destDir target directory
  */
-function downloadServer(npmName, destDir) {
-  console.log('>>> start downloading code', destDir);
-  console.log('>>> ICEWORKS_CORE_VERSION:', process.env.ICEWORKS_CORE_VERSION);
-  const version = process.env.ICEWORKS_CORE_VERSION ? process.env.ICEWORKS_CORE_VERSION : 'latest';
+module.exports = function downloadServer(version) {
+  const npmName = NPM_NAME;
+  const destDir = DEST_DIR;
+  version = version || 'latest';
+
+  console.log('>>> start download iceworks-server', version, destDir, REGISTRY);
   return getNpmTarball(npmName, version, REGISTRY)
     .then((tarballURL) => {
+      console.log('>>> download iceworks-server from npm', tarballURL);
       return extractTarball({ tarballURL, destDir });
     })
-    .then((res) => {
-      if (res.length) {
-        console.log('>>> download completed');
-        console.log('>>> start installing dependencies');
-        install(destDir);
-      }
-    });
-}
-
-function install(cwd) {
-  const child = spawn('npm', ['install', '--loglevel', 'silly', '--registry', REGISTRY], {
-    stdio: ['pipe'],
-    cwd,
-  });
-
-  child.stdout.on('data', data => {
-    console.log(data.toString());
-  });
-
-  child.stderr.on('data', data => {
-    console.log(data.toString());
-  });
-
-  child.on('error', error => {
-    console.log(error);
-    process.exit(1);
-  });
-
-  child.on('close', (code) => {
-    if (code === 0) {
-      console.log('>>> install completed');
-    } else {
+    .catch((err) => {
+      // getNpmTarball|extractTarball error
+      console.log();
+      console.log(chalk.red('Error: download iceworks-server error'));
+      console.log();
+      console.log(err);
+      process.exit(1);
+    })
+    .then(() => {
+      console.log('>>> download iceworks-server completed');
+      console.log('>>> start installing iceworks-serve dependencies');
+      return install(destDir);
+    })
+    .then(() => {
+      console.log('>>> install iceworks-server dependencies completed');
+    })
+    .catch((err) => {
+      // install deps error
       console.log();
       console.log(chalk.red('提示：安装依赖失败，可通过以下步骤进行修复。'));
       console.log();
-      console.log(chalk.green(`   1. cd ${DEST_DIR}`));
+      console.log(chalk.green(`   1. cd ${destDir}`));
       console.log(chalk.green('   2. npm install --registry=https://registry.npm.taobao.org'));
       console.log();
       console.log(chalk.green('   确保依赖正常安装，然后重新启动 iceworks，如还不能正常启动，请通过钉钉群联系我们'));
       console.log(chalk.green('   钉钉群：https://ice.alicdn.com/assets/images/qrcode.png'));
       console.log();
+      console.log(err);
       process.exit(1);
-    }
-  });
+    });
 }
 
-downloadServer(NPM_NAME, DEST_DIR).catch((err) => {
-  console.log(err);
-  process.exit(1);
-});
+function install(cwd) {
+  return new Promise((resolve, reject) => {
+    const child = spawn('npm', ['install', '--loglevel', 'silly', '--registry', REGISTRY], {
+      stdio: ['pipe'],
+      cwd,
+    });
+
+    child.stdout.on('data', data => {
+      console.log(data.toString());
+    });
+
+    child.stderr.on('data', data => {
+      console.log(data.toString());
+    });
+
+    child.on('error', error => {
+      reject(error);
+    });
+
+    child.on('close', (code) => {
+      if (code === 0) {
+        console.log('>>> install completed');
+        resolve();
+      } else {
+        reject(new Error('install deps error'));
+      }
+    });
+  });
+}
