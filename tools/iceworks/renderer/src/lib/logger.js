4537cb1169f19b37af828be9653608192f28d0fa
alvinhui <alvin.hui@qq.com>
2019-11-25 11:19:10 +0800

    fix: make sure __bl.error is function

--- tools/iceworks/renderer/src/lib/logger.js
+++ tools/iceworks/renderer/src/lib/logger.js
@@ -4,8 +4,9 @@ electronLog.transports.file.fileName = 'renderer.log';
 electronLog.transports.file.level = 'info';
 
 electronLog.transports.arms = (msg) => {
-  // eslint-disable-next-line
-  window.__bl && __bl.error(msg.data[0]);
+  if (window.__bl && (typeof window.__bl.error === 'function')) {
+    window.__bl.error(msg.data[0]);
+  }
 };
 electronLog.transports.arms.level = 'error';
 
