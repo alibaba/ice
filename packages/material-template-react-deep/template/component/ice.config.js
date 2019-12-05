5fa521580c407277869447379a359ad410e285a6
陶启立 <taoqili1504@126.com>
2019-12-05 18:38:56 +0800

    chore: fix deep template (#2959)
    * deep template
    
    * deep template
    
    * deep template
    format code

--- packages/material-template-react-deep/template/component/ice.config.js
+++ packages/material-template-react-deep/template/component/ice.config.js
@@ -7,6 +7,14 @@ module.exports = {
       nextLibDir: 'lib',
       style: false,
     }],
+    ['ice-plugin-modular-import', [
+      {
+        'libraryName': '@ali/deep',
+        'libraryDirectory': 'lib',
+        'camel2DashComponentName': true,
+        'style': false,
+      },
+    ]],
     'ice-plugin-component',
     ['ice-plugin-moment-locales', {
       locales: ['zh-cn'],
