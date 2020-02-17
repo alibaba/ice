5b5d046ba268bdd3866d2b1bd0dcc8e70dc5b492
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-02-17 20:15:29 +0800

    feat: the Provider support initialState properties

--- packages/plugin-store/src/module.tsx
+++ packages/plugin-store/src/module.tsx
@@ -3,12 +3,16 @@ import AppStore from '$ice/appModels'
 import PageStores from '$ice/pageModels'
 
 const wrapperComponent = (PageComponent) => {
+  let initialStates = {}
+  if (PageComponent.pageConfig && PageComponent.pageConfig.initialStates) {
+    initialStates = PageComponent.pageConfig.initialStates
+  }
   const StoreWrapperedComponent = () => {
     const pageComponentName = PageComponent.name
     const PageStore = PageStores[pageComponentName]
     if (PageStore) {
       return (
-        <PageStore.Provider>
+        <PageStore.Provider initialStates={initialStates}>
           <PageComponent />
         </PageStore.Provider>
       )
@@ -19,12 +23,17 @@ const wrapperComponent = (PageComponent) => {
 }
 
 
-export default ({ addProvider, wrapperRouteComponent }) => {
+export default ({ addProvider, wrapperRouteComponent, appConfig }) => {
   wrapperRouteComponent(wrapperComponent);
+
+  let initialStates = {}
+  if (appConfig.store && appConfig.store.initialStates) {
+    initialStates = appConfig.store.initialStates
+  }
 
   const StoreProvider = ({children}) => {
     return (
-      <AppStore.Provider>
+      <AppStore.Provider initialStates={initialStates}>
         {children}
       </AppStore.Provider>
     )
