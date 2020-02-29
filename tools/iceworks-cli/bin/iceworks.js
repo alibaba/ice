9f9b2feb5c8d4f13ec9a2414ca05f3ab6c41c4d6
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-02-29 19:26:30 +0800

    chore: add notice

--- tools/iceworks-cli/bin/iceworks.js
+++ tools/iceworks-cli/bin/iceworks.js
@@ -214,6 +214,10 @@ logCLIVersion();
 checkNodeVersion();
 
 if (!process.argv.slice(2).length) {
+  console.log();
+  console.log(chalk.cyan('If you want to start iceworks web page, please use `iceworks start`'));
+  console.log();
+
   program.help();
 }
 
