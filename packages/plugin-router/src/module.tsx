cb3bbb64d49bd8d3b4c30a41f40d35bffc067ac1
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2020-03-27 15:48:19 +0800

    fix: pass location params to StaticRouter (#176)
    * fix: pass location params
    
    * fix: context params
    
    * chore: code optimization
    
    * fix: pass staticRouter context

--- packages/plugin-router/src/module.tsx
+++ packages/plugin-router/src/module.tsx
@@ -4,27 +4,35 @@ import { Router } from './runtime/Router';
 import formatRoutes, { wrapperPage, wrapperPageWithSSR } from './runtime/formatRoutes';
 
 const module = ({ setRenderRouter, appConfig, modifyRoutes, wrapperRouteComponent, buildConfig, context }) => {
-  const { router = {} } = appConfig;
+  const { router: appConfigRouter = {} } = appConfig;
+
   // plugin-router 内置确保了 defaultRoutes 最先被添加
   modifyRoutes(() => {
-    return formatRoutes(router.routes || defaultRoutes, '');
+    return formatRoutes(appConfigRouter.routes || defaultRoutes, '');
   });
+
   const wrapperPageFn = process.env.__IS_SERVER__ ? wrapperPageWithSSR(context, defaultRoutes) : wrapperPage;
-  wrapperRouteComponent(wrapperPageFn)
-  if (router.modifyRoutes) {
-    modifyRoutes(router.modifyRoutes);
+  wrapperRouteComponent(wrapperPageFn);
+  if (appConfigRouter.modifyRoutes) {
+    modifyRoutes(appConfigRouter.modifyRoutes);
   }
 
-  const { router: buildConfigRouter } = buildConfig
+  const lazy = buildConfig.router && buildConfig.router.lazy;
   const renderRouter = (routes) => () => {
-    const routerProps = {
-      ...router,
+    let routerProps = {
+      ...appConfigRouter,
       routes,
-      lazy: buildConfigRouter && buildConfigRouter.lazy,
+      lazy
     };
+
+    if (process.env.__IS_SERVER__) {
+      const { pathname, staticContext = {} } = context;
+      routerProps = Object.assign({}, routerProps, { location: pathname, context: staticContext });
+    }
+
     return <Router {...routerProps} />;
-  }
+  };
   setRenderRouter(renderRouter);
-}
+};
 
 export default module;
