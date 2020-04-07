d57575d665ef1661661e8f0de06c062d63674e3f
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-04-07 17:43:19 +0800

    fix: move types to template

--- packages/plugin-store/src/index.ts
+++ packages/plugin-store/src/index.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as fse from 'fs-extra';
+import * as ejs from 'ejs';
 import Generator from './generator';
 
 export default async (api) => {
@@ -11,12 +12,20 @@ export default async (api) => {
   const appStoreTemplatePath = path.join(templatePath, 'appStore.ts.ejs');
   const pageStoreTemplatePath = path.join(templatePath, 'pageStore.ts.ejs');
   const pageStoresTemplatePath = path.join(templatePath, 'pageStores.ts.ejs');
+  const typesTemplatePath = path.join(templatePath, 'types.ts.ejs');
   const projectType = getValue('PROJECT_TYPE');
 
+  // copy types/index.ts to .ice/store/index.ts
   await fse.copy(path.join(__dirname, '..', 'src/types'), path.join(targetPath, './store'));
-  // TODO: 考虑合并为一个
   applyMethod('addIceTypesExport', { source: './store', specifier: '{ IStore }', exportName: 'store?: IStore' });
-  applyMethod('addIceTypesExport', { source: './store/icestore' });
+
+  // render template/types.ts.ejs to .ice/store/types.ts
+  const typesTemplateContent = fse.readFileSync(typesTemplatePath, 'utf-8');
+  const typesTargetPath = path.join(targetPath, 'store', 'types.ts');
+  const content = ejs.render(typesTemplateContent);
+  fse.ensureFileSync(typesTargetPath);
+  fse.writeFileSync(typesTargetPath, content, 'utf-8');
+  applyMethod('addIceTypesExport', { source: './store/types' });
 
   onGetWebpackConfig(config => {
     if (command === 'build') {
