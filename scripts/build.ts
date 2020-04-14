316f98b600235e20532ca221eb2e1ca72a70cfab
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2020-04-14 21:22:34 +0800

    chore: ci support dependency check (#238)
    * feat: support dependency check
    
    * feat: setup ci
    
    * fix: move dependency-check to devDependencies
    
    * chore: fix lint
    
    * fix: update yarn lock
    
    * chore: test ci
    
    * chore: add the missing dependencies
    
    * fix: use fs-extra@8.x

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,25 +1,28 @@
-import * as glob from 'glob'
-import * as path from 'path'
-import { run } from './fn/shell'
-import * as fs from 'fs-extra'
+import * as glob from 'glob';
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import { run } from './fn/shell';
 
 (async () => {
-  await run('npm run clean')
-  await run('npx tsc --build ./tsconfig.json')
+  await run('npm run clean');
+  await run('npx tsc --build ./tsconfig.json');
 
-  const fileParten = '*/src/**/!(*.ts|*.tsx)'
-  console.log(`[COPY]: ${fileParten}`)
+  const fileParten = '*/src/**/!(*.ts|*.tsx)';
+  console.log(`[COPY]: ${fileParten}`);
 
-  const cwd = path.join(__dirname, '../packages')
-  const files = glob.sync(fileParten, { cwd, nodir: true })
+  const cwd = path.join(__dirname, '../packages');
+  const files = glob.sync(fileParten, { cwd, nodir: true });
+  // eslint-disable-next-line
   for (const file of files) {
-    const from = path.join(cwd, file)
-    const to = path.join(cwd, file.replace(/\/src\//, '/lib/'))
-    await fs.mkdirp(path.dirname(to))
-    await fs.copyFile(from, to)
+    const from = path.join(cwd, file);
+    const to = path.join(cwd, file.replace(/\/src\//, '/lib/'));
+    // eslint-disable-next-line
+    await fs.mkdirp(path.dirname(to));
+    // eslint-disable-next-line
+    await fs.copyFile(from, to);
   }
 
 })().catch((e) => {
-  console.trace(e)
-  process.exit(128)
+  console.trace(e);
+  process.exit(128);
 });
--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,9 +1,9 @@
-import * as spawn from 'cross-spawn'
+import * as spawn from 'cross-spawn';
 import getPackages from './fn/getPackages';
 
 (async function () {
-  const { packageNames } = await getPackages()
-  const npmClient = 'tnpm'
-  console.log('syncing packages...')
-  spawn.sync(npmClient, ['sync', ...packageNames], { stdio: 'inherit' })
-})()
+  const { packageNames } = await getPackages();
+  const npmClient = 'tnpm';
+  console.log('syncing packages...');
+  spawn.sync(npmClient, ['sync', ...packageNames], { stdio: 'inherit' });
+})();
