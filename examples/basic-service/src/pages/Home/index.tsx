97f5aaedaba88d0a92ee896b89c0177eb86429ee
许文涛 <alvin.hui@qq.com>
2020-04-17 11:09:35 +0800

    feat: support service (#230)

---
+++ examples/basic-service/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Fetch from '@/components/Fetch';
+import User from './components/User';
+
+const Home = () => {
+  return (
+    <>
+      <Fetch />
+      <User />
+    </>
+  );
+};
+
+export default Home;
