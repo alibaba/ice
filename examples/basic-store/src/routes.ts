61ed17dcb3335ec50feeef0e86fdf42f077c36eb
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2020-04-20 10:18:25 +0800

    chore: update store example (#249)

--- examples/basic-store/src/routes.ts
+++ examples/basic-store/src/routes.ts
@@ -2,18 +2,30 @@ import { lazy } from 'ice';
 
 // import Home from '@/pages/Home';
 // import About from '@/pages/About';
+import Layout from '@/layouts/index';
 
 const Home = lazy(() => import('@/pages/Home'));
-const About =lazy (() => import('@/pages/About'));
+const About =lazy(() => import('@/pages/About'));
+const NotFound = lazy(() => import('@/pages/NotFound'));
 
 export default [
   {
     path: '/',
-    exact: true,
-    component: Home
-  },
-  {
-    path: '/about',
-    component: About
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        exact: true,
+        component: Home
+      },
+      {
+        path: '/about',
+        component: About
+      },
+      {
+        path: '*',
+        component: NotFound,
+      }
+    ]
   }
 ];
