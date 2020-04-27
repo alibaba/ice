a0d29ed55f4f8f025ff696099cdc9bb9f19db53b
chenbin92 <chenbinbin.cb@alibaba-inc.com>
2020-04-27 14:42:57 +0800

    Revert "feat: useRequest support fn (#268)" (#288)
    This reverts commit 62052efed4ff7f5d909ff877babd86083cca54d6 [formerly 1c2d80a1e805e8e19ebba10f110c43aae9917bf5].

--- examples/basic-service/src/components/Fetch/index.tsx
+++ examples/basic-service/src/components/Fetch/index.tsx
@@ -1,26 +1,16 @@
 import * as React from 'react';
-import { useRequest } from 'ice';
 import todoService from '@/services/todo';
 
 const Fetch = () => {
-  const { request, response, loading, error } = useRequest(todoService.getAll);
-
   async function handleRequest() {
     const data = await todoService.getAll();
     console.log('getAllResult', data);
   }
 
-  React.useEffect(() => {
-    request();
-  // eslint-disable-next-line
-  }, []);
-
-  console.log(response, loading, error);
-
   return (
     <div>
       <button type="button" onClick={handleRequest}>
-        获取任务列表
+        请求数据
       </button>
     </div>
   );
