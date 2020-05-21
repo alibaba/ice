d9308a9bb0b3ad43125f64fdb0b700f752503a42
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-05-21 17:51:17 +0800

    refactor: store getInitialStates deprecated

--- packages/plugin-store/src/module.tsx
+++ packages/plugin-store/src/module.tsx
@@ -5,9 +5,19 @@ export default ({ addProvider, appConfig, context }) => {
 
   const StoreProvider = ({children}) => {
     const storeConfig = appConfig.store || {};
-    const initialStates = storeConfig.getInitialStates
-      ? storeConfig.getInitialStates(context && context.initialData)
-      : storeConfig.initialStates || {};
+    const initialData = context ? context.initialData : {};
+
+    let initialStates = {}
+
+    if (initialData.initialStates) {
+      initialStates = initialData.initialStates;
+    } else if (storeConfig.getInitialStates) {
+      // @deprecated
+      console.warn('Detected that you are using store.getInitialStates, please use app.getInitialData method, Visit https://ice.work/docs/guide/basic/store.');
+      initialStates = storeConfig.getInitialStates(initialData)
+    } else if (storeConfig.initialStates) {
+      initialStates = storeConfig.initialStates;
+    }
 
     return (
       <AppStore.Provider initialStates={initialStates}>
