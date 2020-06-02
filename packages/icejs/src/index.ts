e03a71d9ad5a850a1ae417e71e40c3eada77bd54
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-06-02 19:15:07 +0800

    fix: optimize code

--- packages/icejs/src/index.ts
+++ packages/icejs/src/index.ts
@@ -1,27 +1,26 @@
 const getBuiltInPlugins = (userConfig) => {
-  // built-in plugins for icejs
-  const builtInPlugins = userConfig.disableRuntime
-    ? ['build-plugin-react-app']
-    : ['build-plugin-ice-core',
-      'build-plugin-react-app',
-      'build-plugin-ice-router',
-      'build-plugin-ice-helpers',
-      'build-plugin-ice-logger',
-      'build-plugin-ice-config',
-      'build-plugin-ice-request'];
-
-  if (userConfig.mpa) {
-    builtInPlugins.push('build-plugin-ice-mpa');
+  if (userConfig.disableRuntime) {
+    return ['build-plugin-react-app'];
   }
+
+  // built-in plugins for icejs
+  const builtInPlugins = [
+    'build-plugin-ice-core',
+    'build-plugin-react-app',
+    'build-plugin-ice-router',
+    'build-plugin-ice-helpers',
+    'build-plugin-ice-logger',
+    'build-plugin-ice-config',
+    'build-plugin-ice-request',
+    'build-plugin-ice-mpa'
+  ];
 
   if (userConfig.ssr) {
     builtInPlugins.push('build-plugin-ice-ssr');
   }
 
-  if (!userConfig.disableRuntime) {
-    if (!Object.prototype.hasOwnProperty.call(userConfig, 'store') || userConfig.store !== false) {
-      builtInPlugins.push('build-plugin-ice-store');
-    }
+  if (!Object.prototype.hasOwnProperty.call(userConfig, 'store') || userConfig.store !== false) {
+    builtInPlugins.push('build-plugin-ice-store');
   }
 
   return builtInPlugins;
