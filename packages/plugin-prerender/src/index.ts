52968ec51eeb5cd5cc870cf141d13efbfaf76a0e
luhc228 <luhengchang228@126.com>
2020-06-09 10:43:52 +0800

    feat: support minify and renderer

--- packages/plugin-prerender/src/index.ts
+++ packages/plugin-prerender/src/index.ts
@@ -2,33 +2,24 @@ import { IPlugin } from '@alib/build-scripts';
 import * as path from 'path';
 import * as  PrerenderSPAPlugin from 'prerender-spa-plugin';
 
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
 const plugin: IPlugin = ({ context, onGetWebpackConfig }, options) => {
   const { rootDir, userConfig } = context;
 
+  // const isMpa = userConfig.mpa as boolean;
   const outputDir = (userConfig.outputDir || 'build') as string;
-  let { routes }: any = options;
-
-  routes = routes || ['/'];
+  const { routes = ['/'], minify = {}, renderer = {} }: any = options;
 
   onGetWebpackConfig((config) => {
     config.optimization
       .minimizer('prerenderSPAPlugin')
       .use(PrerenderSPAPlugin, [{
         staticDir: path.join(rootDir, outputDir),
-        routes,  // Required - Routes to render.
-        // outputDir: path.join(__dirname, 'prerendered'),
-        // indexPath: path.join(__dirname, 'dist', 'index.html'),
-        // minify: {
-        //   collapseBooleanAttributes: true,
-        //   collapseWhitespace: true,
-        //   decodeEntities: true,
-        //   keepClosingSlash: true,
-        //   sortAttributes: true
-        // },
-        // server: {
-        //   port: 8001
-        // },
-        // postProcess,
+        routes,
+        indexPath: path.join(rootDir, outputDir, 'index.html'),
+        minify,
+        renderer: new Renderer(renderer)
       }]);
   });
 };
