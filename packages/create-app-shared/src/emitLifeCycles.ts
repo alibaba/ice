b0b4052c423e87c662f3acc4b2aafae85a1c0d8b
狒狒神 <350740249@qq.com>
2020-08-04 16:29:51 +0800

    chore: miniapp-renderer support miniapp runtime single package mode (#3382)

--- packages/create-app-shared/src/emitLifeCycles.ts
+++ packages/create-app-shared/src/emitLifeCycles.ts
@@ -3,11 +3,9 @@ import router from './router';
 import { LAUNCH, SHOW, HIDE } from './constants';
 import { emit as appEmit } from './appLifeCycles';
 import { emit as pageEmit } from './pageLifeCycles';
+import { isMiniAppPlatform } from './env';
 
 function emitLifeCycles() {
-  appEmit(LAUNCH);
-  appEmit(SHOW);
-
   // Get history
   const history = getHistory();
   const pathname = history.location.pathname;
@@ -18,20 +16,26 @@ function emitLifeCycles() {
     visibiltyState: true
   };
 
-  // Listen history change
-  history.listen((location) => {
-    if (location.pathname !== router.current.pathname) {
+  if (!isMiniAppPlatform) {
+    // Emit app lifecycle
+    appEmit(LAUNCH);
+    appEmit(SHOW);
+
+    // Listen history change
+    history.listen((location) => {
+      if (location.pathname !== router.current.pathname) {
       // Flow router info
-      router.prev = router.current;
-      router.current = {
-        pathname: location.pathname,
-        visibiltyState: true
-      };
-      router.prev.visibiltyState = false;
-      pageEmit(HIDE, router.prev.pathname);
-      pageEmit(SHOW, router.current.pathname);
-    }
-  });
+        router.prev = router.current;
+        router.current = {
+          pathname: location.pathname,
+          visibiltyState: true
+        };
+        router.prev.visibiltyState = false;
+        pageEmit(HIDE, router.prev.pathname);
+        pageEmit(SHOW, router.current.pathname);
+      }
+    });
+  }
 }
 
 export default emitLifeCycles;
