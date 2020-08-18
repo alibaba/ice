8bc86aaf787578c36a6099e8638576896312a6f0
fushen <fushen.jzw@alibaba-inc.com>
2020-08-18 15:09:06 +0800

    chore: bump version

--- packages/plugin-app-core/src/runtime.tsx
+++ packages/plugin-app-core/src/runtime.tsx
@@ -1,25 +1,12 @@
 import * as queryString from 'query-string';
-// @ts-ignore
-import { ErrorBoundary } from 'ice';
 
 const module = ({ addProvider, appConfig, wrapperRouteComponent, context: { createElement } }) => {
   const { app = {} } = appConfig;
-  const { ErrorBoundaryFallback, onErrorBoundaryHander, parseSearchParams } = app;
+  const { parseSearchParams } = app;
 
   const wrapperPageComponent = (PageComponent) => {
-    const { pageConfig = {} } = PageComponent;
     const WrapperedPageComponent = (props) => {
       const searchParams = getSearchParams(parseSearchParams, props.location.search);
-      if (pageConfig.errorBoundary) {
-        return (
-          createElement(ErrorBoundary, {
-            Fallback: ErrorBoundaryFallback,
-            onError: onErrorBoundaryHander
-          }, createElement(PageComponent, {
-            ... Object.assign({}, props, searchParams)
-          }))
-        );
-      }
       return createElement(PageComponent, {... Object.assign({}, props, searchParams)});
     };
     return WrapperedPageComponent;
