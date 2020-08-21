2d9c5323879ef20ba4f4f735d4f4845fd7f482d4
fushen <fushen.jzw@alibaba-inc.com>
2020-08-21 23:59:49 +0800

    feat: support custom babel rule name

--- examples/with-react-miniapp/src/pages/Home/index.tsx
+++ examples/with-react-miniapp/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { usePageShow, usePageHide } from 'ice';
+import Title from 'mini-ali-ui/es/title/index';
 import Text from './components/Text';
 import styles from './index.module.scss';
 
@@ -22,6 +23,13 @@ const Home = (props) => {
         console.log('Click');
         history.push('/about');
       }}>go about</div>
+      <Title
+        hasLine
+        type="arrow"
+        onActionTap={() => {
+          console.log('标题被击中了');
+        }}
+      >我是 mini-ali-ui 的 title 组件</Title>
     </div>
   );
 };
