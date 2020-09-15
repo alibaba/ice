2e106554d3e70b018275f5f5276d30cf53337724
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-09-15 18:15:34 +0800

    fix: catch debug webpackConfig error

--- packages/plugin-react-app/src/index.js
+++ packages/plugin-react-app/src/index.js
@@ -60,7 +60,7 @@ module.exports = (api) => {
 
   if (command === 'test') {
     onHook('before.test.run', ({ config }) => {
-      debug(JSON.stringify(config, null, 2));
+      logWebpackConfig(config);
     });
 
     onGetJestConfig((jestConfig) => {
@@ -88,7 +88,7 @@ module.exports = (api) => {
 
   if (command === 'start') {
     onHook('before.start.run', ({ config }) => {
-      debug(JSON.stringify(config, null, 2));
+      logWebpackConfig(config);
     });
 
     onHook('after.start.compile', ({ urls, stats }) => {
@@ -146,7 +146,7 @@ module.exports = (api) => {
 
   if (command === 'build') {
     onHook('before.build.run', ({ config }) => {
-      debug(JSON.stringify(config, null, 2));
+      logWebpackConfig(config);
     });
 
     targets.forEach((target) => {
@@ -168,4 +168,12 @@ module.exports = (api) => {
 
 function getLocalUrl(url, entryHtml) {
   return entryHtml ? `${url}${entryHtml}` : url;
+}
+
+function logWebpackConfig(config) {
+  try {
+    debug(JSON.stringify(config, null, 2));
+  } catch (error) {
+    // ignore error
+  }
 }
