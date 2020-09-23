7b8f2da4ba809e17269e113e8e8c68bdc5344f39
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-09-23 17:29:56 +0800

    chore: eslint

--- packages/plugin-faas/src/index.js
+++ packages/plugin-faas/src/index.js
@@ -11,13 +11,14 @@ module.exports = async ({ context, onGetWebpackConfig, onHook, log }) => {
 
       config.merge({ devServer: {
         before(app, server) {
+          // eslint-disable-next-line react-hooks/rules-of-hooks
           app.use(useExpressDevPack({
             functionDir: rootDir,
             sourceDir: path.resolve(rootDir, 'src/apis'),
             ignoreWildcardFunctions: ['render'],
             // 忽略静态文件地址
             ignorePattern: (req) => {
-              const { pathname } = URL.parse(req.url)
+              const { pathname } = URL.parse(req.url);
               return /\.(js|css|map|json|png|jpg|jpeg|gif|svg|eot|woff2|ttf)$/.test(pathname);
             }
           }));
