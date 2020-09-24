19b9ba8b2baabe94db8706b5c1a5211c7a19a4c2
luhc228 <luhengchang228@gmail.com>
2020-09-24 22:55:17 +0800

    fix: store is undefined in class component

---
+++ examples/basic-store/src/pages/Detail/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { store } from 'ice/Detail';
+
+console.log(store);
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+
+  public render() {
+    const { default: detailStore } = this.props as any;
+    const [state, dispatcher] = detailStore;
+    return (
+      <>
+        <div>Detail Page</div>
+        <div>Title: {state.title}</div>
+        <button onClick={() => dispatcher.updateDetailTitle('test')} type="button">Change Title</button>
+      </>
+    );
+  }
+}
+
+export default store.withModel('default')(Detail);
