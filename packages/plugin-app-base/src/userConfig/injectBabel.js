7709ea81f226324bd47b414a85d8e99aa4b767e6
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-09-27 10:09:23 +0800

    chore: reset code

--- packages/plugin-app-base/src/userConfig/injectBabel.js
+++ packages/plugin-app-base/src/userConfig/injectBabel.js
@@ -33,29 +33,29 @@ module.exports = (config, injectBabel, context) => {
         });
     });
   } else if (injectBabel === 'polyfill') {
-    // const entries = config.toConfig().entry;
-    // const rule = config.module.rule('polyfill').test(/\.jsx?|\.tsx?$/);
-    // const fileList = [];
-    // console.log('entries:', entries);
-    // Object.keys(entries).forEach((key) => {
-    //   console.log(key);
-    //   let addPolyfill = false;
-    //   // only include entry path
-    //   for (let i = 0; i < entries[key].length; i += 1) {
-    //     // filter node_modules file add by plugin
-    //     if (!/node_modules/.test(entries[key][i])) {
-    //       rule.include.add(entries[key][i]);
-    //       fileList.push(entries[key][i]);
-    //       addPolyfill = true;
-    //       break;
-    //     }
-    //   }
-    //   if (!addPolyfill) {
-    //     rule.include.add(entries[key][0]);
-    //     fileList.push(entries[key][0]);
-    //   }
-    // });
-    // rule.use('polyfill-loader').loader(require.resolve('../utils/polyfillLoader')).options({});
-    // addBablePlugins(config, [[require.resolve('../utils/babelPluginCorejsLock.js'), { fileList }]]);
+    const entries = config.toConfig().entry;
+    const rule = config.module.rule('polyfill').test(/\.jsx?|\.tsx?$/);
+    const fileList = [];
+    console.log('entries:', entries);
+    Object.keys(entries).forEach((key) => {
+      console.log(key);
+      let addPolyfill = false;
+      // only include entry path
+      for (let i = 0; i < entries[key].length; i += 1) {
+        // filter node_modules file add by plugin
+        if (!/node_modules/.test(entries[key][i])) {
+          rule.include.add(entries[key][i]);
+          fileList.push(entries[key][i]);
+          addPolyfill = true;
+          break;
+        }
+      }
+      if (!addPolyfill) {
+        rule.include.add(entries[key][0]);
+        fileList.push(entries[key][0]);
+      }
+    });
+    rule.use('polyfill-loader').loader(require.resolve('../utils/polyfillLoader')).options({});
+    addBablePlugins(config, [[require.resolve('../utils/babelPluginCorejsLock.js'), { fileList }]]);
   }
 };
