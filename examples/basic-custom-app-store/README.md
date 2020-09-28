abda1a0a8048001ff8032be97e05564c6a6810f4
luhc228 <luhengchang228@gmail.com>
2020-09-28 15:54:50 +0800

    feat: add basic custom app store example

---
+++ examples/basic-custom-app-store/README.md
@@ -0,0 +1,3 @@
+# basic custom appStore with icejs
+
+https://github.com/ice-lab/icejs/tree/master/examples
---
+++ examples/basic-custom-app-store/src/typings.d.ts
@@ -0,0 +1,5 @@
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
