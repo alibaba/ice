ab88e96e9133a15262a696203fa8720bae268405
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-11-05 12:30:43 +0800

    chore: optimize code

--- packages/build-webpack-config/src/config/validation.js
+++ packages/build-webpack-config/src/config/validation.js
@@ -1,20 +1,4 @@
-const assert = require('assert');
-const { isPlainObject } = require('lodash');
-
-const validation = (key, value, types) => {
-  const validateResult = types.split('|').some((type) => {
-    if (type === 'array') {
-      return Array.isArray(value);
-    } else if (type === 'object') {
-      return isPlainObject(value);
-    } else {
-      // eslint-disable-next-line valid-typeof
-      return typeof value === type;
-    }
-  });
-  assert(validateResult, `Config ${key} should be ${types.replace('|', ' | ')}, but got ${value}`);
-  return validateResult;
-};
+const { validation } = require('build-app-helpers');
 
 module.exports = {
   alias: 'object',
