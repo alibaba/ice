ef95aae8c6b1f0b8da0aaf1c8f810352c6f0b53c
ClarkXia <xiawenwu41@gmail.com>
2020-11-11 18:02:15 +0800

    feat: support uniqueName of plugin-icestark

--- packages/plugin-icestark/src/index.ts
+++ packages/plugin-icestark/src/index.ts
@@ -1,11 +1,14 @@
 import * as path from 'path';
 import * as glob from 'glob';
 import * as fse from 'fs-extra';
-import { IPlugin } from '@alib/build-scripts';
+import { IPlugin, Json } from '@alib/build-scripts';
 
-const plugin: IPlugin = async ({ onGetWebpackConfig, getValue, applyMethod, context }) => {
-  const { rootDir } = context;
+const plugin: IPlugin = async ({ onGetWebpackConfig, getValue, applyMethod, context }, options) => {
+  const { uniqueName } = options as Json;
+  const { rootDir, webpack } = context;
   const iceTempPath = getValue('TEMP_PATH');
+  // remove output.jsonpFunction in webpack5 see: https://webpack.js.org/blog/2020-10-10-webpack-5-release/#automatic-unique-naming
+  const isWebpack5 = (webpack as any).version?.startsWith('5');
 
   const hasDefaultLayout = glob.sync(`${path.join(rootDir, 'src/layouts/index')}.@(ts?(x)|js?(x))`).length;
   onGetWebpackConfig((config) => {
@@ -15,6 +18,9 @@ const plugin: IPlugin = async ({ onGetWebpackConfig, getValue, applyMethod, cont
     ['@ice/stark', '@ice/stark-app'].forEach((pkgName) => {
       config.resolve.alias.set(pkgName, require.resolve(pkgName));
     });
+    if (!isWebpack5 && uniqueName) {
+      config.output.jsonpFunction(`webpackJsonp_${uniqueName}`);
+    }
   });
 
   await fse.copy(path.join(__dirname, '..', 'src/types/index.ts'), path.join(iceTempPath, 'types/icestark.ts'));
