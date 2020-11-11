b24e728bacb2bab29df79838530dabcdaca00a87
ClarkXia <xiawenwu41@gmail.com>
2020-11-11 19:57:28 +0800

    feat: support wrappers of Layout

--- packages/plugin-router/src/runtime/Router.tsx
+++ packages/plugin-router/src/runtime/Router.tsx
@@ -39,13 +39,17 @@ function getRouteComponent(component, routerWrappers?: IRouteWrapper[]) {
 function parseRoutes(routes: RouteItemProps[]) {
   return routes.map((route) => {
     const { children, component, routeWrappers, wrappers, ...others } = route;
-    let mergedRouteWrappers = routeWrappers;
+    let mergedRouteWrappers = [];
+    if (children) {
+      // do not add wrappers added by runtime api wrapperRouteComponent
+      mergedRouteWrappers = routeWrappers;
+    }
     if (wrappers && wrappers.length) {
-      mergedRouteWrappers = routeWrappers.concat(route.wrappers);
+      mergedRouteWrappers = mergedRouteWrappers.concat(wrappers);
     }
     const parsedRoute: IRouterConfig = { ...others };
     if (component) {
-      parsedRoute.component = getRouteComponent(component, children ? [] : mergedRouteWrappers);
+      parsedRoute.component = getRouteComponent(component, mergedRouteWrappers);
     }
     if (children) {
       parsedRoute.children = parseRoutes(children);
