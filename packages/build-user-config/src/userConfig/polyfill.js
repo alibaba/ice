6b6ecaca51c098a251fe13ed1b38fdbc8ad080fa
fushen <fushen.jzw@alibaba-inc.com>
2020-11-12 14:03:38 +0800

    fix: polyfill-loader

--- packages/build-user-config/src/userConfig/polyfill.js
+++ packages/build-user-config/src/userConfig/polyfill.js
@@ -1,5 +1,6 @@
 const { injectTransformRuntime } = require('@builder/app-helpers');
 const processPresetEnv  = require('../utils/processPresetEnv');
+const addBablePlugins = require('../utils/addBabelPlugins');
 
 module.exports = (config, polyfill) => {
   const presetEnvParam = {
@@ -9,6 +10,30 @@ module.exports = (config, polyfill) => {
     presetEnvParam.corejs = 3;
     if (polyfill === 'usage') {
       injectTransformRuntime(config);
+    } else if (polyfill === 'entry') {
+      const entries = config.toConfig().entry;
+      const rule = config.module.rule('polyfill').test(/\.jsx?|\.tsx?$/);
+      const fileList = [];
+      if (!entries) return;
+      Object.keys(entries).forEach((key) => {
+        let addPolyfill = false;
+        // only include entry path
+        for (let i = 0; i < entries[key].length; i += 1) {
+          // filter node_modules file add by plugin
+          if (!/node_modules/.test(entries[key][i])) {
+            rule.include.add(entries[key][i]);
+            fileList.push(entries[key][i]);
+            addPolyfill = true;
+            break;
+          }
+        }
+        if (!addPolyfill) {
+          rule.include.add(entries[key][0]);
+          fileList.push(entries[key][0]);
+        }
+      });
+      rule.use('polyfill-loader').loader(require.resolve('../utils/polyfillLoader')).options({});
+      addBablePlugins(config, [[require.resolve('../utils/babelPluginCorejsLock.js'), { fileList }]]);
     }
   }
   processPresetEnv(config, presetEnvParam);
--- packages/plugin-react-app/src/config.js
+++ packages/plugin-react-app/src/config.js
@@ -1,39 +1,44 @@
 const { validation } = require('@builder/app-helpers');
 
 /* eslint global-require: 0 */
-module.exports = [
-  {
-    name: 'entry',
-    defaultValue: 'src/index.jsx',
-    configWebpack: require('./userConfig/entry'),
-    validation: (val) => {
-      // entry: string | array
-      // entry : { [name]: string | array }
-      return validation('entry', val, 'string|array|object');
-    },
-  },
-  {
-    name: 'ignoreHtmlTemplate',
-    defauleValue: false,
-    configWebpack: require('./userConfig/ignoreHtmlTemplate'),
-    validation: 'boolean',
-  },
-  {
-    name: 'outputDir',
-    defaultValue: 'build',
-    configWebpack: require('./userConfig/outputDir'),
-    validation: 'string'
-  },
-  {
-    name: 'injectBabel',
-    defaultValue: 'polyfill',
-    configWebpack: require('./userConfig/injectBabel'),
-    validation: (val) => {
-      return validation('injectBabel', val, 'string|boolean');
-    }
-  },
-  {
-    name: 'polyfill',
-    defaultValue: 'entry'
+module.exports = function(userConfig) {
+  let polyfillDefaultValue = 'entry';
+  if (Object.prototype.hasOwnProperty.call(userConfig, 'injectBabel') && userConfig.injectBabel === false) {
+    polyfillDefaultValue = false;
   }
-];
+  return [
+    {
+      name: 'entry',
+      defaultValue: 'src/index.jsx',
+      configWebpack: require('./userConfig/entry'),
+      validation: (val) => {
+        // entry: string | array
+        // entry : { [name]: string | array }
+        return validation('entry', val, 'string|array|object');
+      },
+    },
+    {
+      name: 'ignoreHtmlTemplate',
+      defauleValue: false,
+      configWebpack: require('./userConfig/ignoreHtmlTemplate'),
+      validation: 'boolean',
+    },
+    {
+      name: 'outputDir',
+      defaultValue: 'build',
+      configWebpack: require('./userConfig/outputDir'),
+      validation: 'string'
+    },
+    {
+      name: 'injectBabel',
+      defaultValue: 'polyfill',
+      validation: (val) => {
+        return validation('injectBabel', val, 'string|boolean');
+      }
+    },
+    {
+      name: 'polyfill',
+      defaultValue: polyfillDefaultValue
+    }
+  ];
+};
