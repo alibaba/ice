e144b57b37c7d187f8192d5afd7c8d11583a1045
ClarkXia <xiawenwu41@gmail.com>
2020-12-01 15:06:13 +0800

    feat: support source code of runtime

--- packages/plugin-app-core/src/utils/getRuntimeModules.ts
+++ packages/plugin-app-core/src/utils/getRuntimeModules.ts
@@ -1,37 +1,60 @@
 import * as path from 'path';
 import * as fse from 'fs-extra';
+import * as globby from 'globby';
 import formatPath from './formatPath';
 
 export default (plugins: any = [], targetDir: string) => {
-  console.log('pluginInfo', plugins);
   return plugins.map(({ pluginPath, name }) => {
     // compatible with function plugin
     if (!pluginPath) return false;
     // NOTE: module.js will be discarded in future.
-    let modulePath = path.join(path.dirname(pluginPath), 'runtime.js');
-    const moduleDir = path.join(path.dirname(pluginPath), '..');
+    const pluginDir = path.dirname(pluginPath);
+    let modulePath = path.join(pluginDir, 'runtime.js');
+    const moduleDir = path.join(pluginDir, '..');
     if(!fse.existsSync(modulePath)){
-      modulePath = path.join(path.dirname(pluginPath), 'module.js');
+      modulePath = path.join(pluginDir, 'module.js');
       if(!fse.existsSync(modulePath)){
         return false;
       }
       console.log(`WARN: module.ts(x) will not be supported in the future. Please rename as runtime.ts(x) in ${modulePath}`);
     } else if (name){
       // copy module dir to target dir
-      const pluginDir = path.join(targetDir, 'plugins', name);
-      fse.ensureDirSync(pluginDir);
-      fse.copySync(moduleDir, pluginDir);
+      const tempDir = path.join(targetDir, 'plugins', name);
+      fse.ensureDirSync(tempDir);
+      // ensure source dir
+      const srcDir = path.join(moduleDir, 'src');
+      if (fse.existsSync(srcDir)) {
+        fse.copySync(srcDir, tempDir);
+        const runtimePaths = globby.sync('runtime.@((t|j)s?(x))', { cwd: tempDir });
+        console.log(runtimePaths);
+        if (runtimePaths.length > 0) {
+          modulePath = path.join(tempDir, runtimePaths[0]);
+        }
+      } else {
+        fse.copySync(pluginDir, tempDir);
+        modulePath = path.join(tempDir, 'runtime.js');
+      }
     }
-    return formatPath(modulePath);
+    // read package.json
+    let pluginConfig = {};
+    const pkgPath = path.join(moduleDir, 'package.json');
+    try {
+      pluginConfig = fse.readJSONSync(pkgPath).pluginConfig;
+    } catch(error) {
+      console.log(`ERROR: fail to load package.json of plugin ${name}`);
+    }
+    console.log(pluginConfig, modulePath);
+    return {
+      pluginConfig,
+      modulePath: formatPath(modulePath),
+    };
   })
     .filter(Boolean)
-    .map(pluginPath => {
-      const pkgPath = path.join(pluginPath, '../../package.json');
-      const { pluginConfig } = fse.readJSONSync(pkgPath);
+    .map(({ modulePath, pluginConfig }) => {
       const staticModule = (pluginConfig && pluginConfig.staticModule) || false;
       return {
         staticModule,
-        path: pluginPath
+        path: modulePath
       };
     });
 };
