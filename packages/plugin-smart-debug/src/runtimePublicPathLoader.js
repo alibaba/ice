dc0ba3fd52d511cf22bc93a20d74e9a93774d374
luhc228 <luhengchang228@gmail.com>
2020-12-02 11:06:07 +0800

    fix: use debug in ssr

--- packages/plugin-smart-debug/src/runtimePublicPathLoader.js
+++ packages/plugin-smart-debug/src/runtimePublicPathLoader.js
@@ -1,6 +1,6 @@
 module.exports = (content) => {
   return `
-    if (window.ICE && window.ICE.debug) {
+    if (!process.env.__IS_SERVER__ && window.ICE && window.ICE.debug) {
       var debug = window.ICE.debug;
       __webpack_public_path__ = '//' + debug.origin + ':' + debug.port + (debug.output ? ('/' + debug.output + '/') : '/');
     }
