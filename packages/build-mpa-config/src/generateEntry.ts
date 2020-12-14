ffa977c18ec3a41e8fe4c04666163b8def266978
fushen <fushen.jzw@alibaba-inc.com>
2020-12-14 13:48:42 +0800

    feat: define hydrate value when build

--- packages/build-mpa-config/src/generateEntry.ts
+++ packages/build-mpa-config/src/generateEntry.ts
@@ -2,10 +2,15 @@ import * as path from 'path';
 import { formatPath } from '@builder/app-helpers';
 
 function generateEntry(api, { framework, targetDir, pageEntry, entryName }) {
+  const { context: { userConfig: { web: webConfig } } } = api;
   const entryFolder = path.join(targetDir, 'mpaEntry');
   const entryPath = path.join(entryFolder, `${entryName}.tsx`);
   const templatePath = path.join(__dirname, `./template/${framework}.ts.ejs`);
-  api.applyMethod('addRenderFile', templatePath, entryPath, { resourcePath: `${formatPath(pageEntry.split('.').slice(0, -1).join('.'))}`});
+  api.applyMethod('addRenderFile', templatePath, entryPath, {
+    hydrate: webConfig.ssr || webConfig.snapshot,
+    resourcePath: `${formatPath(path.extname(pageEntry) ? pageEntry.split('.').slice(0, -1).join('.') : pageEntry)}`
+  },
+  );
   return entryPath;
 }
 
