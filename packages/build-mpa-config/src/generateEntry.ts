9f4b3a9eb8f4e9e263d7d53c0eed6eea19dae530
ClarkXia <xiawenwu41@gmail.com>
2020-12-14 10:48:47 +0800

    refactor: support generator api for plugins (#3899)

--- packages/build-mpa-config/src/generateEntry.ts
+++ packages/build-mpa-config/src/generateEntry.ts
@@ -1,15 +1,11 @@
 import * as path from 'path';
 import { formatPath } from '@builder/app-helpers';
-import { ensureDirSync, writeFileSync } from 'fs-extra';
 
-function generateEntry({ framework, type, targetDir, pageEntry, entryName }) {
-  // eslint-disable-next-line
-  const entryCode = require(`./template/${framework}`).default({ type, resourcePath: `${formatPath(pageEntry)}`});
+function generateEntry(api, { framework, targetDir, pageEntry, entryName }) {
   const entryFolder = path.join(targetDir, 'mpaEntry');
   const entryPath = path.join(entryFolder, `${entryName}.tsx`);
-  const entryDir = path.dirname(entryPath);
-  ensureDirSync(entryDir);
-  writeFileSync(path.join(entryFolder, `${entryName}.tsx`), entryCode);
+  const templatePath = path.join(__dirname, `./template/${framework}.ts.ejs`);
+  api.applyMethod('addRenderFile', templatePath, entryPath, { resourcePath: `${formatPath(pageEntry.split('.').slice(0, -1).join('.'))}`});
   return entryPath;
 }
 
