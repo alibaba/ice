5bc4a9116b437786fc4a3345322dda948ed8f037
ClarkXia <xiawenwu41@gmail.com>
2021-01-13 21:08:22 +0800

    chore: migrate packages (#4016)
    * chore: migrate packages
    
    * fix: dependencies

---
+++ packages/webpack-plugin-extract-css-assets/test/cases/absolute-path/webpack.config.js
@@ -0,0 +1,39 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractCssAssetsPlugin = require('../../../src/cjs');
+
+module.exports = {
+  entry: './index',
+  output: {
+    publicPath: '/build/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|eot)(.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'assets/[hash].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFileName: 'css/[id].css',
+    }),
+    new ExtractCssAssetsPlugin({
+      outputPath: 'cssassets/',
+      relativeCssPath: '../'
+    }),
+  ],
+};
