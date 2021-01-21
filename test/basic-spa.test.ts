d35db0c3f259a545cb940f38e80916e81786b068
ClarkXia <xiawenwu41@gmail.com>
2021-01-21 14:40:23 +0800

    feat: dev test

--- test/basic-spa.test.ts
+++ test/basic-spa.test.ts
@@ -1,28 +1,49 @@
 import { buildFixture, setupBrowser } from './utils/build';
+import { startFixture, setupStartBrowser } from './utils/start';
 import { IPage } from './utils/browser';
 
 const example = 'basic-spa';
-let page: IPage = null;
-let browser = null;
 
-buildFixture(example);
+describe(`build ${example}`, () => {
+  let page: IPage = null;
+  let browser = null;
 
-test('open /', async () => {
-  const res = await setupBrowser({ example });
-  page = res.page;
-  browser = res.browser;
-  expect(await page.$$text('h2')).toStrictEqual(['SPA', 'Home Page...1']);
+  buildFixture(example);
+
+  test('open /', async () => {
+    const res = await setupBrowser({ example });
+    page = res.page;
+    browser = res.browser;
+    expect(await page.$$text('h2')).toStrictEqual(['SPA', 'Home Page...1']);
+  });
+
+  test('open /dashboard', async () => {
+    await page.push('#/dashboard');
+    // lazy load
+    expect(await page.html()).toContain('加载中...');
+    // wait for render
+    await page.waitForFunction(`document.getElementsByTagName('h2').length > 1`);
+    expect(await page.$$text('h2')).toStrictEqual(['SPA', 'Dashboard Page...']);
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
 });
 
-test('open /dashboard', async () => {
-  await page.push('#/dashboard');
-  // lazy load
-  expect(await page.html()).toContain('加载中...');
-  // wait for render
-  await page.waitForFunction(`document.getElementsByTagName('h2').length > 1`);
-  expect(await page.$$text('h2')).toStrictEqual(['SPA', 'Dashboard Page...']);
-});
+describe(`start ${example}`, () => {
+  let page: IPage = null;
+  let browser = null;
 
-afterAll(async () => {
-  await browser.close();
+  test('setup devServer', async () => {
+    const { devServer, port } = await startFixture(example);
+    const res = await setupStartBrowser({ server: devServer, port });
+    page = res.page;
+    browser = res.browser;
+    expect(await page.$$text('h2')).toStrictEqual(['SPA', 'Home Page...1']);
+  }, 120000);
+
+  afterAll(async () => {
+    await browser.close();
+  });
 });
