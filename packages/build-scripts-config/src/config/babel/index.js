eaf6169fa3d7e56c582445662e79a5add411671c
ClarkXia <xiawenwu41@gmail.com>
2021-02-02 14:52:52 +0800

    feat: react-jsx transform

--- packages/build-scripts-config/src/config/babel/index.js
+++ packages/build-scripts-config/src/config/babel/index.js
@@ -1,8 +1,23 @@
 const getBabelPreset = require('@builder/babel-preset-ice').default;
 
 module.exports = () => {
+  const hasJsxRuntime = (() => {
+    if (process.env.DISABLE_JSX_TRANSFORM === 'true') {
+      return false;
+    }
+    try {
+      require.resolve('react/jsx-runtime');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  })();
+
   return getBabelPreset({
-    react: true,
+    react: {
+      development: process.env.NODE_ENV === 'development',
+      runtime: hasJsxRuntime ? 'automatic' : 'classic',
+    },
     typescript: true,
     env: {
       modules: false,
