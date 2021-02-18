652fe5a6122b4216d095d5dd94f68533a5f78e5e
fushen <fushen.jzw@alibaba-inc.com>
2021-02-18 14:45:28 +0800

    chore: optimize dynamic import chunk name

--- packages/build-user-config/src/utils/getRouteName.js
+++ packages/build-user-config/src/utils/getRouteName.js
@@ -1,4 +1,5 @@
 module.exports = ({ name, source }) => {
   if (name) return name;
-  return source.match(/pages\/([^/]*)/)[1].toLocaleLowerCase();
+  // pages/Home/index => pages_Home_index
+  return source.replace(/\//g, '_');
 };
