c59a1531faaae89285359b858a273d5d08f13099
zh-D <1641245614@qq.com>
2021-03-03 13:26:39 +0800

    feat:add "Provider" for pages and distinguish them

--- packages/plugin-hooks-store/src/index.ts
+++ packages/plugin-hooks-store/src/index.ts
@@ -1,8 +1,26 @@
 import { IPlugin } from '@alib/build-scripts';
 import * as path from 'path';
 import Generator from './generator';
+import * as fse from 'fs-extra';
+import { getAppHooksStorePath } from './utils/getPath';
 
-const plugin: IPlugin = ({ applyMethod, getValue, context, onHook }) => {
+const plugin: IPlugin = ({ applyMethod, getValue, context, onHook, onGetWebpackConfig, modifyUserConfig }) => {
+
+  const { rootDir, userConfig } = context;
+  const { mpa: isMpa, entry } = userConfig;
+  const templatePath = path.join(__dirname, 'template');
+  const appHooksStoreTemplatePath = path.join(templatePath, 'appHooksStore.ts.ejs');
+  const pageHooksStoreTemplatePath = path.join(templatePath, 'pageHooksStore.ts.ejs');
+  const targetPath = getValue('TEMP_PATH');
+  const tempDir = (path.basename(targetPath) || '').split('.')[1];
+  const projectType = getValue('PROJECT_TYPE');
+
+  const srcDir = isMpa ? 'src' : applyMethod('getSourceDir', entry);
+  const pages = applyMethod('getPages', rootDir, srcDir);
+
+  const appHooksStoreFile = applyMethod('formatPath', getAppHooksStorePath({ rootDir, srcDir, projectType }));
+  const existsAppHooksStoreFile = fse.pathExistsSync(appHooksStoreFile);
+
   applyMethod('addExport', {
     source: '@ice/hooks-store',
     specifier: '{ createStore as createHooksStore }',
@@ -11,14 +29,53 @@ const plugin: IPlugin = ({ applyMethod, getValue, context, onHook }) => {
     exportMembers: ['createHooksStore'],
   });
 
-  const templatePath = path.join(__dirname, 'template');
-  const appHooksStoreTemplatePath = path.join(templatePath, 'appHooksStore.ts.ejs');
-  const pageHooksStoreTemplatePath = path.join(templatePath, 'pageHooksStore.ts.ejs');
-  const targetPath = getValue('TEMP_PATH');
-  const projectType = getValue('PROJECT_TYPE');
-  const { rootDir, userConfig } = context;
-  const { mpa: isMpa, entry } = userConfig;
-  const srcDir = isMpa ? 'src' : applyMethod('getSourceDir', entry);
+  // add babel plugins for ice lazy
+  // @ts-ignore
+  const { configPath } = userConfig.router || {};
+
+  let { routesPath } = applyMethod('getRoutes', {
+    rootDir,
+    tempDir: targetPath,
+    configPath,
+    projectType,
+    isMpa,
+    srcDir
+  });
+
+  if (isMpa) {
+    const routesFile = `routes.${projectType}`;
+    const pagesPath = path.join(rootDir, 'src', 'pages');
+    const pagesRoutePath = pages.map(pageName => {
+      return path.join(pagesPath, pageName, routesFile);
+    });
+    routesPath = pagesRoutePath;
+  }
+
+  modifyUserConfig('babelPlugins',
+    [
+      ...(userConfig.babelPlugins as [] || []),
+      [
+        require.resolve('./babelPluginReplacePath'),
+        {
+          routesPath,
+          alias: userConfig.alias,
+          applyMethod,
+          tempDir
+        }
+      ]
+    ]
+  );
+
+  onGetWebpackConfig(config => {
+    config.module.rule('appJSON')
+      .test(/app\.json$/)
+      .use('page-source-loader')
+      .loader(require.resolve('./pageSourceLoader'))
+      .options({
+        targetPath
+      });
+    config.resolve.alias.set('$hooksStore', existsAppHooksStoreFile ? appHooksStoreFile : path.join(targetPath, 'hooksStore', 'index.ts'));
+  });
 
   const gen = new Generator({
     appHooksStoreTemplatePath,
