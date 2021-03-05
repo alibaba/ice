c7be81492476f520272ad4aaa89f5be33a7d419d
ClarkXia <xiawenwu41@gmail.com>
2021-03-05 17:19:59 +0800

    refactor: generate runtime module

--- packages/plugin-app-core/src/utils/getRuntimeModules.ts
+++ packages/plugin-app-core/src/utils/getRuntimeModules.ts
@@ -4,7 +4,7 @@ import * as globby from 'globby';
 import formatPath from './formatPath';
 
 export default (plugins: any = [], targetDir: string, debugRuntime: boolean) => {
-  return plugins.map(({ pluginPath, name }) => {
+  return plugins.map(({ pluginPath, name }, index) => {
     // compatible with function plugin
     if (!pluginPath) return false;
     // NOTE: module.js will be discarded in future.
@@ -43,6 +43,7 @@ export default (plugins: any = [], targetDir: string, debugRuntime: boolean) => 
       console.log(`ERROR: fail to load package.json of plugin ${name}`);
     }
     return {
+      moduleName: `module${index}`,
       pluginConfig,
       modulePath: formatPath(modulePath),
     };
