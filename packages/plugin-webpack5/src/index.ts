95b45d877cbeb3492a0601a5f87ad99d931216a4
ClarkXia <xiawenwu41@gmail.com>
2021-03-10 10:39:00 +0800

    feat: remote runtime by module federation (#4129)
    * feat: remote runtime by module federation
    
    * fix: active rule

--- packages/plugin-webpack5/src/index.ts
+++ packages/plugin-webpack5/src/index.ts
@@ -1,14 +1,86 @@
-import { IPlugin } from '@alib/build-scripts';
+import * as path from 'path';
+import * as fse from 'fs-extra';
+import { IPlugin, Json } from '@alib/build-scripts';
+import analyzeNext from './analyzeNext';
+import filterPackages from './filterPackages';
+import remoteConfig from './remoteConfig';
+import compileRemote from './compileRemote';
 
-const plugin: IPlugin = ({ onGetWebpackConfig, registerUserConfig }) => {
+interface IOptions {
+  remoteRuntime?: boolean;
+}
+
+const plugin: IPlugin = (api, options = {}) => {
+  const { onGetWebpackConfig, registerUserConfig, context } = api;
+  const { remoteRuntime } = options as IOptions;
+  const { pkg, userConfig, webpack, command } = context;
+  const { ModuleFederationPlugin } = (webpack as any).container;
+
+  // only active in development mode
+  const activeRemoteRuntime = command === 'start' && remoteRuntime;
+
+  const cacheFolder = path.join(context.rootDir, 'node_modules', '.cache', 'runtime');
+  const depsPath = path.join(cacheFolder, 'deps.json');
+  const runtimeFolder = path.join(cacheFolder, 'remoteRuntime');
+  const remoteName = 'remote_runtime';
+  const remoteEntry = 'remoteEntry.js';
+  const injectBundles = [];
+
+  const pkgDeps = {
+    ...(pkg.dependencies as Json || {}),
+    ...(pkg.devDependencies as Json || {}),
+  };
+
+  // external react and react-dom in case of cause error when bundle multi instance
+  const externalMap = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  };
+  // check @alifd/next
+  const [cssPath, removePackage] = analyzeNext(userConfig, context.rootDir);
+  if (removePackage) {
+    // compile next
+    delete pkgDeps[removePackage];
+  }
+  // read pkgDep from cache
+  let cacheContent = {};
+  try {
+    cacheContent = fse.readJSONSync(depsPath);
+  } catch(err) {
+    // ignore err
+  }
+  // filter dependencies
+  const compileKeys = filterPackages(Object.keys(pkgDeps));
+  const needCompile = activeRemoteRuntime && JSON.stringify(pkgDeps) !== JSON.stringify(cacheContent);
+  if (activeRemoteRuntime) {
+    // ensure folder before compile and copy
+    fse.ensureDirSync(runtimeFolder);
+    const externalBundles = [
+      ['react.js', require.resolve('react/umd/react.development.js')],
+      ['react-dom.js', require.resolve('react-dom/umd/react-dom.development.js')],
+      ...(cssPath ? [['next.css' ,cssPath]]: []),
+    ];
+    injectBundles.push(`/remoteRuntime/${remoteEntry}`);
+    // copy external bundles
+    externalBundles.forEach(([fileName, filePath]) => {
+      fse.copyFileSync(filePath, path.join(runtimeFolder, fileName));
+      injectBundles.push(`/remoteRuntime/${fileName}`);
+    });
+    remoteConfig(api, { remoteName, runtimeFolder, injectBundles, externalMap, compileKeys });
+  }
+  // if missmatch cache compile remote runtime
+  if (needCompile) {
+    compileRemote(api, { runtimeFolder, cacheFolder, externalMap, remoteEntry, remoteName, depsPath, compileKeys, pkgDeps });
+  }
+
   registerUserConfig({
     name: 'moduleFederation',
     validation: 'object',
-    async configWebpack(config, value, context) {
-      const { ModuleFederationPlugin } = (context.webpack as any).container;
+    async configWebpack(config, value) {
       config.plugin('ModuleFederationPlugin').use(ModuleFederationPlugin, [value]);
     },
   });
+
   onGetWebpackConfig((config) => {
     // compatible with process
     config
@@ -26,6 +98,15 @@ const plugin: IPlugin = ({ onGetWebpackConfig, registerUserConfig }) => {
 
     // remove CaseSensitivePathsPlugin which do not compatible with webpack 5
     config.plugins.delete('CaseSensitivePathsPlugin');
+
+    // filesystem cache
+    config.merge({
+      cache: {
+        type: 'filesystem',
+        buildDependencies: {},
+        cacheDirectory: path.join(context.rootDir, 'node_modules', '.cache', 'webpack'),
+      }
+    });
   });
 };
 
