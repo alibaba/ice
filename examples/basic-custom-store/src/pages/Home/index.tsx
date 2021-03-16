7ac2d1cab35655f1360666de0e4a84d09d388b8c
luhc228 <luhengchang228@gmail.com>
2021-03-16 12:22:50 +0800

    chore: add example case

--- examples/basic-custom-store/src/pages/Home/index.tsx
+++ examples/basic-custom-store/src/pages/Home/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { Link } from 'ice';
 import appStore from '../../store';
+import store from './store';
 
 export default () => {
   const [counterState, counterActions] = appStore.useModel('counter');
+  const [todoState, todoActions] = store.useModel('todo');
+  let input;
 
   return (
     <>
       <h2>HOME</h2>
 
-      <div>
+      <div style={{marginTop: 20}}>
         <button type="button" onClick={() => counterActions.increment()}>+</button>
         <span>{counterState.count}</span>
         <button type="button" onClick={() => counterActions.decrementAsync()}>-</button>
       </div>
+
+      <form
+        style={{marginTop: 20}}
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          todoActions.add(input.value);
+          input.value = '';
+        }}>
+        <input ref={node => input = node} />
+        <button type="submit">
+          Add todo
+        </button>
+      </form>
+      <ul style={{marginTop: 20}}>
+        {
+          todoState.todos.map(todo => (
+            <li>{todo}</li>
+          ))
+        }
+      </ul>
 
       <Link to="/about">about</Link>
     </>
