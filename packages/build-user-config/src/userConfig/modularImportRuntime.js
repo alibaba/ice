26d55fb2f7e772a5da3f756a79a3893b48761e89
fushen <fushen.jzw@alibaba-inc.com>
2021-03-23 10:59:10 +0800

    chore: add context for modularImportRuntime

--- packages/build-user-config/src/userConfig/modularImportRuntime.js
+++ packages/build-user-config/src/userConfig/modularImportRuntime.js
@@ -1,14 +1,12 @@
 const addBabelPlugins = require('./babelPlugins');
 
 module.exports = (config, value, context, api) => {
-  const { userConfig: { swc } } = context;
-  if (swc) return;
   if (value) {
     addBabelPlugins(config, [[
       require.resolve('../utils/babelPluginImport'),
       {
         importDeclarations: api.getValue('importDeclarations'),
       },
-    ]]);
+    ]], context);
   }
 };
