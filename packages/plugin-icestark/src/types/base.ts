6b624b2f1a0c2d4820c63fe6b11e612d4336b5d9
maoxiaoke <maoxiaoke@outlook.com>
2021-03-30 11:36:15 +0800

    refactor: 💡 omit unset values

--- packages/plugin-icestark/src/types/base.ts
+++ packages/plugin-icestark/src/types/base.ts
@@ -1,7 +1,7 @@
 import { CompatibleAppConfig } from '@ice/stark/lib/AppRoute';
 import { AppRouterProps } from '@ice/stark/lib/AppRouter';
 
-export type IAppRouter = AppRouterProps;
+export type IAppRouter = Omit<AppRouterProps, 'onRouteChange' | 'onAppEnter' | 'onAppLeave'>;
 
 export interface IGetApps {
   (): CompatibleAppConfig[] | Promise<CompatibleAppConfig[]>;
