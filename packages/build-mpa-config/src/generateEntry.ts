1ce537bbfef4044112dd60262678e1de547d4e30
狒狒神 <fushen.jzw@alibaba-inc.com>
2021-04-15 11:20:45 +0800

    feat: custom tab bar in MPA (#4225)

--- packages/build-mpa-config/src/generateEntry.ts
+++ packages/build-mpa-config/src/generateEntry.ts
@@ -3,14 +3,19 @@ import * as globby from 'globby';
 import { formatPath } from '@builder/app-helpers';
 
 function generateEntry(api, { framework, targetDir, pageEntry, entryName }) {
-  const { context: { userConfig: { web: webConfig = {} }, rootDir } } = api;
+  const { context: { userConfig: { web: webConfig = {} }, rootDir }, getValue } = api;
   const entryFolder = path.join(targetDir, 'mpaEntry');
   const entryPath = path.join(entryFolder, `${entryName}.tsx`);
   const templatePath = path.join(__dirname, `./template/${framework}.ts.ejs`);
   const globalStyles = globby.sync(['src/global.@(scss|less|css)'], { cwd: rootDir });
+  const customTabBarPath = formatPath(path.join(rootDir, 'src/CustomTabBar/index'));
+  const staticConfig = getValue('staticConfig');
+  const needCustomTabBar = staticConfig && getValue('CUSTOM_TAB_BAR') && staticConfig.tabBar?.list.some(name => entryName === name);
   api.applyMethod('addRenderFile', templatePath, entryPath, {
     hydrate: Boolean(webConfig.hydrate || webConfig.ssr || webConfig.snapshot || webConfig.staticExport),
     resourcePath: `${formatPath(path.extname(pageEntry) ? pageEntry.split('.').slice(0, -1).join('.') : pageEntry)}`,
+    customTabBarPath,
+    needCustomTabBar,
     globalStyle: globalStyles.length && formatPath(path.join(rootDir, globalStyles[0]))
   });
   return entryPath;
