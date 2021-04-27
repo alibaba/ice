6281d6c01f83944c916238c1ef7598a371971912
luhc228 <luhengchang228@gmail.com>
2021-04-27 16:24:34 +0800

    fix: comment

--- packages/plugin-router/src/utils/joinPath.ts
+++ packages/plugin-router/src/utils/joinPath.ts
@@ -1,5 +1,5 @@
 /**
- * Join the route paths. Fork from url-join@4.0.1
+ * Join the route paths.
  * @param strArray
  * @returns string
  */
@@ -11,20 +11,21 @@ function joinPath(...strArray: string[]): string {
   const filterStrArray = strArray.filter(str => str !== '');
   filterStrArray.forEach((str, index) => {
     if (typeof str !== 'string') {
-      throw new Error(`path must be a string. Received ${str}`);
+      throw new Error(`Path must be a string. Received ${str}`);
     }
-    let path = str;
+    let routePath = str;
+    // Fork from https://github.com/jfromaniello/url-join@4.0.1
     if (index > 0) {
-      path = path.replace(/^[/]+/, '');
+      routePath = routePath.replace(/^[/]+/, '');
     }
     if (index < filterStrArray.length - 1) {
       // Removing the ending slashes for each component but the last.
-      path = path.replace(/[/]+$/, '');
+      routePath = routePath.replace(/[/]+$/, '');
     } else {
       // For the last component we will combine multiple slashes to a single one.
-      path = path.replace(/[/]+$/, '/');
+      routePath = routePath.replace(/[/]+$/, '/');
     }
-    resultArray.push(path);
+    resultArray.push(routePath);
   });
   return resultArray.join('/');
 }
