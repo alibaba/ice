d1fe6755327c85b1507836112b5bbf895738df28
ClarkXia <xiawenwu41@gmail.com>
2021-04-30 15:40:35 +0800

    fix: remove duplicate babel plugin

--- packages/babel-preset-ice/src/index.ts
+++ packages/babel-preset-ice/src/index.ts
@@ -36,7 +36,6 @@ export default (opts: IOpts = {}) => {
     // Stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-json-strings',
   ];
 
