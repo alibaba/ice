f350a58cb47b1f930673f61d219e84c62039d1fa
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-06-29 18:15:31 +0800

    feat(miniapp): support onPageNotFound in miniapp

--- packages/create-app-shared/src/collectAppLifeCycle.ts
+++ packages/create-app-shared/src/collectAppLifeCycle.ts
@@ -1,3 +1,4 @@
+import { isMiniApp } from 'universal-env';
 import { addAppLifeCycle } from './appLifeCycles';
 import { SHOW, LAUNCH, ERROR, HIDE, TAB_ITEM_CLICK, NOT_FOUND, SHARE, UNHANDLED_REJECTION } from './constants';
 import { isMiniAppPlatform, isWeChatMiniProgram, isByteDanceMicroApp } from './env';
@@ -15,20 +16,14 @@ export default function collectAppLifeCycle(appConfig) {
   addAppLifeCycle(HIDE, onHide);
   // Add tab bar item click callback
   addAppLifeCycle(TAB_ITEM_CLICK, onTabItemClick);
-  // Add lifecycle callbacks which only valid in Wechat MiniProgram and ByteDance MicroApp
-  if (isWeChatMiniProgram || isByteDanceMicroApp) {
-    const { onPageNotFound, onShareAppMessage } = appConfig.app;
-    // Add global share callback
-    addAppLifeCycle(SHARE, onShareAppMessage);
-    // Add page not found callback
-    addAppLifeCycle(NOT_FOUND, onPageNotFound);
-  }
-  // Add lifecycle callbacks which only valid in Alibaba MiniApp
+
   if (isMiniAppPlatform) {
-    const { onShareAppMessage, onUnhandledRejection } = appConfig.app;
+    const { onPageNotFound, onShareAppMessage, onUnhandledRejection } = appConfig.app;
     // Add global share callback
     addAppLifeCycle(SHARE, onShareAppMessage);
     // Add unhandledrejection callback
     addAppLifeCycle(UNHANDLED_REJECTION, onUnhandledRejection);
+    // Add page not found callback
+    addAppLifeCycle(NOT_FOUND, onPageNotFound);
   }
 }
