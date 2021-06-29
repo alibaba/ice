ba8a413da860876ff630dcea32b6a76b02d9bc35
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-06-29 18:58:43 +0800

    chore: remove unused variables

--- packages/create-app-shared/src/collectAppLifeCycle.ts
+++ packages/create-app-shared/src/collectAppLifeCycle.ts
@@ -1,7 +1,6 @@
-import { isMiniApp } from 'universal-env';
 import { addAppLifeCycle } from './appLifeCycles';
 import { SHOW, LAUNCH, ERROR, HIDE, TAB_ITEM_CLICK, NOT_FOUND, SHARE, UNHANDLED_REJECTION } from './constants';
-import { isMiniAppPlatform, isWeChatMiniProgram, isByteDanceMicroApp } from './env';
+import { isMiniAppPlatform } from './env';
 
 export default function collectAppLifeCycle(appConfig) {
   const { onLaunch, onShow, onError, onHide, onTabItemClick } = appConfig.app;
