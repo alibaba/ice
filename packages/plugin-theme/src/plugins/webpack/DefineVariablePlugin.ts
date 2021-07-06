6e5655b296bbe4d830d094cb35aa79e42b8b6be5
Fuzzy Fade <1339184537@qq.com>
2021-07-06 11:39:01 +0800

    fix: optimizeChunkAssets api deprecated in webpack5

--- packages/plugin-theme/src/plugins/webpack/DefineVariablePlugin.ts
+++ packages/plugin-theme/src/plugins/webpack/DefineVariablePlugin.ts
@@ -1,5 +1,6 @@
-import { Compiler, Plugin } from 'webpack';
-import { ConcatSource } from 'webpack-sources';
+import * as webpack from 'webpack';
+import { ConcatSource, RawSource } from 'webpack-sources';
+import { get } from 'lodash';
 import { getThemesDataStr } from '../../utils/themesUtil';
 
 export interface Options {
@@ -9,7 +10,7 @@ export interface Options {
 /**
  * 将主题数据在 optimizeChunkAssets 阶段注入到 window 上
  */
-export class DefineVariablePlugin implements Plugin {
+export class DefineVariablePlugin implements webpack.Plugin {
   private readonly pluginName = this.constructor.name;
 
   private readonly options: Partial<Options> = {};
@@ -18,17 +19,45 @@ export class DefineVariablePlugin implements Plugin {
     this.options = { ...this.options, ...options };
   }
 
-  public apply(compiler: Compiler): void {
+  public apply(compiler: webpack.Compiler): void {
     const { defaultName } = this.options;
 
     compiler.hooks.compilation.tap(this.pluginName, (compilation) => {
+      // webpack 5 get version
+      if (get(compiler, 'webpack.version')?.startsWith('5')) {
+        (compilation.hooks as any).processAssets.tap(
+          {
+            name: this.pluginName,
+            stage: (webpack as any).Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
+            additionalAssets: true
+          },
+          (assets: webpack.AssetInfo) => {
+            Object.keys(assets).forEach(i => {
+              const asset = compilation.getAsset(i);
+              const contents = asset.source.source();
+
+              compilation.updateAsset(
+                i,
+                new RawSource(`window.__themesData__ = ${getThemesDataStr(defaultName)};\n${contents}`),
+              );
+            });
+          }
+        );
+        return;
+      }
+
+      /**
+       * @deprecated optimizeChunkAssets API
+       */
       compilation.hooks.optimizeChunkAssets.tap(this.pluginName, (chunks) => {
         chunks.forEach((chunk) => {
           chunk.files.forEach((fileName) => {
-            compilation.assets[fileName] = new ConcatSource(
-              `window.__themesData__ = ${getThemesDataStr(defaultName)};\n`,
-              compilation.assets[fileName],
-            );
+            if (fileName.includes('.js')) {
+              compilation.assets[fileName] = new ConcatSource(
+                `window.__themesData__ = ${getThemesDataStr(defaultName)};\n`,
+                compilation.assets[fileName],
+              );
+            }
           });
         });
       });
