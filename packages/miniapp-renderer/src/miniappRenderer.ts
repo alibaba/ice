d9faa102184921dfd436fca4cf79154cca9ede32
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-07-07 14:21:20 +0800

    feat(miniapp-renderer): support plugin situation without global getApp

--- packages/miniapp-renderer/src/miniappRenderer.ts
+++ packages/miniapp-renderer/src/miniappRenderer.ts
@@ -54,11 +54,15 @@ function miniappRenderer(
       setDocument(value) {
         // eslint-disable-next-line no-global-assign
         document = value;
+        // getApp doesn't exist in plugin situation
         // @ts-ignore
-        const MiniAppGlobalInstance = getApp();
-        const dispatchDocumentModify = MiniAppGlobalInstance._dispatchDocumentModify;
-        if (typeof dispatchDocumentModify === 'function') {
-          dispatchDocumentModify.call(MiniAppGlobalInstance, value);
+        if (typeof getApp !== 'undefined') {
+          // @ts-ignore
+          const MiniAppGlobalInstance = getApp();
+          const dispatchDocumentModify = MiniAppGlobalInstance._dispatchDocumentModify;
+          if (typeof dispatchDocumentModify === 'function') {
+            dispatchDocumentModify.call(MiniAppGlobalInstance, value);
+          }
         }
       }
     };
