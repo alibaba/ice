00f7cb1104c85d955d73a50244000cd6463d160b
炽翎 <45777252+wjq990112@users.noreply.github.com>
2021-07-08 12:06:09 +0800

    feat: support `forceEnableCSS` config (#4431)
    * feat: support `forceEnableCSS` config
    
    * fix: config css loader options
    
    * fix: typo

--- packages/build-user-config/src/userConfig/cssLoaderOptions.js
+++ packages/build-user-config/src/userConfig/cssLoaderOptions.js
@@ -3,10 +3,13 @@ module.exports = (config, cssLoaderOptions) => {
     [
       'scss',
       'scss-module',
+      'scss-global',
       'css',
       'css-module',
+      'css-global',
       'less',
       'less-module',
+      'less-global',
     ].forEach(rule => {
       if (config.module.rules.get(rule)) {
         config.module
