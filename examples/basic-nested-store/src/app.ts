0ba91458af71b0b68dd7258757a50ce682b5b22a
ClarkXia <xiawenwu41@gmail.com>
2021-07-12 14:46:03 +0800

    chore: deprecate packages and api (#4452)
    * chore: deprecate packages and api
    
    * fix: upgrade copy-webpack-plugin
    
    * chore: dependencies
    
    * fix: remove deprecate api call
    
    * fix: revert copy-webpack-plugin upgrade
    
    * feat: upgrade copy-webpack-plugin
    
    * fix: noErrorOnMissing for copy plugin

--- examples/basic-nested-store/src/app.ts
+++ examples/basic-nested-store/src/app.ts
@@ -1,4 +1,4 @@
-import { createApp, IAppConfig } from 'ice';
+import { runApp, IAppConfig } from 'ice';
 
 const appConfig: IAppConfig = {
   app: {
@@ -6,4 +6,4 @@ const appConfig: IAppConfig = {
   },
 };
 
-createApp(appConfig);
+runApp(appConfig);
