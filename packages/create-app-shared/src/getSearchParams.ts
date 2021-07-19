2327ef7e8054cfb5904646cdf50861e8ffd0ed15
fushen <fushen.jzw@alibaba-inc.com>
2021-07-19 18:04:36 +0800

    chore: test case

--- packages/create-app-shared/src/getSearchParams.ts
+++ packages/create-app-shared/src/getSearchParams.ts
@@ -1,7 +1,10 @@
 import * as queryString from 'query-string';
 import { getHistory } from './storage';
 
-export default function(history = getHistory() || (window as any)?.history) {
+export default function(history = getHistory()) {
+  if (!history && typeof window !== 'undefined' && window.history) {
+    history = (window as any).history;
+  }
   if (history && history.location && history.location.search) {
     return queryString.parse(history.location.search);
   }
