0d7254e7a19b92c3788962ebcfda607a506ec069
ClarkXia <xiawenwu41@gmail.com>
2021-07-28 16:34:08 +0800

    fix: compatible with plugins when swc is enabled

--- packages/build-user-config/src/userConfig/swc.js
+++ packages/build-user-config/src/userConfig/swc.js
@@ -1,12 +1,19 @@
+const path = require('path');
 const { merge } = require('@builder/pack/deps/lodash');
 
-const EXCLUDE_REGX = /node_modules/;
+const EXCLUDE_REGEX = /node_modules/;
 
-module.exports = (config, swcOptions) => {
+module.exports = (config, swcOptions, context, { log }) => {
+  const { rootDir } = context;
   if (swcOptions) {
-    // Delete babel loader
+    log.info('[swc]', 'swc enabled, configurations about babel will be ignored');
     ['jsx', 'tsx'].forEach((rule) => {
-      config.module.rules.delete(rule).end();
+      if (config.module.rules.get(rule)) {
+        // add include rule while source file will never matched
+        config.module.rule(rule)
+          .exclude.clear().end()
+          .include.clear().add(path.join(rootDir, '__not_exists_file__.js'));
+      }
     });
     const swcLoader = require.resolve('@builder/swc-loader');
 
@@ -43,9 +50,9 @@ module.exports = (config, swcOptions) => {
     }, commonOptions);
 
     config.module
-      .rule('jsx')
+      .rule('swc')
       .test(/\.(j|t)sx?$/)
-      .exclude.add(EXCLUDE_REGX)
+      .exclude.add(EXCLUDE_REGEX)
       .end()
       .use('swc-loader')
       .loader(swcLoader)
