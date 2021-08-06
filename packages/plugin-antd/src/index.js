853707dc75c62e8f12e13b0a3d70b11009d52a4c
ClarkXia <xiawenwu41@gmail.com>
2021-08-06 15:28:57 +0800

    feat: support style import in vite

--- packages/plugin-antd/src/index.js
+++ packages/plugin-antd/src/index.js
@@ -1,5 +1,25 @@
-module.exports = async ({ onGetWebpackConfig, log }, pluginOptions = {}) => {
+module.exports = async ({ onGetWebpackConfig, log, context, modifyUserConfig }, pluginOptions = {}) => {
   const { themeConfig = {}, importOptions = {} } = pluginOptions;
+  const { userConfig } = context;
+
+  if (userConfig.vite) {
+    modifyUserConfig('vite.plugins', (vitePlugins) => {
+      return [
+        ...(vitePlugins || []),
+        // eslint-disable-next-line global-require
+        require('vite-plugin-style-import').default({
+          libs: [{
+            libraryName: 'antd',
+            esModule: true,
+            resolveStyle: (name) => {
+              return `antd/es/${name}/style/index`;
+            },
+          }]
+        }),
+      ];
+    });
+  }
+
   onGetWebpackConfig((config) => {
     // support less modify vars
     if (themeConfig) {
