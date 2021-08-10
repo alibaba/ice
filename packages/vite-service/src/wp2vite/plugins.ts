10bd717d20df9152ee48b84f06d2fe5f1260b167
Rhuzerv <1339184537@qq.com>
2021-08-10 11:28:54 +0800

    fix: polyfill file in hash and outputAssetsPath (#4533)
    * fix: polyfill be control by hash
    
    * fix: polyfill path in html and adapt outputAssetsPath
    
    * fix: deps check for fast-glob

--- packages/vite-service/src/wp2vite/plugins.ts
+++ packages/vite-service/src/wp2vite/plugins.ts
@@ -3,8 +3,10 @@ import { redirectImport } from '@builder/app-helpers';
 import legacy from '@vitejs/plugin-legacy';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import * as glob from 'fast-glob';
 import * as log from 'npmlog';
 import { all } from 'deepmerge';
+import { replace } from 'lodash';
 
 interface HtmlOption {
   entry: any // only spa
@@ -150,15 +152,75 @@ export const importPlugin = ({ rootDir }): Plugin => {
   };
 };
 
+interface PolyfillOption {
+  value: 'usage' | 'entry' | false,
+  browserslist: any,
+  rootDir: string,
+  hash: boolean,
+  outputAssetsPath: { css: string; js: string }
+}
+
 export const polyfillPlugin = (
-  option: { value: 'usage' | 'entry' | false, browserslist: any }
-): Plugin => {
-  const { value, browserslist } = option;
+  option: PolyfillOption
+): Plugin[] => {
+  const { value, browserslist, rootDir, hash, outputAssetsPath } = option;
 
   if (!value) return;
 
-  return legacy({
-    targets: browserslist,
-    polyfills: value === 'usage'
-  });
+  let outDir = '';
+  let assetsDirName = '';
+
+  /**
+   * hash 为开启时，清除 polyfill hash 后缀
+   */
+  const plugin: Plugin = {
+    name: 'rollup-plugin-polyfills-clear',
+    config(cfg, { command }) {
+      if (command === 'build') {
+        outDir = cfg.build?.outDir ?? 'dist';
+        assetsDirName = cfg.build?.assetsDir ?? 'assets';
+      }
+    },
+    async closeBundle() {
+      const distPath = path.resolve(rootDir, outDir);
+      const assetsPath = path.resolve(distPath, assetsDirName);
+      const jsDirPath = path.resolve(distPath, outputAssetsPath.js);
+      let polyfillsFile = glob.sync(path.resolve(assetsPath, 'polyfills-legacy.*.js'))[0];
+
+      if (!polyfillsFile) return;
+
+      const hashName = path.relative(assetsPath, polyfillsFile);
+      let fileName = hashName;
+
+      // 拖到 js 文件夹下
+      fs.moveSync(polyfillsFile, path.resolve(jsDirPath, fileName));
+      polyfillsFile = path.resolve(jsDirPath, fileName);
+
+      if (!hash) {
+        // 清除 hash
+        const dest = path.resolve(jsDirPath, 'polyfills-legacy.js');
+        fs.renameSync(polyfillsFile, dest);
+        fileName = 'polyfills-legacy.js';
+      }
+
+      // remove assets dir
+      fs.rmdirSync(assetsPath);
+
+      // html 字符替换
+      const htmlPath = path.resolve(distPath, 'index.html');
+      const html = fs.readFileSync(htmlPath, 'utf-8');
+      fs.writeFileSync(
+        htmlPath,
+        replace(html, `/${assetsDirName}/${hashName}`, `/${outputAssetsPath.js}/${fileName}`)
+      );
+    }
+  };
+
+  return [
+    plugin,
+    legacy({
+      targets: browserslist,
+      polyfills: value === 'usage'
+    })
+  ];
 };
