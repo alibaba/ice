dae1b008ca552f0e6739a79253329e796eec41d5
Fuzzy Fade <1339184537@qq.com>
2021-08-13 13:40:09 +0800

    feat: vite:importPlugin support spa and mpa

--- packages/vite-service/src/plugins/import.ts
+++ packages/vite-service/src/plugins/import.ts
@@ -1,46 +1,68 @@
 import type { Plugin } from 'vite';
 import { redirectImport } from '@builder/app-helpers';
 import * as path from 'path';
+import { isObject } from 'lodash';
 
 interface Option {
   rootDir: string;
-  entries: Record<string, string[]>
+  entries: string | Record<string, string[]>
 }
 
-const getData = (rootDir: string, list: string[]) => {
+const getMpaRunAppPathsMap = (rootDir: string, list: string[]) => {
   return list.reduce((acc, key) => {
     acc[key] = path.resolve(rootDir, `.ice/entries/${key}/runApp`);
     return acc;
   }, {});
 };
 
+const getTransformedCode = async (code: string, id: string, tempPath: string) => {
+  const url = path.relative(path.dirname(id), tempPath);
+
+  return await redirectImport(code, {
+    source: 'ice', redirectImports: [
+      {
+        name: 'runApp',
+        redirectPath: url,
+        default: false,
+      }
+    ]
+  });
+};
+
+/**
+ * @from import { runApp } from 'ice'
+ * @to import { runApp } from '../.ice/core/runApp'
+ * 
+ * @tip both support spa and mpa
+ */
 export const importPlugin = ({ rootDir, entries }: Option): Plugin => {
-  const entryList = Object.keys(entries);
-  const isMpa = entryList.length > 1;
-  const mpaData = getData(rootDir, entryList);
+  const isMpa = isObject(entries);
+
+  if (isMpa) {
+    const entryList = Object.keys(entries);
+    const mpaRunAppPathsMap = getMpaRunAppPathsMap(rootDir, entryList);
+    return {
+      name: 'vite-plugin-import-mpa',
+      transform: async (code, id) => {
+        if (!/\.(?:[jt]sx?|[jt]s?)$/.test(id)) return;
+
+        const relativePath = path.relative(rootDir, id).toLocaleLowerCase();
+        const name = entryList.find(pageName => relativePath.includes(pageName));
+        const iceTempPath = mpaRunAppPathsMap[name];
+
+        if (!iceTempPath) return code;
+
+        return await getTransformedCode(code, id, iceTempPath);
+      }
+    };
+  }
 
   return {
-    name: 'vite-plugin-import',
+    name: 'vite-plugin-import-spa',
     transform: async (code, id) => {
       if (!/\.(?:[jt]sx?|[jt]s?)$/.test(id)) return;
 
-      const relativePath = path.relative(rootDir, id).toLocaleLowerCase();
-      const name = entryList.find(pageName => relativePath.includes(pageName));
-      const iceTempPath = isMpa ? mpaData[name] : path.resolve(rootDir, '.ice/core/runApp');
-
-      if (!iceTempPath) return code;
-
-      // 获取相对路径
-      const url = path.relative(path.dirname(id), iceTempPath);
-      return await redirectImport(code, {
-        source: 'ice', redirectImports: [
-          {
-            name: 'runApp',
-            redirectPath: url,
-            default: false,
-          }
-        ]
-      });
+      return await getTransformedCode(code, id, path.resolve(rootDir, '.ice/core/runApp'));
     }
   };
 };
