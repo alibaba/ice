d2bd6a0b0ba03cf1b9bcffc183017d6922bc9b37
luhc228 <luhengchang228@126.com>
2021-08-18 18:13:53 +0800

    fix: unnecessary logic

--- packages/plugin-ice-ssr/src/generateStaticPages.ts
+++ packages/plugin-ice-ssr/src/generateStaticPages.ts
@@ -14,23 +14,20 @@ interface PageData {
  */
 async function generateStaticPages(buildDir: string, renderPagesBundlePath: string) {
   const pagesDataPath = join(buildDir, 'server', 'pages-data.json');
-  const pagesData = [];
 
   // eslint-disable-next-line
-  const ssgRender = require(renderPagesBundlePath);
+  const renderPages = require(renderPagesBundlePath);
   const htmlTemplate = fse.readFileSync(join(buildDir, 'index.html'), 'utf8');
-  const renderPages: PageData[] = await ssgRender.default({ htmlTemplate });
+  const pagesData: PageData[] = await renderPages.default({ htmlTemplate });
 
-  renderPages.forEach((renderPage) => {
-    const { html, path } = renderPage;
+  // write pages html
+  pagesData.forEach((pageData) => {
+    const { html, path } = pageData;
     const htmlPath = join(buildDir, path, 'index.html');
-    // write page html
     fse.ensureFileSync(htmlPath);
     fse.writeFileSync(htmlPath, html);
-    // save html and other route configs to json, ensure ssgRender function can find the html
-    // e.g.: const renderRoute = { path: "/", html: "<html>xx</html>",  strict: true }
-    pagesData.push(renderPage);
   });
+
   // write pages data to json
   fse.ensureFileSync(pagesDataPath);
   fse.writeJSONSync(pagesDataPath, pagesData, { spaces: 2 });
