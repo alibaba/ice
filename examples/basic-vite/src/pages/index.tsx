549313baae40c04997b2a0631e85cd53db1496b2
luhc228 <luhengchang228@126.com>
2021-08-18 19:44:10 +0800

    chore: example

--- examples/basic-vite/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div>333</div>
-  );
-};
--- examples/basic-vite/src/routes.ts
+++ examples/basic-vite/src/routes.ts
@@ -1,13 +1,8 @@
 import { IRouterConfig, lazy } from 'ice';
 
 const Home = lazy(() => import('@/pages/Home'));
-const Index = lazy(() => import('@/pages/index'));
 
 const routes: IRouterConfig[] = [
-  {
-    path: '/index',
-    component: Index,
-  },
   {
     path: '/',
     component: Home,
