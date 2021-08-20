fd9661fc3c6d2fc59c910bbe0027ecb712a3b4f1
ClarkXia <xiawenwu41@gmail.com>
2021-08-20 16:15:34 +0800

    fix: optimize code

--- packages/vite-service/src/wp2vite/config.ts
+++ packages/vite-service/src/wp2vite/config.ts
@@ -47,7 +47,7 @@ const transformPreProcess = (loaderName: string, rule: string): Transformer => {
 
   return (...args) => {
     const opt = args[2].module.rules.get(rule).use(loaderName).get('options');
-    return optionsKey ? opt && opt[optionsKey] : opt;
+    return optionsKey ? opt?.[optionsKey] : opt;
   };
 };
 
