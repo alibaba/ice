a35cc7b5883b0f733f3253a75e172c674ac9b197
luhc228 <luhengchang228@126.com>
2021-09-24 20:43:18 +0800

    Revert "chore: generator to Generator"
    This reverts commit c852f660021e02190016702ce052c82f658419db.

--- packages/plugin-store/src/index.ts
+++ packages/plugin-store/src/index.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import Generator from './Generator';
+import Generator from './generator';
 import checkStoreExists from './utils/checkStoreExists';
 import { getAppStorePath } from './utils/getPath';
 import { getRouteFileType } from './utils/getFileType';
