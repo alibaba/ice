db2a56a161d87e1f0dd48e885310134950963071
luhc228 <luhengchang228@126.com>
2021-09-29 11:46:34 +0800

    chore: remove config

--- examples/basic-faas/src/apis/config/config.daily.ts
+++
@@ -1,9 +0,0 @@
-export const bucLogin = {
-  appname: 'fn-iceworks-idp',
-  appcode: 'b1aa812d6a984f938e61b169f49e57d2',
-  ignore: '/api',
-  host: 'iceworks-idp.fx.alibaba.net',
-  protocol: 'http',
-  refreshToken: true,
-  ssoURL: 'https://login-test.alibaba-inc.com',
-}
--- examples/basic-faas/src/apis/config/config.default.ts
+++
@@ -1,5 +0,0 @@
-export const bucLogin = {
-  appname: 'fn-iceworks-idp',
-  appcode: 'b1aa812d6a984f938e61b169f49e57d2',
-  ignore: '/api',
-}
