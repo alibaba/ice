7c86b7afcd3125a36ab9456bb24ee2b7095ba7e3
那吒 <maoxiaoke@outlook.com>
2021-10-28 15:13:53 +0800

    fix: 🐛 avoid RootApp re-rendering when route changes (#4767)
    * fix: 🐛 avoid RootApp re-rendering when route changes

--- packages/plugin-icestark/src/runtime.tsx
+++ packages/plugin-icestark/src/runtime.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppConfig, AppRouter, AppRoute } from '@ice/stark';
 import {
@@ -171,6 +171,12 @@ const module = ({
         updateApps: setApps,
       };
 
+      // RootApp will re-render on every AppRoute's update if RootApp were matched.
+      const MemoRootApp = useMemo(
+        () => React.memo(() => <RootApp routes={routes} />),
+        []
+      );
+
       return (
         <BasicLayout {...appInfo}>
           {apps && (
@@ -192,7 +198,7 @@ const module = ({
                 <RenderAppRoute
                   path="/"
                   render={() => {
-                    return <RootApp routes={routes} />;
+                    return <MemoRootApp />;
                   }}
                 />
               )}
