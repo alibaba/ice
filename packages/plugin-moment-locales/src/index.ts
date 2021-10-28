73f9f37b73edad6d7fcf413a13d965acec1fcd8c
ClarkXia <xiawenwu41@gmail.com>
2021-10-28 11:49:43 +0800

    fix: pre build moment locales (#4804)

--- packages/plugin-moment-locales/src/index.ts
+++ packages/plugin-moment-locales/src/index.ts
@@ -8,11 +8,17 @@ interface PluginOptions {
 const plugin: IPlugin = ({ onGetWebpackConfig, modifyUserConfig, context }, options) => {
   const { webpack, userConfig } = context;
   const { locales } = options as unknown as PluginOptions || {};
+  const localeArray = typeof locales === 'string' ? [locales] : locales;
   if (userConfig.vite) {
-    modifyUserConfig('vite.plugins', [vitePluginMoment(locales)] , { deepmerge: true });
+    modifyUserConfig('vite', {
+      plugins: [vitePluginMoment(locales)],
+      optimizeDeps: {
+        // pre build moment locales
+        include: localeArray.map((locale => `moment/dist/locale/${locale}`)),
+      },
+    } , { deepmerge: true });
   } else {
     onGetWebpackConfig((config) => {
-      const localeArray = typeof locales === 'string' ? [locales] : locales;
       if (localeArray.length) {
         const localesRegExp = new RegExp(localeArray.join('|'));
         config.plugin('context-replacement')
