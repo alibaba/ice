00fc88281408305fb3d718c3b97f5ab5444160f0
luhc228 <luhengchang228@gmail.com>
2021-11-18 13:33:35 +0800

    fix: fail to copy dts file to correct dir

--- packages/icejs/bin/copy-dts-to-ice-temp.js
+++ packages/icejs/bin/copy-dts-to-ice-temp.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
+const projectRootDir = process.env.INIT_CWD; // the original working directory that npm was executed from
 const dtsBundleFile = 'ice.d.ts';
-const iceTempDir = path.join(process.cwd(), '.ice');
+
+const iceTempDir = path.join(projectRootDir, '.ice');
 const iceTempDtsBundlePath = path.join(iceTempDir, dtsBundleFile);
 const dtsBundlePath = path.join(__dirname, '..', 'lib', dtsBundleFile);
 
