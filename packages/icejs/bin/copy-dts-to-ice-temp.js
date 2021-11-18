1af391f0db55490b436d8dcb44e50cf91b66c057
Hengchang Lu <44047106+luhc228@users.noreply.github.com>
2021-11-18 14:52:11 +0800

    fix: process.env.INIT_CWD is undefined in low version npm (#4937)

--- packages/icejs/bin/copy-dts-to-ice-temp.js
+++ packages/icejs/bin/copy-dts-to-ice-temp.js
@@ -2,11 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 const projectRootDir = process.env.INIT_CWD; // the original working directory that npm was executed from
-const dtsBundleFile = 'ice.d.ts';
+if (!projectRootDir) {
+  // process.env.INIT_CWD may be undefined in low version npm or cnpm
+  return;
+}
 
 const iceTempDir = path.join(projectRootDir, '.ice');
-const iceTempDtsBundlePath = path.join(iceTempDir, dtsBundleFile);
-const dtsBundlePath = path.join(__dirname, '..', 'lib', dtsBundleFile);
+const iceTempDtsBundlePath = path.join(iceTempDir, 'index.d.ts');
+const dtsBundlePath = path.join(__dirname, '..', 'lib', 'ice.d.ts');
 
 if (fs.existsSync(dtsBundlePath)) {
   if (!fs.existsSync(iceTempDir)) {
