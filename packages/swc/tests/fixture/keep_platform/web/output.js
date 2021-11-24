b1d82f60d272043a0d0156f64f39284b6d751534
fushen <350740249@qq.com>
2021-11-24 19:53:36 +0800

    chore: migrate swc

--- packages/swc/tests/fixture/keep_platform/web/output.js
+++
@@ -1,9 +0,0 @@
-var isWeex = false;
-var isWeb = true;
-if (isWeb) {
-    console.log("This is web");
-} else if (isWeex) {
-    console.log("This is weex");
-} else {
-    console.log("others");
-}
