f6fe928df429e1aa524db0046512bacf24786ee8
fushen <350740249@qq.com>
2021-12-01 15:17:35 +0800

    feat: ssr search params

--- packages/create-app-shared/src/getSearchParams.ts
+++ packages/create-app-shared/src/getSearchParams.ts
@@ -3,10 +3,6 @@ import { getHistory } from './storage';
 
 // TODO: 分支逻辑可以优化下
 export default function(history = getHistory()) {
-  if (!history && typeof window !== 'undefined' && window.history) {
-    history = (window as any).history;
-  }
-
   let search = history?.location?.search;
 
   if (!search && typeof window !== 'undefined') {
