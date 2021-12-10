73e9981156c888262d199e6203f4bc0b867c1433
ClarkXia <xiawenwu41@gmail.com>
2021-12-10 11:55:31 +0800

    fix: enhance alias for node_modules dependencies

--- packages/build-user-config/src/userConfig/alias.js
+++ packages/build-user-config/src/userConfig/alias.js
@@ -4,12 +4,22 @@ module.exports = (config, alias, context) => {
   const { rootDir } = context;
   const aliasWithRoot = {};
   Object.keys(alias).forEach((key) => {
-    if (path.isAbsolute(alias[key])) {
+    if (typeof alias[key] === 'boolean' || path.isAbsolute(alias[key])) {
       aliasWithRoot[key] = alias[key];
     } else {
-      aliasWithRoot[key] = path.resolve(rootDir, alias[key]);
+      let resolvePath = '';
+      try {
+        // 检测是否可以在 node_modules 下找到依赖，如果可以直接使用该依赖
+        if (require.resolve(alias[key], { paths: [rootDir]})) {
+          resolvePath = alias[key];
+        }
+      } catch (e) {
+        // ignore errors
+      }
+      aliasWithRoot[key] = resolvePath || path.resolve(rootDir, alias[key]);
     }
   });
+  console.log(aliasWithRoot);
   config.merge({
     resolve: {
       alias: aliasWithRoot,
