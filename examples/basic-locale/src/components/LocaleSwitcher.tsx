2590a60d1aff51e0a7c9838a7d9186dd9f39f8a7
luhc228 <luhengchang228@gmail.com>
2021-12-22 14:57:47 +0800

    fix: activeLocale is not correct

--- examples/basic-locale/src/components/LocaleSwitcher.tsx
+++ examples/basic-locale/src/components/LocaleSwitcher.tsx
@@ -4,6 +4,7 @@ import { useLocale, getAllLocales } from 'ice';
 export default function LocaleSwitcher() {
   const [activeLocale, setLocale] = useLocale();
   const allLocales = getAllLocales();
+  console.log('activeLocale==>', activeLocale);
   const otherLocales = allLocales.filter((locale) => activeLocale !== locale);
   return (
     <div>
