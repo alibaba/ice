fd164dd1cd9f8b1f0c03cd9b83f82a2815f41a36
maoxiaoke <maoxiaoke@outlook.com>
2021-12-25 17:25:13 +0800

    feat: 🎸 support pnpm

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
 import * as pathToRegexp from 'path-to-regexp';
+import reactAppRenderer, { getInitialData } from 'react-app-renderer';
 import axiosUtils from './axiosUtils';
 
+// for core
+export * as createAppShared from 'create-app-shared';
+
+export type { RenderAppConfig } from 'react-app-renderer';
+
 export {
+  // for core
+
+  // for plugin-request
   axios,
   axiosUtils,
-  pathToRegexp
+  pathToRegexp,
+  reactAppRenderer,
+  getInitialData
 };
