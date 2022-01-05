087ff45e382eca66b23f1e09f8cbe94ca58d58e0
ClarkXia <xiawenwu41@gmail.com>
2022-01-05 16:38:30 +0800

    fix: vite ssr

--- packages/plugin-ice-ssr/src/vite/ssrPlugin.ts
+++ packages/plugin-ice-ssr/src/vite/ssrPlugin.ts
@@ -1,12 +1,18 @@
 import type { Plugin, UserConfig } from 'vite';
 import createSSRDevHandler from './ssrDevHandler';
 
-const invalidPackages = [
-  // node dependencies with exports
+const externalPackages = [
+  // server 入口依赖
   'cheerio',
   '@loadable/server',
   'chalk',
   'parseurl',
+  // @ice/runtime 依赖
+  'axios',
+  'path-to-regexp',
+  // @ice/store 依赖
+  'react-redux',
+  'lodash.isfunction',
 ];
 
 const vitePluginSSR = (ssrEntry: string): Plugin => {
@@ -16,9 +22,10 @@ const vitePluginSSR = (ssrEntry: string): Plugin => {
     config() {
       return {
         ssr: {
-          // 添加 no external 配置，通过 ssrLoadModule 加载时不会默认使用 node_modules 源码
-          // 配合预编译逻辑，将非标准包支持 node 环境下加载执行
-          noExternal: ['create-app-shared', 'react-app-renderer', 'miniapp-history'],
+          // 主要由 server 入口和 noExternal 引入的 cjs 依赖，基于 cjs 规范，可以直接通过 ssrImport，如果不设置将通过 esm 方式执行导致 require / module 是 undefined 报错
+          external: externalPackages,
+          // 设置 noExternal 逻辑，以下四个包均只存在 esm 产物，但未设置 type: "module"，设置 noExternal 将不会通过 ssrImport 直接执行
+          noExternal: ['create-app-shared', 'react-app-renderer', '@ice/runtime', '@ice/store'],
         },
       } as UserConfig;
     },
