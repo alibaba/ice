92bda90c35b3e1fe54cf16d08f17b8ce81f1468a
luhc228 <luhengchang228@gmail.com>
2022-02-23 12:26:56 +0800

    feat: add more options

--- packages/plugin-app/src/index.ts
+++ packages/plugin-app/src/index.ts
@@ -1,28 +1,87 @@
 import * as path from 'path';
 import type { IFrameworkPlugin } from '@ice/service';
+import { merge } from '@builder/pack/deps/lodash';
+
+type JSXSuffix = 'jsx' | 'tsx';
 
 const plugin: IFrameworkPlugin = ({ registerTask, context }) => {
   const { command, rootDir } = context;
   const mode = command === 'start' ? 'development' : 'production';
 
-  const loaders = getBaseLoaders();
-
   registerTask('web', {
     entry: path.join(rootDir, 'src/app'),
     mode,
-    loaders,
+    loaders: [
+      ['jsx', 'tsx'].map((suffix: JSXSuffix) => ({
+        test: new RegExp(`\\.${suffix}?$`),
+        use: {
+          loader: require.resolve('@builder/swc-loader'),
+          options: getSwcLoaderOptions(suffix),
+      },
+      })),
+    ],
    });
 };
 
-function getBaseLoaders() {
-  return [
-    {
-      test: /\.(js|jsx|tsx)$/,
-      use: [
-        require.resolve('@builder/swc-loader'),
-      ],
+function getSwcLoaderOptions(suffix: JSXSuffix) {
+  // let reactTransformConfig = getValue('REACT_TRANSFORM_CONFIG');
+  //   if (!reactTransformConfig) {
+  //     reactTransformConfig = getValue('HAS_JSX_RUNTIME') ? { runtime: 'automatic' } : {};
+  //   }
+  // TODO: get reactTransformConfig by JSX_RUNTIME
+  const reactTransformConfig = { runtime: 'automatic' };
+  // TODO: user swc options?
+  const swcOptions = {};
+  const commonOptions = {
+    jsc: {
+      transform: {
+        react: reactTransformConfig,
+        legacyDecorator: true,
+      },
+      externalHelpers: false,
     },
-  ];
+    module: {
+      type: 'es6',
+      noInterop: false,
+      // webpack will evaluate dynamic import, so there need preserve it
+      ignoreDynamic: true,
+    },
+    env: {
+      loose: true,
+    },
+    ...swcOptions,
+  };
+
+  const jsOptions = merge({
+    jsc: {
+      parser: {
+        jsx: true,
+        dynamicImport: true,
+        functionBind: true,
+        exportDefaultFrom: true,
+        exportNamespaceFrom: true,
+        decorators: true,
+      },
+    },
+  }, commonOptions);
+
+  const tsOptions = merge({
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        tsx: true,
+        decorators: true,
+        dynamicImport: true,
+      },
+    },
+  }, commonOptions);
+
+  if (suffix === 'jsx') {
+    return jsOptions;
+  } else if (suffix === 'tsx') {
+    return tsOptions;
+  }
+  return commonOptions;
 }
 
 export default plugin;
