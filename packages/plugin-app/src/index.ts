9a062c08a82ca8d8175fb48c808e06308ed900dc
水澜 <chenjun_1011@hotmail.com>
2022-02-24 19:57:57 +0800

    feat: render document (#8)
    * feat: render document
    
    * fix: lint
    
    * fix: ts lint
    
    * chore: update lock info
    
    * fix: lint
    
    * fix: conflict
    
    * fix: conflict
    
    Co-authored-by: luhc228 <luhengchang228@gmail.com>

--- packages/plugin-app/src/index.ts
+++ packages/plugin-app/src/index.ts
@@ -1,11 +1,40 @@
 import type { FrameworkPlugin } from '@ice/service';
+import { setupRenderServer } from './ssr/server';
+import { buildServerEntry } from './ssr/build';
 
-const plugin: FrameworkPlugin = ({ registerTask, context }) => {
-  const { command } = context;
+const plugin: FrameworkPlugin = ({ registerTask, context, onHook }) => {
+  const { command, rootDir } = context;
   const mode = command === 'start' ? 'development' : 'production';
+
+  // mock routeManifest
+  const routeManifest = {
+    '/': '/src/pages/index',
+  };
+
+  onHook(`before.${command}.run`, async () => {
+    // TODO: watch file changes and rebuild
+    await buildServerEntry({
+      rootDir,
+    });
+  });
 
   registerTask('web', {
     mode,
+    middlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      middlewares.push({
+        name: 'document-render-server',
+        middleware: setupRenderServer({
+          rootDir,
+          routeManifest,
+        }),
+      });
+
+      return middlewares;
+    },
    });
 };
 
