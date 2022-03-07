72a6f3bfb71057d26d29b3d176c4fe1c357b723b
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2022-03-07 18:29:39 +0800

    feat: rework entry

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,19 +1,24 @@
 import Runtime from './runtime.js';
 import App from './App.js';
 import render from './render.js';
+import runApp from './runApp.js';
+import runServerApp from './runServerApp.js';
 import {
   RuntimePlugin,
   Context,
   BuildConfig,
+  AppConfig,
 } from './types.js';
 
 export {
   Runtime,
   App,
   render,
-
+  runApp,
+  runServerApp,
   // types
   RuntimePlugin,
   BuildConfig,
   Context,
+  AppConfig,
 };
