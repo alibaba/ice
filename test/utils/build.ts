a4f717c9bb86cb5cbe9034fb2b939d4f75df157d
shuilan.cj <shuilan.cj@taobao.com>
2022-03-09 14:12:34 +0800

    fix: lint

--- test/utils/build.ts
+++ test/utils/build.ts
@@ -26,6 +26,7 @@ export const buildFixture = function(example: string) {
   test(`setup ${example}`, async () => {
     const rootDir = path.join(__dirname, `../../examples/${example}`);
     process.env.DISABLE_FS_CACHE = 'true';
+    process.env.JEST_TEST = 'true';
     const service = await createService({ rootDir, command: 'build', commandArgs: {}, getBuiltInPlugins });
     await service.run();
   }, 120000);
