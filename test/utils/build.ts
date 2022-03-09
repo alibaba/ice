a5a7f9cc971345e135b2a2632c1a7f52c7956b64
shuilan.cj <shuilan.cj@taobao.com>
2022-03-09 15:20:08 +0800

    fix: hydrate error when preview build html

--- test/utils/build.ts
+++ test/utils/build.ts
@@ -33,12 +33,14 @@ export const buildFixture = function(example: string) {
 }
 
 export const setupBrowser: SetupBrowser = async (options) => {
-  const { example, outputDir = 'build' } = options;
+  const { example, outputDir = 'build', defaultHtml = 'index.html' } = options;
   const rootDir = path.join(__dirname, `../../examples/${example}`);
   const port = await getPort();
   const browser = new Browser({ cwd: path.join(rootDir, outputDir), port });
   await browser.start();
-  const page = await browser.page(`http://127.0.0.1:${port}/`);
+  const disableJS = true;
+  // when preview html generate by build, the path will not match the router info, so hydrate will not found the route component
+  const page = await browser.page(`http://127.0.0.1:${port}/${defaultHtml}`, disableJS);
   return {
     browser,
     page,
