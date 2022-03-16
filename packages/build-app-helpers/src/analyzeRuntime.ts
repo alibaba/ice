bbacbb0935f7b32cd5309e04b3c074d10b9beb29
ClarkXia <xiawenwu41@gmail.com>
2022-03-16 12:00:06 +0800

    fix: filter js file when analyze

--- packages/build-app-helpers/src/analyzeRuntime.ts
+++ packages/build-app-helpers/src/analyzeRuntime.ts
@@ -164,7 +164,11 @@ export default async function analyzeRuntime(files: string[], options: Options):
             if (!path.isAbsolute(importPath)) {
               importPath = getImportPath(importPath, filePath, { rootDir, alias, mode });
             }
-            if (importPath && !sourceFiles.includes(importPath) && fs.existsSync(importPath)) {
+            if (importPath
+              && !sourceFiles.includes(importPath)
+              && fs.existsSync(importPath)
+              && importPath.match(/\.(j|t)sx?$/)
+            ) {
               await analyzeFile(importPath);
             }
           }
