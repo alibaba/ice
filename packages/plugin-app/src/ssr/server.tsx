0392debd91eaff06fb2212690094a52523171615
shuilan.cj <shuilan.cj@taobao.com>
2022-03-22 11:06:32 +0800

    chore: log test result

--- packages/plugin-app/src/ssr/server.tsx
+++ packages/plugin-app/src/ssr/server.tsx
@@ -9,12 +9,17 @@ export function setupRenderServer(options) {
   const routes = formatNestedRouteManifest(routeManifests);
 
   return async (req, res) => {
+    console.log('----------------------------------------------------------------');
+    console.log(req.path);
+
     // If not match pages routes, hand over to webpack dev server for processing
     let matches = matchRoutes(routes, req.path);
     if (!matches) return;
 
+    console.log(entry);
     const serverEntry = await import(entry);
 
+    console.log(serverEntry);
     // TODO: disable cache
     const html = renderDocument(path.join(outDir, 'document.js'));
 
