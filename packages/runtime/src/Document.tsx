d1d5912394da588fff08eefa2c137da0d2231060
ClarkXia <xiawenwu41@gmail.com>
2022-05-09 17:05:58 +0800

    fix: hmr failed when reload (#202)

--- packages/runtime/src/Document.tsx
+++ packages/runtime/src/Document.tsx
@@ -69,7 +69,8 @@ export function Scripts() {
   const routeScripts = getScripts(matches, routesConfig);
   const pageAssets = getPageAssets(matches, assetsManifest);
   const entryAssets = getEntryAssets(assetsManifest);
-  const scripts = pageAssets.concat(entryAssets).filter(path => path.indexOf('.js') > -1);
+  // entry assets need to be load before page assets
+  const scripts = entryAssets.concat(pageAssets).filter(path => path.indexOf('.js') > -1);
 
   const matchedIds = matches.map(match => match.route.id);
 
