7df30b5eb537e31156f7719b834abfe16df932bb
luhc228 <44047106+luhc228@users.noreply.github.com>
2022-05-27 16:14:48 +0800

    fix: oom in dev (#234)
    * fix: oom in dev
    
    * fix: lint

--- packages/webpack-config/src/index.ts
+++ packages/webpack-config/src/index.ts
@@ -271,7 +271,18 @@ const getWebpackConfig: GetWebpackConfig = ({ rootDir, config, webpack }) => {
       https,
     },
   };
+  // tnpm / cnpm 安装时，webpack 5 的持久缓存无法生成，长时间将导致 OOM
+  // 原因：[managedPaths](https://webpack.js.org/configuration/other-options/#managedpaths) 在 tnpm / cnpm 安装的情况下失效，导致持久缓存在处理 node_modules
+  // 通过指定 [immutablePaths](https://webpack.js.org/configuration/other-options/#immutablepaths) 进行兼容
+  // 依赖路径中同时包含包名和版本号即可满足 immutablePaths 的使用
 
+  // 通过安装后的 package.json 中是否包含 __npminstall_done 字段来判断是否为 tnpm / cnpm 安装模式
+  if (require('../package.json').__npminstall_done) {
+    const nodeModulesPath = path.join(rootDir, 'node_modules');
+    webpackConfig.snapshot = {
+      immutablePaths: [nodeModulesPath],
+    };
+  }
   if (dev) {
     if (!webpackConfig.optimization) {
       webpackConfig.optimization = {};
