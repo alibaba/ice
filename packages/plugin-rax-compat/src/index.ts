b5791a461a80081aa4686859f39689a1e58def75
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-06-06 18:55:19 +0800

    feat: add alias for plugin rax compat

--- packages/plugin-rax-compat/src/index.ts
+++ packages/plugin-rax-compat/src/index.ts
@@ -2,12 +2,21 @@ import type { Plugin } from '@ice/types';
 
 const plugin: Plugin = ({ onGetConfig }) => {
   onGetConfig((config) => {
+    // @ts-ignore
     config.configureWebpack = [...(config.configureWebpack || []), (webpackConfig) => {
-      webpackConfig.resolve.alias = {
-        ...(webpackConfig.resolve.alias || {}),
+      return Object.assign(webpackConfig.resolve.alias, {
+        // Add rax compat packages.
         rax: 'rax-compat',
-      };
-      return webpackConfig;
+        'rax-children': 'rax-compat/children',
+        'rax-clone-element': 'rax-compat/clone-element',
+        'rax-create-class': 'rax-compat/create-class',
+        'rax-create-factory': 'rax-compat/create-factory',
+        'rax-create-portal': 'rax-compat/create-portal',
+        'rax-find-dom-node': 'rax-compat/find-dom-node',
+        'rax-is-valid-element': 'rax-compat/is-valid-element',
+        'rax-set-native-props': 'rax-compat/set-native-props',
+        'rax-unmount-component-at-node': 'rax-compat/unmount-component-at-node',
+      });
     }];
   });
 };
