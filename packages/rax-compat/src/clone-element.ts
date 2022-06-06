607dae4418dad4c2397b01f1626dc66457ddba84
answershuto <answershuto@gmail.com>
2022-06-06 16:38:02 +0800

    :sparkles: feat: compat packages of rax.

---
+++ packages/rax-compat/src/clone-element.ts
@@ -0,0 +1,5 @@
+import { cloneElement } from 'react';
+
+// See https://github.com/alibaba/rax/blob/master/packages/rax-clone-element/src/index.js
+// Imported by 'rax-compat/cloneElement'
+export default cloneElement;
