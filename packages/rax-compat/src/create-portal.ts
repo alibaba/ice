040d88ac43c5b169fb5a5ea876488b49ad559c46
answershuto <answershuto@gmail.com>
2022-06-06 16:38:02 +0800

    :sparkles: feat: compat packages of rax.

---
+++ packages/rax-compat/src/create-portal.ts
@@ -0,0 +1,5 @@
+import { createPortal } from 'react-dom';
+
+// See https://github.com/alibaba/rax/blob/master/packages/rax-create-portal/src/index.js
+// Imported by 'rax-compat/createPortal'
+export default createPortal;
