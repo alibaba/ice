cd96b3fcb9a778abd645db592d9c9584953a2a41
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-06-07 14:23:35 +0800

    chore: restore sample project

--- examples/basic-project/src/app.tsx
+++ examples/basic-project/src/app.tsx
@@ -1,5 +1,14 @@
 import { GetAppData, GetAppConfig } from 'ice';
 
+if (process.env.ICE_CORE_ERROR_BOUNDARY === 'true') {
+  console.error('__REMOVED__');
+}
+
+console.log('__LOG__');
+console.warn('__WARN__');
+console.error('__ERROR__');
+console.log('process.env.HAHA', process.env.HAHA);
+
 export const getAppData: GetAppData = () => {
   return new Promise((resolve) => {
     resolve({
