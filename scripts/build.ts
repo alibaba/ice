d70cc9e7a0e4d86ec89d72620bebad35f4d44bc5
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-06-13 16:18:28 +0800

    chore: change order

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,15 +1,16 @@
 import { run } from './shell';
 
 (async () => {
+  // DO NOT CHANGE ORDERS due to dependencies.
   await run('npm run build', { cwd: './packages/bundles' });
-  await run('npm run build', { cwd: './packages/types' });
   await run('npm run build', { cwd: './packages/jsx-runtime' });
   await run('npm run build', { cwd: './packages/runtime' });
+  await run('npm run build', { cwd: './packages/route-manifest' });
+  await run('npm run build', { cwd: './packages/types' });
+  await run('npm run build', { cwd: './packages/webpack-config' });
   await run('npm run build', { cwd: './packages/ice' });
   await run('npm run build', { cwd: './packages/plugin-auth' });
   await run('npm run build', { cwd: './packages/rax-compat' });
-  await run('npm run build', { cwd: './packages/route-manifest' });
-  await run('npm run build', { cwd: './packages/webpack-config' });
 })().catch((e) => {
   console.trace(e);
   process.exit(128);
