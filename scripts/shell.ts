b0ba0b8548afdbfb78042747cdd1dcc013bea29d
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-06-13 15:59:11 +0800

    chore: monorepo

--- scripts/shell.ts
+++ scripts/shell.ts
@@ -1,6 +1,6 @@
 import { execaCommand } from 'execa';
 
-export async function run(command: string) {
+export async function run(command: string, options: object) {
   console.log(`[RUN]: ${command}`);
-  return execaCommand(command, { stdio: 'inherit' });
+  return execaCommand(command, { stdio: 'inherit', cwd: options['cwd'] });
 }
