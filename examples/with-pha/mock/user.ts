e097ad9022e8c11b62f1a9031f96a53519f5b976
ClarkXia <xiawenwu41@gmail.com>
2022-06-20 17:56:29 +0800

    chore: bump dependencies version

--- examples/with-pha/mock/user.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-
-export default {
-  'GET /api/users': ['a', 'b'],
-  'POST /api/users/:id': (req: Request, res: Response) => {
-    const { id } = req.params;
-    res.send({ id: id });
-  },
-};
