2fb58f3f974c6465158596347d3b56419eaf7e5a
answershuto <answershuto@gmail.com>
2022-06-27 12:10:13 +0800

    test: add test for rax compat

---
+++ packages/rax-compat/tests/createReactClass.test.tsx
@@ -0,0 +1,22 @@
+import { expect, it, describe } from 'vitest';
+import React from 'react';
+import { Component, createElement } from '../src/index';
+import createReactClass from '../src/create-class';
+import { render } from '@testing-library/react';
+
+describe('createReactClass', () => {
+  it('basic', () => {
+    const ReactClass = createReactClass({
+      name: '',
+      id: '',
+      render: function() {
+        return <div data-testid={this.props.id}>Hello, {this.props.name}</div>;
+      }
+    });
+
+    const wrapper = render(<ReactClass id="reactClassId" name="raxCompat" />);
+    let res = wrapper.getAllByTestId('reactClassId');
+
+    expect(res.length).toBe(1);
+  });
+});
