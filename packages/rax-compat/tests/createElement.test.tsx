6423d19dcfdef33157e0376fceadecdfbcace6c1
answershuto <answershuto@gmail.com>
2022-06-28 14:40:55 +0800

    test: modify test of createElement

--- packages/rax-compat/tests/createElement.test.tsx
+++ packages/rax-compat/tests/createElement.test.tsx
@@ -5,18 +5,12 @@ import { render } from '@testing-library/react';
 
 describe('createElement', () => {
   it('basic', () => {
-    class TextComponent extends Component {
-      render() {
-        return createElement(
-          'div',
-          { 'data-testid': 'test' },
-          createElement('p', null, 'hello world')
-        );
-      }
-    }
-
-    const wrapper = render(<TextComponent />);
-    const node = wrapper.queryByTestId('test');
-    expect(node.childNodes[0].tagName).toBe('P');
+    const str = 'hello world';
+    const wrapper = render(createElement(
+      'div',
+      null,
+      str
+    ));
+    expect(wrapper.container.childNodes[0].textContent).toBe(str);
   });
 });
