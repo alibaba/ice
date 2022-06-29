61c28f3f0920bc8ac9b4242d8e5861b66a84d555
answershuto <answershuto@gmail.com>
2022-06-29 14:31:25 +0800

    chore: remove unuse impoty

--- packages/rax-compat/tests/children.test.tsx
+++ packages/rax-compat/tests/children.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { expect, it, describe } from 'vitest';
-import { Component } from '../src/index';
 import { render } from '@testing-library/react';
 
 const arrText = ['hello', 'rax'];
