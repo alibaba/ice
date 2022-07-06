bab8dc3eb004b8be7c5143265806d08aada508dc
smat <112093112@qq.com>
2022-07-06 14:46:48 +0800

    Update proxy.js (#5369)

--- packages/build-user-config/src/userConfig/proxy.js
+++ packages/build-user-config/src/userConfig/proxy.js
@@ -24,12 +24,17 @@ module.exports = (config, proxyConfig) => {
           },
           onError: function onError(err, req, res) {
             // proxy server error can't trigger onProxyRes
-            res.writeHead(500, {
-              'x-proxy-by': 'ice-proxy',
-              'x-proxy-match': match,
-              'x-proxy-target': target,
-            });
-            res.end(`proxy server error: ${err.message}`);
+            if (res) {
+              res.writeHead(500, {
+                'x-proxy-by': 'ice-proxy',
+                'x-proxy-match': match,
+                'x-proxy-target': target,
+              });
+              res.end(`proxy server error: ${err.message}`);
+            } else {
+              // res is undefined when proxy for websocket
+              console.log(`proxy server error: ${err.message}`);
+            }
           },
         }, proxyRule, { context: match });
       }
