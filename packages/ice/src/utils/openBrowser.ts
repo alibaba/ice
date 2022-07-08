ed147421bb3b9b6d6e6e5cefc969147b3af40ee3
luhc228 <44047106+luhc228@users.noreply.github.com>
2022-07-08 12:24:56 +0800

    fix: fail to reuse existed tab (#320)
    * fix: fail to reuse existed tab
    
    * chore: move openChrome.applescript to outside

--- packages/ice/src/utils/openBrowser.ts
+++ packages/ice/src/utils/openBrowser.ts
@@ -80,6 +80,8 @@ function startBrowserProcess(browser, url, args) {
     const supportedChromiumBrowsers = [
       'Google Chrome Canary',
       'Google Chrome',
+      'Google Chrome Dev',
+      'Google Chrome Beta',
       'Microsoft Edge',
       'Brave Browser',
       'Vivaldi',
@@ -98,7 +100,7 @@ function startBrowserProcess(browser, url, args) {
             chromiumBrowser
             }"`,
           {
-            cwd: __dirname,
+            cwd: path.join(__dirname, '../../'),
             stdio: 'ignore',
           },
         );
