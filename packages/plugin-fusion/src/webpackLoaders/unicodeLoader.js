44f8660479ebbdf3a8d4f5a855b8ab682575905e
ClarkXia <xiawenwu41@gmail.com>
2022-08-11 16:35:50 +0800

    fix: return sourceMap of custom loader

--- packages/plugin-fusion/src/webpackLoaders/unicodeLoader.js
+++ packages/plugin-fusion/src/webpackLoaders/unicodeLoader.js
@@ -1,7 +1,8 @@
 const convertCharStr2CSS = require('../utils/convertCharStr');
 
-module.exports = (source) => {
-  return source.replace(/content:\s*(?:'|")([\u0080-\uffff])(?:'|")/g, (str, $1) => {
+module.exports = (source, sourceMap) => {
+  const callback = this.async();
+  callback(null, source.replace(/content:\s*(?:'|")([\u0080-\uffff])(?:'|")/g, (str, $1) => {
     return `content: "${convertCharStr2CSS($1)}"`;
-  });
+  }), sourceMap) ;
 };
