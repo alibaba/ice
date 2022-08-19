3cf525fe98ea733e2b0bb1da8448e0d47d7ed2ea
srhb18 <srhb18@163.com>
2022-08-19 12:12:05 +0800

    feat: no defaultLocale return from getDetectedLocaleFromPathname & adjust detect locale sequence in getDetectedLocale function (#5458)
    * feat: add setting for block cookie setting i18n locale
    
    * add I18nAppConfig to IAppConfig & change appConfig key to i18n
    
    * fix: fix locale will always in default when execute history.push in cookie blocked mode
    
    * fix: fix modifyHistory params delivery error
    
    * feat: no defaultLocale return from getDetectedLocaleFromPathname & adjust detect locale sequence in getDetectedLocale function
    
    Co-authored-by: liyidong <liyidong@baidu.com>
    Co-authored-by: leo.lyd <leo.lyd@alibaba-inc.com>

--- packages/plugin-i18n/src/utils/getDetectedLocaleFromPathname.ts
+++ packages/plugin-i18n/src/utils/getDetectedLocaleFromPathname.ts
@@ -4,12 +4,12 @@ import replaceBasename from './replaceBasename';
  * 开启国际化路由时，通过 pathname 获取当前语言
  */
 function getDetectedLocaleFromPathname(
-  { 
-    pathname, 
+  {
+    pathname,
     locales,
     defaultLocale,
     basename,
-  }: { 
+  }: {
     pathname: string;
     locales: string[];
     defaultLocale: string;
@@ -18,7 +18,8 @@ function getDetectedLocaleFromPathname(
   const normalizedPathname = replaceBasename(pathname, basename);
   const pathnameParts = normalizedPathname.split('/').filter(pathnamePart => pathnamePart);
 
-  let detectedLocale = defaultLocale;
+  // 从路径获取locale时，默认路径不再命中默认语言，返回空字符串，调用业务需执行兜底默认语言
+  let detectedLocale = '';
   for (let index = 0; index < locales.length; index++) {
     const locale = locales[index];
     if (pathnameParts[0] === locale) {
