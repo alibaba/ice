447b8e9d84817506617bfa287719bd4ea575cefa
ZeroLing <i@zeroling.com>
2022-09-07 10:36:15 +0800

    feat: support baseurl in internal website (#511)

--- website/src/components/Button/index.jsx
+++ website/src/components/Button/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { join } from '../../utils/path';
 import styles from './button.module.css';
 
 function Button({ primary = true, url, children }) {
+  const context = useDocusaurusContext();
+  const source = join(context.siteConfig.baseUrl, url);
   return (
-    <a href={url} className={clsx(styles.button, primary && styles.primary)}>
+    <a href={source} className={clsx(styles.button, primary && styles.primary)}>
       {children}
     </a>
   );
