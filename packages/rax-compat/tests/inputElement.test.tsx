147876fbec1566e1faa8e2e13bbd033413a5e518
染陌同学 <answershuto@gmail.com>
2022-09-09 10:38:40 +0800

    Fix/input onchange (#507)
    * fix: fix input onchange is invalid
    
    * fix: onChange event is SyntheticEvent in React but it is dom event in Rax
    
    * fix: prevent INPUT from being created repeatedly
    
    * fix: should update value when props update
    
    * test: add test for input value
    
    * chore: update version
    
    * test: add test for inputElement should not recreate when update props
    
    * feat: modify useCallback
    
    * test: add test for onchange
    
    * feat: modify useCallback
    
    * fix: usecallback should not use in createElement
    
    * chore: remove line
    
    * chore: remove onChange
    
    * chore: dealwith lint
    
    * chore: modify transformPrototypes to transformPrototype

---
+++ packages/rax-compat/tests/inputElement.test.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { expect, it, describe } from 'vitest';
+import { render } from '@testing-library/react';
+import { useState } from '../src/index';
+import { createElement } from '../src/create-element';
+
+describe('inputElement', () => {
+  it('should work with update input value', () => {
+    function TestInput() {
+      const [val, setVal] = useState('input value');
+      return (<div>
+        <input
+          data-testid="inputValue"
+          value={val}
+        />
+
+        <div
+          data-testid="inputValueDiv"
+          onClick={() => {
+            setVal('111');
+          }}
+        >
+          click me...
+        </div>
+      </div>);
+    }
+
+    const wrapper = render(createElement(TestInput));
+    wrapper.queryByTestId('inputValueDiv')?.click();
+    const node = wrapper.queryByTestId('inputValue');
+
+    setTimeout(() => {
+      // Wait for click handler.
+      expect(node.value).toBe('111');
+    }, 0);
+  });
+
+  it('inputElement should not recreate when update props', () => {
+    function TestInput() {
+      const [val, setVal] = useState('input value');
+      return (<div>
+        <input
+          data-testid="sameInput"
+          value={val}
+        />
+
+        <div
+          data-testid="sameInputDiv"
+          onClick={() => {
+            setVal('111');
+          }}
+        >
+          click me...
+        </div>
+      </div>);
+    }
+
+    const wrapper = render(createElement(TestInput));
+
+    const node = wrapper.queryByTestId('sameInput');
+    node?.setAttribute('date-value', 'val');
+
+    wrapper.queryByTestId('sameInputDiv')?.click();
+
+    setTimeout(() => {
+      // Wait for click handler.
+      expect(node?.getAttribute('date-value')).toBe('val');
+    }, 0);
+  });
+
+  it('should work with onChange', () => {
+    return new Promise((resolve) => {
+      function TestInput() {
+        return createElement('input', {
+          'data-testid': 'changeInput',
+          onChange: () => resolve(),
+        });
+      }
+
+      const wrapper = render(createElement(TestInput));
+
+      const node = wrapper.queryByTestId('changeInput');
+      node!.dispatchEvent(new Event('change'));
+    });
+  });
+});
