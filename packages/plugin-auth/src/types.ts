0e7735c35802c38b72b577be0e0e9530a7fd80b1
ClarkXia <xiawenwu41@gmail.com>
2022-09-16 14:34:24 +0800

    feat: support initialize auth by app data (#519)

--- packages/plugin-auth/src/types.ts
+++ packages/plugin-auth/src/types.ts
@@ -9,7 +9,7 @@ export interface AuthConfig {
 
 export type AuthType = Record<string, boolean>;
 export type ContextType = [AuthType, React.Dispatch<React.SetStateAction<AuthType>>];
-export type Auth = () => Promise<AuthConfig> | AuthConfig;
+export type Auth = (data?: any) => Promise<AuthConfig> | AuthConfig;
 
 export function defineAuthConfig(fn: Auth) {
   return fn;
