d7bdb3d8c35026319f8cf45040748c26beecc4f5
luhc228 <luhengchang228@126.com>
2022-11-16 16:13:31 +0800

    feat: basename example

---
+++ examples/with-basename/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'ice';
+
+export default function Home() {
+  return (
+    <>
+      <h2>Home</h2>
+      <Link to="/about">about</Link>
+    </>
+  );
+}
---
+++ examples/with-basename/src/pages/layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'ice';
+
+export default () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Outlet />
+    </div>
+  );
+};
+
