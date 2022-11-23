e226682d135aa0d75a7034727c3d66a9d550e15c
luhc228 <luhengchang228@126.com>
2022-11-23 18:18:55 +0800

    fix: test error

--- packages/ice/src/service/serverCompiler.ts
+++ packages/ice/src/service/serverCompiler.ts
@@ -173,7 +173,7 @@ export function createServerCompiler(options: Options) {
 
       if (removeOutputs && esbuildResult.metafile) {
         // build/server/a.mjs -> a.mjs
-        const currentOutputFiles = Object.keys(esbuildResult.metafile.outputs).map(output => output.replace(RegExp(`^${path.relative(rootDir, buildOptions.outdir)}${path.sep}`), ''));
+        const currentOutputFiles = Object.keys(esbuildResult.metafile.outputs).map(output => path.join(process.cwd(), output).replace(RegExp(`^${buildOptions.outdir}${path.sep}`), ''));
         const allOutputFiles = fg.sync('**', { cwd: buildOptions.outdir });
         const outdatedFiles = difference(allOutputFiles, currentOutputFiles);
         outdatedFiles.forEach(outdatedFile => fse.removeSync(path.join(buildOptions.outdir, outdatedFile)));
