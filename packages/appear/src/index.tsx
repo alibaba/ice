b209050efe5c175e8bedcf989b86e7466fc996ee
answershuto <answershuto@gmail.com>
2022-12-09 16:30:06 +0800

    fix: fix ref of Visibility

--- packages/appear/src/index.tsx
+++ packages/appear/src/index.tsx
@@ -2,17 +2,18 @@ import { Children, useRef, useEffect, useCallback } from 'react';
 import { isFunction } from './type';
 import { observerElement, VisibilityChangeEvent } from './visibility';
 
-function VisibilityChange(props: any) {
+function VisibilityChange(props: any, ref: any) {
   const {
     onAppear,
     onDisappear,
     children,
   } = props;
 
-  const ref = useRef(null);
+  const defaultRef = useRef();
+  const visibilityRef = ref || defaultRef;
 
   const listen = useCallback((eventName: string, handler: Function) => {
-    const { current } = ref;
+    const { current } = visibilityRef;
     // Rax components will set custom ref by useImperativeHandle.
     // So We should get eventTarget by _nativeNode.
     // https://github.com/raxjs/rax-components/blob/master/packages/rax-textinput/src/index.tsx#L151
@@ -22,18 +23,18 @@ function VisibilityChange(props: any) {
       eventTarget.addEventListener(eventName, handler);
     }
     return () => {
-      const { current } = ref;
+      const { current } = visibilityRef;
       if (current) {
         const eventTarget = current._nativeNode || current;
         eventTarget.removeEventListener(eventName, handler);
       }
     };
-  }, [ref]);
+  }, [visibilityRef]);
 
-  useEffect(() => listen(VisibilityChangeEvent.appear, onAppear), [ref, onAppear, listen]);
-  useEffect(() => listen(VisibilityChangeEvent.disappear, onDisappear), [ref, onDisappear, listen]);
+  useEffect(() => listen(VisibilityChangeEvent.appear, onAppear), [visibilityRef, onAppear, listen]);
+  useEffect(() => listen(VisibilityChangeEvent.disappear, onDisappear), [visibilityRef, onDisappear, listen]);
 
-  return Children.only({ ...children, ref });
+  return Children.only({ ...children, visibilityRef });
 }
 
 export default VisibilityChange;
