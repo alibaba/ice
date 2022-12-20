8c7a9fdc20bb727ed5ba631dfae92bdcae2f42c7
ClarkXia <xiawenwu41@gmail.com>
2022-12-20 14:16:15 +0800

    fix: reduce code size by externalHelpers

--- packages/webpack-config/src/unPlugins/compilation.ts
+++ packages/webpack-config/src/unPlugins/compilation.ts
@@ -4,7 +4,7 @@ import type { SwcConfig, ReactConfig } from '@ice/bundles';
 import type { UnpluginOptions } from '@ice/bundles/compiled/unplugin/index.js';
 import lodash from '@ice/bundles/compiled/lodash/index.js';
 import type { Config } from '../types.js';
-import transformCoreJs from '../utils/transformCoreJs.js';
+import transformImport from '../utils/transformImport.js';
 
 const { merge } = lodash;
 
@@ -150,11 +150,10 @@ const compilationPlugin = (options: Options): UnpluginOptions => {
         const { code } = output;
         let { map } = output;
         return {
-          code: polyfill
-            ? await transformCoreJs(
-              code,
-              coreJsPath,
-            ) : code,
+          code: await transformImport(
+            code,
+            coreJsPath,
+          ),
           map,
         };
       } catch (error) {
@@ -199,7 +198,7 @@ function getJsxTransformOptions({
       },
       // This option will greatly reduce your file size while bundling.
       // This option depends on `@swc/helpers`.
-      externalHelpers: false,
+      externalHelpers: true,
     },
     module: {
       type: 'es6',
