c1d9e454e58de04f966984782ce0d9b562342d1e
长喆 <2316371523@qq.com>
2022-12-23 09:36:08 +0800

    feat: support lazy parse & add type (#5750)
    * feat: lazy parse
    
    * fix: loadDataByCustomFetcher type

--- packages/runtime/src/dataLoader.ts
+++ packages/runtime/src/dataLoader.ts
@@ -1,4 +1,4 @@
-import type { DataLoaderConfig, DataLoaderResult, RuntimeModules, AppExport, StaticRuntimePlugin, CommonJsRuntime } from './types.js';
+import type { DataLoaderConfig, DataLoaderResult, RuntimeModules, AppExport, StaticRuntimePlugin, CommonJsRuntime, DataLoader, StaticDataLoader } from './types.js';
 import getRequestContext from './requestContext.js';
 
 interface Loaders {
@@ -41,25 +41,35 @@ export function setFetcher(customFetcher) {
 /**
  * Parse template for static dataLoader.
  */
-export function parseTemplate(config) {
+export function parseTemplate(config: StaticDataLoader) {
   const queryParams = {};
-  if (location.search.includes('?')) {
-    location.search.substring(1).split('&').forEach(query => {
-      const res = query.split('=');
-      // ?test=1&hello=world
-      if (res[0] !== undefined && res[1] !== undefined) {
-        queryParams[res[0]] = res[1];
+  const getQueryParams = () => {
+    if (Object.keys(queryParams).length === 0) {
+      if (location.search.includes('?')) {
+        location.search.substring(1).split('&').forEach(query => {
+          const res = query.split('=');
+          // ?test=1&hello=world
+          if (res[0] !== undefined && res[1] !== undefined) {
+            queryParams[res[0]] = res[1];
+          }
+        });
       }
-    });
-  }
+    }
+    return queryParams;
+  };
 
   const cookie = {};
-  document.cookie.split(';').forEach(c => {
-    const res = c.split('=');
-    if (res[0] !== undefined && res[1] !== undefined) {
-      cookie[res[0].trim()] = res[1].trim();
+  const getCookie = () => {
+    if (Object.keys(cookie).length === 0) {
+      document.cookie.split(';').forEach(c => {
+        const res = c.split('=');
+        if (res[0] !== undefined && res[1] !== undefined) {
+          cookie[res[0].trim()] = res[1].trim();
+        }
+      });
     }
-  });
+    return cookie;
+  };
 
   const regexp = /\$\{(queryParams|cookie|storage)(\.(\w|-)+)?}/g;
   let strConfig = JSON.stringify(config);
@@ -67,10 +77,10 @@ export function parseTemplate(config) {
     if (item && item[0] && item[1] && item[2].startsWith('.') && item[2]) {
       if (item[1] === 'queryParams') {
         // Replace query params.
-        strConfig = strConfig.replace(item[0], queryParams[item[2].substring(1)]);
+        strConfig = strConfig.replace(item[0], getQueryParams()[item[2].substring(1)]);
       } else if (item[1] === 'cookie') {
         // Replace cookie.
-        strConfig = strConfig.replace(item[0], cookie[item[2].substring(1)]);
+        strConfig = strConfig.replace(item[0], getCookie()[item[2].substring(1)]);
       } else if (item[1] === 'storage') {
         // Replace storage.
         strConfig = strConfig.replace(item[0], localStorage.getItem(item[2].substring(1)));
@@ -84,7 +94,7 @@ export function parseTemplate(config) {
   return JSON.parse(strConfig);
 }
 
-export function loadDataByCustomFetcher(config: Object) {
+export function loadDataByCustomFetcher(config: StaticDataLoader) {
   let parsedConfig = config;
   try {
     parsedConfig = parseTemplate(config);
