dd2741dd66df92c0decbe3973b82628bd0b36b2d
ClarkXia <xiawenwu41@gmail.com>
2023-01-10 11:57:19 +0800

    fix: consider format of cjs

--- packages/ice/src/service/serverCompiler.ts
+++ packages/ice/src/service/serverCompiler.ts
@@ -154,7 +154,7 @@ export function createServerCompiler(options: Options) {
       define,
       absWorkingDir: rootDir,
       external: Object.keys(externals),
-      banner: customBuildOptions.platform === 'node'
+      banner: customBuildOptions.platform === 'node' && server?.format !== 'cjs'
         ? {
             // See https://github.com/evanw/esbuild/issues/1921#issuecomment-1152991694
             js: 'import { createRequire } from \'module\';const require = createRequire(import.meta.url);',
