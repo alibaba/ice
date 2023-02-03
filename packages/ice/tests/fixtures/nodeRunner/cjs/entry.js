9943cd373b485ffabfef261971d6c20e3eb7c9b7
ClarkXia <xiawenwu41@gmail.com>
2023-02-03 11:41:30 +0800

    test: test case

---
+++ packages/ice/tests/fixtures/nodeRunner/cjs/entry.js
@@ -0,0 +1,3 @@
+const { a } = require('./a.js');
+
+module.exports = a;
--- packages/ice/tests/nodeRunner.test.ts
+++ packages/ice/tests/nodeRunner.test.ts
@@ -8,19 +8,32 @@ import NodeRunner from '../src/service/nodeRunner';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 describe('node runner', () => {
+  const basicResolve = async (id, importee) => {
+    return path.resolve(path.dirname(importee!), id);
+  };
+  const basicLoad = async (id) => {
+    const code = fs.readFileSync(id, 'utf-8');
+    return {
+      code,
+    };
+  };
+
   it('basic', async () => {
     const rootDir = path.join(__dirname, './fixtures/nodeRunner/basic/');
     const nodeRunner = new NodeRunner({
       rootDir,
-      resolveId: async (id, importee) => {
-        return path.resolve(path.dirname(importee!), id);
-      },
-      load: async (id) => {
-        const code = fs.readFileSync(id, 'utf-8');
-        return {
-          code,
-        };
-      },
+      resolveId: basicResolve,
+      load: basicLoad,
+    });
+    const result = await nodeRunner.run('entry.js');
+    expect(result.default).toBe(1);
+  });
+  it('basic', async () => {
+    const rootDir = path.join(__dirname, './fixtures/nodeRunner/cjs/');
+    const nodeRunner = new NodeRunner({
+      rootDir,
+      resolveId: basicResolve,
+      load: basicLoad,
     });
     const result = await nodeRunner.run('entry.js');
     expect(result.default).toBe(1);
