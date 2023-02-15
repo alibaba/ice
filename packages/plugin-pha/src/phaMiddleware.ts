227cb3abd1bff1befcc4d9c7abec64bb648fff08
shuilan.cj <shuilan.cj@taobao.com>
2023-02-15 16:42:01 +0800

    fix: request path

--- packages/plugin-pha/src/phaMiddleware.ts
+++ packages/plugin-pha/src/phaMiddleware.ts
@@ -72,9 +72,10 @@ const createPHAMiddleware = ({
           sendResponse(res, JSON.stringify(multipleManifest[manifestKey]), 'application/json');
           return;
         }
+      } else if (requestPath === 'manifest.json') {
+        sendResponse(res, JSON.stringify(phaManifest), 'application/json');
+        return;
       }
-
-      sendResponse(res, JSON.stringify(phaManifest), 'application/json');
     }
     next();
   };
