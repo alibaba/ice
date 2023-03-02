dfdfa4745a9e80311a13c30a68e5aad46372d88d
HomyeeKing <HomyeeKing@gmail.com>
2023-03-02 23:27:02 +0800

    feat: revert useFnTimes

--- packages/appear/src/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-export const useFnTimes = (fn: Function, times: number) => {
-  const count = useRef(1);
-  if (count.current <= times) {
-    ++count.current;
-    return fn;
-  }
-  return undefined;
-};
