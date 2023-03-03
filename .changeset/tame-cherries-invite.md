f1aafec8637740b0119c1288520146cf0e9430be
ClarkXia <xiawenwu41@gmail.com>
2023-03-03 14:09:46 +0800

    fix: make sure server env variable is always false

---
+++ .changeset/tame-cherries-invite.md
@@ -0,0 +1,5 @@
+---
+'@ice/webpack-config': patch
+---
+
+fix: make sure ssr / ssg env variable is always false in csr
--- packages/webpack-config/src/index.ts
+++ packages/webpack-config/src/index.ts
@@ -102,6 +102,9 @@ function getDefineVars(
   return {
     ...define,
     ...runtimeDefineVars,
+    // Make sure ICE_CORE_SSR and ICE_CORE_SSG is always false in csr mode.
+    'process.env.ICE_CORE_SSR': 'false',
+    'process.env.ICE_CORE_SSG': 'false',
   };
 }
 
