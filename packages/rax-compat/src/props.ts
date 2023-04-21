faee94c234c46216716ec77008f7b72e58beb3d7
luhc228 <luhengchang228@126.com>
2023-04-21 13:32:25 +0800

    chore: add .js suffix

--- packages/rax-compat/src/props.ts
+++ packages/rax-compat/src/props.ts
@@ -1,6 +1,6 @@
 import type { ComponentProps, JSXElementConstructor } from 'react';
-import { registrationNameToReactEvent } from './events';
-import possibleStandardNames from './possible-standard-names';
+import { registrationNameToReactEvent } from './events.js';
+import possibleStandardNames from './possible-standard-names.js';
 
 // String#indexOf is usually faster than any other methods.
 // https://www.measurethat.net/Benchmarks/Show/12312/0/js-regex-vs-startswith-vs-indexof-vs-endswith-vs-charat
--- packages/rax-compat/src/runtime/jsx-dev-runtime.ts
+++ packages/rax-compat/src/runtime/jsx-dev-runtime.ts
@@ -1,7 +1,7 @@
 
 // @ts-ignore
 import { jsxDEV as _jsxDEV } from 'react/jsx-dev-runtime';
-import { createJSXElementFactory } from '../compat/element';
+import { createJSXElementFactory } from '../compat/element.js';
 
 export { Fragment } from 'react';
 export const jsxDEV = createJSXElementFactory(_jsxDEV);
