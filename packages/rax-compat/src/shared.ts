7e40174a9d80aed8bcded494475627d6b4bf2f76
soar.gy <soar.gy@alibaba-inc.com>
2023-05-15 14:08:01 +0800

    style: optimise

--- packages/rax-compat/src/shared.ts
+++ packages/rax-compat/src/shared.ts
@@ -1,61 +1,59 @@
-import react, { ReactElement } from 'react';
+import * as React from 'react';
 import { unmountComponentAtNode } from 'react-dom';
+
+import { ReactElement } from 'react';
 type ChildrenElement<T> = ChildrenElement<T>[] | T;
 
+// ref: https://github.com/alibaba/rax/blob/master/packages/rax/src/vdom/instance.js#L11
 const INSTANCE_KEY = '_r';
 const REACT_ELEMENT_TYPE = Symbol.for('react.element');
 function ReactElement(type: any, key: any, ref: any, self: any, source: any, owner: any, props: any) {
-    const element = {
-      // This tag allows us to uniquely identify this as a React Element
-      $$typeof: REACT_ELEMENT_TYPE,
+  const element = {
+    // This tag allows us to uniquely identify this as a React Element
+    $$typeof: REACT_ELEMENT_TYPE,
 
-      // Built-in properties that belong on the element
-      type: type,
-      key: key,
-      ref: ref,
-      props: props,
+    // Built-in properties that belong on the element
+    type: type,
+    key: key,
+    ref: ref,
+    props: props,
 
-      // Record the component responsible for creating this element.
-      _owner: owner,
-    };
-    return element;
+    // Record the component responsible for creating this element.
+    _owner: owner,
+  };
+  return element;
 }
 
 // Mocked `Rax.shared`.
 const shared = {
   get Element() {
     return function (type: any, key: any, ref: any, props: any, owner: any) {
-        return ReactElement(type, key, ref, /* self */ undefined, /* source */ undefined, owner, props);
+      return ReactElement(type, key, ref, /* self */ undefined, /* source */ undefined, owner, props);
     };
   },
   Host: {
     get owner() {
-        return (react as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner.current;
+      return (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner.current;
     },
   },
   Instance: {
     get(node: any) {
-        let instance = node[INSTANCE_KEY] || {};
-        return {
-            ...instance,
-            _internal: {
-                ...instance._internal,
-                unmountComponent: () => {
-                    if (node.parentNode?.removeChild) {
-                        node.parentNode.removeChild(node);
-                    }
-                    return unmountComponentAtNode(node);
-                },
-            },
-        };
+      return Object.assign({}, node[INSTANCE_KEY], {
+        _internal: {
+          unmountComponent: () => {
+            node.parentNode?.removeChild(node);
+            return unmountComponentAtNode(node);
+          },
+        },
+      });
     },
     remove(node: any) {
-        node[INSTANCE_KEY] = null;
+      node[INSTANCE_KEY] = null;
     },
     set(node: any, instance: any) {
-        if (!node[INSTANCE_KEY]) {
-            node[INSTANCE_KEY] = instance;
-        }
+      if (!node[INSTANCE_KEY]) {
+        node[INSTANCE_KEY] = instance;
+      }
     },
   },
   flattenChildren,
