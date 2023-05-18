7929ca92411ae86c4ddb3e50598bca605cd598c6
ClarkXia <xiawenwu41@gmail.com>
2023-05-18 13:50:37 +0800

    fix: optimize code

--- packages/runtime/src/runClientApp.tsx
+++ packages/runtime/src/runClientApp.tsx
@@ -33,23 +33,25 @@ export interface RunClientAppOptions {
 
 // Add polyfill of Request.prototype.signal for some browser compatibility.
 if (import.meta.renderer === 'client' && window.Request && !window.Request.prototype.hasOwnProperty('signal')) {
-  const OriginalRequest = window.Request;
-  function Request(input: RequestInfo | URL, init?: RequestInit) {
-    if (input instanceof OriginalRequest) {
-      this.signal = input.signal;
-    }
-    this.signal = init.signal || this.signal || (function () {
-      if ('AbortController' in window) {
-        let ctrl = new AbortController();
-        return ctrl.signal;
+  (function (self) {
+      const OriginalRequest = window.Request;
+      function Request(input: RequestInfo | URL, init?: RequestInit) {
+        if (input instanceof OriginalRequest) {
+          this.signal = input.signal;
+        }
+        this.signal = init.signal || this.signal || (function () {
+          if ('AbortController' in window) {
+            let ctrl = new AbortController();
+            return ctrl.signal;
+          }
+        }());
+        OriginalRequest.call(this, input, init);
       }
-    }());
-    OriginalRequest.call(this, input, init);
-  }
-  Request.prototype = Object.create(OriginalRequest.prototype);
-  Request.prototype.constructor = Request;
-  // @ts-expect-error for overwrite the original Request.
-  window.Request = Request;
+      Request.prototype = Object.create(OriginalRequest.prototype);
+      Request.prototype.constructor = Request;
+      // @ts-expect-error for overwrite the original Request.
+      self.Request = Request;
+  })(window);
 }
 
 export default async function runClientApp(options: RunClientAppOptions) {
