87dadb24f186ea7823804c0cee4af3d9e3568e5f
hzyhbk <hzyhbk@163.com>
2023-06-09 11:32:58 +0800

    fix: miniapp-runtime 增加 useSuspenseData 注释

--- packages/miniapp-runtime/src/app/suspense.ts
+++ packages/miniapp-runtime/src/app/suspense.ts
@@ -2,6 +2,8 @@ export function withSuspense(cpt) {
   return cpt;
 }
 
+// TODO: 小程序场景下仅为套壳函数，暂不支持 useSuspenseData
 export function useSuspenseData(request) {
+  console.warn('miniapp-runtime does not support `useSuspenseData` api yet.');
   return request;
 }
