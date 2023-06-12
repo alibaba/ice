391bd52c5b66df30ad32c4fb72644311b4560ff7
ClarkXia <xiawenwu41@gmail.com>
2023-06-12 10:22:38 +0800

    fix: optimize code

--- packages/runtime/src/RouteContext.ts
+++ packages/runtime/src/RouteContext.ts
@@ -2,15 +2,11 @@ import { useLoaderData } from 'react-router-dom';
 import type { RouteConfig } from './types.js';
 
 function useData<T = any>(): T {
-  return process.env.ICE_CORE_ROUTER === 'true' || import.meta.renderer !== 'client'
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-    ? (useLoaderData() as any)?.data : undefined;
+  return (useLoaderData() as any)?.data;
 }
 
 function useConfig<T = {}>(): RouteConfig<T> {
-  return process.env.ICE_CORE_ROUTER === 'true' || import.meta.renderer !== 'client'
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-    ? (useLoaderData() as any)?.pageConfig : undefined;
+  return (useLoaderData() as any)?.pageConfig;
 }
 
 export {
