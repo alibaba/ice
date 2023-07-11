1921158894095670b8ab43e044535aed28c02ef2
answershuto <answershuto@gmail.com>
2023-07-11 19:22:40 +0800

    chore: add try to Storage

--- packages/cache-canvas/src/storage.tsx
+++ packages/cache-canvas/src/storage.tsx
@@ -2,16 +2,25 @@ const cache = {};
 
 export const Storage = {
   setItem: (key, value) => {
-    if (typeof window === 'object' && window.localStorage) {
-      return localStorage.setItem(key, value);
-    }
+    try {
+      if (typeof window === 'object' && window.localStorage) {
+        return localStorage.setItem(key, value);
+      }
 
-    return (cache[key] = value);
+      return (cache[key] = value);
+    } catch (e) {
+      console.error('Storage setItem error:', e);
+    }
   },
   getItem: (key) => {
-    if (typeof window === 'object' && window.localStorage) {
-      return localStorage.getItem(key);
+    try {
+      if (typeof window === 'object' && window.localStorage) {
+        return localStorage.getItem(key);
+      }
+
+      return cache[key] || '';
+    } catch (e) {
+      console.error('Storage getItem error:', e);
     }
-    return cache[key];
   },
 };
