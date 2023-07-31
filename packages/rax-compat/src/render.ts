9a7d1eab64f94521a6bd608d76f2837dbb5e92d2
linbudu599 <linbudu599@gmail.com>
2023-07-31 19:36:45 +0800

    fix(rax-compat): compat for rax driver-dom on nullish render container

--- packages/rax-compat/src/render.ts
+++ packages/rax-compat/src/render.ts
@@ -5,6 +5,22 @@ import { createRoot } from 'react-dom/client';
 import { isFunction } from './type.js';
 import { containerRootMap } from './container-root-map.js';
 
+export default function render(
+  element: RaxElement,
+  container: Element | DocumentFragment | null,
+  options?: RenderOption,
+): RaxElement;
+export default function render(
+  element: RaxElement,
+  container: Element | DocumentFragment | null,
+  callback?: Function,
+): RaxElement;
+export default function render(
+  element: RaxElement,
+  container: Element | DocumentFragment | null,
+  options?: RenderOption,
+  callback?: Function,
+): RaxElement;
 /**
  * Compat render for rax export.
  * https://github.com/alibaba/rax/blob/master/packages/rax/src/render.js#L5
@@ -17,19 +33,28 @@ import { containerRootMap } from './container-root-map.js';
 export default function render(
   element: RaxElement,
   container: Element | DocumentFragment | null,
-  options?: RenderOption,
+  optionsOrCallback?: RenderOption | Function,
   callback?: Function,
 ): RaxElement {
-  if (isFunction(options)) {
-    callback = options;
-    options = null;
+  if (isFunction(optionsOrCallback)) {
+    callback = optionsOrCallback;
+    optionsOrCallback = {} as RenderOption;
   }
 
-  const root = createRoot(container, options as RootOptions);
+  /**
+   * Compat for rax driver-dom behavior, which use body as container for nullish container.
+   * ref: https://github.com/alibaba/rax/blob/13d80a491f18c74034aa9b05c36ac922ff8c3357/packages/rax/src/vdom/instance.js#L44
+   * ref: https://github.com/alibaba/rax/blob/13d80a491f18c74034aa9b05c36ac922ff8c3357/packages/driver-dom/src/index.js#L75
+   */
+  if (container === null) {
+    container = document.querySelector('body')!;
+  }
+
+  const root = createRoot(container, optionsOrCallback as RootOptions);
   root.render(element as ReactNode);
 
   // Save container and root relation.
-  container && containerRootMap.set(container, root);
+  containerRootMap.set(container, root);
 
   if (isFunction(callback)) {
     callback.call(element);
