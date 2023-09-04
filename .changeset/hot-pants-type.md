68f19eaeabb729b11940dafbe4d896037be0548f
ClarkXia <xiawenwu41@gmail.com>
2023-09-04 11:29:51 +0800

    fix: unnecessary to import runtime module and routes when csr (#6507)
    * fix: unnecessary to import runtime module and routes when csr
    
    * fix: optimize code

---
+++ .changeset/hot-pants-type.md
@@ -0,0 +1,6 @@
+---
+'@ice/runtime': patch
+'@ice/app': patch
+---
+
+fix: should not import runtime module and routes when csr
--- examples/with-store/src/store.ts
+++ examples/with-store/src/store.ts
@@ -1,4 +1,4 @@
 import { createStore } from 'ice';
 import user from './models/user';
-
+console.log('app store depend on window', window);
 export default createStore({ user });
