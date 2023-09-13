b2d66fa8cbce573b3c9a90d7abf4300cc0f9036f
ClarkXia <xiawenwu41@gmail.com>
2023-09-13 17:09:30 +0800

    fix: add options for local test

--- packages/plugin-stream-error/src/index.ts
+++ packages/plugin-stream-error/src/index.ts
@@ -1,9 +1,14 @@
 import type { Plugin } from '@ice/app/types';
 
+interface PluginOptions {
+  activeInDev?: boolean;
+}
+
 const PLUGIN_NAME = '@ice/plugin-stream-error';
-const plugin: Plugin<{}> = () => ({
+const plugin: Plugin<PluginOptions> = (options = {}) => ({
   name: PLUGIN_NAME,
   setup: ({ generator, context }) => {
+    const { activeInDev } = options;
     const { userConfig } = context;
     if (userConfig.ssr) {
       generator.addEntryCode((originalCode) => {
@@ -12,7 +17,7 @@ if (import.meta.renderer === 'client') {
   window.addEventListener('load', (event) => {
     // _$ServerTimePoints will returned at the end of last stream,
     // if the value is undefined, try to re-render app with CSR.
-    if (!window._$ServerTimePoints && window.__ICE_APP_CONTEXT__.renderMode === 'SSR') {
+    if (${activeInDev ? '' : 'process.env.NODE_ENV === \'production\' && '}!window._$ServerTimePoints && window.__ICE_APP_CONTEXT__.renderMode === 'SSR') {
       render({ hydrate: false });
     }
   });
