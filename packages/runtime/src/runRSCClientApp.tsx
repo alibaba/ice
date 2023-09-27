7d9990c95276dc43b430698c7a8617e6ff28d000
shuilan.cj <shuilan.cj@taobao.com>
2023-09-27 14:49:40 +0800

    fix: root id

--- packages/runtime/src/runRSCClientApp.tsx
+++ packages/runtime/src/runRSCClientApp.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import pkg from 'react-server-dom-webpack/client';
+import type { AppConfig } from './types.js';
 
 // @ts-ignore
 const { Suspense, use } = React;
 const { createFromFetch } = pkg;
 
-export async function runRSCClientApp() {
+export async function runRSCClientApp(appConfig: AppConfig) {
   function App({ response }) {
     return (
       <Suspense fallback={<h1>Loading...</h1>}>
@@ -20,7 +21,9 @@ export async function runRSCClientApp() {
     fetch(rscPath),
   );
 
-  const container = document.getElementById('app');
+  debugger;
+  const rootId = appConfig.app.rootId || 'app';
+  const container = document.getElementById(rootId);
   const root = ReactDOM.createRoot(container);
   root.render(<App response={response} />);
 }
