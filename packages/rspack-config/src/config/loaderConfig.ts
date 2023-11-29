352b066cb0cf3ccb8fc8583dea373f1a8c0818df
ClarkXia <xiawenwu41@gmail.com>
2023-11-29 11:37:04 +0800

    fix: remove unexpected commit

--- packages/rspack-config/src/config/loaderConfig.ts
+++
@@ -1,7 +0,0 @@
-import { getJsxTransformOptions } from '@ice/shared-config';
-
-function getLoaderConfig() {
-  const builtInLoader = 'builtin:compilation-loader';
-  // Loader of compliation will automatically detect tsx file and override parser options.
-  const defaultSuffix = 'jsx';
-}
