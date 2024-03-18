a0cc472b991cf6283e042d741a1b42a4780ff391
ClarkXia <xiawenwu41@gmail.com>
2024-03-18 17:08:22 +0800

    Update singleRouter.tsx

--- packages/runtime/src/singleRouter.tsx
+++ packages/runtime/src/singleRouter.tsx
@@ -257,13 +257,13 @@ export const matchRoutes = (
   location: Partial<Location> | string,
   basename: string,
 ) => {
-  const pathname = typeof location === 'string' ? location : location.pathname;
+  const pathname = (typeof location === 'string' ? location : location.pathname) || '/';
 
-  let stripedPathname = stripBasename(pathname || '/', basename || '/');
+  let stripedPathname = stripBasename(pathname, basename || '/');
   if (!stripedPathname && basename !== '/') {
     // If pathname is not match, we should ignore the basename,
     // in case of the basename is customized.
-    stripedPathname = stripBasename(pathname || '/', '/');
+    stripedPathname = stripBasename(pathname, '/');
   }
   let branches = flattenRoutes(routes);
   if (branches.length === 1) {
