11a87dc6e572e74327b34e587d2d28ecb38746b9
ClarkXia <xiawenwu41@gmail.com>
2024-05-06 17:22:33 +0800

    feat: support split page chunk in cjs format (#6843)
    * feat: support spilt page chunk in cjs format
    
    * fix: escape route path
    
    * fix: multipile entry for server compile
    
    * chore: changelog

--- packages/ice/src/bundler/rspack/getConfig.ts
+++ packages/ice/src/bundler/rspack/getConfig.ts
@@ -35,9 +35,11 @@ const getConfig: GetConfig = async (context, options, rspack) => {
   const {
     rootDir,
     userConfig,
+    command,
     extendsPluginAPI: {
       serverCompileTask,
       getRoutesFile,
+      getFlattenRoutes,
     },
   } = context;
   const { reCompile, ensureRoutesConfig } = getRouteExportConfig(rootDir);
@@ -57,6 +59,8 @@ const getConfig: GetConfig = async (context, options, rspack) => {
         outputDir,
         serverCompileTask,
         userConfig,
+        getFlattenRoutes,
+        command,
       }),
       // Add ReCompile plugin when routes config changed.
       getReCompilePlugin(reCompile, routeManifest),
