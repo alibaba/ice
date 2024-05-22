9e5c3c196fec0848c6687753d02e7f77b518fd3a
ClarkXia <xiawenwu41@gmail.com>
2024-05-22 11:56:17 +0800

    feat: externals plugin for external dependencies mush easier

---
+++ packages/plugin-externals/README.md
@@ -0,0 +1,45 @@
+# @ice/plugin-externals
+
+`@ice/plugin-externals` is a ice.js plugin. It provides a simple way to add externals support to your application.
+
+## Install
+
+```bash
+$ npm i @ice/plugin-externals --save-dev
+```
+
+## Usage
+
+Set `preset-react` to external react in a easy way.
+
+```js
+import { defineConfig } from '@ice/app';
+import externals from '@ice/plugin-externals';
+
+export default defineConfig(() => ({
+  plugins: [externals({ preset: 'preset-react' })]
+}));
+```
+
+Framework will auto add externals of `react` and `react-dom` to your application, and the cdn url will be inject to the document by default.
+
+Also, you can custom externals and cdn url by yourself:
+
+```js
+import { defineConfig } from '@ice/app';
+import externals from '@ice/plugin-externals';
+
+export default defineConfig(() => ({
+  plugins: [externals({
+    externals: {
+      antd: 'Antd',
+    },
+    cdnMap: {
+      antd: {
+        development: 'https://unpkg.com/antd/dist/antd.js',
+        production: 'https://unpkg.com/antd/dist/antd.min.js',
+      }
+    }
+  })]
+}));
+```
---
+++ packages/plugin-externals/src/index.ts
@@ -0,0 +1,76 @@
+import type { Plugin } from '@ice/app/types';
+import InjectExternalScriptsWebpackPlugin from './webpack-plugin.js';
+
+const PLUGIN_NAME = '@ice/plugin-externals';
+
+type Preset = 'preset-react';
+interface PluginOptions {
+  preset?: Preset | Preset[];
+  externals?: Record<string, string>;
+  cdnMap?: Record<string, {
+    development: string | string[];
+    production: string | string[];
+  }>;
+}
+
+const plugin: Plugin = (options: PluginOptions) => ({
+  name: PLUGIN_NAME,
+  setup: ({ onGetConfig, context }) => {
+    const { command } = context;
+    const reactExternals = {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    };
+    const reactCDN = {
+      react: {
+        development: 'https://g.alicdn.com/code/lib/react/18.3.1/umd/react.development.js',
+        production: 'https://g.alicdn.com/code/lib/react/18.3.1/umd/react.production.min.js',
+      },
+      'react-dom': {
+        development: 'https://g.alicdn.com/code/lib/react-dom/18.3.1/umd/react-dom.development.js',
+        production: 'https://g.alicdn.com/code/lib/react-dom/18.3.1/umd/react-dom.production.min.js',
+      },
+    };
+    onGetConfig((config) => {
+      config.configureWebpack ??= [];
+      config.configureWebpack.push((webpackConfig) => {
+        let externals = options.externals || {};
+        let cdnMap = options.cdnMap || {};
+        if (options.preset && options.preset === 'preset-react') {
+          externals = {
+            ...externals,
+            ...reactExternals,
+          };
+          cdnMap = {
+            ...cdnMap,
+            ...reactCDN,
+          };
+        }
+
+        if (!webpackConfig.externals) {
+          webpackConfig.externals = externals;
+        } else if (typeof webpackConfig.externals === 'object') {
+          webpackConfig.externals = {
+            ...webpackConfig.externals,
+            ...externals,
+          };
+        }
+        const cdnList = [];
+        Object.keys(cdnMap).map((key) => {
+          const url = command === 'start' ? cdnMap[key].development : cdnMap[key].production;
+          const urls = Array.isArray(url) ? url : [url];
+          cdnList.push(...urls);
+        }).filter(Boolean);
+        if (cdnList.length > 0) {
+          // @ts-ignore missmatch type becasue of webpack prebundled.
+          webpackConfig.plugins.push(new InjectExternalScriptsWebpackPlugin({
+            externals: cdnList,
+          }));
+        }
+        return webpackConfig;
+      });
+    });
+  },
+});
+
+export default plugin;
