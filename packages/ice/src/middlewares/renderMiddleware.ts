a9bf6e5d6cbfa8493b1b730a8957dbd65c5a3cb7
ClarkXia <xiawenwu41@gmail.com>
2024-05-30 10:12:17 +0800

    chore: remove console log

--- packages/ice/src/middlewares/renderMiddleware.ts
+++ packages/ice/src/middlewares/renderMiddleware.ts
@@ -43,7 +43,6 @@ export default function createRenderMiddleware(options: Options): Middleware {
       // Ignore static resources.
       !isStaticResources.test(req.path)) {
       const serverModule = await excuteServerEntry();
-      console.log('serverModule', serverModule);
       if (serverModule) {
         const requestContext: ServerContext = {
           req,
