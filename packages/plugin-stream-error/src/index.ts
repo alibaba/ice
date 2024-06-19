6961a1de26f0814efabe78bc5a388ef67a31d75d
那达 <youxiang.lyx@alibaba-inc.com>
2024-06-19 11:16:53 +0800

    feat: 新增触发重新渲染事件ICE_RERENDER

--- packages/plugin-stream-error/src/index.ts
+++ packages/plugin-stream-error/src/index.ts
@@ -14,13 +14,17 @@ const plugin: Plugin<PluginOptions> = (options = {}) => ({
       generator.addEntryCode((originalCode) => {
         return `${originalCode}
 if (import.meta.renderer === 'client') {
-  window.addEventListener('load', (event) => {
+  let hasRendered = false;
+  const tryRerender = (event) => {
     // _$ServerTimePoints will returned at the end of last stream,
     // if the value is undefined, try to re-render app with CSR.
-    if (${activeInDev ? '' : 'process.env.NODE_ENV === \'production\' && '}!window._$ServerTimePoints && window.__ICE_APP_CONTEXT__.renderMode === 'SSR') {
+    if (${activeInDev ? '' : 'process.env.NODE_ENV === \'production\' && '}!window._$ServerTimePoints && window.__ICE_APP_CONTEXT__.renderMode === 'SSR' && !hasRendered) {
+      hasRendered = true;
       render({ hydrate: false });
     }
-  });
+  };
+  window.addEventListener('load', tryRerender);
+  window.addEventListener('ICE_RERENDER', tryRerender);
 }`;
       });
     }
