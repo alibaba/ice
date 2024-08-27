f048b313276736bb7af0690b43174d7f0a0f2a0f
ClarkXia <xiawenwu41@gmail.com>
2024-08-27 16:37:21 +0800

    fix: undefined config

--- packages/ice/src/createService.ts
+++ packages/ice/src/createService.ts
@@ -253,7 +253,7 @@ async function createService({ rootDir, command, commandArgs }: CreateServiceOpt
   const { routeImports, routeDefinition } = getRoutesDefinition({
     manifest: routesInfo.routes,
     lazy,
-    compileRoutes: routesConfig.lazyCompile ? [] : undefined,
+    compileRoutes: routesConfig?.lazyCompile ? [] : undefined,
   });
   const loaderExports = hasExportAppData || Boolean(routesInfo.loaders);
   const hasDataLoader = Boolean(userConfig.dataLoader) && loaderExports;
