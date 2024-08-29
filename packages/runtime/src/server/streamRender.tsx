e3558093e34c79b9176e4a9f6af145e347621b08
染陌 <ranmo.cy@alibaba-inc.com>
2024-08-29 10:05:55 +0800

    chore: modify const

--- packages/runtime/src/server/streamRender.tsx
+++ packages/runtime/src/server/streamRender.tsx
@@ -64,7 +64,7 @@ export function renderToNodeStream(
             matches,
             loaderData,
           } = routerContext;
-          let renderAssets = getAllAssets(loaderData, matches, assetsManifest);
+          const renderAssets = getAllAssets(loaderData, matches, assetsManifest);
           options?.onShellReady && options.onShellReady({
             renderAssets,
           });
