96eaed30bc2d617ec9486a94f337aa4584e4fd41
ClarkXia <xiawenwu41@gmail.com>
2025-02-08 15:26:11 +0800

    fix: simplify ci

--- tests/integration/app-config.test.ts
+++ tests/integration/app-config.test.ts
@@ -9,33 +9,33 @@ const example = 'app-config';
 describe(`build ${example}`, () => {
   test('open /', async () => {
     await buildFixture(example);
-    await setupBrowser({ example });
+    // await setupBrowser({ example });
   });
 });
 
-describe(`start ${example}`, () => {
-  let page: Page;
-  let browser: Browser;
+// describe(`start ${example}`, () => {
+//   let page: Page;
+//   let browser: Browser;
 
-  test('setup devServer', async () => {
-    const { devServer, port } = await startFixture(example);
-    const res = await setupStartBrowser({ server: devServer, port });
-    page = res.page;
-    browser = res.browser;
-    await page.push('/ice');
-    expect(await page.$$text('h1')).toStrictEqual(['home']);
-  });
+//   test('setup devServer', async () => {
+//     const { devServer, port } = await startFixture(example);
+//     const res = await setupStartBrowser({ server: devServer, port });
+//     page = res.page;
+//     browser = res.browser;
+//     await page.push('/ice');
+//     expect(await page.$$text('h1')).toStrictEqual(['home']);
+//   });
 
-  test('error page', async () => {
-    await page.push('/ice/error');
-    await page.waitForNetworkIdle();
-    expect(await page.$$text('h1')).toStrictEqual(['Something went wrong.']);
-  });
+//   test('error page', async () => {
+//     await page.push('/ice/error');
+//     await page.waitForNetworkIdle();
+//     expect(await page.$$text('h1')).toStrictEqual(['Something went wrong.']);
+//   });
 
-  afterAll(async () => {
-    await browser.close();
-  });
-});
+//   afterAll(async () => {
+//     await browser.close();
+//   });
+// });
 
 // describe(`start ${example} in speedup mode`, () => {
 //   let page: Page;
