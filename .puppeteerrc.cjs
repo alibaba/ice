9a512ddd74aa9c40bc491376031386eb54dbcd29
ClarkXia <xiawenwu41@gmail.com>
2025-02-08 16:46:44 +0800

    fix: puppeteer

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const { join } = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  // cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
