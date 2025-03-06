5ba15cd2075734b8263b1430ea42753cff1e2520
HomyeeKing <HomyeeKing@gmail.com>
2025-03-06 16:38:17 +0800

    chore: rm console

--- packages/webpack-config/src/index.ts
+++ packages/webpack-config/src/index.ts
@@ -359,9 +359,6 @@ export function getWebpackConfig(options: GetWebpackConfigOptions): Configuratio
   // 原因：[managedPaths](https://webpack.js.org/configuration/other-options/#managedpaths) 在 tnpm / cnpm 安装的情况下失效，导致持久缓存在处理 node_modules
   // 通过指定 [immutablePaths](https://webpack.js.org/configuration/other-options/#immutablepaths) 进行兼容
   // 依赖路径中同时包含包名和版本号即可满足 immutablePaths 的使用
-if(isServer){
-  console.log('webpackConfig', webpackConfig)
-}
   // 通过安装后的 package.json 中是否包含 __npminstall_done 字段来判断是否为 tnpm / cnpm 安装模式
   if (require('../package.json').__npminstall_done) {
     const nodeModulesPath = path.join(rootDir, 'node_modules');
