fb013fd43f8f2e272501adeee936883f545d849f
yangchang <yc439848@taobao.com>
2025-05-13 14:40:23 +0800

    chore: move handler callback defs inside effect

--- packages/appear/src/weex/index.tsx
+++ packages/appear/src/weex/index.tsx
@@ -1,5 +1,5 @@
 import type { ForwardedRef } from 'react';
-import { useEffect, useRef, forwardRef, cloneElement, Children, useCallback } from 'react';
+import { useEffect, useRef, forwardRef, cloneElement, Children } from 'react';
 import type { AppearProps } from '../typings';
 
 const WeexAppear = forwardRef<any, AppearProps>((props, ref) => {
@@ -8,41 +8,34 @@ const WeexAppear = forwardRef<any, AppearProps>((props, ref) => {
 
   const { children, onAppear, onDisappear } = props;
 
-  const appearHandler = useCallback(
-    (e: CustomEvent) => {
-      onAppear?.(e);
-    },
-    [onAppear],
-  );
-
-  const disappearHandler = useCallback(
-    (e: CustomEvent) => {
-      onDisappear?.(e);
-    },
-    [onDisappear],
-  );
-
   useEffect(() => {
     // Use copy of childrenRef to avoid ref value changed in cleanup phase.
     const nodeRef = typeof childrenRef === 'object' ? childrenRef.current : null;
 
+    const appearHandler = (e: CustomEvent) => {
+      onAppear?.(e);
+    };
     // Return early if onAppear callback not specified.
     onAppear && nodeRef?.addEventListener('appear', appearHandler);
 
     return () => {
       onAppear && nodeRef?.removeEventListener('appear', appearHandler);
     };
-  }, [childrenRef, appearHandler, onAppear]);
+  }, [childrenRef, onAppear]);
 
   useEffect(() => {
     const nodeRef = typeof childrenRef === 'object' ? childrenRef.current : null;
+
+    const disappearHandler = (e: CustomEvent) => {
+      onDisappear?.(e);
+    };
 
     onDisappear && nodeRef?.addEventListener('disappear', disappearHandler);
 
     return () => {
       onDisappear && nodeRef?.removeEventListener('disappear', disappearHandler);
     };
-  }, [childrenRef, onDisappear, disappearHandler]);
+  }, [childrenRef, onDisappear]);
 
   return cloneElement(Children.only(children), { ref: childrenRef });
 });
